2191d2485d07f465c6188a1e0eba91b225c8c75c
Stevespear426 <stephen.k.spear@gmail.com>
2012-10-25 10:33:43 -0400

    NavBar: Tablet Nav bar adjustable width
    Let users chose their width for both port and land
    sliders set to zero uses mike's per button width math.
    
    Change-Id: I686891faaeff2b81a3a717e6699024c1d918b2c3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2661,6 +2661,18 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
 
+        /**
+         *
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_WIDTH_PORT = "navigation_bar_width_port";
+
+        /**
+         *
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_WIDTH_LAND = "navigation_bar_width_land";
+
          /**
          * @hide
          */

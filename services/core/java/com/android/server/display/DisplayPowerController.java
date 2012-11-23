f4ba31d8ebc10c20d1479c73a95de37be92407f2
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-23 14:23:16 +0000

    fw/b: Button backlight brightness
    Squash of:
    
    Author: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Date:   Fri Nov 23 14:23:16 2012 +0000
    
        Reintroduce button-backlight (and respective inactivity timeout)
    
        The power manager rewrite from Change I1d7a52e98f0449f76d70bf421f6a7f245957d1d7
        completely removed support for control of the button backlights, which makes
        all capacitive buttons out there stay dark. The commit message in that change
        mentions it hasn't been implemented _yet_, so this fix should be temporary
        until upstream does their own implementation
    
        Change-Id: I6094c446e0b8c23f57d30652a3cbd35dee5e821a
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Thu Aug 22 08:53:24 2013 +0200
    
        Add PowerManager integration for button and keyboard backlight.
    
        Allows setting button and keyboard backlight brightness as well as
        button timeout.
    
        Change-Id: I550cccafc0a8f90d6347de9261adb26b75955cc4
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Sat Jan 3 05:13:26 2015 -0800
    
        power: Disable keyboard/button lights while dozing/dreaming
    
         * With hardkeys and doze mode enabled, entering suspend results in
           an epic battle over the lights. It's a bad situation. Disable
           them when we're sleepy.
    
        Change-Id: I7f1fc35a1573717d1ea101a07c4171d6f66d1553
    
    Author: nadlabak <pavel@doshaska.net>
    Date:   Sun Jun 7 02:01:05 2015 +0200
    
        PowerManagerService: Fix updating of mUserActivitySummary
    
        I7f1fc35a1573717d1ea101a07c4171d6f66d1553 missed the fact that the primary
        purpose of the affected condition block was to update mUserActivitySummary
        and the button/keyboard light handling was just appended to it later.
    
        This fixes the waking from dream/screensaver by user activity.
    
        I30c5c8c9c09e3d57ace18cac72b783510b9b3bf3 is removed here as well as it was
        just a band aid.
    
        jira: NIGHTLIES-1285
    
        Change-Id: I6b2f6c58e73110787d62e86d4d2ef538638cf491
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Tue Dec 26 17:15:05 2017 +0000
    
        Forward-port button brightness implementation to O
    
         * Reworked for the new handler interface, restoring also removed
           methods (partial revert of commit 86c39f9edee88baa912c694061010483c7da9daf).
    
         * Keyboard backlight brightness support left out for now.
    
        Change-Id: I53f031fa2da394e95a2b29a01eb3c6a8f8132507
    
    Change-Id: I5176a2028c18408c17bac7f25e62b5612fd6c227
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -69,6 +69,7 @@ import com.android.server.display.utils.SensorUtils;
 import com.android.server.display.whitebalance.DisplayWhiteBalanceController;
 import com.android.server.display.whitebalance.DisplayWhiteBalanceFactory;
 import com.android.server.display.whitebalance.DisplayWhiteBalanceSettings;
+import com.android.server.lights.LightsManager;
 import com.android.server.policy.WindowManagerPolicy;
 
 import java.io.PrintWriter;
@@ -166,6 +167,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     // Battery stats.
     @Nullable
     private final IBatteryStats mBatteryStats;
+
+    // The lights service.
+    private final LightsManager mLights;
 
     // The sensor manager.
     private final SensorManager mSensorManager;
@@ -472,6 +476,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
 
         mSettingsObserver = new SettingsObserver(mHandler);
         mCallbacks = callbacks;
+        mLights = LocalServices.getService(LightsManager.class);
         mSensorManager = sensorManager;
         mWindowManagerPolicy = LocalServices.getService(WindowManagerPolicy.class);
         mBlanker = blanker;
@@ -1164,6 +1169,13 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         if (state == Display.STATE_OFF) {
             brightnessState = PowerManager.BRIGHTNESS_OFF_FLOAT;
             mBrightnessReasonTemp.setReason(BrightnessReason.REASON_SCREEN_OFF);
+            mLights.getLight(LightsManager.LIGHT_ID_BUTTONS).setBrightness(brightnessState);
+        }
+
+        // Disable button lights when dozing
+        if (state == Display.STATE_DOZE || state == Display.STATE_DOZE_SUSPEND) {
+            mLights.getLight(LightsManager.LIGHT_ID_BUTTONS)
+                    .setBrightness(PowerManager.BRIGHTNESS_OFF_FLOAT);
         }
 
         // Always use the VR brightness when in the VR state.

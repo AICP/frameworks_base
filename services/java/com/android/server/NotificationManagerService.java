c1b2bb9b5d0c33e790973841bb2acaa5ea5d51fe
Roman Birg <romanbirg@gmail.com>
2012-11-25 16:01:27 -0600

    Notification sound-to-vibration conversion toggle
    Also, I disabled the setting by default, so notifications should be have like they did prior to 4.2 and setting the boolean to true would enable default 4.2 behavior
    
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -1179,7 +1179,8 @@ public class NotificationManagerService extends INotificationManager.Stub
                 final boolean convertSoundToVibration =
                            notification.vibrate == null
                         && (useDefaultSound || notification.sound != null)
-                        && (audioManager.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE);
+                        && (audioManager.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE)
+                        && (Settings.System.getBoolean(mContext.getContentResolver(), Settings.System.NOTIFICATION_CONVERT_SOUND_TO_VIBRATION, false));
 
                 final boolean useDefaultVibrate =
                     (notification.defaults & Notification.DEFAULT_VIBRATE) != 0

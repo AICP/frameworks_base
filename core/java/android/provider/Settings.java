b187573a2f8486807a9f574aca0e259cac272e03
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-11-27 14:09:18 -0500

    Port "Option to hide Adb notification icon (2/2)"
    Change-Id: I5d40f5b0990fa3a1d02210ad4cc2ff5ada15e69c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3863,6 +3863,12 @@ public final class Settings {
         public static final String DEVICE_HOSTNAME = "device_hostname";
 
         /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+        /**
          * Setting to allow mock locations and location provider status to be injected into the
          * LocationManager service for testing purposes during application development.  These
          * locations and status values  override actual location and status information generated

e8fc746a67bac209aca57fe877edb142614a0082
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2012-12-04 14:24:19 +0100

    Quicksettings: add favorite contact tile (2/2)
    PS2+3: fix NPE
    PS4: add observer to update contact on change
    
    Change-Id: I08b8859fbef4f1ee71b3f6fb259afec3a3829199
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2843,6 +2843,13 @@ public final class Settings {
         public static final String QUICK_TOGGLES_PER_ROW = "quick_toggles_per_row";
 
         /**
+         * favorite contact for quick settings
+         *
+         * @hide
+         */
+        public static final String QUICK_TOGGLE_FAV_CONTACT = "quick_toggle_fav_contact";
+
+        /**
          * @hide
          */
         public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD = "mute_annoying_notifications_threshold";

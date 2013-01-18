a02a1372728708bbfe91b7cc35b1f75522d147a2
LorD ClockaN <davor@losinj.com>
2013-01-18 17:01:36 +0100

    Port "Option to control cursor in text fields using volume keys (1/2)" - AOKP
    This feature is moved to framework so it also works with third party keyboards.
    
    Change-Id: Ia22b1e5d1863795d2a632c6fec9981c638737aa7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4340,6 +4340,15 @@ public final class Settings {
          */
          public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
 
+         /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
     }
 
     /**

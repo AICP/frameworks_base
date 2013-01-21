41bb33320abaafe9c3dda2eac47d6c667a223932
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-21 10:30:13 +0100

    Lockscreen: provide haptic feedback on longpress unlock
    Change-Id: Ie7e32a0e3d410da26095bfce1a8e5ba94f0b5eaf
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -46,6 +46,7 @@ import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Slog;
+import android.view.HapticFeedbackConstants;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -263,10 +264,10 @@ public class KeyguardHostView extends KeyguardViewBase {
         showPrimarySecurityScreen(false);
         updateSecurityViews();
 
-        if (Settings.System.getBoolean(getContext().getContentResolver(),
+        mExpandChallengeView = (View) findViewById(R.id.expand_challenge_handle);
+        if (mExpandChallengeView != null) {
+            if (Settings.System.getBoolean(getContext().getContentResolver(),
                 Settings.System.LOCKSCREEN_LONGPRESS_CHALLENGE, false)) {
-            mExpandChallengeView = (View) findViewById(R.id.expand_challenge_handle);
-            if (mExpandChallengeView != null) {
                 mExpandChallengeView.setOnLongClickListener(mFastUnlockClickListener);
             }
         }
@@ -277,6 +278,9 @@ public class KeyguardHostView extends KeyguardViewBase {
     private final OnLongClickListener mFastUnlockClickListener = new OnLongClickListener() {
         @Override
         public boolean onLongClick(View v) {
+            performHapticFeedback(HapticFeedbackConstants.LONG_PRESS,
+                    HapticFeedbackConstants.FLAG_IGNORE_VIEW_SETTING
+                    | HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
             showNextSecurityScreenOrFinish(false);
             return true;
         }

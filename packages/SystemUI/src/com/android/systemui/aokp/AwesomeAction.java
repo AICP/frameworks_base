810c39b5a0ea9295148d638a7924527d9abcda73
Zaphod <mwilson@acesappraisal.com>
2013-01-27 13:24:14 -0500

    LastApp Toggle [1/2]
    Creates an action useable by NavBar & NavRing to toggle
    between most recent apps.  Will try to ignore the launcher
    when possible.  ie, if you've just rebooted and have not opened any
    apps, it will do nothing.  if you open 1 app, it will toggle between
    that 1 app and the launcher.  Once two apps have been opened, it will
    toggle between those two apps.
    It's kind of like opening recents and hitting your #2 slot repeatedly
    
    PS 2:  Add Icons from Hoolm.  {Thanks!}
           Re-wrote the logic for selecting next app.  Rather than
    toggling back to launcher if only 1 app open, it does nothing.
    toggling only occurs now when there are at least two apps open.
    One issue that I still need to work out is that 'Recents' is somehow
    showing up as a valid target to toggle to.  It shouldn't.
    
    ps 3:  getRunningTasks() does not filter out 'Excludefromrecents'.
    Thus we need checks for 'default launcher' and SystemUI.
    as I write this, I realize I might be able to query the
    ExcludeFromRecents flag of the package a bit more easily.
    
    ps 4: Whitespace fix.
    ps 5: one more whitespace fix.
    ps 6: remove unnecessary dependency
    
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>
    Change-Id: I0b59121b07b9ba44a2837a8db0266441ed82f9a3

--- packages/SystemUI/src/com/android/systemui/aokp/AwesomeAction.java
+++ packages/SystemUI/src/com/android/systemui/aokp/AwesomeAction.java
@@ -55,13 +55,14 @@ import com.android.internal.statusbar.IStatusBarService;
 import com.android.systemui.R;
 
 import java.net.URISyntaxException;
+import java.util.List;
 
 /*
  * Helper classes for managing AOKP custom actions
  */
 public class AwesomeAction {
 
-    public final static String TAG = "AOKPTarget";
+    public final static String TAG = "AwesomeAction";
 
     public final static String ACTION_HOME = "**home**";
     public final static String ACTION_BACK = "**back**";
@@ -84,6 +85,7 @@ public class AwesomeAction {
     public final static String ACTION_VOICEASSIST = "**voiceassist**";
     public final static String ACTION_TORCH = "**torch**";
     public final static String ACTION_SEARCH = "**search**";
+    public final static String ACTION_LAST_APP = "**lastapp**";
     public final static String ACTION_NULL = "**null**";
 
     private int mInjectKeyCode;
@@ -251,6 +253,9 @@ public class AwesomeAction {
                 // Let's hope we don't catch one!
             }
             return true;
+        } else if (action.equals(ACTION_LAST_APP)) {
+            toggleLastApp();
+            return true;
         }
         // we must have a custom uri
         try {
@@ -289,6 +294,8 @@ public class AwesomeAction {
             return mContext.getResources().getDrawable(R.drawable.ic_sysbar_search);
         if (uri.equals(ACTION_NOTIFICATIONS))
             return mContext.getResources().getDrawable(R.drawable.ic_sysbar_notifications);
+        if (uri.equals(ACTION_LAST_APP))
+            return mContext.getResources().getDrawable(R.drawable.ic_sysbar_lastapp);
         try {
             return mContext.getPackageManager().getActivityIcon(Intent.parseUri(uri, 0));
         } catch (NameNotFoundException e) {
@@ -320,6 +327,8 @@ public class AwesomeAction {
             return mContext.getResources().getString(R.string.action_search);
         if (uri.equals(ACTION_NOTIFICATIONS))
             return mContext.getResources().getString(R.string.action_notifications);
+        if (uri.equals(ACTION_LAST_APP))
+            return mContext.getResources().getString(R.string.action_lastapp);
         if (uri.equals(ACTION_NULL))
             return mContext.getResources().getString(R.string.action_none);
         try {
@@ -486,6 +495,34 @@ public class AwesomeAction {
                 mScreenshotConnection = conn;
                 H.postDelayed(mScreenshotTimeout, 10000);
             }
+        }
+    }
+
+    private void toggleLastApp() {
+        int lastAppId = 0;
+        int looper = 1;
+        String packageName;
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        final ActivityManager am = (ActivityManager) mContext
+                .getSystemService(Activity.ACTIVITY_SERVICE);
+        String defaultHomePackage = "com.android.launcher";
+        intent.addCategory(Intent.CATEGORY_HOME);
+        final ResolveInfo res = mContext.getPackageManager().resolveActivity(intent, 0);
+        if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
+            defaultHomePackage = res.activityInfo.packageName;
+        }
+        List <ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(5);
+        // lets get enough tasks to find something to switch to
+        // Note, we'll only get as many as the system currently has - up to 5
+        while ((lastAppId == 0) && (looper < tasks.size())) {
+            packageName = tasks.get(looper).topActivity.getPackageName();
+            if (!packageName.equals(defaultHomePackage) && !packageName.equals("com.android.systemui")) {
+                lastAppId = tasks.get(looper).id;
+            }
+            looper++;
+        }
+        if (lastAppId != 0) {
+            am.moveTaskToFront(lastAppId, am.MOVE_TASK_NO_USER_ACTION);
         }
     }
 

d2515c3c5b537fd2e1bd2b1e3ba46caf063db1a0
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-28 22:00:22 +0100

    PowerManager: Don't wake up device when charger is plugged/unplugged (1/2)
    This will add the option to disable waking up the device when a charger
    is plugged or unplugged.
    
    Change-Id: I3367041a05931510f367dafe412b173c80b89df1
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -1192,6 +1192,11 @@ public final class PowerManagerService extends IPowerManager.Stub
             return false;
         }
 
+        if (!Settings.System.getBoolean(mContext.getContentResolver(),
+                Settings.System.WAKEUP_WHEN_PLUGGED_UNPLUGGED, true)) {
+            return false;
+        }
+
         // Otherwise wake up!
         return true;
     }

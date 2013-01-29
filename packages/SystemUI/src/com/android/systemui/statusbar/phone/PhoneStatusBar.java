f8a2c988b44f00fe2b7d1d5c478df456776b7cb9
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-29 09:49:29 +0100

    SystemUI: respect global vibrate settings in the statusbar
    Change-Id: I6df0fe2c6eb00a625e4c0aff6d0c8743979de966
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2447,9 +2447,12 @@ public class PhoneStatusBar extends BaseStatusBar {
     }
 
     void vibrate() {
-        android.os.Vibrator vib = (android.os.Vibrator)mContext.getSystemService(
-                Context.VIBRATOR_SERVICE);
-        vib.vibrate(50);
+        if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1, UserHandle.USER_CURRENT) != 0) {
+            android.os.Vibrator vib = (android.os.Vibrator)mContext.getSystemService(
+                    Context.VIBRATOR_SERVICE);
+            vib.vibrate(50);
+        }
     }
 
     Runnable mStartTracing = new Runnable() {

6310aceff1aa661844e41a3289797ffa2dac1247
Jonathan Steadman <xxwhitehawk@gmail.com>
2013-05-11 01:50:15 +0200

    Revert "Can not mark the same content of browser page after copy"
    This reverts commit 660f4ae57545f3fa1aef2867ca2050f118e72b93
    
    Change-Id: I7d601db85ce74ed61d07265ac3ff461b2ca941e9

--- core/java/android/webkit/WebViewClassic.java
+++ core/java/android/webkit/WebViewClassic.java
@@ -4448,13 +4448,6 @@ public final class WebViewClassic implements WebViewProvider, WebViewProvider.Sc
         return selectText(x, y);
     }
 
-    public void clearSelection() {
-        selectionDone();
-        if (mWebViewCore != null) {
-            mWebViewCore.sendMessage(EventHub.SELECT_TEXT, null);
-        }
-    }
-
     /**
      * Select the word at the indicated content coordinates.
      */
@@ -4472,7 +4465,7 @@ public final class WebViewClassic implements WebViewProvider, WebViewProvider.Sc
     public void onConfigurationChanged(Configuration newConfig) {
         mCachedOverlappingActionModeHeight = -1;
         if (mSelectingText && mOrientation != newConfig.orientation) {
-            clearSelection();
+            selectionDone();
         }
         mOrientation = newConfig.orientation;
         if (mWebViewCore != null && !mBlockWebkitViewMessages) {

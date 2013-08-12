359636190e17bc7fc72e5c935700e2ad740cb0d1
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-08-12 23:30:07 +0200

    Revert "[webkit] Runtime enablement of WebGL"
    This reverts commit bcc8f733796f3fcd1c2180336960d747a6995f0c.
    
    Conflicts:
    
    	core/java/android/webkit/WebSettingsClassic.java
    
    Change-Id: Id2ccdd0128e16363ea9dabfa8c266cf456d497e7

--- core/java/android/webkit/WebSettingsClassic.java
+++ core/java/android/webkit/WebSettingsClassic.java
@@ -126,7 +126,6 @@ public class WebSettingsClassic extends WebSettings {
     private boolean         mEnableSmoothTransition = false;
     private boolean         mForceUserScalable = false;
     private boolean         mPasswordEchoEnabled = true;
-    private boolean         mWebGLEnabled = true;
 
     // AutoFill Profile data
     public static class AutoFillProfile {
@@ -1255,7 +1254,7 @@ public class WebSettingsClassic extends WebSettings {
     @Override
     public synchronized void setAppCachePath(String path) {
         // We test for a valid path and for repeated setting on the native
-        // side, but we can avoid syncing in some simple cases.
+        // side, but we can avoid syncing in some simple cases. 
         if (mAppCachePath == null && path != null && !path.isEmpty()) {
             mAppCachePath = path;
             postSync();
@@ -1651,24 +1650,6 @@ public class WebSettingsClassic extends WebSettings {
     }
 
     /**
-     * @hide
-     */
-    public synchronized boolean isWebGLAvailable() {
-        return nativeIsWebGLAvailable();
-    }
-
-    /**
-     * Sets whether WebGL is enabled.
-     * @param flag Set to true to enable WebGL.
-     */
-    public synchronized void setWebGLEnabled(boolean flag) {
-        if (mWebGLEnabled != flag) {
-            mWebGLEnabled = flag;
-            postSync();
-        }
-    }
-
-    /**
      * Sets whether viewport metatag can disable zooming.
      * @param flag Whether or not to forceably enable user scalable.
      */
@@ -1780,5 +1761,4 @@ public class WebSettingsClassic extends WebSettings {
 
     // Synchronize the native and java settings.
     private native void nativeSync(int nativeFrame);
-    private native boolean nativeIsWebGLAvailable();
 }

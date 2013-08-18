0822620dd9c46f486231e7d8e8192ae1f5f81f56
0xD34D <clark@scheffsblend.com>
2013-08-18 10:29:54 -0700

    Close navigation drawer, if open, when back button pressed.
    Change-Id: I6cb2fdc3e043028049c1f68843f0582a9b480c94

--- core/java/android/preference/PreferenceDrawerActivity.java
+++ core/java/android/preference/PreferenceDrawerActivity.java
@@ -682,6 +682,14 @@ public abstract class PreferenceDrawerActivity extends ListActivity implements
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout != null && mDrawerLayout.isDrawerOpen(mDrawer))
+            mDrawerLayout.closeDrawer(mDrawer);
+        else
+            super.onBackPressed();
+    }
+
     private void loadDrawerDrawables() {
         TypedArray a = getTheme().obtainStyledAttributes(
                 new int[] {

7a85942ab413007fcf51e3729372d6fd8254a1bf
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-09-25 09:21:12 +0200

    Simplify brightness toggle
    fixes http://bugs.aokp.co/issues/26
    
    Change-Id: I51a0cdd2604158094cc027e735a6b176deb6d575
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/BrightnessToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/BrightnessToggle.java
@@ -1,52 +1,46 @@
 
 package com.android.systemui.statusbar.toggles;
 
-import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.RemoteException;
+import android.database.ContentObserver;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.WindowManagerGlobal;
-import android.widget.ImageView;
 
 import com.android.systemui.R;
-import com.android.systemui.settings.BrightnessController;
-import com.android.systemui.settings.BrightnessController.BrightnessStateChangeCallback;
-import com.android.systemui.settings.ToggleSlider;
 
-public class BrightnessToggle extends BaseToggle implements BrightnessStateChangeCallback {
+public class BrightnessToggle extends BaseToggle {
 
-    // get these out of here
-    private Dialog mBrightnessDialog;
-
-    BrightnessController mBrightnessController;
-
-    private int mBrightnessDialogLongTimeout;
-
-    private int mBrightnessDialogShortTimeout;
+    SettingsObserver mObserver;
 
     @Override
     public void init(Context c, int style) {
         super.init(c, style);
-        mBrightnessDialogLongTimeout =
-                mContext.getResources().getInteger(
-                        R.integer.quick_settings_brightness_dialog_long_timeout);
-        mBrightnessDialogShortTimeout =
-                mContext.getResources().getInteger(
-                        R.integer.quick_settings_brightness_dialog_short_timeout);
-        onBrightnessLevelChanged();
+
+        mObserver = new SettingsObserver(new Handler());
+        mObserver.observe();
+    }
+
+    @Override
+    protected void cleanup() {
+        if (mObserver != null) {
+            mContext.getContentResolver().unregisterContentObserver(mObserver);
+            mObserver = null;
+        }
+        super.cleanup();
     }
 
     @Override
     public void onClick(View v) {
         vibrateOnTouch();
         collapseStatusBar();
-        showBrightnessDialog();
+
+        Intent intent = new Intent(Intent.ACTION_SHOW_BRIGHTNESS_DIALOG);
+        mContext.sendBroadcastAsUser(intent, UserHandle.CURRENT_OR_SELF);
+
     }
 
     @Override
@@ -57,68 +51,27 @@ public class BrightnessToggle extends BaseToggle implements BrightnessStateChang
         return super.onLongClick(v);
     }
 
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE), false,
+                    this, UserHandle.USER_ALL);
+            scheduleViewUpdate();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            scheduleViewUpdate();
+        }
+    }
+
     @Override
     protected void updateView() {
-        super.updateView();
-        dismissBrightnessDialog(mBrightnessDialogShortTimeout);
-    }
-
-    private void showBrightnessDialog() {
-        if (mBrightnessDialog == null) {
-            mBrightnessDialog = new Dialog(mContext);
-            mBrightnessDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-            mBrightnessDialog.setContentView(R.layout.quick_settings_brightness_dialog);
-            mBrightnessDialog.setCanceledOnTouchOutside(true);
-
-            mBrightnessController = new BrightnessController(mContext,
-                    (ImageView) mBrightnessDialog.findViewById(R.id.brightness_icon),
-                    (ToggleSlider)
-                    mBrightnessDialog.findViewById(R.id.brightness_slider));
-            mBrightnessController.addStateChangedCallback(BrightnessToggle.this);
-            mBrightnessDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    mBrightnessController = null;
-                    dialog.dismiss();
-                }
-            });
-
-            mBrightnessDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-            mBrightnessDialog.getWindow().getAttributes().privateFlags |=
-                    WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
-            mBrightnessDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-        }
-        if (!mBrightnessDialog.isShowing()) {
-            try {
-                WindowManagerGlobal.getWindowManagerService().dismissKeyguard();
-            } catch (RemoteException e) {
-            }
-            mBrightnessDialog.show();
-            dismissBrightnessDialog(mBrightnessDialogLongTimeout);
-        }
-    }
-
-    private void removeAllBrightnessDialogCallbacks() {
-        mHandler.removeCallbacks(mDismissBrightnessDialogRunnable);
-    }
-
-    private Runnable mDismissBrightnessDialogRunnable = new Runnable() {
-        public void run() {
-            if (mBrightnessDialog != null && mBrightnessDialog.isShowing()) {
-                mBrightnessDialog.dismiss();
-            }
-            removeAllBrightnessDialogCallbacks();
-        };
-    };
-
-    private void dismissBrightnessDialog(int timeout) {
-        removeAllBrightnessDialogCallbacks();
-        if (mBrightnessDialog != null) {
-            mHandler.postDelayed(mDismissBrightnessDialogRunnable, timeout);
-        }
-    }
-
-    public void onBrightnessLevelChanged() {
         int mode = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.SCREEN_BRIGHTNESS_MODE,
                 Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
@@ -132,7 +85,7 @@ public class BrightnessToggle extends BaseToggle implements BrightnessStateChang
 
         setIcon(iconId);
         setLabel(label);
-        scheduleViewUpdate();
+        super.updateView();
     }
 
 }

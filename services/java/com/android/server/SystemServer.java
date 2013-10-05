5ac7f5784c1a4a21a050089326f7c0d083e8bc82
maxwen <max.weninger@gmail.com>
2013-10-05 14:15:00 +0300

    base: fix support for adb over network
    Change-Id: I5090b9de172a35760a38e6c014945aeae2cafd8f

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -27,6 +27,7 @@ import android.content.pm.PackageItemInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources.Theme;
+import android.database.ContentObserver;
 import android.database.sqlite.SQLiteCompatibilityWalFlags;
 import android.database.sqlite.SQLiteGlobal;
 import android.hardware.display.DisplayManagerInternal;
@@ -49,6 +50,7 @@ import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.storage.IStorageManager;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 import android.util.Slog;
@@ -283,6 +285,23 @@ public final class SystemServer {
     private Future<?> mSensorServiceStart;
     private Future<?> mZygotePreload;
 
+    private class AdbPortObserver extends ContentObserver {
+        public AdbPortObserver() {
+            super(null);
+        }
+        @Override
+        public void onChange(boolean selfChange) {
+            try {
+                int adbPort = Settings.Global.getInt(mContentResolver,
+                        Settings.Global.AICP_ADB_PORT, 0);
+                // setting this will control whether ADB runs on TCP/IP or USB
+                SystemProperties.set("service.adb.tcp.port", Integer.toString(adbPort));
+            } catch (Exception e) {
+                Slog.e(TAG, "", e);
+            }
+        }
+    }
+
     /**
      * Start the sensor service. This is a blocking call and can take time.
      */
@@ -440,6 +459,7 @@ public final class SystemServer {
             startBootstrapServices();
             startCoreServices();
             startOtherServices();
+            startAicpAdditions();
             SystemServerInitThreadPool.shutdown();
         } catch (Throwable ex) {
             Slog.e("System", "******************************************");
@@ -1985,5 +2005,16 @@ public final class SystemServer {
 
     private static void traceEnd() {
         BOOT_TIMINGS_TRACE_LOG.traceEnd();
+    }
+
+    // aicp additions start
+    private void startAicpAdditions() {
+        Settings.Global.putInt(mContentResolver, Settings.Global.AICP_ADB_PORT,
+                Integer.parseInt(SystemProperties.get("service.adb.tcp.port", "0")));
+
+        // register observer to listen for settings changes
+        mContentResolver.registerContentObserver(
+                Settings.Global.getUriFor(Settings.Global.AICP_ADB_PORT),
+                false, new AdbPortObserver());
     }
 }

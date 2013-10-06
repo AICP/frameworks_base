6ccfba947f67489efc490e137e9f7be6132b4f41
Alex <mazdarider23@gmail.com>
2013-10-06 01:55:31 -0500

    Have DashClock hide Lockscreen clock
    Change-Id: Ibeb4dc043e9ded6b3509ccb8ba9c51fa82d68272

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
@@ -57,7 +57,9 @@ public class KeyguardWidgetPager extends PagedView implements PagedView.PageSwit
 
     private static final String[] CLOCK_WIDGET_PACKAGES = new String[] {
         "com.cyanogenmod.lockclock",
-        "com.android.deskclock"
+        "com.android.deskclock",
+        "com.google.android.apps.dashclock",
+        "net.nurik.roman.dashclock"
     };
 
     private static final int FLAG_HAS_LOCAL_HOUR = 0x1;

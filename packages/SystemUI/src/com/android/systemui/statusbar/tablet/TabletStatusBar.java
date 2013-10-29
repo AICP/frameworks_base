658aae95f8fdca32e491c2668d35734bcdc7711c
0xD34D <clark@scheffsblend.com>
2013-10-29 14:52:25 +0100

    NetStats: Add net stats to system bar.
    PS2:
    PS3:
    Hide net stats view when notification area is disabled.
    
    AICP:
    PS2: Fix build by adding a missing networks_stats id
    
    Change-Id: Ib35dd46b026dab00abe73966105806c54266e466

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -1226,8 +1226,10 @@ public class TabletStatusBar extends BaseStatusBar implements
             if ((state & StatusBarManager.DISABLE_NOTIFICATION_ICONS) != 0) {
                 Slog.i(TAG, "DISABLE_NOTIFICATION_ICONS: yes" + (mNotificationDNDMode?" (DND)":""));
                 mTicker.halt();
+                mBarContents.findViewById(R.id.network_stats).setAlpha(0f);
             } else {
                 Slog.i(TAG, "DISABLE_NOTIFICATION_ICONS: no" + (mNotificationDNDMode?" (DND)":""));
+                mBarContents.findViewById(R.id.network_stats).setAlpha(1f);
             }
 
             // refresh icons to show either notifications or the DND message

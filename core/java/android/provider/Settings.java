d6d35ca2403fca42ff7a1eefa851fd43a4732698
Lars Greiss <kufikugel@googlemail.com>
2013-12-01 14:14:38 +0100

    Bring back less notification sounds [1/2]
    abc: make it work together with screen On suppression feature
    and default to 30seconds threshold
    
    Add back the 4.3 feature like it was.
    User can setup how often notification sounds
    for an app come through so that it does
    not annoy too much.
    
    AICPfied: keep app default as option default
    
    Change-Id: I3d7b2ef2e2ff99b7c23880626e0268ec4c5d8543

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4660,6 +4660,16 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Whether to mute annoying notifications
+         * @hide
+         */
+        public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD = "mute_annoying_notifications_threshold";
+
+        /** @hide */
+        private static final Validator MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD_VALIDATOR =
+              new SettingsValidators.InclusiveIntegerRangeValidator(0, 120000);
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -4774,6 +4784,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_STYLE);
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_ANIMATE);
             PRIVATE_SETTINGS.add(NOTIFICATION_SOUND_VIB_SCREEN_ON);
+            PRIVATE_SETTINGS.add(MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD);
             // AICP Settings end
         }
 
@@ -4880,6 +4891,8 @@ public final class Settings {
             VALIDATORS.put(STATUSBAR_BATTERY_BAR_ANIMATE, STATUSBAR_BATTERY_BAR_ANIMATE_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_SOUND_VIB_SCREEN_ON,
                     NOTIFICATION_SOUND_VIB_SCREEN_ON_VALIDATOR);
+            VALIDATORS.put(MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD,
+                    MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD_VALIDATOR);
             // AICP Settings end
         }
 

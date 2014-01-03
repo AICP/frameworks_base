cd14b3d2fb351f5bf65e3502a3161a0d7a26fb34
Gergely Szell (sethyx) <szell@hu.ibm.com>
2014-01-03 17:52:33 +0100

    [1/3] frameworks/base: Add NFC polling setting
    PS2: rename setting
    PS3: fix build and proper author
    
    Change-Id: I29b4a49d846da3b8eebf6c92cc3745f035034603

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3230,6 +3230,11 @@ public final class Settings {
         public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
 
         /**
+         * Define when Nfc polling is activated.
+         */
+        public static final String NFC_POLLING_MODE = "nfc_polling";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

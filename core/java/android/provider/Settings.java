68ad55ec1f624a7f84e9a6ecc15d68cdf1630654
Andrew Warfield <hiasant@gmail.com>
2014-01-04 00:05:29 -0800

    base: Transparent Lockscreen from PA
    Lockscreen: See through (1/2)
    
    Add the ability to see through lockscreen.
    Adapted to kitkat from jellybean by @BigBrother1984
    
    Signed-off-by: D4rKn3sSyS <jesdga95@gmail.com>
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS2:
    
    * Adding missing sem-colon.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS3:
    
    * Replace and remove undefined mContext.
    * Define the boolean enableScreenRotation inside new method.
    * Whitespaces clean up.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS4:
    
    * Restore mContext in setKeyguardParams().
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS5:
    
    * Fixes for setKeyguardParams() and maybeCreateKeyguardLocked() methods, kitkat changed some things.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS6:
    
    * Missing sem-colon.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS7: Fix all the things.
    
    * Effects doesn't vanish after first unlock.
    * Created condition in updateShowWallpaper() method that just return if see through is activated, he was restoring the wallpaper at second lock.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS8: Cosmetic changes.
    
    * Correct java style name to seeThrough() and place it above updateShowWallpaper().
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS9: Other cosmetic changes.
    
    * Better name to the see through boolean.
    * Removed a space nitpick.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS10:
    
    * We had two booleans for the same thing, ditch the one defined in one class only and take the global.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/wm/WindowAnimator.java
    
    Fix: Correct possible null pointer when enabling see through lockscreen
    
    PS2: Remove unused variable
    
    Fix: Ensure lockscreen see through setting persists after reboot
    
    If see through was enabled and the device was rebooted,
    the user had to toggle the setting off and on again to activate.
    
    Change-Id: Ib97226d979227ba93eea61e4cd194e59430a9ecf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2013, ParanoidAndroid Project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -6972,6 +6973,12 @@ public final class Settings {
          * @hide
          */
         public static final String VOLUME_MUSIC_CONTROLS = "volume_music_controls";
+
+        /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
 
     }
 

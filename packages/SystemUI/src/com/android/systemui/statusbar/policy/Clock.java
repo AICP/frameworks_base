7514030816ae1784320e3098be0a35320520234f
Jun Su <juns@nvidia.com>
2014-01-19 12:43:58 +0800

    Disable statusbar time refresh when screen off
    When screen is off, don't refresh the status bar to save power.
    When screen is on, refresh immediately for user experience.
    
    PS: (updateClock(); => getHandler().post(() -> updateClock());)
    updated for android 8.0
    
    Change-Id: Ief2cd2eea2e54f51364308598cd9e01d1bb9bfe1

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -102,6 +102,7 @@ public class Clock extends TextView implements
     private Locale mLocale;
     private DateTimePatternGenerator mDateTimePatternGenerator;
     private Configuration oldConfig = new Configuration();
+    private boolean mScreenOn = true;
 
     private static final int AM_PM_STYLE_NORMAL  = 0;
     private static final int AM_PM_STYLE_SMALL   = 1;
@@ -212,6 +213,8 @@ public class Clock extends TextView implements
             filter.addAction(Intent.ACTION_TIME_CHANGED);
             filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
             filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
+            filter.addAction(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
 
             // NOTE: This receiver could run before this method returns, as it's not dispatching
             // on the main thread and BroadcastDispatcher may not need to register with Context.
@@ -294,7 +297,16 @@ public class Clock extends TextView implements
                     }
                 });
             }
-            handler.post(() -> updateClock());
+
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                mScreenOn = true;
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                mScreenOn = false;
+            }
+
+            if (mScreenOn) {
+                handler.post(() -> updateClock());
+            }
         }
     };
 
@@ -601,4 +613,3 @@ public class Clock extends TextView implements
         }
     };
 }
-

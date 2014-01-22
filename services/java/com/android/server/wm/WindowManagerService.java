4ef7f839d2a643abf601bf2a2ffcc270c0a99b9e
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2014-01-22 10:06:58 +0100

    fix build
    the variable fullscreen was removed in the previous commit.
    
    Change-Id: I11e896955be97d83a11ee79f03fa5cf8d1d9a828
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -5655,10 +5655,6 @@ public class WindowManagerService extends IWindowManager.Stub
                         screenshotReady = true;
                     }
 
-                    if (fullscreen) {
-                        // No point in continuing down through windows.
-                        break;
-                    }
                 }
 
                 if (appToken != null && appWin == null) {

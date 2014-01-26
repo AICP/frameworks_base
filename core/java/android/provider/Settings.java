ab6502c98ba835ac9df68eac0e16c842cae85935
Lars Greiss <kufikugel@googlemail.com>
2014-01-26 17:21:41 +0100

    fb: notification transparency (1/2)
    this add back notification transparency...compared to 4.3
    it controlls as well the notification icon backgound transparency
    which felt in the past dissplaced. As well codecleanup of the old
    implementation.
    
    largeIcon support I did not add due that google does not support it
    as well at current status...it is only a placeholder in
    NotificationData
    
    Change-Id: Ic5bb8998f64cac573d70005fad909104accc9e96

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2992,6 +2992,12 @@ public final class Settings {
         public static final String NOTIFICATION_BACKGROUND_ALPHA = "notification_background_alpha";
 
         /**
+         * Sets the alpha (transparency) of the notification
+         * @hide
+         */
+        public static final String NOTIFICATION_ALPHA = "notification_alpha";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3027,6 +3027,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.NOTIFICATION_BACKGROUND_ALPHA),
                         false, this, UserHandle.USER_ALL);
+
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.NOTIFICATION_ALPHA),
+                        false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -3039,8 +3043,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             updateSettings();
             if(uri != null && uri.equals(Settings.AOKP.getUriFor(Settings.AOKP.TOGGLES_STYLE))) {
                 recreateStatusBar();
-            }
-            if (uri.equals(Settings.System.getUriFor(
+            } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_BACKGROUND))
                 || uri.equals(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_BACKGROUND_LANDSCAPE))
@@ -3052,6 +3055,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                 if (mSettingsPanel != null) {
                     mSettingsPanel.setBackgroundDrawables();
                 }
+            } else if (uri.equals(Settings.System.getUriFor(
+                   Settings.System.NOTIFICATION_ALPHA))) {
+                setNotificationAlpha();
             }
         }
     }
@@ -3085,5 +3091,31 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             }
         }
         return true;
+    }
+
+    private void setNotificationAlpha() {
+        if (mPile == null || mNotificationData == null) {
+            return;
+        }
+        float notifAlpha = Settings.System.getFloatForUser(
+            mContext.getContentResolver(), Settings.System.NOTIFICATION_ALPHA,
+            0.0f, UserHandle.USER_CURRENT);
+        int alpha = (int) ((1 - notifAlpha) * 255);
+        int dataSize = mNotificationData.size();
+        for (int i = 0; i < dataSize; i++) {
+            Entry ent = mNotificationData.get(dataSize - i - 1);
+            View expanded = ent.expanded;
+            if (expanded !=null && expanded.getBackground() != null) {
+                expanded.getBackground().setAlpha(alpha);
+            }
+            View expandedBig = ent.getBigContentView();
+            if (expandedBig != null && expandedBig.getBackground() != null) {
+                expandedBig.getBackground().setAlpha(alpha);
+            }
+            StatusBarIconView icon = ent.icon;
+            if (icon !=null && icon.getBackground() != null) {
+                icon.getBackground().setAlpha(alpha);
+            }
+        }
     }
 }

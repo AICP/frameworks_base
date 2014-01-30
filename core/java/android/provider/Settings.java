0b9cc5024c12a85ca6a44470719216fec6cbe59f
Muhammed Nazim <mnm9994u@gmail.com>
2014-01-30 19:37:29 +0100

    National Data Roaming [3/3]
    Change-Id: I2d3809fdc1d1af6b528df4920f0bb35fe0b1d5d7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3136,6 +3136,14 @@ public final class Settings {
         public static final String NOTIFICATION_ALPHA = "notification_alpha";
 
         /**
+         * Width and height of items in sidebar
+         *
+         * Whether national data roaming should be used.
+         * @hide
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

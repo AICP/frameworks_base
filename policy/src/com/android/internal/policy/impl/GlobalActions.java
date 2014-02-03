1496bf0ca34fcc16aebf71fc96ea3baf2d9aa5c3
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2014-02-03 14:33:02 +0100

    GlobalActions: Set the initial status of airplane mode toggle
    Actually, the initial status of airplane mode toggle is set to false
    when the power menu dialog is initialized.
    This causes an issue if you set airplane mode and then reboot.
    After the reboot, the dialog displays the wrong airplane mode status,
    eg. "Airplane mode is not active", and if you toggle that option, a wrong
    intent is sent again to put ON the airplane mode, instead of OFF, the
    toggle (that is in transition state) will be set disabled.
    
    This commit fixes this issue.
    
    Change-Id: Id30355c1e090355645c437086c79ab59093c27a8

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -170,6 +170,9 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 com.android.internal.R.bool.config_useFixedVolume);
 
         mLockPatternUtils = new LockPatternUtils(mContext);
+
+        // set the initial status of airplane mode toggle
+        mAirplaneState = getUpdatedAirplaneToggleState();
     }
 
     /**
@@ -1268,15 +1271,17 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
     };
 
+    private ToggleAction.State getUpdatedAirplaneToggleState() {
+        return (Settings.Global.getInt(mContext.getContentResolver(),
+                    Settings.Global.AIRPLANE_MODE_ON, 0) == 1) ?
+                ToggleAction.State.On : ToggleAction.State.Off;
+    }
+
     private void onAirplaneModeChanged() {
         // Let the service state callbacks handle the state.
         if (mHasTelephony) return;
 
-        boolean airplaneModeOn = Settings.Global.getInt(
-                mContext.getContentResolver(),
-                Settings.Global.AIRPLANE_MODE_ON,
-                0) == 1;
-        mAirplaneState = airplaneModeOn ? ToggleAction.State.On : ToggleAction.State.Off;
+        mAirplaneState = getUpdatedAirplaneToggleState();
         mAirplaneModeOn.updateState(mAirplaneState);
     }
 

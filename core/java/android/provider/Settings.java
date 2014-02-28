8f85d08e23a0e0532bc98dea51a4c60d3a698a89
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-02-28 17:15:12 +0100

    FWB: Custom scrolling values 1/2
    ported/modified from https://gerrit.omnirom.org/#/c/4251/
    originally written by Yanuar Harry <ai.the.smarties.physics@gmail.com>
    
    AICP:
    PS2: fix build
    PS3: Use default values as defaults
    
    Change-Id: I7f23fe736507f836137c5d6dc0b5fd2ad4a95462

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3435,6 +3435,41 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
 
+       /**
+         * @hide
+         */
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+
+        /**
+         *
+         * @hide
+         */
+        public static final String ANIMATION_CONTROLS_NO_SCROLL = "animation_controls_no_scroll";
+
+        /**
+         * Determine custom scroll friction.
+         * @hide
+         */
+        public static final String CUSTOM_SCROLL_FRICTION = "custom_scroll_friction";
+
+        /**
+         * Determine custom fling velocity.
+         * @hide
+         */
+        public static final String CUSTOM_FLING_VELOCITY = "custom_fling_velocity";
+
+        /**
+         * Determine custom overscroll distance.
+         * @hide
+         */
+        public static final String CUSTOM_OVERSCROLL_DISTANCE = "custom_overscroll_distance";
+
+        /**
+         * Determine custom overfling distance.
+         * @hide
+         */
+        public static final String CUSTOM_OVERFLING_DISTANCE = "custom_overfling_distance";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.

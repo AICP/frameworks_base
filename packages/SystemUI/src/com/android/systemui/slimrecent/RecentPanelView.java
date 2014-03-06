9949ee59d8eda49563b776633d19e8837dd01369
doc <doc.divxm@gmail.com>
2014-03-06 20:56:48 +0200

    Revert "Frameworks: Slimrecents: cover bad 3rd party apps behaviour"
    This reverts commit 1a4efc954973b18b6bcf1f5a542d96418cc9fc6e.
    
    
    Change-Id: I3341e4f3fc979f609cefd5744b1f76e1fbcc8f73
    Signed-off-by: doc <doc.divxm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -59,7 +59,7 @@ import java.util.List;
 public class RecentPanelView {
 
     private static final String TAG = "RecentPanelView";
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
 
     private static final int DISPLAY_TASKS = 20;
     public static final int MAX_TASKS = DISPLAY_TASKS + 1; // allow extra for non-apps
@@ -340,7 +340,7 @@ public class RecentPanelView {
      */
     protected void loadTasks() {
         if (DEBUG) Log.v(TAG, "loading tasks");
-        setTasksLoaded(false);
+        mTasksLoaded = false;
         updateExpandedTaskStates();
         mTasks.clear();
 
@@ -521,12 +521,8 @@ public class RecentPanelView {
     protected void setCancelledByUser(boolean cancelled) {
         mCancelledByUser = cancelled;
         if (cancelled) {
-            setTasksLoaded(false);
+            mTasksLoaded = false;
         }
-    }
-
-    protected void setTasksLoaded(boolean loaded) {
-        mTasksLoaded = loaded;
     }
 
     protected boolean isCancelledByUser() {
@@ -542,7 +538,7 @@ public class RecentPanelView {
      */
     private void tasksLoaded() {
         if (mOnTasksLoadedListener != null) {
-            setTasksLoaded(true);
+            mTasksLoaded = true;
             mOnTasksLoadedListener.onTasksLoaded();
         }
     }
@@ -553,6 +549,7 @@ public class RecentPanelView {
     private void exit() {
         if (mOnExitListener != null) {
             mOnExitListener.onExit();
+            mTasksLoaded = false;
         }
     }
 

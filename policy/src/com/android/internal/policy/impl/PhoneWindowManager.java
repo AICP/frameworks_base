798664598546bcbeae0683d22ed7948ec7ed75a0
sethyx <sethyx@gmail.com>
2014-03-07 19:51:52 +0100

    Prevent wakeup when receiving AVRCP media KeyEvents
    Devices connected via AVRCP use different codes.
    Added playback events to the filter.
    
    Change-Id: I359a5c34a29ef524d5781f9ab35bd2c4f2ae8be1
    Signed-off-by: sethyx <sethyx@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4655,6 +4655,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             isWakeKey = false;
         }
 
+        // don't wake the screen for media events
+        if ((keyCode == KeyEvent.KEYCODE_HEADSETHOOK
+                || ((keyCode >= KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE)
+                && (keyCode <= KeyEvent.KEYCODE_MEDIA_FAST_FORWARD))
+                || (keyCode == KeyEvent.KEYCODE_MEDIA_PLAY)
+                || (keyCode == KeyEvent.KEYCODE_MEDIA_PAUSE))
+                && isWakeKey) {
+            isWakeKey = false;
+        }
+
         if (DEBUG_INPUT) {
             Log.d(TAG, "interceptKeyTq keycode=" + keyCode
                     + " screenIsOn=" + isScreenOn + " keyguardActive=" + keyguardActive

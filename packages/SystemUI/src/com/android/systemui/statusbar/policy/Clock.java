3c7dc0c2a959a636a79a596306c08f22a17a3226
Sven Dawitz <sven@dawitz.de>
2014-03-26 12:57:04 +0100

    SystemUI: Forward port Statusbar Clock and Date actions
    Port of http://review.cyanogenmod.org/#/c/27795/4
    
    PS2: Cleanup and address comments
    PS3: Remove unused variables
    PS4: Remove unused imports
    PS5: Declare AP_PM_STYLE final
    PS6: Add in @drawable/ic_notify_button_bg to header
    PS7: Fix DateView layout. Clear activity stack
    
    Begin old commit message-----------
    
    Patch set 1-2: Initial port
    Patch set 3  : Fixes date click action
    Patch set 4  : Remove unused import
    
    Includes:
    - Add onClick action to notification clock and date
      In expanded StatusBar
      - clicking clock opens alarms
      - clicking date opens calendar - today selected
      Id: I850732dcca0e65af59ce009aaf9f7ba18bcaf915
    
    - Add onLongClick action to notication clock and date
      - As discussed with ciwrl on google+, LongClick opens DateTimeSettings
      - Moved on-touch color change to superior xml solution, as suggested by
        Andrew Boren <preludedrew@gmail.com> here: http://review.evervolv.com/#/c/3676/"
      Id: I1fae71e347ffcb786227ba0b2651a66f2df35e27
    
    - Add SET_ALARM permission for SystemUI
      If one or more third-party alarm clock app is installed, clicking clock should
      open an app chooser for the alarm clock. However, permission for
      com.android.alarm.permission.SET_ALARM is required to make this behaviour
      work properly.
    
    AICP:
    Modified for AICP
    
    Change-Id: I8006e57a953fe0c4be480e0aa27a4c28c672f982

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -16,6 +16,8 @@
 
 package com.android.systemui.statusbar.policy;
 
+import android.app.ActivityManagerNative;
+import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -27,6 +29,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.UserHandle;
+import android.provider.AlarmClock;
 import android.provider.Settings;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
@@ -35,6 +38,8 @@ import android.text.style.CharacterStyle;
 import android.text.style.RelativeSizeSpan;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.TextView;
 
 import com.android.systemui.DemoMode;
@@ -51,7 +56,7 @@ import libcore.icu.LocaleData;
 /**
  * Digital clock for the status bar.
  */
-public class Clock extends TextView implements DemoMode {
+public class Clock extends TextView implements DemoMode, OnClickListener, OnLongClickListener {
     private boolean mAttached;
     private Calendar mCalendar;
     private String mClockFormatString;
@@ -111,6 +116,11 @@ public class Clock extends TextView implements DemoMode {
 
     public Clock(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+
+        if (isClickable()) {
+            setOnClickListener(this);
+            setOnLongClickListener(this);
+        }
     }
 
     @Override
@@ -330,6 +340,40 @@ public class Clock extends TextView implements DemoMode {
                 break;
         }
      }
+
+    private void collapseStartActivity(Intent what) {
+        // collapse status bar
+        StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(
+                Context.STATUS_BAR_SERVICE);
+        statusBarManager.collapsePanels();
+
+        // dismiss keyguard in case it was active and no passcode set
+        try {
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (Exception ex) {
+            // no action needed here
+        }
+
+        // start activity
+        what.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        mContext.startActivity(what);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
+        collapseStartActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Intent intent = new Intent("android.settings.DATE_SETTINGS");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        collapseStartActivity(intent);
+
+        // consume event
+        return true;
+    }
 
     private boolean mDemoMode;
 

793df6509abb961801b6a8140c60e06760e45843
Daniele Di Conza <danidicky@gmail.com>
2014-03-28 10:57:50 +0100

    frameworks/base: forward port DC-HSPAP support from cm-10
    PS10: add new drawables, I need to add new QS drawables and also RTL support
    PS11: fix blank space and prevents checkapi to fail
    PS13: fixed tile icon and rebase
    
    Change-Id: I1503c464b2db841fc193e57311e62415f5ff40f2

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -641,6 +641,11 @@ public class TelephonyManager {
     /** Current network is HSPA+ */
     public static final int NETWORK_TYPE_HSPAP = 15;
 
+    /** Current network is DC-HSPAP
+    * @hide
+    */
+    public static final int NETWORK_TYPE_DCHSPAP = 30;
+
     /**
      * @return the NETWORK_TYPE_xxxx for current data connection.
      */
@@ -669,7 +674,7 @@ public class TelephonyManager {
      * @see #NETWORK_TYPE_LTE
      * @see #NETWORK_TYPE_EHRPD
      * @see #NETWORK_TYPE_HSPAP
-     *
+     * @see #NETWORK_TYPE_DCHSPAP
      * @hide
      */
     public int getDataNetworkType() {
@@ -745,6 +750,7 @@ public class TelephonyManager {
             case NETWORK_TYPE_EVDO_B:
             case NETWORK_TYPE_EHRPD:
             case NETWORK_TYPE_HSPAP:
+            case NETWORK_TYPE_DCHSPAP:
                 return NETWORK_CLASS_3_G;
             case NETWORK_TYPE_LTE:
                 return NETWORK_CLASS_4_G;
@@ -797,6 +803,8 @@ public class TelephonyManager {
                 return "iDEN";
             case NETWORK_TYPE_HSPAP:
                 return "HSPA+";
+            case NETWORK_TYPE_DCHSPAP:
+                return "DC-HSPA+";
             default:
                 return "UNKNOWN";
         }

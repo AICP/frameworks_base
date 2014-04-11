22cbcc57d387d859245fb208d0e9ec67598f6dd1
Sam Mortimer <sam@mortimer.me.uk>
2014-04-11 15:58:03 +0200

    [1/2] Power connect/disconnect notification support
    AICP:
    PS2:
    Move to Settings.System
    PS3:
    Make it work
    
    Change-Id: I36a6b9f924d2cd52191a8e83a744745b37c5b068

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6376,6 +6376,24 @@ public final class Settings {
         public static final String LOW_BATTERY_SOUND = "low_battery_sound";
 
         /**
+         * Whether to sound when charger power is connected/disconnected
+         * @hide
+         */
+        public static final String POWER_NOTIFICATIONS_ENABLED = "power_notifications_enabled";
+
+        /**
+         * Whether to vibrate when charger power is connected/disconnected
+         * @hide
+         */
+        public static final String POWER_NOTIFICATIONS_VIBRATE = "power_notifications_vibrate";
+
+        /**
+         * URI for power notification sounds
+         * @hide
+         */
+        public static final String POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
+
+        /**
          * Whether to play a sound for low-battery alerts.
          * @hide
          */
@@ -7607,6 +7625,9 @@ public final class Settings {
             AUTO_TIME,
             AUTO_TIME_ZONE,
             POWER_SOUNDS_ENABLED,
+            POWER_NOTIFICATIONS_ENABLED,
+            POWER_NOTIFICATIONS_VIBRATE,
+            POWER_NOTIFICATIONS_RINGTONE,
             DOCK_SOUNDS_ENABLED,
             USB_MASS_STORAGE_ENABLED,
             ENABLE_ACCESSIBILITY_GLOBAL_GESTURE_ENABLED,

e03c596378bca79d81950fce010b838a8edb8784
maxwen <max.weninger@gmail.com>
2014-04-29 14:45:32 +0200

    base: simplify OmniSwitch recents
    The handshake between SystemUI and OmniSwitch is nice
    but causes issues if one of the parts gets restarted
    thanks to android. So simplify it and only use the
    settings value. Worst thing that can happen is that
    OmniSwitch recents is enabled but OmniSwitch is not running
    then nothing will happen if recents is triggered
    
    Change-Id: I43d93ea475f72e55fbc767c65b5050acaf4977eb

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -202,8 +202,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected AppCircleSidebar mAppCircleSidebar;
 
-    private Map<Integer, Boolean> mOmniSwitchStarted = new HashMap<Integer, Boolean>();
-
     public Ticker getTicker() {
         return mTicker;
     }
@@ -304,14 +302,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mCurrentUserId = intent.getIntExtra(Intent.EXTRA_USER_HANDLE, -1);
                 if (true) Log.v(TAG, "userId " + mCurrentUserId + " is in the house");
                 userSwitched(mCurrentUserId);
-            } else if (OmniSwitchConstants.ACTION_SERVICE_START.equals(action)) {
-                int userId = intent.getIntExtra(Intent.EXTRA_USER_HANDLE, -1);
-                Log.v(TAG, "OmniSwitch service started " + userId);
-                mOmniSwitchStarted.put(userId, true);
-            } else if (OmniSwitchConstants.ACTION_SERVICE_STOP.equals(action)) {
-                int userId = intent.getIntExtra(Intent.EXTRA_USER_HANDLE, -1);
-                Log.v(TAG, "OmniSwitch service stoped " + userId);
-                mOmniSwitchStarted.put(userId, false);
             }
         }
     };
@@ -411,8 +401,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_USER_SWITCHED);
-        filter.addAction(OmniSwitchConstants.ACTION_SERVICE_START);
-        filter.addAction(OmniSwitchConstants.ACTION_SERVICE_STOP);
         mContext.registerReceiver(mBroadcastReceiver, filter);
         SidebarObserver observer = new SidebarObserver(mHandler);
         observer.observe();
@@ -840,14 +828,10 @@ public abstract class BaseStatusBar extends SystemUI implements
     };
 
     private boolean isOmniSwitchEnabled() {
-        // TODO no user specific value here
-        int settingsValue = Settings.System.getInt(
-                mContext.getContentResolver(), Settings.System.RECENTS_USE_OMNISWITCH, 0);
-        boolean omniSwitchStarted = false;
-        if (mOmniSwitchStarted.containsKey(mCurrentUserId)){
-            omniSwitchStarted = mOmniSwitchStarted.get(mCurrentUserId);
-        }
-        return (settingsValue == 1) && omniSwitchStarted;
+        int settingsValue = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.RECENTS_USE_OMNISWITCH, 0
+                , UserHandle.USER_CURRENT);
+        return (settingsValue == 1);
     }
 
     protected void toggleRecentsActivity() {

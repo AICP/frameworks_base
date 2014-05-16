de0447c48b5462c8a627de2b8d69ee4aad5c2751
Clark Scheff <clark@cyngn.com>
2014-05-16 12:11:06 -0700

    CM11 Themes: Properly handle themeing apps on install
    Previously when an app was installed or re-installed it would not
    pick up the currently applied theme.
    
    mOverlays was not being properly updated and therefore newly
    installed or re-installed apps would not pick up the currently
    applied theme.
    
    Change-Id: I7ce5454af11fb44e9a73b30858d893050b2fba8a

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -5358,13 +5358,13 @@ public class PackageManagerService extends IPackageManager.Stub {
 
             // Generate Idmaps and res tables if pkg is a theme
             for(String target : pkg.mOverlayTargets) {
+                insertIntoOverlayMap(target, pkg);
                 if (!shouldCreateIdmap(mPackages.get(target), pkg)) {
                     continue;
                 }
                 if (pkg.mIsLegacyThemeApk) {
                     if (target != null) {
                         try {
-                            insertIntoOverlayMap(target, pkg);
                             generateIdmapForLegacyTheme(target, pkg);
                         } catch (Exception e) {
                             mLastScanError = PackageManager.INSTALL_FAILED_INTERNAL_ERROR;
@@ -5386,7 +5386,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                     }
                     ThemeUtils.createResourcesDirIfNotExists(target, pkg.applicationInfo.publicSourceDir);
                     compileResources(target, pkg);
-                    insertIntoOverlayMap(target, pkg);
                     generateIdmap(target, pkg);
                 } catch(Exception e) {
                     Log.w(TAG, "Unable to process theme " + pkgName, e);

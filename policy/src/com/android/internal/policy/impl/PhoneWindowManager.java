f1f93f31f30e2041652ff090d2c5f64bb323956a
kunleiz <quic_kunleiz@codeaurora.org>
2014-05-23 01:07:25 +0300

    audio: Volume can't adjust during HFP call when screen is off.
    PhoneWindowManager don't pass the key to the application when screen is
    off. In handleVolumeKey(), the STREAM_VOICE_CALL volume was set, so the
    STREAM_BLUETOOTH_SCO volume can't be adjusted.
    Add a judgement to choose the correct stream to adjust when screen is off.
    
    CRs-Fixed: 649548
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: I1ab06617d59b9bf2eabed9ed8b6892bc20db75b9

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4755,7 +4755,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         final boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
         final boolean canceled = event.isCanceled();
-        final int keyCode = event.getKeyCode();
+        int keyCode = event.getKeyCode();
+        int scanCode = event.getScanCode();
+        AudioManager audioManager = (AudioManager) mContext.getSystemService(
+                Context.AUDIO_SERVICE);
 
         if (SystemProperties.getInt("sys.quickboot.enable", 0) == 1) {
 
@@ -4954,7 +4957,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                                     && (result & ACTION_PASS_TO_USER) == 0) {
                                 // If we are in call but we decided not to pass the key to
                                 // the application, handle the volume change here.
-                                handleVolumeKey(AudioManager.STREAM_VOICE_CALL, keyCode);
+                                if(audioManager.isBluetoothScoOn()) {
+                                    handleVolumeKey(AudioManager.STREAM_BLUETOOTH_SCO, keyCode);
+                                } else {
+                                    handleVolumeKey(AudioManager.STREAM_VOICE_CALL, keyCode);
+                                }
                                 break;
                             }
                         } catch (RemoteException ex) {

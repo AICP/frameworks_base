d56151fd111951262f8bb2f042dec906bab5f2d6
Lars Greiss <kufikugel@googlemail.com>
2014-06-06 22:18:02 +0200

    fix listanimation mode
    Change-Id: Iffb888bb4f16f0a72dd88fa667813929f226b622

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -2278,8 +2278,13 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         }
 
         final View scrapView = mRecycler.getScrapView(position);
-        final View child = mAdapter.getView(position, scrapView, this);
+        View child = mAdapter.getView(position, scrapView, this);
+
         if (scrapView != null) {
+            if (mListAnimationMode != 0 && !mIsWidget) {
+                child = setAnimation(child);
+            }
+
             if (child != scrapView) {
                 // Failed to re-bind the data, return scrap to the heap.
                 mRecycler.addScrapView(scrapView, position);

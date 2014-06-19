98c5929f8d14314eeca019bf770bc99388420c0e
Jorge Ruesga <jorge@ruesga.com>
2014-06-19 18:33:16 +0200

    base: don't show global actions if shutdown sequence was started
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    Change-Id: Icd77aa7529faf438dcc80e45434826465a11aab8

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -430,6 +430,7 @@ public interface WindowManagerPolicy {
         public void shutdown(boolean confirm);
         public void reboot(String reason);
         public void rebootSafeMode(boolean confirm);
+        public boolean isShutdownSequenceStarted();
 
         /**
          * Return the window manager lock needed to correctly call "Lw" methods.
--- services/java/com/android/server/power/ShutdownThread.java
+++ services/java/com/android/server/power/ShutdownThread.java
@@ -94,7 +94,13 @@ public final class ShutdownThread extends Thread {
     
     private ShutdownThread() {
     }
- 
+
+    public static boolean isStarted() {
+        synchronized (sIsStartedGuard) {
+            return sIsStarted;
+        }
+    }
+
     /**
      * Request a clean shutdown, waiting for subsystems to clean up their
      * state etc.  Must be called from a Looper thread in which its UI

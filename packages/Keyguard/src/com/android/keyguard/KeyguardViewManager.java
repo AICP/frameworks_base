4d126cded49ef6ef3d403f466d13bad08c2f0c37
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2014-06-25 22:51:37 +0300

    Keyguard: fix NPE setting lockscreen wallpaper
    After boot the view might not be laid out and so getWidth() and
    getHeight() return 0 and will cause a NullPointerException in
    createBitmap().
    
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>
    Change-Id: Ibacf4199a69709dc34d21695732c2a5360533a23

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -357,6 +357,7 @@ public class KeyguardViewManager {
         }
 
         public void setCustomBackground(Drawable d) {
+            if (!isLaidOut()) return;
             if (!ActivityManager.isHighEndGfx()) {
                 mCustomBackground = d;
                 if (d != null) {

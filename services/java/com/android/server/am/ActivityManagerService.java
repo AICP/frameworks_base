32347863f43a75bd00112a16bdd2c54a825d4cf4
riddle_hsu <riddle_hsu@htc.com>
2014-07-04 12:19:41 +0800

    [ActivityManager] Prevent kill a restarted process again.
    Root Cause:
    Use removeTask with flag ActivityManager.REMOVE_TASK_KILL_PROCESS will set waitingToKill = "remove task"
    to the target process when its setSchedGroup is not BG_NONINTERACTIVE.
    Later the target process may be killed when applying oom-adj due to setSchedGroup has changed to BG_NONINTERACTIVE.
    If the process is needed to restart, the process record will be resued.
    Then the restarted process may be killed again because its waitingToKill is not null.
    
    Solution:
    Clean waitingToKill when process is dead.
    
    Change-Id: I5ffb5388127f4221da4c700d3f1c224f7ca6e7b2

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -12507,6 +12507,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         app.resetPackageList(mProcessStats);
         app.unlinkDeathRecipient();
         app.makeInactive(mProcessStats);
+        app.waitingToKill = null;
         app.forcingToForeground = null;
         app.foregroundServices = false;
         app.foregroundActivities = false;

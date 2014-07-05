80449a08700da00b83f802b30cd3508bd78a4148
Jubakuba <Jubakuba@gmail.com>
2014-07-05 18:47:44 +0200

    Frameworks: Quiet Hours Change Logic (1/2)
    12:00am - 12:00am = 24 hours
    	(Or any "==" time)
    
    This makes more sense for the user
    	if they have the direct ability
    	to toggle quiet hours.
    	The feature should work as-is
    	and in a 24-hour on/off manner
    	through using the toggle.
    
    Change-Id: I9431f5d166fb5e0485cf84b6607aa0c57165cc3c

--- core/java/com/android/internal/util/cm/QuietHoursUtils.java
+++ core/java/com/android/internal/util/cm/QuietHoursUtils.java
@@ -17,12 +17,14 @@
 package com.android.internal.util.cm;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.provider.Settings;
 
 import java.util.Calendar;
 
 public class QuietHoursUtils {
     public static boolean inQuietHours(Context context, String option) {
+        boolean mode = true;
         boolean quietHoursEnabled = Settings.AOKP.getInt(context.getContentResolver(),
                 Settings.AOKP.QUIET_HOURS_ENABLED, 0) != 0;
         int quietHoursStart = Settings.AOKP.getInt(context.getContentResolver(),
@@ -31,17 +33,27 @@ public class QuietHoursUtils {
                 Settings.AOKP.QUIET_HOURS_END, 0);
         boolean quietHoursOption = Settings.AOKP.getInt(context.getContentResolver(),
                 option, 0) != 0;
-        if (quietHoursEnabled && quietHoursOption && (quietHoursStart != quietHoursEnd)) {
-                // Get the date in "quiet hours" format.
-                Calendar calendar = Calendar.getInstance();
-                int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60
-                                           + calendar.get(Calendar.MINUTE);
-                if (quietHoursEnd < quietHoursStart) {
-                    // Starts at night, ends in the morning.
-                    return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
-                } else {
-                    return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
-                }
+
+        if (option != null) {
+            mode = Settings.System.getIntForUser(context.getContentResolver(),
+                    option, 0,
+                    UserHandle.USER_CURRENT_OR_SELF) != 0;
+        }
+
+        if (quietHoursEnabled && mode) {
+            // 24-hours toggleable
+            if (quietHoursStart == quietHoursEnd) {
+                return true;
+            }
+            // Get the date in "quiet hours" format.
+            Calendar calendar = Calendar.getInstance();
+            int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+            if (quietHoursEnd < quietHoursStart) {
+                // Starts at night, ends in the morning.
+                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
+            } else {
+                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
+            }
         }
         return false;
     }

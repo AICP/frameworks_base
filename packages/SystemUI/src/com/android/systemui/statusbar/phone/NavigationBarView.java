f8969afea024f94b5b7142db27f5aff5a144bdd8
Danny Baumann <dannybaumann@web.de>
2014-07-15 13:20:45 +0200

    Allow translucent navigation bar in landscape mode.
    Change-Id: I5388f3f8a34533624619714bacfba52f95fc6d85

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -953,6 +953,7 @@ public class NavigationBarView extends LinearLayout {
 
     public void setLeftInLandscape(boolean leftInLandscape) {
         mLeftInLandscape = leftInLandscape;
+        mBarTransitions.setLeftIfVertical(leftInLandscape);
         mDeadZone.setStartFromRight(leftInLandscape);
     }
 

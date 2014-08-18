c1ececc997f3aaa87a900b96ed4173fa2818c756
liquid0624 <rjbirnesser@gmail.com>
2014-08-18 18:38:21 -0400

    Fix lock screen notifications proximity sensors Fixes pocket mode and wake on notification options
    Thanks Steve Spencer :)
    
    Change-Id: I3e7ee05be12c86ce31c2db4d8150c0f5b3c3dadc

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -205,9 +205,11 @@ public class NotificationViewManager {
             boolean showNotification = !mHostView.containsNotification(sbn) || mHostView.getNotification(sbn).when != sbn.getNotification().when;
             boolean added = mHostView.addNotification(sbn, (screenOffAndNotCovered || mIsScreenOn) && showNotification,
                     config.forceExpandedView);
-            if ( added && config.wakeOnNotification && screenOffAndNotCovered
-                        && showNotification && mTimeCovered == 0) {
+            if (added && config.wakeOnNotification && screenOffAndNotCovered
+                      && showNotification && mTimeCovered == 0
+                      && !inQuietHours()) {
                 wakeDevice();
+                mHostView.showAllNotifications();
             }
         }
         @Override

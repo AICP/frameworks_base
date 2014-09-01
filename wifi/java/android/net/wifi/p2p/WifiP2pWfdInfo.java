Neoteric-OS/android_frameworks_base

WifiP2pWfdInfo set device type bugfix
Change-Id: I37a53aed80574e78c80dc4a4192bd8cc4320de9d
Signed-off-by: Grigoriy Kraynov <kraynov@mgpro.ru>

wifi/java/android/net/wifi/p2p/WifiP2pWfdInfo.java
@@ -77,6 +77,7 @@ public class WifiP2pWfdInfo implements Parcelable {
 
     public boolean setDeviceType(int deviceType) {
         if (deviceType >= WFD_SOURCE && deviceType <= SOURCE_OR_PRIMARY_SINK) {
+            mDeviceInfo &= ~DEVICE_INFO;
             mDeviceInfo |= deviceType;
             return true;
         }

AICP/frameworks_base

wifi/java/android/net/wifi/p2p/WifiP2pWfdInfo.java
@@ -77,6 +77,7 @@ public class WifiP2pWfdInfo implements Parcelable {
 
     public boolean setDeviceType(int deviceType) {
         if (deviceType >= WFD_SOURCE && deviceType <= SOURCE_OR_PRIMARY_SINK) {
+            mDeviceInfo &= ~DEVICE_TYPE;
             mDeviceInfo |= deviceType;
             return true;
         }
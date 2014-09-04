CyanogenMod/android_frameworks_base

Fix hiding ADB notification
When the notification is already shown (the device is connected in ADB mode)
and the user turns it off, it doesn't go away. Let's hide it properly.

Change-Id: Ie411a89d5549ae9662c59ee9ee5d18a9568e1ede

services/java/com/android/server/usb/UsbDeviceManager.java
@@ -745,8 +745,13 @@ public class UsbDeviceManager {
             if (mAdbEnabled && mConnected) {
                 if ("0".equals(SystemProperties.get("persist.adb.notify"))
                  || Settings.Secure.getInt(mContext.getContentResolver(),
-                    Settings.Secure.ADB_NOTIFY, 1) == 0)
+                    Settings.Secure.ADB_NOTIFY, 1) == 0) {
+                    if (mAdbNotificationShown) {
+                        mAdbNotificationShown = false;
+                        mNotificationManager.cancelAsUser(null, id, UserHandle.ALL);
+                    }
                     return;
+                }
 
                 if (!mAdbNotificationShown) {
                     Resources r = mContext.getResources();

AICP/frameworks_base

services/java/com/android/server/usb/UsbDeviceManager.java
@@ -769,8 +769,13 @@ public class UsbDeviceManager {
             if (mAdbEnabled && mConnected) {
                 if ("0".equals(SystemProperties.get("persist.adb.notify"))
                  || Settings.AOKP.getInt(mContext.getContentResolver(),
-                    Settings.AOKP.ADB_NOTIFY, 1) == 0)
+                    Settings.AOKP.ADB_NOTIFY, 1) == 0) {
+                    if (mAdbNotificationShown) {
+                        mAdbNotificationShown = false;
+                        mNotificationManager.cancelAsUser(null, id, UserHandle.ALL);
+                    }
                     return;
+                }
 
                 if (!mAdbNotificationShown) {
                     Resources r = mContext.getResources();
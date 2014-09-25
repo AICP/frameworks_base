cc052fb354424fb3873858c167022576948af87c
LorDClockaN <davor@losinj.com>
2014-09-25 18:07:07 +0200

    Fix white space before first notification
    Change-Id: I32f3ad51a98ad36b3ca562ca267307ea49ff46e2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4016,7 +4016,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 Settings.System.SYSTEM_ICON_COLOR, -2, UserHandle.USER_CURRENT);
 
         mShowStatusBarCarrier = Settings.System.getInt(
-            cr, Settings.System.STATUS_BAR_CARRIER, 1) == 1;
+            cr, Settings.System.STATUS_BAR_CARRIER, 0) == 1;
         showStatusBarCarrierLabel(mShowStatusBarCarrier);
 
         updateBatteryIcons();

d88e688c158c43c8d02bb2c711c26a9dfe44a0f7
Jmz <jmztaylor@gmail.com>
2014-09-30 18:50:35 +0200

    Force Expanded Notifications (1/2)
    This will force expanded notifications for all apps that support
    expanded notifications
    
    Change-Id: I1f6fac658a3e58488bc37cafec9208a75da78ab8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4546,6 +4546,12 @@ public final class Settings {
          */
         public static final String STATUS_BAR_CLOCK_LOCKSCREEN = "status_bar_clock_lockscreen";
 
+        /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
         /**         
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.

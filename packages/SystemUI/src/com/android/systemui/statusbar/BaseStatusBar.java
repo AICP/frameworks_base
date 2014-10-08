5cc0108c6d388043ed4f47501182662e75e73d48
Francesco Colella <mnemonyc@hotmail.com>
2014-10-08 15:41:22 +0200

    SystemUI: Fix potential NPE
    Change-Id: I163526431fdf77a788e56d6bb860d769689e3063

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -772,6 +772,9 @@ public abstract class BaseStatusBar extends SystemUI implements
             @Override
             public boolean onLongClick(View v) {
                 final NotificationData.Entry  entry = (NotificationData.Entry) v.getTag();
+                if (entry.notification == null) {
+                    return false;
+                }
                 final StatusBarNotification sbn = entry.notification;
 
                 final String packageNameF = sbn.getPackageName();

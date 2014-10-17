9550df730ce5d49582d1d8773846e4c635b1f5bf
Jubakuba <Jubakuba@gmail.com>
2014-10-17 00:39:40 -0600

    Framework: StatusBar Ticker (1/2)
    Change-Id: I6135297792c7b42172e08e14acd37b252cb00102

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2048,6 +2048,13 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Whether to enable ticker animation in statusbar
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
+
+        /**
          * The keyboard brightness to be used while the screen is on.
          * Valid value range is between 0 and {@link PowerManager#getMaximumKeyboardBrightness()}
          * @hide

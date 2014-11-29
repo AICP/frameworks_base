e1fd4b0b7189475239fd1738315a0dcc20b62e41
Muhammed Nazim <mnm9994u@gmail.com>
2014-11-29 21:43:37 +0100

    Forward Port: Volume button seek on screen off [1/2]
    PS : Fix the short press event type
    
    Change-Id: Id55a1240a133b5144502fced862bae040b390662

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3235,6 +3235,12 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
         /**
+         * Volume music controls
+         * @hide
+         */
+        public static final String VOLUME_MUSIC_CONTROLS = "volume_music_controls";
+
+        /**
          * Status bar battery %
          * @hide
          */

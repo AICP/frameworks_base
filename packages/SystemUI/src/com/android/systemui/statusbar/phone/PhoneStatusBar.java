8d05845c3a456c989d1d9a26931f05c577d1f07e
0xD34D <clark@scheffsblend.com>
2014-12-14 21:23:05 +0100

    FWB: Gesture Anywhere - bring it back [1/2]
    AICPfy lp5.0:
    - Lollipopify LOL
    - By request from doc
    - Added TriggerOverlayView from AppSidebar
    - some lines from Locskcreen gestures to get
      it compiled and working
    
    Change-Id: I04acb9a88fe0011921a1bafdc1919d76ce57aedf

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -35,6 +35,8 @@ import static com.android.systemui.statusbar.phone.BarTransitions.MODE_WARNING;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.TimeInterpolator;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
 import android.annotation.NonNull;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
@@ -762,6 +764,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // ================================================================================
     // Constructing the view
     // ================================================================================
+    @ChaosLab(name="GestureAnywhere", classification=Classification.CHANGE_CODE)
     protected PhoneStatusBarView makeStatusBarView() {
         final Context context = mContext;
 
@@ -847,6 +850,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         updateShowSearchHoldoff();
+
+        addGestureAnywhereView();
 
         try {
             boolean showNav = mWindowManagerService.hasNavigationBar();

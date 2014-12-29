f9ec9b03a4a9dcc0f23bd8b50dbdf93eaee944a1
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-12-29 06:07:45 +0000

    policy: don't use EdgeGestureService for navigation bar if were on the keyguard
    with this option enabled if you swipe from off the screen to unlock it will not register
    the first time requiring a second swipe.
    
    Patch set 2: don't disable as default
    
    Patch set 3: use !isStatusBarKeyguard() thanks to Kiril Mikos
    
    Change-Id: I7b2bf3002bd510df3b65a2d533933ebd81188e70

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1221,7 +1221,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (mStatusBar != null && !mStatusBar.isVisibleLw()) {
                 flags |= EdgeGesturePosition.TOP.FLAG;
             }
-            if (mNavigationBar != null && !mNavigationBar.isVisibleLw()) {
+            if (mNavigationBar != null && !mNavigationBar.isVisibleLw() && !isStatusBarKeyguard()) {
                 if (mNavigationBarOnBottom) {
                     flags |= EdgeGesturePosition.BOTTOM.FLAG;
                 } else {

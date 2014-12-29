538a795874f32a5fa9487a750ae973cc6f0fa7a7
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5092,6 +5092,24 @@ public final class Settings {
         private static final Validator GLOBAL_ACTIONS_SCREENRECORD_VALIDATOR =
               BOOLEAN_VALIDATOR;
 
+        /**
+         * AOKP Custom System Animations
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+        };
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /***************************
          * AICP System Settings end
          ***************************/

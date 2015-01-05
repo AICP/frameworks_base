6a5206e37cd9ee79b4f1c123195eb47e0365925d
Shashi Shekar Shankar <ssbang@codeaurora.org>
2015-01-05 17:40:39 +0530

    Performance: Memory Optimizations
    The following optimizations are squashed in this change.
    
    1. Enable Aggressive trim settings.
    
    This change will enable aggressive trim settings for targets
    up to 1GB. The change can be turned on/off from system properties.
    By default, the properties are set for targets up to 1GB.
    
    Changes spread in to : ActivityManagerConstants.java
    CRs-Fixed: 783020
    
    2. Propagate B-services to higher adj.
    
    Depending on the inactivity of a service, move the B-services to
    highest adj 906. Under memory pressure, these services will
    be killed first ahead of cached apps which results in better
    concurrency numbers with bg apps. Inactivity time and minumum
    no of services to be maintained are configurable as system properties.
    Improves concurrency.
    
    Changes spread in to : ActivityManagerService.java
    CRs-Fixed: 792122
    
    3. Postpone service restart during app launch.
    
    In the android framework, when the service gets killed,
    it will be rescheduled. Postpone the service restart if
    the app is in process of startup. By Default this feature
    is disabled. It can be enabled from build.prop of target.
    
    Changes spread in to : ActiveServices.java
    CRs-Fixed: 879756 911145
    
    4. Performance: Make Cached apps limit configurable.
    
    Set it via a target specific property
    file. Allows flexibility to set the limit
    based on device config.
    
    Normally, setting a smaller value for
    low-end device helps with performance.
    
    Changes spread in to : ActivityManagerConstants.java
    
    Squash of following change-IDs:
        Change-Id: I233dddbff07e7ec1fe2ee96402fe1d411903beb5
        Change-Id: Ied6cfcc3d11951f32f18de680b0e3483db8e163e
        Change-Id: Ia86edf027e20df2c7aa6f34e3aa293f7a8e6f1fa
        Change-Id: I90a733d7134d86efffadf6815ce3d2944dd09419
    
    Change-Id: I5c63beb179681f19c2c8adb8466c1b3b81bbfe47

--- services/core/java/com/android/server/am/ActivityManagerConstants.java
+++ services/core/java/com/android/server/am/ActivityManagerConstants.java
@@ -70,7 +70,7 @@ final class ActivityManagerConstants extends ContentObserver {
     static final String KEY_BOUND_SERVICE_CRASH_MAX_RETRY = "service_crash_max_retry";
 
     private static final int DEFAULT_MAX_CACHED_PROCESSES =
-            SystemProperties.getInt("ro.vendor.qti.sys.fw.bg_apps_limit",32);
+            SystemProperties.getInt("ro.vendor.qti.sys.fw.bg_apps_limit", 32);
     private static final long DEFAULT_BACKGROUND_SETTLE_TIME = 60*1000;
     private static final long DEFAULT_FGSERVICE_MIN_SHOWN_TIME = 2*1000;
     private static final long DEFAULT_FGSERVICE_MIN_REPORT_TIME = 3*1000;
@@ -223,15 +223,15 @@ final class ActivityManagerConstants extends ContentObserver {
     public int CUR_MAX_CACHED_PROCESSES;
 
     static final boolean USE_TRIM_SETTINGS =
-            SystemProperties.getBoolean("ro.vendor.qti.sys.fw.use_trim_settings",true);
-    static final int EMPTY_APP_PERCENT = SystemProperties.getInt("ro.vendor.qti.sys.fw.empty_app_percent",50);
+            SystemProperties.getBoolean("ro.vendor.qti.sys.fw.use_trim_settings", true);
+    static final int EMPTY_APP_PERCENT =
+            SystemProperties.getInt("ro.vendor.qti.sys.fw.empty_app_percent", 50);
     static final int TRIM_EMPTY_PERCENT =
-            SystemProperties.getInt("ro.vendor.qti.sys.fw.trim_empty_percent",100);
+            SystemProperties.getInt("ro.vendor.qti.sys.fw.trim_empty_percent", 100);
     static final int TRIM_CACHE_PERCENT =
-            SystemProperties.getInt("ro.vendor.qti.sys.fw.trim_cache_percent",100);
+            SystemProperties.getInt("ro.vendor.qti.sys.fw.trim_cache_percent", 100);
     static final long TRIM_ENABLE_MEMORY =
-            SystemProperties.getLong("ro.vendor.qti.sys.fw.trim_enable_memory",1073741824);
-    public static boolean allowTrim() { return Process.getTotalMemory() < TRIM_ENABLE_MEMORY ; }
+            SystemProperties.getLong("ro.vendor.qti.sys.fw.trim_enable_memory", 1073741824);
 
     // The maximum number of empty app processes we will let sit around.
     public int CUR_MAX_EMPTY_PROCESSES;
@@ -267,26 +267,10 @@ final class ActivityManagerConstants extends ContentObserver {
     }
 
     public static int computeEmptyProcessLimit(int totalProcessLimit) {
-        if(USE_TRIM_SETTINGS && allowTrim()) {
-            return totalProcessLimit*EMPTY_APP_PERCENT/100;
-        } else {
-            return totalProcessLimit/2;
-        }
-    }
-
-    public static int computeTrimEmptyApps(int rawMaxEmptyProcesses) {
         if (USE_TRIM_SETTINGS && allowTrim()) {
-            return rawMaxEmptyProcesses*TRIM_EMPTY_PERCENT/100;
+            return totalProcessLimit * EMPTY_APP_PERCENT / 100;
         } else {
-            return rawMaxEmptyProcesses/2;
-        }
-    }
-
-    public static int computeTrimCachedApps(int rawMaxEmptyProcesses, int totalProcessLimit) {
-        if (USE_TRIM_SETTINGS && allowTrim()) {
-            return totalProcessLimit*TRIM_CACHE_PERCENT/100;
-        } else {
-            return (totalProcessLimit-rawMaxEmptyProcesses)/3;
+            return totalProcessLimit / 2;
         }
     }
 
@@ -374,8 +358,8 @@ final class ActivityManagerConstants extends ContentObserver {
         // additional enforced limit.
         final int rawMaxEmptyProcesses = computeEmptyProcessLimit(MAX_CACHED_PROCESSES);
         CUR_TRIM_EMPTY_PROCESSES = computeTrimEmptyApps(rawMaxEmptyProcesses);
-        CUR_TRIM_CACHED_PROCESSES =
-                computeTrimCachedApps(rawMaxEmptyProcesses, MAX_CACHED_PROCESSES);
+        CUR_TRIM_CACHED_PROCESSES = computeTrimCachedApps(
+                rawMaxEmptyProcesses, MAX_CACHED_PROCESSES);
     }
 
     void dump(PrintWriter pw) {
@@ -439,5 +423,25 @@ final class ActivityManagerConstants extends ContentObserver {
         pw.print("  CUR_MAX_EMPTY_PROCESSES="); pw.println(CUR_MAX_EMPTY_PROCESSES);
         pw.print("  CUR_TRIM_EMPTY_PROCESSES="); pw.println(CUR_TRIM_EMPTY_PROCESSES);
         pw.print("  CUR_TRIM_CACHED_PROCESSES="); pw.println(CUR_TRIM_CACHED_PROCESSES);
+    }
+
+    private static boolean allowTrim() {
+        return Process.getTotalMemory() < TRIM_ENABLE_MEMORY;
+    }
+
+    private static int computeTrimEmptyApps(int rawMaxEmptyProcesses) {
+        if (USE_TRIM_SETTINGS && allowTrim()) {
+            return rawMaxEmptyProcesses * TRIM_EMPTY_PERCENT / 100;
+        } else {
+            return rawMaxEmptyProcesses / 2;
+        }
+    }
+
+    private static int computeTrimCachedApps(int rawMaxEmptyProcesses, int totalProcessLimit) {
+        if (USE_TRIM_SETTINGS && allowTrim()) {
+            return totalProcessLimit * TRIM_CACHE_PERCENT / 100;
+        } else {
+            return (totalProcessLimit - rawMaxEmptyProcesses) / 3;
+        }
     }
 }

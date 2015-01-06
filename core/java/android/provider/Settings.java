bc549921eaca623c103b7679f8bf1a43a2162be8
Daxxmax <>
2015-01-06 08:47:44 +0100

    FWB: Statusbar clock font style (1/2)
    AICPfy
    Bring back to L
    
    Change-Id: Iafb811b9f819a100940fdeeaa2c657483c039950

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3580,6 +3580,12 @@ public final class Settings {
         public static final String NAVBAR_LEFT_IN_LANDSCAPE = "navigation_bar_left";
 
         /**
+         * Settings for clock font style
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

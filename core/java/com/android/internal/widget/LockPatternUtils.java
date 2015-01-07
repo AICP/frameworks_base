4b9d18ccda55119b89e8bbd237125e99539ee256
Roman Birg <roman@cyngn.com>
2015-01-07 13:20:04 -0800

    LockPatternUtils: don't crash when no profile is selected
    If there's no profile currently active, then return the default lock
    mode.
    
    Change-Id: Ifb26438d055a7fb72c6fdaa6586b445f5798b102
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/com/android/internal/widget/LockPatternUtils.java
+++ core/java/com/android/internal/widget/LockPatternUtils.java
@@ -1636,7 +1636,7 @@ public class LockPatternUtils {
             return Profile.LockMode.DEFAULT;
         }
         final Profile profile = mProfileManager.getActiveProfile();
-        return profile.getScreenLockMode();
+        return profile == null ? Profile.LockMode.DEFAULT : profile.getScreenLockMode();
     }
 
     /**

7fca305b3f05dad8a6aa9388a035b617728b5ac5
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-06 08:26:58 -0200

    HeadsUp: fix headsup for incoming calls
    Change-Id: I5b310e7efb21d88f16a761cfe6c2a59a8a8bf06c

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2196,7 +2196,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         boolean interrupt = (isFullscreen || (isHighPriority && (isNoisy || hasTicker))
                 || asHeadsUp == Notification.HEADS_UP_REQUESTED)
                 && isAllowed
-                && !isOngoing
                 && !accessibilityForcesLaunch
                 && mPowerManager.isScreenOn()
                 && !keyguardIsShowing;

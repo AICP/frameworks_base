ac0fdd4aa60df36dd6f659fbf316ad3dd7d0fe3a
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-08 20:01:09 +0100

    SystemUI: option to disable search panel (navrings) (1/2)
    Change-Id: I0d8999e19ce34495f600c25084418df7364ae79e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6331,6 +6331,12 @@ public final class Settings {
         };
 
         /**
+         * Whether search panel is enabled.
+         * @hide
+         */
+        public static final String SEARCH_PANEL_ENABLED = "search_panel_enabled";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear

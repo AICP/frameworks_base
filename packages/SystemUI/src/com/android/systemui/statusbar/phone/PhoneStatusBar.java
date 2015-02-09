35b9d9d99f64fc82e850937abec0b9a86ec27bb7
Davor Bertovic <lordclockan@gmail.com>
2015-02-09 19:19:15 +0100

    Revert "Revert "FWB: Heads Up switch (1/2)""
    This reverts commit 5779e1739772bccbe6521f56a5a02236bdd4946f.
    
    Change-Id: I55c580b2255d7ae0a11b26cc54d8ea38e14aecff

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -290,6 +290,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private DozeServiceHost mDozeServiceHost;
     private boolean mScreenOnComingFromTouch;
     private boolean mHeadsUpViewAttached;
+    private boolean mHeadsUpSwitch = false;
 
     int mPixelFormat;
     Object mQueueLock = new Object();
@@ -438,6 +439,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.HEADS_UP_SNOOZE_TIME),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_SWITCH),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BATTERY_SAVER_MODE_COLOR),
                     false, this, UserHandle.USER_ALL);
             update();
@@ -467,7 +471,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                             UserHandle.USER_CURRENT);
                     resetHeadsUpDecayTimer();
             } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.HEADS_UP_NOTIFCATION_DECAY))) {
+                    Settings.System.STATUS_BAR_TICKER_ENABLED))) {
                     mTickerEnabled = Settings.System.getIntForUser(
                             mContext.getContentResolver(),
                             Settings.System.STATUS_BAR_TICKER_ENABLED,
@@ -3716,6 +3720,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         EventLog.writeEvent(EventLogTags.SYSUI_HEADS_UP_STATUS,
                 vis ? mHeadsUpNotificationView.getKey() : "",
                 vis ? 1 : 0);
+        mHeadsUpSwitch = Settings.System.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.System.HEADS_UP_SWITCH,
+                1, UserHandle.USER_CURRENT) == 1;
+        vis = vis && mHeadsUpSwitch;
         mHeadsUpNotificationView.setVisibility(vis ? View.VISIBLE : View.GONE);
         if (!vis) {
             mHeadsUpPackageName = null;

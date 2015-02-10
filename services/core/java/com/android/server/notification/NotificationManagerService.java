c09a94c17f2d365f0cede9c76f409b7fa20b9572
Adnan Begovic <adnan@cyngn.com>
2015-02-10 02:58:49 +0000

    Lock screen notifications: fix "Disallowed call for uid <system uid>" security exception.
    Change-Id: I9ecced525d153f8e677e94a96ee39851710d8b23

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1283,6 +1283,7 @@ public class NotificationManagerService extends SystemService {
 
         @Override
         public int getShowNotificationForPackageOnKeyguard(String pkg, int uid) {
+            enforceSystemOrSystemUI("INotificationManager.getShowNotificationForPackageOnKeyguard");
             return mRankingHelper.getShowNotificationForPackageOnKeyguard(pkg, uid);
         }
 

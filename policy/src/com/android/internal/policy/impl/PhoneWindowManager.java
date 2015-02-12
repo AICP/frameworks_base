bbd00760ef9c1a15d48d1c61b78a044149021e7a
Matt Garnes <matt@cyngn.com>
2015-02-11 18:37:57 -0800

    Do not play the default ringtone if it cannot be retrieved.
    In performAuditoryFeedbackForAccessibilityIfNeed() if the default
    ringtone cannot be retrieved, do not try to set the stream or play it.
    
    TODO: Find the invalid condition that caused this scenario to occur in the first place.
    
    Change-Id: I79d00e016fc07e66f32723c49090a2098163f905

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -7195,8 +7195,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         Ringtone ringTone = RingtoneManager.getRingtone(mContext,
                 Settings.System.DEFAULT_NOTIFICATION_URI);
-        ringTone.setStreamType(AudioManager.STREAM_MUSIC);
-        ringTone.play();
+        if (ringTone != null) {
+            ringTone.setStreamType(AudioManager.STREAM_MUSIC);
+            ringTone.play();
+        }
     }
 
     private boolean isTheaterModeEnabled() {

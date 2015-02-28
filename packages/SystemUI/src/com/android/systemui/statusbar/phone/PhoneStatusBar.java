52b24afe7fb2b56aebc43cc55bff976f4b903591
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-28 17:29:59 +0200

    Fix: Battery saver mode
    Status bar & Nav bar Colors were resetting to default after screen rotation.
    
    Change-Id: I15a61c49a2cb9874a6d9f5936e75e4930531da40

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4031,6 +4031,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationBarView != null)  {
             mNavigationBarView.updateResources(getNavbarThemedResources());
             updateSearchPanel();
+            checkBarModes();
         }
     }
 

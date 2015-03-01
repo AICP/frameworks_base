edbfc6fe55a22012855ba84b4a4642ff16c9461a
LorDClockaN <davor@losinj.com>
2015-03-01 15:43:51 +0100

    FWB: Greeting label timeout (1/2)
    Also include Themes persistance
    
    Change-Id: I8e736cdbad733a66814d72bc108ce6116eec9e1a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -373,6 +373,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private boolean mShowCarrierInPanel = false;
     private boolean mShowLabel;
+    private int mShowLabelTimeout;
 
     // Status bar carrier
     private boolean mShowStatusBarCarrier;
@@ -477,6 +478,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_GREETING),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -518,6 +522,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             if (mGreeting != null && !TextUtils.isEmpty(mGreeting)) {
                 mAicpLabel.setText(mGreeting);
             }
+
+            mShowLabelTimeout = Settings.System.getIntForUser(resolver,
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT, 400, mCurrentUserId);
         }
     }
 
@@ -2477,7 +2484,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         mAicpLabel.animate().cancel();
                         mAicpLabel.animate()
                                 .alpha(1f)
-                                .setDuration(400)
+                                .setDuration(mShowLabelTimeout)
                                 .setInterpolator(ALPHA_IN)
                                 .setStartDelay(50)
                                 .withEndAction(new Runnable() {

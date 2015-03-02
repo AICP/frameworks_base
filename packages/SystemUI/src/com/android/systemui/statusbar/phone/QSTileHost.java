531531ebff13e60e598a1a0b1504a00f3814af9c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-02 11:46:56 +0000

    QS: add Ambient display tile
    Drawables thanks to Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: Ia1279d9f2295e6be42fbc1b8cd79b458de7816f9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -37,6 +37,7 @@ import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.ApnTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -349,6 +350,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ScreenOffTile(this);
             case QSConstants.TILE_EXPANDED_DESKTOP:
                 return new ExpandedDesktopTile(this);
+            case QSConstants.TILE_AMBIENT_DISPLAY:
+                return new AmbientDisplayTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }

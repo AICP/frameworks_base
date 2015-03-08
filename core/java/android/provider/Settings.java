9e53f87b39e56a7979cb9caf283d755ba456a75f
maxwen <max.weninger@gmail.com>
2015-03-08 21:26:14 +0100

    Wakelock Blocker [1/2]
    Change-Id: Iafc48d380d8789f48c272fa052b9a39421617086

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1820,6 +1820,18 @@ public final class Settings {
          */
         public static final int END_BUTTON_BEHAVIOR_DEFAULT = END_BUTTON_BEHAVIOR_SLEEP;
 
+	/**
+	 * Enable blocking wakelock
+	 * @hide
+	 */
+	public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+	/**
+	 * List of wakelock blocks selected
+	 * @hide
+	 */
+	public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
         /**
          * Is advanced settings mode turned on. 0 == no, 1 == yes
          * @hide
@@ -8881,6 +8893,7 @@ public final class Settings {
           * @hide
           */
         public static final String TUNE_AWAY_STATUS = "tune_away";
+
     }
 
     /**

97f8067e59de30d94c9a6a65ce435e079a88f3bf
Andy Mast <andy@cyngn.com>
2015-03-19 11:55:35 -0700

    Clear Theme in System UI
    When SystemUI first starts an android theme object is created
    which contains references to the currently applied CM theme.
    
    When the theme is changed to system, the theme remains in memory
    with old references to the prior applied theme. (See mPackages in ResTable::Theme)
    
    This patch introduces a recreate theme method into Context so that
    SystemUI can recreate its own theme object.
    
    Change-Id: I086a76afa6f456a69c0390573bc8af2eafa4fb4e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4166,7 +4166,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
      * meantime, just update the things that we know change.
      */
     void updateResources(Configuration newConfig) {
-        final Context context = mContext;
         SettingsObserver observer = new SettingsObserver(mHandler);
 
         // detect theme change.
@@ -4174,6 +4173,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         final boolean updateStatusBar = shouldUpdateStatusbar(mCurrentTheme, newTheme);
         if (newTheme != null) mCurrentTheme = (ThemeConfig) newTheme.clone();
         if (updateStatusBar) {
+            mContext.recreateTheme();
             recreateStatusBar();
             observer.update();
 

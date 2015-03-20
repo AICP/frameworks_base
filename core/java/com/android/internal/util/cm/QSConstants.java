9786af7215149a49d640fac2ef728bb8a6857106
Lars Greiss <kufikugel@googlemail.com>
2015-03-20 13:02:44 -0400

    FWB: Add music tile (1/2)
    picked from here: http://goo.gl/ehaoMU
    
    Adapted to lollipop from commit:
    
    Author: kufikugel
    Quicksettings customizations Slim style
    d85304fac9bde550beeeeca0dba2cd49686f20d7
    
    Adapted double click to open music app from commit:
    
    Author: Jubakuba
    SystemUI: Pimp-up Media Tile (1/1)
    5a14de5db237969607e0aabb9b8c04737b122dd3
    
    AICPfy:
    PS1: Gave proper credit to kufikugel for the original code
    PS2: Fix build by overriding all needed methids
    
    Change-Id: I1d93c207a74d04ae0f6ee3d3c9c7ccf5f1444123

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -57,6 +57,7 @@ public class QSConstants {
     public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
     public static final String TILE_REBOOT = "reboot";
     public static final String TILE_SLIMACTION = "slimaction";
+    public static final String TILE_MUSIC = "music";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -104,5 +105,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
         TILES_AVAILABLE.add(TILE_REBOOT);
         TILES_AVAILABLE.add(TILE_SLIMACTION);
+        TILES_AVAILABLE.add(TILE_MUSIC);
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.LteTile;
+import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.ProfilesTile;
 import com.android.systemui.qs.tiles.PerfProfileTile;
@@ -358,6 +359,8 @@ public class QSTileHost implements QSTile.Host {
                 return new RebootTile(this);
             case QSConstants.TILE_SLIMACTION:
                 return new SlimActionTile(this);
+            case QSConstants.TILE_MUSIC:
+                return new MusicTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }

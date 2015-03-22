bd5bcb0eaed90fee49987694c49ec9f414ebeef5
LorDClockaN <davor@losinj.com>
2015-03-22 22:25:58 +0100

    Revert "Keep quiet when volume keys are used to wake up device"
    This reverts commit 9f59809691bc17d630df4b8c26483b5436383283.
    
    Change-Id: Ib77b1709eb41e3ff0fc072aaaebecb09ffb5a4f7

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -440,11 +440,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mAppSwitchWakeScreen;
     boolean mVolumeWakeScreen;
 
-    // During wakeup by volume keys, we still need to capture subsequent events
-    // until the key is released. This is required since the beep sound is produced
-    // post keypressed.
-    boolean mVolumeWakeTriggered;
-
     int mPointerLocationMode = 0; // guarded by mLock
 
     int mLongPressPoweronTime = DEFAULT_LONG_PRESS_POWERON_TIME;
@@ -5318,17 +5313,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_VOLUME_DOWN:
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_MUTE: {
-                // Eat all down & up keys when using volume wake.
-                // This disables volume control, music control, and "beep" on key up.
-                if (isWakeKey && mVolumeWakeScreen) {
-                    mVolumeWakeTriggered = true;
-                    break;
-                } else if (mVolumeWakeTriggered && !down) {
-                    result &= ~ACTION_PASS_TO_USER;
-                    mVolumeWakeTriggered = false;
-                    break;
-                }
-
                 if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
                     if (down) {
                         if (interactive && !mScreenshotChordVolumeDownKeyTriggered
@@ -5393,7 +5377,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     }
                 }
 
-                if ((result & ACTION_PASS_TO_USER) == 0) {
+                // Disable music and volume control when used as wake key
+                if ((result & ACTION_PASS_TO_USER) == 0 && !mVolumeWakeScreen) {
                     boolean mayChangeVolume = false;
 
                     if (isMusicActive()) {

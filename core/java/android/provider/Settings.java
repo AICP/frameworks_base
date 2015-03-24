4468df633b9c1217581532770d173b90b47bd941
AlmightyMegadeth00 <activethrasher00@gmail.com>
2015-03-24 20:11:21 +0100

    Status bar greeting [1/2]
    play a text greeting in the statusbar when exiting the keyguard
    
    AICPfy:
    Add msim support
    
    Change-Id: Idd7a8c6bda2d2411fdd0a9a25d6709e541084158

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3375,6 +3375,12 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
+         * Status bar greeting
+         * @hide
+         */
+        public static final String STATUS_BAR_GREETING = "status_bar_greeting";
+
+        /**
          * Disable FC Notifications
          * @hide
          */

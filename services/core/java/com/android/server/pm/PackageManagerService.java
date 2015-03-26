84ffa9a5eabb9176fed55be76c2a22e9cf5b08f1
Ali B <abittin@gmail.com>
2015-03-26 22:25:54 +0100

    Revert "Fix up first boot message"
    This reverts commit 816aaa501029bc6a7eed4b59670487ddb3d59a30.
    
    Change-Id: I1e37d10f8ae13ddf077bf4578acdd5ae808d6028

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4943,16 +4943,14 @@ public class PackageManagerService extends IPackageManager.Stub {
         if (DEBUG_DEXOPT) {
             Log.i(TAG, "Optimizing app " + curr + " of " + total + ": " + pkg.packageName);
         }
-
-        final int messageRes = isFirstBoot() ?
-                R.string.android_installing_apk : R.string.android_upgrading_apk;
-        try {
-            ActivityManagerNative.getDefault().showBootMessage(
-                    mContext.getResources().getString(messageRes,
-                            curr, total), true);
-        } catch (RemoteException e) {
+        if (!isFirstBoot()) {
+            try {
+                ActivityManagerNative.getDefault().showBootMessage(
+                        mContext.getResources().getString(R.string.android_upgrading_apk,
+                                curr, total), true);
+            } catch (RemoteException e) {
+            }
         }
-
         PackageParser.Package p = pkg;
         synchronized (mInstallLock) {
             performDexOptLI(p, null /* instruction sets */, false /* force dex */,

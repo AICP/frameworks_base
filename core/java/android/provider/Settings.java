9f1d1636bf2bbb44096a884d07177e74ed5b18bc
martincz <martincz@mokeedev.com>
2015-04-02 22:21:36 +0800

    PhoneWindowManager: Tap volume buttons to answer call
    Change-Id: I5ebe1ad88950ba56ce1445b77b7f8cdf030463da

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6443,6 +6443,13 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_RING_TONE =
                 "volume_keys_control_ring_tone";
 
+        /**
+         * Whether to answer the call with the volume keys, the value is boolean.
+         * 0 = 0ff, 1 = on
+         */
+        @SuppressLint("NoSettingsProvider")
+        public static final String VOLUME_ANSWER_CALL = "volume_answer_call";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6684,6 +6691,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SWAP_VOLUME_BUTTONS);
             PRIVATE_SETTINGS.add(VOLUME_ROCKER_WAKE);
             PRIVATE_SETTINGS.add(VOLUME_KEYS_CONTROL_RING_TONE);
+            PRIVATE_SETTINGS.add(VOLUME_ANSWER_CALL);
         }
 
         /**

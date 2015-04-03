e17012f51c3651f8686cff36fcd4dc333aa1e695
Michael Bestas <mikeioannina@gmail.com>
2015-04-03 14:33:37 -0700

    Forward port CM Screen Security settings (1/2)
     * Variable size pattern lockscreen
     * Toggle dots/error pattern visibility
    
    Change-Id: Ie109e82c1fb2fd96b07e977e1cd76ae3acb865ff
    
    Fix pattern visibility settings (1/2)
    
    Change-Id: Ic627953c5df854c442671a98b5da539b994da18b
    
    LockPatternUtils: Use the actual user id to set pattern size
    
    Ticket: CYNGNOS-2462
    Change-Id: Ia68e26ec2dfc23317135d933bc25204c1380bb02
    
    LockSettings: fix build
    
    Change-Id: Ic65b1a2af398faffb83776ee4013d47f79ab6619
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -1562,6 +1562,10 @@ public class LockSettingsService extends ILockSettings.Stub {
                 && getSeparateProfileChallengeEnabledInternal(userId);
     }
 
+    public byte getLockPatternSize(int userId) {
+        return mStorage.getLockPatternSize(userId);
+    }
+
     /**
      * Send credentials for user {@code userId} to {@link RecoverableKeyStoreManager} during an
      * unlock operation.
@@ -2259,10 +2263,10 @@ public class LockSettingsService extends ILockSettings.Stub {
         });
     }
 
-    private LockscreenCredential createPattern(String patternString) {
+    private LockscreenCredential createPattern(String patternString, byte patternSize) {
         final byte[] patternBytes = patternString.getBytes();
         LockscreenCredential pattern = LockscreenCredential.createPattern(
-                LockPatternUtils.byteArrayToPattern(patternBytes));
+                LockPatternUtils.byteArrayToPattern(patternBytes, patternSize), patternSize);
         Arrays.fill(patternBytes, (byte) 0);
         return pattern;
     }
@@ -2305,7 +2309,7 @@ public class LockSettingsService extends ILockSettings.Stub {
             final LockscreenCredential credential;
             switch (getCredentialTypeInternal(userId)) {
                 case CREDENTIAL_TYPE_PATTERN:
-                    credential = createPattern(password);
+                    credential = createPattern(password, mStorage.getLockPatternSize(userId));
                     break;
                 case CREDENTIAL_TYPE_PIN:
                     credential = LockscreenCredential.createPin(password);
@@ -2547,7 +2551,10 @@ public class LockSettingsService extends ILockSettings.Stub {
             Secure.LOCK_PATTERN_ENABLED,
             Secure.LOCK_BIOMETRIC_WEAK_FLAGS,
             Secure.LOCK_PATTERN_VISIBLE,
-            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED
+            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED,
+            Secure.LOCK_PATTERN_SIZE,
+            Secure.LOCK_DOTS_VISIBLE,
+            Secure.LOCK_SHOW_ERROR_PATH
     };
 
     // Reading these settings needs the contacts permission

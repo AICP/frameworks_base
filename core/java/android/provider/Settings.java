761a82df323ef15f013502e8eae03852e74f78bd
Jmz <jmztaylor@gmail.com>
2015-04-21 20:19:54 +0200

    Force Expanded Notifications (1/2)
    This will force expanded notifications for all apps that support
    expanded notifications
    
    Change-Id: I1f6fac658a3e58488bc37cafec9208a75da78ab8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4181,6 +4181,13 @@ public final class Settings {
         public static final String NONE_IS_SILENT = "none_is_silent";
 
         /**
+         * Height of the gesture anywhere trigger.
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

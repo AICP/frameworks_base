b54ad763e672e1e6f7798e0191b86d549b3f099d
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    QS: Add heads up tile (1/2)
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -59,6 +59,7 @@ public class QSConstants {
     public static final String TILE_REBOOT = "reboot";
     public static final String TILE_SLIMACTION = "slimaction";
     public static final String TILE_MUSIC = "music";
+    public static final String TILE_HEADS_UP = "heads_up";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -108,5 +109,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_REBOOT);
         TILES_AVAILABLE.add(TILE_SLIMACTION);
         TILES_AVAILABLE.add(TILE_MUSIC);
+        TILES_AVAILABLE.add(TILE_HEADS_UP);
     }
 }

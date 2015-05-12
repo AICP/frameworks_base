a622d416537387999266ed8b67cfa675b6991732
UtkarshGupta <utkarsh.eminem@gmail.com>
2015-05-12 21:41:05 +0200

    FWB: Battery light: 100% charged level (1/2)
    Change-Id: I37d3ba83b6246b549f6be1096798fccd0c949f11

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4932,6 +4932,12 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
+         * What color to use for the battery LED while charging - really full (100%)
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

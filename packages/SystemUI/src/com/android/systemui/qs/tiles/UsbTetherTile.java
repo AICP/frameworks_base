229fba3d824f309d157329751a102c226d4e4269
LorDClockaN <davor@losinj.com>
2015-05-13 16:15:32 +0200

    USBteether tile: fix missing icon
    Change-Id: I85988694f19a1e49230ea5cd57b2b0fe982711dc

--- packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
@@ -107,7 +107,7 @@ public class UsbTetherTile extends QSTile<QSTile.BooleanState> {
         state.visible = mUsbConnected && mConnectivityManager.isTetheringSupported();
         state.value = mUsbTethered;
         state.label = mContext.getString(R.string.quick_settings_usb_tether_label);
-        state.iconId = mUsbTethered ? R.drawable.ic_qs_usb_tether_on
-                : R.drawable.ic_qs_usb_tether_off;
+        state.icon = mUsbTethered ? ResourceIcon.get(R.drawable.ic_qs_usb_tether_on)
+                : ResourceIcon.get(R.drawable.ic_qs_usb_tether_off);
     }
 }

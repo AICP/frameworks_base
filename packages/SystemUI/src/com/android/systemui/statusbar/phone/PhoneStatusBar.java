347da33dcac91ed6025c1d323991b413920198cf
westcripp <korukaltan@gmail.com>
2015-06-03 03:26:19 +0300

    On switch, recreate the statusbar to fix task view
    Change-Id: I0fd66cc0c50674f6a6e6e0f88104f3bf1507e9df

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -554,7 +554,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 updateSpeedbump();
                 updateClearAll();
                 updateEmptyShadeView();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.ENABLE_TASK_MANAGER))) {
+                    mShowTaskManager = Settings.System.getIntForUser(
+                            mContext.getContentResolver(),
+                            Settings.System.ENABLE_TASK_MANAGER,
+                            0, UserHandle.USER_CURRENT) == 1;
+                             recreateStatusBar();
             }
+
             update();
         }
 

cdc1f7edd1978769eae2b61ae47aa13d13edcc7c
Roman Birg <roman@cyngn.com>
2015-06-29 19:27:21 -0700

    SystemUI: recalculate clock position on removing empty shade views
    Change-Id: I70cc2dd3e1da99d4467cd43fc81018ccf135cd77
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2075,6 +2075,9 @@ public class NotificationPanelView extends PanelView implements
 
         // Hide "No notifications" in QS.
         mNotificationStackScroller.updateEmptyShadeView(mShadeEmpty && !mQsExpanded);
+        if (mStatusBarState == StatusBarState.KEYGUARD) {
+            positionClockAndNotifications();
+        }
     }
 
     public void setQsScrimEnabled(boolean qsScrimEnabled) {

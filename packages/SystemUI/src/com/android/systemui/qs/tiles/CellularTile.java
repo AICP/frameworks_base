f54119ef414647b0da46521071983215ac81ae0d
Janson Kang <temasek71@gmail.com>
2015-08-13 10:33:48 +0800

    CelluarTile: Secondary click opens mobile network settings
    Compliment 64b3742e5c4abbe80d361bad8d5584eedbbdfbc2
    
    Long click triggers detailed view
    
    Secondary click don't replicate detailed view, triggers
    mobile network settings instead.
    
    Change-Id: I21c76a89148ac2afb580fbba7ae6f8d72baa4ee5
    Signed-off-by: Janson Kang <temasek71@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -115,8 +115,8 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
 
     @Override
     protected void handleSecondaryClick() {
-        if (mDataController.isMobileDataSupported()) {
-            showDetail(true);
+        if (mTelephonyManager.getDefault().getPhoneCount() > 1) {
+            mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS_MSIM);
         } else {
             mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS);
         }

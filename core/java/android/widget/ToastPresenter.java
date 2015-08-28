a7c00e82f97c7c86588b450a3d27a583396526d2
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    fw_base: Toast icon switch [1/2]
     ** AICP Edit: added back backup validators
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928

--- core/java/android/widget/ToastPresenter.java
+++ core/java/android/widget/ToastPresenter.java
@@ -29,6 +29,7 @@ import android.graphics.drawable.Drawable;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -232,15 +233,17 @@ public class ToastPresenter {
                 horizontalMargin, verticalMargin, removeWindowAnimations);
 
         ImageView appIcon = (ImageView) mView.findViewById(android.R.id.icon);
-        if (appIcon != null) {
-            PackageManager pm = mContext.getPackageManager();
-            Drawable icon = null;
-            try {
-                icon = pm.getApplicationIcon(mPackageName);
-            } catch (PackageManager.NameNotFoundException e) {
-                // nothing to do
+        if ((Settings.System.getInt(mContext.getContentResolver(), Settings.System.TOAST_ICON, 1) == 1)) {
+            if (appIcon != null) {
+                PackageManager pm = mContext.getPackageManager();
+                Drawable icon = null;
+                try {
+                    icon = pm.getApplicationIcon(mPackageName);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // nothing to do
+                }
+                appIcon.setImageDrawable(icon);
             }
-            appIcon.setImageDrawable(icon);
         }
 
         addToastView();

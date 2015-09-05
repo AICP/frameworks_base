327d0bbc272970e33a250ca4180301817dc66aef
rogersb11 <brettrogers11@gmail.com>
2015-09-05 12:52:51 +0200

    FWB: Status bar carrier label font size (2/2)
    AICPfy
    
    Change-Id: I58e4ab0ca07f45b9fd63687946aa572061c68299

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3988,6 +3988,12 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
 
         /**
+         * Settings for carrier label font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_FONT_SIZE = "status_bar_carrier_font_size";
+
+        /**
          * show carrier in statusbae while on lockscreen. The value is
          * int (1: Show or 0: Hide).
          */

7b9f1c9ba6af8f66e0040b5bc6cac9a79a94c1f3
Alexander Martinz <eviscerationls@gmail.com>
2015-09-05 19:47:38 +0200

    SystemUI: screenshot: set a boost hint when taking a screenshot
    Change-Id: I217b968c2fdded7b00001f54e3fba5bb78d35e9b
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -44,6 +44,7 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.PowerManager;
 import android.os.Process;
 import android.provider.MediaStore;
 import android.util.DisplayMetrics;
@@ -374,6 +375,7 @@ class GlobalScreenshot {
     private final int mPreviewHeight;
 
     private Context mContext;
+    private PowerManager mPowerManager;
     private WindowManager mWindowManager;
     private WindowManager.LayoutParams mWindowLayoutParams;
     private NotificationManager mNotificationManager;
@@ -409,6 +411,8 @@ class GlobalScreenshot {
         mContext = context;
         LayoutInflater layoutInflater = (LayoutInflater)
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 
         // Inflate the screenshot layout
         mDisplayMatrix = new Matrix();
@@ -522,6 +526,9 @@ class GlobalScreenshot {
     }
 
     private void takeScreenshotInternal(Runnable finisher, boolean statusBarVisible, boolean navBarVisible) {
+        // a lot of work ahead, help out a bit
+        mPowerManager.cpuBoost(1500000);
+
         // We need to orient the screenshot correctly (and the Surface api seems to take screenshots
         // only in the natural orientation of the device :!)
         mDisplay.getRealMetrics(mDisplayMetrics);

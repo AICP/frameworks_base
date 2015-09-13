23f7ae6a26332c8a5a60793b10d182dad67d8fb0
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-09-14 00:55:11 +0100

    SystemUI: fix switching from task manager to detailed view
    - temp fix for panel closing when in detail view and trying to access task manager
    - fix not being able to access multiuser detailed view when task manager is visible
    
    Change-Id: I8a46d4af16a2940b023b2449e05f679b1b904f1d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -393,6 +393,11 @@ public class NotificationPanelView extends PanelView implements
                 if (!showing) {
                     mHandler.removeCallbacks(mCloseQsRunnable);
                 }
+                // if TaskManager is showing reset back to QSPanel
+                // otherwise multiuser switch will not work.
+                if (mTaskManagerShowing) {
+                    mStatusBar.resetQsPanelVisibility();
+                }
             }
         });
     }
@@ -1841,7 +1846,9 @@ public class NotificationPanelView extends PanelView implements
         if (mQsExpanded) {
             if (isQsDetailShowing()) {
                 mHandler.removeCallbacks(mCloseQsRunnable);
-                mHandler.postDelayed(mCloseQsRunnable, 200);
+                // temp fix for QSPanel closing when switching
+                // to TaskManager while in detail view
+                //mHandler.postDelayed(mCloseQsRunnable, 200);
             }
             requestScrollerTopPaddingUpdate(false /* animate */);
             requestPanelHeightUpdate();

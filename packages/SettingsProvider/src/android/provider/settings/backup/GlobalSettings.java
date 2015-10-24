65ea8398bba207672df1373490f5e499a7048d62
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-23 23:44:43 -0400

    Base: Hide power menu on secure lockscreen [1/2]
    Adds an option to enable/disable power menu on secure lockscreen.
    
    Change-Id: Idc96d99bf824e1f7b425e3707fa18f662617172d

--- packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -78,5 +78,6 @@ public class GlobalSettings {
         Settings.Global.POWER_BUTTON_LONG_PRESS,
         Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS,
         Settings.Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
+        Settings.Global.LOCKSCREEN_ENABLE_POWER_MENU,
     };
 }

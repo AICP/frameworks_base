ba622830c9e1e83fa9dfca025d2a4bc7847b218b
LorDClockaN <davor@losinj.com>
2015-11-14 21:19:17 +0100

    Toast icon switch [1/2]
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3463,6 +3463,13 @@ public final class Settings {
         public static final String VOICE_LAUNCH_INTENT = "voice_launch_intent";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

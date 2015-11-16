1fd75fc4233801da53169720b528703f643896f5
TheMrcool212 <munirmohamadrani@gmail.com>
2015-11-15 20:40:11 -0500

    SystemUI: option to ignore the last app when clearing recents (1/2)
    Change-Id: I63f2f7777ee78b51ee279d53a32164ddbdd48341

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -25,6 +25,8 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageDataObserver;
@@ -625,14 +627,26 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
         mPrevAccessibilityFocusedIndex = -1;
     }
 
+    private boolean dismissAll() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.RECENTS_CLEAR_ALL_DISMISS_ALL, 1, UserHandle.USER_CURRENT) == 1;
+    }
+
     public void dismissAllTasks() {
         post(new Runnable() {
             @Override
             public void run() {
                 ArrayList<Task> tasks = new ArrayList<Task>();
                 tasks.addAll(mStack.getTasks());
+                if (!dismissAll() && tasks.size() > 1) {
+                    // Ignore the visible foreground task
+                    Task foregroundTask = tasks.get(tasks.size() - 1);
+                    tasks.remove(foregroundTask);
+                }
+
                 // Remove visible TaskViews
                 int childCount = getChildCount();
+                if (!dismissAll() && childCount > 1) childCount--;
                 for (int i = 0; i < childCount; i++) {
                     TaskView tv = (TaskView) getChildAt(i);
                     tasks.remove(tv.getTask());
@@ -640,11 +654,14 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                 }
 
                 int size = tasks.size();
-                // Remove any other Tasks
-                for (int i = 0; i < size; i++) {
-                    Task t = tasks.get(i);
-                    if (mStack.getTasks().contains(t)) {
-                        mStack.removeTask(t);
+
+                if (size > 0) {
+                    // Remove possible alive Tasks
+                    for (int i = 0; i < size; i++) {
+                        Task t = tasks.get(i);
+                        if (mStack.getTasks().contains(t)) {
+                            mStack.removeTask(t);
+                        }
                     }
                 }
             }

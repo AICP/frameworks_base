1141440d40d5e2d78838251e4bc78cc95822db6d
maxwen <max.weninger@gmail.com>
2015-11-18 21:43:53 +0100

    Revert "Allow unlinking ringer with notification volume [1/2]"
    This reverts commit 9cd4ed21e34925947dc86c67b44711e0cc55185e.
    
    Change-Id: I73763dc1617e0eedfb2d4c048669610e7f722c4b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3843,12 +3843,6 @@ public final class Settings {
         public static final String POWER_MENU_LOCKSCREEN = "power_menu_lockscreen";
 
         /**
-         * Boolean value whether to link ringtone and notification volume
-         * @hide
-         */
-        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

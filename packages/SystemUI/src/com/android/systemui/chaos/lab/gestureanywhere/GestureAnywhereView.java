b10bc53b5f28e86f74207bd43388cd78f70c1711
LorDClockaN <davor@losinj.com>
2015-11-26 16:34:16 +0100

    GA: use db file on external storage, same as in AicpExtras
    PS2: typo of course
    PS3: missing import
    
    Change-Id: I253272d243c21905968f3b55ba85280630927216

--- packages/SystemUI/src/com/android/systemui/chaos/lab/gestureanywhere/GestureAnywhereView.java
+++ packages/SystemUI/src/com/android/systemui/chaos/lab/gestureanywhere/GestureAnywhereView.java
@@ -36,6 +36,7 @@ import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
 import android.gesture.GestureOverlayView;
 import android.gesture.Prediction;
+import android.os.Environment;
 import android.os.Handler;
 import android.provider.Settings;
 import android.util.AttributeSet;
@@ -59,7 +60,7 @@ import static android.view.KeyEvent.KEYCODE_BACK;
 @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_CLASS)
 public class GestureAnywhereView extends TriggerOverlayView implements GestureOverlayView.OnGestureListener {
     private static final String TAG = "GestureAnywhere";
-    private final File mStoreFile = new File("/data/system", "ga_gestures");
+    private final File mStoreFile = new File(Environment.getExternalStorageDirectory(), "/data/ga_gestures");
     State mState = State.Collapsed;
     private View mContent;
     private GestureOverlayView mGestureView;

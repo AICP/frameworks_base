6369f87eb5a98dab63bfe7c0c3a9af0b83a3c148
cmorlok <christianmorlok@gmail.com>
2015-12-03 18:40:38 +0100

    FWB: Hide power menu on lockscreen [1/2]
    Change-Id: I108742d658d5a7babb8e9d1fa5ed51ae1cec3128

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3836,6 +3836,13 @@ public final class Settings {
         public static final String LOCKSCREEN_BLUR_RADIUS = "lockscreen_blur_radius";
 
         /**
+         * Whether to display powermenu on secure lockscreen
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_LOCKSCREEN = "power_menu_lockscreen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

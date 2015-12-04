c44c58fb74c2e3f7b8d8136a1f29e06d92c32e3b
Danny Baumann <dannybaumann@web.de>
2015-12-04 10:11:47 +0100

    Fix lockscreen disabling for MSIM.
    The prompt for the PIN for second SIM was missing if lockscreen is
    disabled.
    
    Change-Id: Ia98771929e905243967c3be6a428f813c2c3d0a9

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -502,7 +502,7 @@ public class KeyguardViewMediator extends SystemUI {
                     break;
                 case READY:
                     synchronized (this) {
-                        if (mInternallyDisabled) {
+                        if (mInternallyDisabled && !mUpdateMonitor.isSimPinSecure()) {
                             hideLocked();
                         } else if (isShowing()) {
                             resetStateLocked();

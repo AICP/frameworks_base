261ab20196bc7d681d9c2873bee386bc4adfb702
fefifofum <jorsellis@gmail.com>
2015-12-07 15:28:44 +0100

    Proximity: Fix prevent accidental wakeup
    This reverts https://github.com/CyanogenMod/android_frameworks_base/commit/2293e60c7de011782e59486605c04b9f67a1e24f
    
    Without this, prevent accidental wakeup doesn't work as expected,
    and screen can be powered on even if proximity sensor is covered.
    
    Change-Id: If094f90b01f93507026e12c26ccb49bf5ce66a7c

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -3402,11 +3402,7 @@ public final class PowerManagerService extends SystemService
                     }
                 }
             };
-            if (checkProximity) {
-                runWithProximityCheck(r);
-            } else {
-                r.run();
-            }
+            runWithProximityCheck(r);
         }
 
         private void runWithProximityCheck(Runnable r) {

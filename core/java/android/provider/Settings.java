e54cde7b8600a50f5fddd3ad317885bde7fa37e5
LorDClockaN <davor@losinj.com>
2015-12-13 22:11:08 +0100

    FWB: PIE: Always on the right switch (1/2)
    Change-Id: I1b5937af6d36d8272261ca1668f290c5b51ee395

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4073,6 +4073,11 @@ public final class Settings {
         public static final String PA_PIE_CHEVRON = "pa_pie_chevron";
 
         /**
+         * @hide
+         */
+        public static final String PA_PIE_ALWAYS_RIGHT = "pa_pie_always_right";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

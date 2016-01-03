d9c513e2cf0a4badd889995b5dc24ee8a19c5516
htchoi1995 <htchoi1995@gmail.com>
2016-01-03 20:26:36 +0100

    FEB: Disable/Enable screenshot sound [1/2]
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3898,6 +3898,12 @@ public final class Settings {
          */
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

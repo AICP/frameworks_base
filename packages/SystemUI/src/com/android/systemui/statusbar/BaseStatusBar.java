6697bd27c002e5f5b1bf2272c5f59d04373919f6
Randall Rushing <randall.rushing@gmail.com>
2016-01-07 02:41:02 -0800

    statusbar: add screenPinningStateChanged(boolean enabled) api and pipe to navigation bar
    Let activity stack notify statusbar when task locking state changes. This is needed for
    alternate task managers (OmniSwitch, SlimRecents) that implement screen pinning but don't
    interact with the screen pinning pop-up. NavigationController needs to be notified of the
    event to load a secure navigation bar.
    
    Change-Id: I948667509d448bb91bea9bd560820d8bbf456f3f

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1316,6 +1316,11 @@ public abstract class BaseStatusBar extends SystemUI implements
         mHandler.sendEmptyMessage(msg);
     }
 
+    @Override
+    public void screenPinningStateChanged(boolean enabled) {
+        Log.d(TAG, "StatusBar API screenPinningStateChanged = " + enabled);
+    }
+
     protected H createHandler() {
          return new H();
     }

8300d98f8f905cee03848e0503c9eafcdf004593
MrApocalypse <ensabahnur16@gmail.com>
2016-01-09 22:40:10 +0100

    App Sidebar Fixup
    Various fixes and updates, contains:
    
    App sidebar: Fix Left/Right position by @NL-BlackDragon
    55e05c7641ad2e68d02c8fe067deb737c4322f2b
    
    AppSidebar: remove duplicate call by @tobitege
    08ed06129e6579821f2cd9101c0592ac07d572cd
    
    AICPfy:
    - Bring it back from AICP LP
    
    Change-Id: I63b6cc850553fa11bed73362b534aed0027cc847

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -709,8 +709,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             }
 
-            int sidebarPosition = Settings.System.getInt(
-                    resolver, Settings.System.APP_SIDEBAR_POSITION, AppSidebar.SIDEBAR_POSITION_LEFT);
+            int sidebarPosition = Settings.System.getInt(resolver,
+                    Settings.System.APP_SIDEBAR_POSITION,
+                    AppSidebar.SIDEBAR_POSITION_LEFT);
             if (sidebarPosition != mSidebarPosition) {
                 mSidebarPosition = sidebarPosition;
                 removeSidebarView();
@@ -1255,7 +1256,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         addAppCircleSidebar();
-        addSidebarView();
         addGestureAnywhereView();
 
 	if (mNavigationBarView == null) {
@@ -3907,16 +3907,20 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 Configuration config = mContext.getResources().getConfiguration();
                 try {
                     // position app sidebar on left if in landscape orientation and device has a navbar
-                    if (mWindowManagerService.hasNavigationBar() &&
-                            config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                        mWindowManager.updateViewLayout(mAppSidebar,
+                    ContentResolver resolver = mContext.getContentResolver();
+                    boolean sidebarEnabled = Settings.System.getInt(
+                        resolver, Settings.System.APP_SIDEBAR_ENABLED, 0) == 1;
+                    if (sidebarEnabled
+                        && mWindowManagerService.hasNavigationBar()
+                        && config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                            mWindowManager.updateViewLayout(mAppSidebar,
                                 getAppSidebarLayoutParams(AppSidebar.SIDEBAR_POSITION_LEFT));
-                        mHandler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                mAppSidebar.setPosition(AppSidebar.SIDEBAR_POSITION_LEFT);
-                            }
-                        }, 500);
+                            mHandler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mAppSidebar.setPosition(AppSidebar.SIDEBAR_POSITION_LEFT);
+                                }
+                            }, 500);
                     }
                 } catch (RemoteException e) {
                 }

7c39298da5f5adaaf3275cf3a12ad616cd6b9d1b
Varun <date.varun123@gmail.com>
2016-01-10 15:41:58 +0200

    SystemUI: Add Three New Tiles[1/2]
    New Tiles:
    -Navbar Tile
    -App Circle Sidebar Tile
    -Pie Tile
    
    Ported From Lollipop
    adapted For Marshmallow By Varun Date (varund7726)
    
    Credits:
    
    Thanks To Alex Cruz (Mazda) for Navbar and AppcircleBar Tile
    Original Navbar commit here : http://gerrit.dirtyunicorns.com/#/c/16762/
    Original RR commit here : https://github.com/ResurrectionRemix/android_frameworks_base/commit/95d9f1e15196f6008c00e2edcf2308a5cd502e47
    
    Thanks to Nico60 for Pie Tile
    Original RR Commit Here :
    https://github.com/ResurrectionRemix/android_frameworks_base/commit/eb2af9a4ba2023e3e45f1ecbe5984b985fea094a
    
    AICP
    PS3: fix PIE strings and capitalization
    
    Change-Id: Idecdd212630b0f54518292c39059bcc56162a711
    Signed-off-by: Varun <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -55,6 +55,9 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.AppCircleBarTile;
+import com.android.systemui.qs.tiles.NavBarTile;
+import com.android.systemui.qs.tiles.PieTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.LteTile;
@@ -390,6 +393,9 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("music")) return new MusicTile(this);
         else if (tileSpec.equals("reboot")) return new RebootTile(this);
         else if (tileSpec.equals("lte")) return new LteTile(this);
+        else if (tileSpec.equals("navbar")) return new NavBarTile(this);
+        else if (tileSpec.equals("appcirclebar")) return new AppCircleBarTile(this);
+        else if (tileSpec.equals("pie")) return new PieTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -483,6 +489,9 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("reboot")) return R.string.quick_settings_reboot_label;
         else if (spec.equals("expanded_desktop")) return R.string.quick_settings_expanded_desktop_label;
         else if (spec.equals("lte")) return R.string.qs_lte_label;
+        else if (spec.equals("navbar")) return R.string.quick_settings_navbar_title;
+        else if (spec.equals("appcirclebar")) return R.string.quick_settings_appcirclebar_title;
+        else if (spec.equals("pie")) return R.string.quick_settings_pie_title;
         return 0;
     }
 
@@ -518,6 +527,9 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("expanded_desktop")) return R.drawable.ic_qs_expanded_desktop;
         else if (spec.equals("reboot")) return R.drawable.ic_qs_reboot;
         else if (spec.equals("lte")) return R.drawable.ic_qs_lte_on;
+        else if (spec.equals("navbar")) return R.drawable.ic_qs_navbar_on;
+        else if (spec.equals("appcirclebar")) return R.drawable.ic_qs_appcirclebar_on;
+        else if (spec.equals("pie")) return R.drawable.ic_qs_pie_on;	
         return 0;
     }
 

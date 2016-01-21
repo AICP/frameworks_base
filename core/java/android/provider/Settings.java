a9f80228c0604c43a620b04ce2f54cc0c13e057f
LorDClockaN <davor@losinj.com>
2016-01-21 23:05:00 +0200

    SystemUI: Power/reboot menu transparency [1/2]
    SDB: adapt for A12-14 as GlobalActionDialog got a sibling, GlobalActionsDialogLite.
    
    Change-Id: Ib703c2574aca9bc3c534a155d165f07b603c0535
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6772,6 +6772,18 @@ public final class Settings {
          */
 
         /**
+         * Transparent power menu and dialogs
+         * @hide
+         */
+        public static final String TRANSPARENT_POWER_MENU = "transparent_power_menu";
+
+        /**
+         * Dim amount around power/reboot menu dialogs
+         * @hide
+         */
+        public static final String TRANSPARENT_POWER_DIALOG_DIM = "transparent_power_dialog_dim";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6923,6 +6935,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ON_THE_GO_SERVICE_RESTART);
             PRIVATE_SETTINGS.add(ON_THE_GO_CAMERA);
             PRIVATE_SETTINGS.add(CHARGING_ANIMATION);
+            PRIVATE_SETTINGS.add(TRANSPARENT_POWER_MENU);
+            PRIVATE_SETTINGS.add(TRANSPARENT_POWER_DIALOG_DIM);
         }
 
         /**

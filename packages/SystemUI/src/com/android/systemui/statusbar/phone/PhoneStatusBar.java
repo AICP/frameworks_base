91ac823a70c05f1834a4b9ff4f21ce0b3c8182dc
Roman Birg <roman@cyngn.com>
2016-01-25 16:28:52 -0800

    SystemUI: improve tile caching mechanism
    - Always remove all tile views before setting new tiles
    
    - Save and restore the viewpager page when setting tiles as we need to
      reset this page when we're done (after re-setting the adapter)
    
    - Reset the pager adapter to force page removes and fix ghost scrolling
      to non existent pages.
    
    - No need to set tiles to 0 while recreating status bar or switching
      themes
    
    - Always recalculate heights after switching brightness slider, and
      explicitly set the visibility on the slider too
    
    Ref: CYNGNOS-1644 OPO-367
    
    Change-Id: Id08fbbcd38f24c16ee044c0d63389b36845c81b9
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1623,9 +1623,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                                 Log.e(TAG, "Unable to unregister custom tile listener", e);
                             }
 
-                            // clear out old tile states and views
-                            mQSPanel.setTiles(new ArrayList<QSTile<?>>());
-
                             mQSTileHost.resetTiles();
 
                             // reregister service
@@ -4279,7 +4276,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         mQSPanel.getHost().setCustomTileListenerService(null);
-        mQSPanel.setTiles(new ArrayList<QSTile<?>>());
         mQSPanel.setListening(false);
 
         makeStatusBarView();

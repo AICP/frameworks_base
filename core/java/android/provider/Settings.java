94ffa550d7faa4eafbdf6b23f9cd67639d4e147a
LorDClockaN <davor@losinj.com>
2016-02-04 20:54:24 +0100

    FWB: Carrier label location (1/2)
    Option to place on the left or right side
    
    Thanks to Brett Rogers for the initail patch
    
    Change-Id: Ic34b1a02e6f2ec02a3a1c6a21a7e9f46cd48cdc0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3815,6 +3815,12 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_FONT_SIZE = "status_bar_carrier_font_size";
 
         /**
+         * Custom carrier label placement
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_SPOT = "status_bar_carrier_spot";
+
+        /**
          * Switch for headset icon
          * @hide
          */

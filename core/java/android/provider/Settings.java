c9b733493960eef8d36beee93142da62a037aa44
Alex Cruz <mazdarider23@gmail.com>
2016-02-26 04:44:13 -0500

    Toggle Dashboard switches [2/2]
    Change-Id: If7970bc06b9323423a152435a869dc3e80156966

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4612,7 +4612,14 @@ public final class Settings {
         public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD =
                 "mute_annoying_notifications_threshold";
 
-	    /**
+        /**
+         *
+         * Whether to show switches for wifi/cell/bt in the settings dashboard
+         * @hide
+         */
+        public static final String DASHBOARD_SWITCHES = "dashboard_switches";
+
+        /**
          * Statusbar AICP logo
          * @hide
          */

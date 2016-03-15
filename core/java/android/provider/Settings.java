97434b5102393a6f29ed155eae9847ca41d93532
rogersb11 <brettrogers11@gmail.com>
2016-03-15 00:43:15 -0400

    Dashboard Text Size 2/2
    Change-Id: I9ce437eff5d8164822c20bb06ae0e3be4cb439e1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4990,6 +4990,18 @@ public final class Settings {
         public static final String NFC_POLLING_MODE = "nfc_polling";
 
         /**
+         * Dashboard title text size
+         * @hide
+         */
+        public static final String SETTINGS_TITLE_TEXT_SIZE = "settings_title_text_size";
+
+        /**
+         * Dashboard category text size
+         * @hide
+         */
+        public static final String SETTINGS_CATEGORY_TEXT_SIZE = "settings_category_text_size";
+
+        /**
          * Nfc sound mode.
          */
         public static final String NFC_SOUND_MODE = "nfc_sound_mode";

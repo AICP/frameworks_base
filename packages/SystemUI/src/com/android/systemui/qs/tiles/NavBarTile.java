a66e18794e4e812f954a8218f283f6585a6f6f19
Nico60 <ndhouailly@gmail.com>
2016-03-16 14:35:56 +0100

    Navigation bar tile: fix toggle state
    Curently whhen adding the tile on a device without navigation bar by default the toggle state is on even if the navigation bar is disabled.
    Check if the device has native navigation bar to set the right toggle state.
    
    Change-Id: If03130111c91515c279b43a63d4550e5c372b653

--- packages/SystemUI/src/com/android/systemui/qs/tiles/NavBarTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NavBarTile.java
@@ -32,6 +32,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.cyanogenmod.internal.logging.CMMetricsLogger;
+import com.android.internal.utils.du.DUActionUtils;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSDetailItemsList;
 import com.android.systemui.qs.QSTile;
@@ -122,7 +123,8 @@ public class NavBarTile extends QSTile<NaviBarTile.NavbarState> {
 
     private boolean navbarEnabled() {
         return Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.NAVIGATION_BAR_VISIBLE, 1) == 1;
+            Settings.Secure.NAVIGATION_BAR_VISIBLE,
+            DUActionUtils.hasNavbarByDefault(mContext) ? 1 : 0) == 1;
     }
 
     @Override

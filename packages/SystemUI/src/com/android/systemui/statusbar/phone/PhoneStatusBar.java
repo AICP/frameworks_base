64cd4275e207b4b3fc8d6ec45230e8782e1ed7c9
Roman Birg <roman@cyngn.com>
2016-03-18 09:08:28 -0700

    SystemUI: improve battery saver tile
    - Fix the tile not showing, the javadoc states that it will return 0 if
      profiles are not supported, but it was actually returning -1.
    
    - Now disables itself when the device is plugged in to reflect to the user
      that they cannot toggle it on right now.
    
    - Don't wait for power manager to update its internal state before
      updating the visual tile state.
    
    Change-Id: I8ab6adb2bfa728a242368294c1f066d468a86f09
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1623,7 +1623,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         mNetworkController, mZenModeController, mHotspotController,
                         mCastController, mFlashlightController,
                         mUserSwitcherController, mKeyguardMonitor,
-                        mSecurityController);
+                        mSecurityController, mBatteryController);
             }
             mQSPanel.setHost(mQSTileHost);
             if (mBrightnessMirrorController == null) {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -80,6 +80,7 @@ import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.statusbar.CustomTileData;
+import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.BluetoothController;
 import com.android.systemui.statusbar.policy.CastController;
 import com.android.systemui.statusbar.policy.FlashlightController;
@@ -131,6 +132,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
     private final UserSwitcherController mUserSwitcherController;
     private final KeyguardMonitor mKeyguard;
     private final SecurityController mSecurity;
+    private final BatteryController mBattery;
 
     private CustomTileData mCustomTileData;
     private CustomTileListenerService mCustomTileListenerService;
@@ -143,7 +145,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
             ZenModeController zen, HotspotController hotspot,
             CastController cast, FlashlightController flashlight,
             UserSwitcherController userSwitcher, KeyguardMonitor keyguard,
-            SecurityController security) {
+            SecurityController security, BatteryController battery) {
         mContext = context;
         mStatusBar = statusBar;
         mBluetooth = bluetooth;
@@ -157,6 +159,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         mUserSwitcherController = userSwitcher;
         mKeyguard = keyguard;
         mSecurity = security;
+        mBattery = battery;
         mCustomTileData = new CustomTileData();
 
         final HandlerThread ht = new HandlerThread(QSTileHost.class.getSimpleName(),
@@ -309,6 +312,11 @@ public class QSTileHost implements QSTile.Host, Tunable {
     @Override
     public KeyguardMonitor getKeyguardMonitor() {
         return mKeyguard;
+    }
+
+    @Override
+    public BatteryController getBatteryController() {
+        return mBattery;
     }
 
     public UserSwitcherController getUserSwitcherController() {

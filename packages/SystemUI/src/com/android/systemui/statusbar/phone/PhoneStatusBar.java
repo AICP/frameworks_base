9cd110a383092b1dfa46c4fdeee298a65ae2a15c
Danesh M <danesh@cyngn.com>
2016-06-01 11:16:44 -0700

    SystemUI : Ensure we unset focus when showKeyguard is called
    Repro steps :
    
    1) Swipe left to enter INTERACTIVE live lockscreen
    2) Turn screen off
    3) Turn screen on
    4) Notice left swipe does not work
    
    CYNGNOS-2950
    
    Change-Id: I1af24e2bc3377afc9e8236a35afc208ab676bcc0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4962,6 +4962,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             e.printStackTrace();
         }
         if (mLiveLockScreenController.isShowingLiveLockScreenView()) {
+            mLiveLockScreenController.onLiveLockScreenFocusChanged(false);
             mLiveLockScreenController.getLiveLockScreenView().onKeyguardShowing(
                     mStatusBarKeyguardViewManager.isScreenTurnedOn());
         }

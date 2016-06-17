d11eedf9361a70dabafbfff128a9b540b016d5b2
Danesh M <daneshm90@gmail.com>
2016-06-17 15:22:59 -0700

    Revert "Revert "Only show keyguard panel if on lockscreen + no activity on top""
    CYNGNOS-2980
    
    This reverts commit 226797c8e2aba40ecbfb646f2ef5085893dff46a.
    
    Change-Id: I0440faf2dd07b427f639277eec2f3cfa89a7de6e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5135,6 +5135,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         return mStatusBarKeyguardViewManager != null && mStatusBarKeyguardViewManager.isSecure();
     }
 
+    public boolean isKeyguardInputRestricted() {
+        return mStatusBarKeyguardViewManager != null && mStatusBarKeyguardViewManager.isInputRestricted();
+    }
+
     public long calculateGoingToFullShadeDelay() {
         return mKeyguardFadingAwayDelay + mKeyguardFadingAwayDuration;
     }

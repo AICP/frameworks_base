4ad359de9d1caa150a80d17c9619e42c3047f983
Varun Date <date.varun123@gmail.com>
2016-07-15 16:39:37 +0200

    FWB:Pulse Solid Bar Sanity Levels & Lavalamp Anim Speed [1/3]
    Change-Id: I8ab484cceff96c32a075a9d6a1205846aa12b7cd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7891,6 +7891,18 @@ public final class Settings {
          */
         public static final String PULSE_EMPTY_BLOCK_SIZE = "pulse_empty_block_size";
 
+        /**
+         * Pulse Fudge Factor
+         * @hide
+         */
+        public static final String PULSE_SOLID_FUDGE_FACTOR = "pulse_solid_fudge_factor";
+
+        /**
+         * Pulse Lavamp Animation Speed
+         * @hide
+         */
+        public static final String PULSE_LAVALAMP_SOLID_SPEED = "lava_lamp_solid_speed";
+        
          /**
          * Whether user is allowed to pull down quick settings on secure keyguard.
          * @hide

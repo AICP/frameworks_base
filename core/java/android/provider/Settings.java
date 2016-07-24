17e8c1cdeacaa0902799606d2b62d90ebcd5185c
LorDClockaN <lordclockan@gmail.com>
2016-07-24 17:24:53 +0200

    FWB: Notifications transparency (1/2)
    Change-Id: I2b0b15444727f5b1e9442f23830b9b61e0e967aa
    Signed-off-by: LorDClockaN <lordclockan@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5333,6 +5333,13 @@ public final class Settings {
         public static final String BOOT_DIALOG_PACKAGES_RANDOM_COLOR = "boot_dialog_packages_random_color";
 
         /**
+         * Notifications alpha
+         *
+         * @hide
+         */
+        public static final String NOTIFICATION_ALPHA = "notification_alpha";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

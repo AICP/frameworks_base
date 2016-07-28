7b7a1bd677ff6366b156b8e6ccb0ec787df609b8
Steve Kondik <steve@cyngn.com>
2016-07-27 17:36:47 -0700

    mountservice: Shut down volumes before restarting framework
     * Vold assumes that when FUSE is killed, the mount is gone. This
       isn't true with sdcardfs, since the daemon doesn't continue
       to run. Cryptfs was assuming this behavior, and the system would
       hang during decryption since the emulated storage doesn't get
       unmounted.
     * Send the shutdown command before committing suicide.
    
    Change-Id: Ifcfa63f0499a717f34482754cea95b90622290c6

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -2630,6 +2630,7 @@ class StorageManagerService extends IStorageManager.Stub
                 mHandler.postDelayed(new Runnable() {
                     public void run() {
                         try {
+                            mConnector.execute("volume", "shutdown");
                             mCryptConnector.execute("cryptfs", "restart");
                         } catch (NativeDaemonConnectorException e) {
                             Slog.e(TAG, "problem executing in background", e);

945385e180dbcc5cc7f61490a674025f725291bd
varund7726 <date.varun123@gmail.com>
2016-08-09 17:53:55 +0200

    Halo: Add option to Enable/Disable Halo Button in Header [1/2]
    AICPfy
    
    Change-Id: I2818a636012b3347503e730d497301bc2eae6de6
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -113,6 +113,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private SettingsButton mSettingsButton;
     private View mSettingsContainer;
     private View mHaloButton;
+    private boolean mShowhaloButton;
     private View mQsDetailHeader;
     private TextView mQsDetailHeaderTitle;
     private Switch mQsDetailHeaderSwitch;
@@ -524,7 +525,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mAlarmStatus.setVisibility(mExpanded && mAlarmShowing ? View.VISIBLE : View.INVISIBLE);
         mSettingsContainer.setVisibility(mExpanded ? View.VISIBLE : View.INVISIBLE);
         mWeatherContainer.setVisibility(mExpanded && mShowWeather ? View.VISIBLE : View.GONE);
-        mHaloButton.setVisibility(mExpanded ? View.VISIBLE : View.INVISIBLE);
+        mHaloButton.setVisibility(mExpanded && mShowhaloButton ? View.VISIBLE : mShowhaloButton ? View.INVISIBLE : View.GONE);
         mQsDetailHeader.setVisibility(mExpanded && mShowingDetail ? View.VISIBLE : View.INVISIBLE);
         mTaskManagerButton.setVisibility(mExpanded && mShowTaskManager ? View.VISIBLE : View.GONE);
         mQsDetailHeader.setVisibility(mExpanded && mShowingDetail ? View.VISIBLE : View.GONE);
@@ -1315,21 +1316,24 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 UserHandle.USER_CURRENT);
             setStatusBarHeaderFontStyle(mStatusBarHeaderFontStyle);
 
-            headerShadow = Settings.System.getIntForUser(mContext.getContentResolver(),
+            headerShadow = Settings.System.getIntForUser(resolver,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, 0,
                     UserHandle.USER_CURRENT);
 
-            textShadow = Settings.System.getFloatForUser(mContext.getContentResolver(),
+            textShadow = Settings.System.getFloatForUser(resolver,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_TEXT_SHADOW, 0,
                     UserHandle.USER_CURRENT);
 
-            tShadowColor = Settings.System.getIntForUser(mContext.getContentResolver(),
+            tShadowColor = Settings.System.getIntForUser(resolver,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_TEXT_SHADOW_COLOR, 0,
                     UserHandle.USER_CURRENT);
 
-            customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+            customHeader = Settings.System.getIntForUser(resolver,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
                     UserHandle.USER_CURRENT);
+
+            mShowhaloButton = Settings.Secure.getInt(resolver,
+                    Settings.Secure.HALO_ENABLE, 0) == 1 ;
 
             updateVisibilities();
             requestCaptureValues();

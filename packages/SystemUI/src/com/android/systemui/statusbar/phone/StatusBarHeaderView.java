7876bd66aa9fa77cc4babd4003140b826cc32085
Dominik Baronelli <dominik-kassel@gmx.de>
2016-08-10 20:13:35 +0200

    Halo: fix behaviour of statusbar header icon
    - Allow toggling Halo instead of only 'on'
    - Represent current state
    
    Change-Id: I575d725d59da55f9a18d8eded71aca0bd49e73d7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -114,6 +114,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private View mSettingsContainer;
     private View mHaloButton;
     private boolean mShowhaloButton;
+    private boolean mHaloActive;
     private View mQsDetailHeader;
     private TextView mQsDetailHeaderTitle;
     private Switch mQsDetailHeaderSwitch;
@@ -1241,9 +1242,9 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         }
     };
 
-    private boolean toggleHalo() {
-        return Settings.Secure.putInt(mContext.getContentResolver(),
-                    Settings.Secure.HALO_ACTIVE, 1);
+    private void toggleHalo() {
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.HALO_ACTIVE, !mHaloActive ? 1 : 0);
     }
 
     class SettingsObserver extends UserContentObserver {
@@ -1274,6 +1275,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_TEXT_SHADOW_COLOR), false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CUSTOM_HEADER), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.HALO_ACTIVE), false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -1334,7 +1337,11 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                     UserHandle.USER_CURRENT);
 
             mShowhaloButton = Settings.Secure.getInt(resolver,
-                    Settings.Secure.HALO_ENABLE, 0) == 1 ;
+                    Settings.Secure.HALO_ENABLE, 0) == 1;
+
+            mHaloActive = Settings.Secure.getInt(resolver,
+                    Settings.Secure.HALO_ACTIVE, 0) == 1;
+            mHaloButton.setActivated(mHaloActive);
 
             updateVisibilities();
             requestCaptureValues();

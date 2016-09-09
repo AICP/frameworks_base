911c60f5f796cf606c8d606b91a71307b0e93a7a
Alex <mazdarider23@gmail.com>
2016-09-09 01:09:13 -0400

    Immersive Recents [1/2]
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only
    
    Change-Id: I471051b5ae1ae72863960da3297cdf7ad3b7da49
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4181,6 +4181,17 @@ public final class Settings {
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
 
         /**
+         * Immersive recents options
+         *
+         * 0 = Default AOSP look
+         * 1 = Statusbar only
+         * 2 = Navbar only
+         * 3 = Fullscreen
+         * @hide
+         */
+        public static final String IMMERSIVE_RECENTS = "immersive_recents";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

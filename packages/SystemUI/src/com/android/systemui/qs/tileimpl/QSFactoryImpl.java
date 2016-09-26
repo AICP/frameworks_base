c771dcb265c75ca07a5fbea6f9e005053cac13ad
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-09-26 05:10:28 +0300

    QS: Add Sync QS Tile
    Also includes
    7fb7847017ef57afba7b06c200ae423d84841d93
    	Title: SystemUI: stop crashing qs tiles during boot
    	Author: maxwen
    
    2f9d7be3857cf5887f494042bdf1cf2fdb1e9bb0
    	Title: SystemUI: long-press to settings on SyncTile
    	Author: Ali B.
    
    @mydongistiny: updated for oreo/pie
    
    @eyosen: updated for pie:
    	no more slashify
    	no more on off drawables
    
    Change-Id: I1c98c9d306a204aef436e806fcd39f7540967413

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.SoundTile;
+import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -166,6 +167,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new WeatherTile(mHost);
             case "pie":
                 return new PieTile(mHost);
+            case "sync":
+                return new SyncTile(mHost);
         }
         // Intent tiles.
         if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);

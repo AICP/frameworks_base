2a07d16fde662101d079fa06a1ce89ebfe6276c5
Alex Cruz <mazdarider23@gmail.com>
2016-11-06 20:37:36 +0100

    Optional screenshot type [1/2]
    Change-Id: Iaa579c1f53e504f7dfc3189e1dfb8e399e26da0b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4091,6 +4091,12 @@ public final class Settings {
         public static final String STATUS_BAR_TICKER_FONT_SIZE = "status_bar_ticker_font_size";
 
         /**
+         * Whether to allow fullscreen screenshots or let the user select which area to screenshot
+         * @hide
+         */
+        public static final String SCREENSHOT_TYPE = "screenshot_type";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

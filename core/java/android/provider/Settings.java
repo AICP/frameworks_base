7355fb8cf531a5d9c60534695cf954626190c0e1
LorDClockaN <lordclockan@gmail.com>
2016-11-12 14:01:46 +0100

    FWB: Lock screen AM/PM switch (1/2)
    Change-Id: I645f04a9e100c6e26bf52b7d70bb45e889a93f6f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4097,6 +4097,12 @@ public final class Settings {
         public static final String SCREENSHOT_TYPE = "screenshot_type";
 
         /**
+         * Option to hide AM/PM sufix
+         * @hide
+         */
+        public static final String LOCK_SCREEN_HIDE_AMPM = "lock_screen_hide_ampm";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

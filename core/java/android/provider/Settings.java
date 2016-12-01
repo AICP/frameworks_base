0688607664929849f9480488ec9394e97d92eeab
Joe Maples <joe@frap129.org>
2016-12-01 17:31:00 +0300

    Header Date+Time positions [1/2]
    AICPfied: Change default to stock behavior
    PS2: - add updateFontSize to CenterGroup
         - update layout using d448def03115c7b9699c3e1767220780e7b5c54e
            @crdroid fwb thanks to @neobuddy89
    
    PS3: - show normal and collapsed alarm at center
         - make center date, clock, alarm clickable
    
    Change-Id: I2747792519ee96e6540de5b53bcc8f1c30982842

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5338,6 +5338,12 @@ public final class Settings {
         public static final String FP_SWIPE_CALL_ACTIONS = "fp_swipe_call_actions";
 
         /**
+         * Whether to center or left justify date time group
+         * @hide
+         */
+        public static final String QS_DATE_TIME_CENTER = "qs_date_time_center";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

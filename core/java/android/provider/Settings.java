2da880bd0dc05b0e431404f5b6dc0517e4797568
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-12-10 16:28:19 +0100

    [2/2] frameworks: show charging current & voltage on lockscreen
    Thanx to ezio84 for the switch ;)
    
    Change-Id: I0a49bb204684a178f4977ea368b2970f2e5ec5ad
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3835,6 +3835,12 @@ public final class Settings {
          */
         public static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
 
+          /**
+          * Whether to show the negociated charger current in the lockscreen
+          * @hide
+          */
+         public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+ 
         /**
          * Network traffic indicator, goes from least to greatest significant bitwise
          * 0 = Display up-stream traffic if set

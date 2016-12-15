3cfbcfda7b88932dd2f0b5e9929781f8e0791328
Varun Date <date.varun123@gmail.com>
2016-12-15 22:39:06 +0300

    FWB:Add a Switch to Enable Disable Dyanamic Navbar[1/2]
    Everything works as it should. To see changes within the app after enabling/disabling this switch, app must be restarted everytime switch is toggled .
    
    Conflicts:
    	core/res/res/values/aicp_symbols.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: I7e4695d6c7970078d17890c9061aa52510e541c8
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4145,6 +4145,12 @@ public final class Settings {
         public static final String STATUS_BAR_DATE_FORMAT = "status_bar_date_format";
 
         /**
+         * Dyanamic Navbar Switch
+         * @hide
+         */
+        public static final String NAV_BAR_DYNAMIC = "nav_bar_dynamic";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

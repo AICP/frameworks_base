1dfd1005a23819c342d1aa060bfb4bd777fe6272
Varun Date <date.varun123@gmail.com>
2016-12-23 11:11:27 +0530

    QS: Battery Saver Easy Toggle [1/2]
    Change-Id: Iba550a6aa03e09217b7e76b32c36ca09fd97721f
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6931,6 +6931,12 @@ public final class Settings {
         public static final String WAKE_GESTURE_ENABLED = "wake_gesture_enabled";
 
         /**
+         * Enable single click to turn Battery Saver on or off.
+         * @hide
+         */
+        public static final String QS_BATTERY_EASY_TOGGLE = "qs_battery_easy_toggle";
+
+        /**
          * Whether the device should doze if configured.
          * @hide
          */

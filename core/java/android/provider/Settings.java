31bf8104420d8fe2edf8621bde84b74a6752d54e
Marko Man <darkobas@gmail.com>
2016-12-29 01:07:25 +0100

    [1/2] base: configure lock screen media artwork blur level
    Change-Id: I68895ae6a471bbc99b538b85337e15e8793665ce

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6150,6 +6150,12 @@ public final class Settings {
          */
         public static final String POCKET_JUDGE = "pocket_judge";
 
+        /**
+         * media artwork wallpaper blur level on lockscreen
+         * @hide
+         */
+        public static final String OMNI_LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6354,6 +6360,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(TOAST_ICON);
             PRIVATE_SETTINGS.add(FINGERPRINT_WAKE_UNLOCK);
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
+            PRIVATE_SETTINGS.add(OMNI_LOCKSCREEN_MEDIA_BLUR);
         }
 
         /**

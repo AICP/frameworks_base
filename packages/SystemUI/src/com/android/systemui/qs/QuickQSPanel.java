3949e17855a078df70525183dc8d6ac576e084c4
maxwen <max.weninger@gmail.com>
2017-01-21 04:09:00 +0300

    base: SystemUI: auto calculate quickbar tiles amount
    based on screen width and dpi
    
    Change-Id: I83cb3ef00da58758c910b20615be3df8d553b581

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -53,6 +53,7 @@ public class QuickQSPanel extends QSPanel {
     private int mMaxTiles = NUM_QUICK_TILES_DEFAULT;
     private QSPanel mFullPanel;
     private View mHeader;
+    private boolean mIsScrolling;
 
     public QuickQSPanel(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -139,7 +140,7 @@ public class QuickQSPanel extends QSPanel {
         ArrayList<QSTile<?>> quickTiles = new ArrayList<>();
         for (QSTile<?> tile : tiles) {
             quickTiles.add(tile);
-            if (quickTiles.size() == mMaxTiles) {
+            if (!mIsScrolling && quickTiles.size() == mMaxTiles) {
                 break;
             }
         }
@@ -171,15 +172,18 @@ public class QuickQSPanel extends QSPanel {
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+        setMaxTiles(((HeaderTileLayout) mTileLayout).calcNumTiles());
         ((HeaderTileLayout) mTileLayout).updateTileGaps();
     }
 
     @Override
     public void updateSettings() {
         super.updateSettings();
-        setMaxTiles(Settings.System.getIntForUser(mContext.getContentResolver(),
+        mIsScrolling = (Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.QS_QUICKBAR_SCROLL_ENABLED, 0, UserHandle.USER_CURRENT) == 0 ?
-                NUM_QUICK_TILES_DEFAULT : NUM_QUICK_TILES_ALL);
+                NUM_QUICK_TILES_DEFAULT : NUM_QUICK_TILES_ALL) == NUM_QUICK_TILES_ALL;
+        setMaxTiles(((HeaderTileLayout) mTileLayout).calcNumTiles());
+        ((HeaderTileLayout) mTileLayout).updateTileGaps();
     }
 
     private static class HeaderTileLayout extends LinearLayout implements QSTileLayout {
@@ -189,6 +193,7 @@ public class QuickQSPanel extends QSPanel {
         private int mTileSize;
         private int mScreenWidth;
         private int mStartMargin;
+        private int mMinTileGap;
 
         public HeaderTileLayout(Context context) {
             super(context);
@@ -199,6 +204,7 @@ public class QuickQSPanel extends QSPanel {
             mTileSize = mContext.getResources().getDimensionPixelSize(R.dimen.qs_quick_tile_size);
             mStartMargin = mContext.getResources().getDimensionPixelSize(R.dimen.qs_scroller_margin);
             mScreenWidth = mContext.getResources().getDisplayMetrics().widthPixels;
+            mMinTileGap = mContext.getResources().getDimensionPixelSize(R.dimen.qs_scroller_min_tile_gap);
         }
 
         @Override
@@ -288,13 +294,24 @@ public class QuickQSPanel extends QSPanel {
             }
         }
 
+        public int calcNumTiles() {
+            int panelWidth = mContext.getResources().getDimensionPixelSize(R.dimen.notification_panel_width);
+            if (panelWidth == -1) {
+                panelWidth = mScreenWidth;
+            }
+            panelWidth -= 2 * mStartMargin;
+            int maxNumTiles = panelWidth / (mTileSize + 2 * mMinTileGap);
+            return maxNumTiles;
+        }
+
         public void updateTileGaps() {
             int panelWidth = mContext.getResources().getDimensionPixelSize(R.dimen.notification_panel_width);
             if (panelWidth == -1) {
                 panelWidth = mScreenWidth;
             }
             panelWidth -= 2 * mStartMargin;
-            int tileGap = (panelWidth - mTileSize * NUM_QUICK_TILES_DEFAULT) / (NUM_QUICK_TILES_DEFAULT - 1);
+            int maxNumTiles = panelWidth / (mTileSize + 2 * mMinTileGap);
+            int tileGap = (panelWidth - mTileSize * maxNumTiles) / (maxNumTiles - 1);
             final int N = getChildCount();
             for (int i = 0; i < N; i++) {
                 if (getChildAt(i) instanceof Space) {

7e88d02bbfce4e91db2bc3644081d40212725a44
Varun Date <date.varun123@gmail.com>
2017-03-03 08:54:43 +0530

    SystemUI:Add MemBar to Slim Recents [1/2]
    Taken from Recents commit https://github.com/ResurrectionRemix/android_frameworks_base/commit/01bbf565946e923498aa3ec0e8afcb74d8aa005f
    
    extended it to Slim Recents
    
    Change-Id: I8388c89959e15d5f1695e04e0b743671a12c5adc
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4884,6 +4884,12 @@ public final class Settings {
          */
         public static final String RECENT_CARD_TEXT_COLOR = "recent_card_text_color";
 
+	    /**
+         * show the membar in slim recents
+         *  @hide
+         */
+        public static final String SLIM_RECENTS_MEM_DISPLAY = "slim_recents_mem_display";
+
         /**
          * Slim recents AICPfied empty recents drawable
          *

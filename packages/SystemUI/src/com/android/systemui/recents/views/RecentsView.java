842898ffc1b2337dc49bbc6db64b835a6cfbbf30
SpiritCroc <spiritcroc@gmail.com>
2017-03-08 20:14:40 +0100

    AICP empty recents: clean up unneeded code, make it animate again
    Change-Id: I622a002aec2aae9da33dbe31f2a7dd76c35709c2

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -34,6 +34,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Outline;
 import android.graphics.Rect;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -53,7 +54,6 @@ import android.view.ViewOutlineProvider;
 import android.view.ViewPropertyAnimator;
 import android.view.WindowInsets;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.ImageButton;
@@ -326,11 +326,10 @@ public class RecentsView extends FrameLayout {
         mTaskStackView.setVisibility(View.INVISIBLE);
         mEmptyView.setText(msgResId);
 
-        // Customize empty recents view drawable
-        Drawable drawable = getResources().getDrawable(R.drawable.no_recents, null);
-        ImageView imageView = (ImageView) mEmptyView.findViewById(R.id.no_recents_holder);
-        if (imageView != null){
-            imageView.setImageDrawable(drawable);
+        // AICP animated empty recents view
+        Drawable[] emptyDrawables = mEmptyView.getCompoundDrawables();
+        if (emptyDrawables[1] instanceof AnimatedVectorDrawable) {
+            ((AnimatedVectorDrawable) emptyDrawables[1]).start();
         }
 
         mEmptyView.setVisibility(View.VISIBLE);
@@ -353,6 +352,12 @@ public class RecentsView extends FrameLayout {
      * Shows the task stack and hides the empty view.
      */
     public void hideEmptyView() {
+        // AICP animated empty recents view
+        Drawable[] emptyDrawables = mEmptyView.getCompoundDrawables();
+            if (emptyDrawables[1] instanceof AnimatedVectorDrawable) {
+                ((AnimatedVectorDrawable) emptyDrawables[1]).stop();
+        }
+
         mEmptyView.setVisibility(View.INVISIBLE);
         mTaskStackView.setVisibility(View.VISIBLE);
         mTaskStackView.bringToFront();

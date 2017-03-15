031b81bcf303757b60663fe51a170e52fc3db73e
Pranav Vashi <neobuddy89@gmail.com>
2017-03-15 07:36:19 +0530

    fwb: Make roaming indicator optional [1/2]
    * Roaming indicator on signal icon is useless most of the times
      since a carrier might not charge differently to user if in same
      country.
    * So, allow user to hide roaming indicator and see what kind of signal
      strength is available to him.
    * This commit will only allow to hide indicator not roaming warnings.
    
    Change-Id: Ia048b08f20f1e2fe276ee0da112f9e2a892fee07

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6318,6 +6318,11 @@ public final class Settings {
          */
         public static final String AUTHORIZATION_RIPPLE_EFFECT = "authorization_ripple_effect";
 
+        /**
+         * @hide
+         */
+        public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6613,6 +6618,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CAMERA_SLEEP_ON_RELEASE);
             PRIVATE_SETTINGS.add(CAMERA_LAUNCH);
             PRIVATE_SETTINGS.add(AUTHORIZATION_RIPPLE_EFFECT);
+            PRIVATE_SETTINGS.add(ROAMING_INDICATOR_ICON);
         }
 
         /**
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -251,5 +251,6 @@ public class SystemSettings {
         Settings.System.CAMERA_SLEEP_ON_RELEASE,
         Settings.System.CAMERA_LAUNCH,
         Settings.System.AUTHORIZATION_RIPPLE_EFFECT,
+        Settings.System.ROAMING_INDICATOR_ICON,
     };
 }

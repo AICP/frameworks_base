b1b59dbe5f3e5c3ecf59a87c3227a8de849cb8d0
Ali B <abittin@gmail.com>
2017-03-17 14:42:33 +0300

    QS: add MagiskManager tile
    PS2: add settings activity to longpress
         update qs icon
    PS3: remove from tiles if not available
    
    Change-Id: Ie0183e1df267b409b8e21250bdd50bf36fe0079e
    Signed-off-by: Ali B <abittin@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -61,6 +61,7 @@ import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocaleTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.MagiskManagerTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NavigationBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
@@ -478,6 +479,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("weather")) return new WeatherTile(this);
         else if (tileSpec.equals("substratum")) return new SubstratumTile(this);
+        else if (tileSpec.equals("magiskmanager")) return new MagiskManagerTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);

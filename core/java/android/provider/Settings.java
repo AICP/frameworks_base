32eca7d6c61e74776fd06c7c6da2ba1193f12f87
Pranav Vashi <neobuddy89@gmail.com>
2017-03-19 01:48:25 +0530

    Add ability to toggle bluetooth battery level [1/2]
    Change-Id: I1b7dbc58c9db25261292dae09491c2318ed87df5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4038,6 +4038,12 @@ public final class Settings {
          */
 
         /**
+         * Enable\Disable Bluetooth Battery bar
+         * @hide
+         */
+        public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

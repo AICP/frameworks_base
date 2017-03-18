bcc6aacc694d4996e124b040e0d1d53c4538ff9f
Pranav Vashi <neobuddy89@gmail.com>
2017-03-19 01:48:25 +0530

    Add ability to toggle bluetooth battery level [1/2]
    AICP edit for P: changed default to false
    
    Ported to 11: show if type is supported and toggle is on
    	Use existing observer in statusbar. Dont add a new one.
    
    Also includes
    	Statusbar bluetooth battery level: fix level toggle
    	Change-Id: I1f2a6171a79ed6999745d2163e0c401e4989c0eb
    
    Change-Id: I1b7dbc58c9db25261292dae09491c2318ed87df5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -161,6 +161,8 @@ public class PhoneStatusBarPolicy
 
     private boolean mManagedProfileIconVisible = false;
 
+    private boolean mShowBluetoothBattery = false;
+
     private BluetoothController mBluetooth;
     private AlarmManager.AlarmClockInfo mNextAlarm;
 
@@ -352,6 +354,11 @@ public class PhoneStatusBarPolicy
         onUserSetupChanged();
     }
 
+    public void updateSettings(boolean showBluetoothBattery) {
+        mShowBluetoothBattery = showBluetoothBattery;
+        updateBluetooth();
+    }
+
     @Override
     public void onZenChanged(int zen) {
         updateVolumeZen();
@@ -467,7 +474,8 @@ public class PhoneStatusBarPolicy
                         if (state == BluetoothProfile.STATE_CONNECTED) {
                             int batteryLevel = device.getBatteryLevel();
                             BluetoothClass type = device.getBtClass();
-                            if (batteryLevel != BluetoothDevice.BATTERY_LEVEL_UNKNOWN && showBatteryForThis(type)) {
+                            if (batteryLevel != BluetoothDevice.BATTERY_LEVEL_UNKNOWN && showBatteryForThis(type)
+                                    && mShowBluetoothBattery) {
                                 iconId = getBtLevelIconRes(batteryLevel);
                             } else {
                                 iconId = R.drawable.stat_sys_data_bluetooth_connected;

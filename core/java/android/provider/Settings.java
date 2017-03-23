da6a6a17ca0bc197ac3755d814f2292ffc635203
Danesh Mondegarian <daneshm90@gmail.com>
2017-03-23 21:16:01 +0300

    Slim Recents iconpack support [1/2]
    ezio:
    adapted IconPackHelper from CM trebuchet (https://review.lineageos.org/#/c/136600/)
    with latest OmniSwitch improvements;
    splitted the whole dialog part and moved it into Settings;
    i've set here Danesh as commit author because he made the original IconPackHelper
    code long time ago.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    Change-Id: If1c8c040506e156a4e4ee9fbd72aa28c13a5f09e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5121,6 +5121,12 @@ public final class Settings {
         public static final String SCREEN_STATE_ON_DELAY = "screen_state_on_delay";
 
         /**
+         * Custom icon pack name to use for Slim Recents
+         * @hide
+         */
+        public static final String SLIM_RECENTS_ICON_PACK = "slim_recents_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

f759fd4da84fdc4e6bcdd47ce245b6f21dfe89e3
ezio84 <brabus84@gmail.com>
2017-04-23 16:47:32 +0200

    Fix DUI slippery flag logic and IllegalArgumentException [2/2]
    Change-Id: I28fb3a7ab3ba66728a41fcd955d54726eff08be8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2089,7 +2089,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                     | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
                     | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH
-                    | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                    | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
+                    | WindowManager.LayoutParams.FLAG_SLIPPERY,
                 PixelFormat.TRANSLUCENT);
         // this will allow the navbar to run in an overlay on devices that support this
 /*        if (ActivityManager.isHighEndGfx()) {
@@ -3598,6 +3599,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mNotificationPanel.closeQs();
 
         mExpandedVisible = false;
+
         visibilityChanged(false);
 
         // Shrink the window to the size of the status bar only

65b2bc4506d8a4a8c48f195645b970dfb278bd3e
Alexander Westphal <westphal.alex94@gmail.com>
2017-04-26 09:20:53 +0100

    SystemUI: Update brightness icons
    AICPfy:
    * Back to original icon name
    * Remove unnecessary png icon
    
    Change-Id: I1ee9ec7004d51ee129e585aabd29391885925782

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -458,8 +458,8 @@ public class BrightnessController implements ToggleSlider.Listener {
     private void updateIcon(boolean automatic) {
         if (mIcon != null) {
             mIcon.setImageResource(automatic ?
-                    com.android.systemui.R.drawable.ic_qs_brightness_auto_on_new :
-                    com.android.systemui.R.drawable.ic_qs_brightness_auto_off_new);
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_on :
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_off);
         }
     }
 

e4d80006bfa9b18b569e9b6c8888ba0fd09a9e14
Pranav Vashi <neobuddy89@gmail.com>
2017-05-14 08:55:06 +0530

    [SQUASHED] Custom logo customizations [1/2]
    * Original placement logic and custom logo patches
      by LorDClockaN and varund7726 respectively.
    * @neobuddy89: Complete rewrite for oreo with darkicon dispatcher.
    
    -AICP'fied by @eyosen:
      * We are AICP after all.
      * Also make naming rom independent.
      * Brought up to Aicp Pie, 10, 11, 12
      * Includes 27dc548f8c483429af93a265492f2e0e7e514112
    	fwb: statusbar logo code cleanup
      * and more code cleanup
    
    Change-Id: I9f4a9331a3867712f69579f764ef2cebd0bbe717
    Signed-off-by: Julian Veit <claymore1298@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -216,5 +216,10 @@ public class SystemSettings {
         Settings.System.STATUSBAR_BATTERY_BAR_STYLE,
         Settings.System.STATUSBAR_BATTERY_BAR_ANIMATE,
         Settings.System.STATUSBAR_BATTERY_BAR_LOCATION,
+        Settings.System.STATUS_BAR_LOGO,
+        Settings.System.STATUS_BAR_LOGO_COLOR,
+        Settings.System.STATUS_BAR_LOGO_POSITION,
+        Settings.System.STATUS_BAR_LOGO_STYLE,
+        Settings.System.STATUS_BAR_LOGO_COLOR_ACCENT,
     };
 }

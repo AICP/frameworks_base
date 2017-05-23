6491c5a2aca83cccf977a7e9533b26b8e40b3d2d
AirOne70 <finot.erwan@gmail.com>
2017-05-23 10:25:44 +0200

    QS: FPC: pulldown with one swipe on fp sensor [1/2]
    Allow the user to expand QS with only one slide.
    Keep the stock behavior when disabled.
    
    Change-Id: Ia65721fe0a5ca64cb32bbcdd82a1058082667533
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -480,6 +480,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     private int mMaxKeyguardNotifConfig;
     private boolean mCustomMaxKeyguard;
 
+    private boolean mFingerprintQuickPulldown;
+
     // the tracker view
     int mTrackingPosition; // the position of the top of the tracking view.
 
@@ -3047,8 +3049,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         } else if (KeyEvent.KEYCODE_SYSTEM_NAVIGATION_DOWN == key) {
             mMetricsLogger.action(MetricsEvent.ACTION_SYSTEM_NAVIGATION_KEY_DOWN);
             if (mNotificationPanel.isFullyCollapsed()) {
-                mNotificationPanel.expand(true /* animate */);
-                mMetricsLogger.count(NotificationPanelView.COUNTER_PANEL_OPEN, 1);
+                if (mFingerprintQuickPulldown) {
+                    mNotificationPanel.expandWithQs();
+                    mMetricsLogger.count(NotificationPanelView.COUNTER_PANEL_OPEN_QS, 1);
+                } else {
+                    mNotificationPanel.expand(true /* animate */);
+                    mMetricsLogger.count(NotificationPanelView.COUNTER_PANEL_OPEN, 1);
+                }
             } else if (!mNotificationPanel.isInSettings() && !mNotificationPanel.isExpanding()){
                 mNotificationPanel.flingSettings(0 /* velocity */, true /* expand */);
                 mMetricsLogger.count(NotificationPanelView.COUNTER_PANEL_OPEN_QS, 1);
@@ -5762,6 +5769,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_ROTATION),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN_FP),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -5829,11 +5839,18 @@ public class StatusBar extends SystemUI implements DemoMode,
             setLockscreenMaxNotifications();
             updateTickerSettings();
             updateLockScreenRotation();
+            updateFPQuickPulldown();
 
             // Update slim recents
             updateRecentsMode();
             rebuildRecentsScreen();
         }
+    }
+
+    private void updateFPQuickPulldown() {
+        mFingerprintQuickPulldown = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN_FP, 0,
+                UserHandle.USER_CURRENT) == 1;
     }
 
     private void updateTickerSettings() {

35d1fad84b9802000030c23aa7f087c2740cb3be
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-06-13 15:39:17 -0400

    Net monitor: fix arrows not showing when data saver is enabled
    to hide arrows if no connection is available (e.g. both wifi and data are
    OFF), we can just check if networ!=null, no need to check the actual
    connection state of the network itself.
    
    to replicate the issue:
    net arrows auto hide feature off
    disable wifi but keep data connection enabled
    enable data saver
    arrows will disappear
    
    after this patch, steps above won't hide the arrows, but
    they will correctly if you disable both wifi and data connection
    Change-Id: I78be44db5a5d7865447d0b6cb7f33dde2cc58b26

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -275,7 +275,7 @@ public class NetworkTraffic extends TextView {
         ConnectivityManager connManager =
                 (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo network = (connManager != null) ? connManager.getActiveNetworkInfo() : null;
-        return network != null && network.isConnected();
+        return network != null;
     }
 
     private void updateSettings() {

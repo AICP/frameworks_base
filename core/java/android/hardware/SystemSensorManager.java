db145aaaa66b11b43fa82c757986c04088684b66
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-09 20:53:52 +0200

    SystemSensorManager: sensor block per-package
    aicp edits:
    add facebook messenger, and slack
    
    Change-Id: I88ece292a6ef6867577b2e9cca5ea5e2d1d471b2

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -290,6 +290,17 @@ public class SystemSensorManager extends SensorManager {
                     + "). Most common: " + maxEntry.getKey() + " (" + maxEntry.getValue()
                     + ")");
         }
+        if (sensor.getType() == Sensor.TYPE_SIGNIFICANT_MOTION) {
+            String pkgName = mContext.getPackageName();
+            for (String blockedPkgName : mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_blockPackagesSensorDrain)) {
+                if (pkgName.equals(blockedPkgName)) {
+                    Log.w(TAG, "Preventing " + pkgName + "from draining battery using " +
+                                    "significant motion sensor");
+                    return false;
+                }
+            }
+        }
 
         // Invariants to preserve:
         // - one Looper per SensorEventListener

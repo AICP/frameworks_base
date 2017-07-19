98361251c7d8b9e49bad5efe2a77652dc2735818
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    Allow using 4G icon instead LTE [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I704a44145c1a3e5080b379eb92eb22d61bd9fd22

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5976,6 +5976,12 @@ public final class Settings {
          */
         public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
 
+        /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
         /***************************
          * AICP System Settings end
          ***************************/

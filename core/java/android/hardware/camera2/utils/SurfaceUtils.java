b9ec6b44ec2f5b7bed4506ad6ea52d60876b8716
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2017-08-08 16:27:19 +0530

    camera: Skip HFR checks for privileged apps.
    Issue: HAL3 supports HFR only if its greater than or equal
           to 120fps.
    
    Fix: Skip the checks to allow HFR 60 and 90 fps in HAL3 for
         specific apps.
         The following property need to be set to allow these
         HFR configurations :
         adb shell setprop persist.camera.privapp.list <pack1,pack2,etc>
    
    Additionally allows extension of the privapp list via sdk resources
    
    
    CRs-Fixed: 2258892
    Change-Id: I955d5c3883ccb58bdfbc0e514f42729987db0bc9

--- core/java/android/hardware/camera2/utils/SurfaceUtils.java
+++ core/java/android/hardware/camera2/utils/SurfaceUtils.java
@@ -20,15 +20,20 @@ import static android.system.OsConstants.EINVAL;
 
 import static com.android.internal.util.Preconditions.checkNotNull;
 
+import android.app.ActivityThread;
 import android.compat.annotation.UnsupportedAppUsage;
+import android.content.res.Resources;
 import android.graphics.ImageFormat;
 import android.graphics.PixelFormat;
 import android.hardware.HardwareBuffer;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.SystemProperties;
+import android.text.TextUtils;
 import android.util.Range;
 import android.util.Size;
 import android.view.Surface;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
@@ -304,6 +309,19 @@ public class SurfaceUtils {
         }
     }
 
+    private static boolean isPrivilegedApp() {
+        String packageName = ActivityThread.currentOpPackageName();
+        List<String> packageList = new ArrayList<>(Arrays.asList(
+                SystemProperties.get("persist.vendor.camera.privapp.list", ",").split(",")));
+
+        // Append packages from frameworks/base resources
+        Resources res = ActivityThread.currentApplication().getResources();
+        packageList.addAll(Arrays.asList(res.getStringArray(
+                com.android.internal.R.array.config_cameraHFRPrivAppList)));
+
+        return packageList.contains(packageName);
+    }
+
     private static native int nativeDetectSurfaceType(Surface surface);
 
     private static native int nativeDetectSurfaceDataspace(Surface surface);
@@ -315,20 +333,4 @@ public class SurfaceUtils {
 
     private static native long nativeGetSurfaceId(Surface surface);
 
-    private static boolean isPrivilegedApp() {
-        String packageName = ActivityThread.currentOpPackageName();
-        String packageList = SystemProperties.get("persist.vendor.camera.privapp.list");
-
-        if (packageList.length() > 0) {
-            TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
-            splitter.setString(packageList);
-            for (String str : splitter) {
-                if (packageName.equals(str)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
 }

c811f023a79220500a3337ab8894b9f7c12c7c52
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-08-28 03:06:59 +0300

    NotificationManagerService: Disable channel warning toast by default
    Change-Id: I3875b55bf2e0d1d1e621bf6d32048f3e3eac1f88
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -5949,7 +5949,7 @@ public class NotificationManagerService extends SystemService {
 
     private void doChannelWarningToast(CharSequence toastText) {
         Binder.withCleanCallingIdentity(() -> {
-            final int defaultWarningEnabled = Build.IS_ENG ? 1 : 0;
+            final int defaultWarningEnabled = 0;
             final boolean warningEnabled = Settings.Global.getInt(getContext().getContentResolver(),
                     Settings.Global.SHOW_NOTIFICATION_CHANNEL_WARNINGS, defaultWarningEnabled) != 0;
             if (warningEnabled) {

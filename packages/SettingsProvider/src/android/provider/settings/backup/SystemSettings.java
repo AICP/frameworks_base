2d1265d9de36ba6119062d0d8d7b743e465baf3d
maxwen <max.weninger@gmail.com>
2017-09-01 15:54:53 +0200

    base: allow to swap volume buttons rotation based [1/2]
    Change-Id: Ia50d9d1d8a72a649dd86b99f9701b0de05252ebe

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -203,5 +203,6 @@ public class SystemSettings {
         Settings.System.CAMERA_WAKE_SCREEN,
         Settings.System.CAMERA_SLEEP_ON_RELEASE,
         Settings.System.CAMERA_LAUNCH,
+        Settings.System.SWAP_VOLUME_BUTTONS,
     };
 }

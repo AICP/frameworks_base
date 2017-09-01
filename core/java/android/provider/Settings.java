fcb6ffc0e5ae8a03ea83f846a6856572d5a93f35
maxwen <max.weninger@gmail.com>
2017-09-01 15:54:53 +0200

    base: allow to swap volume buttons rotation based [1/2]
    Change-Id: Ia50d9d1d8a72a649dd86b99f9701b0de05252ebe

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6478,6 +6478,13 @@ public final class Settings {
          */
         public static final String CAMERA_LAUNCH = "camera_launch";
 
+        /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        @Readable
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6725,6 +6732,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CAMERA_WAKE_SCREEN);
             PRIVATE_SETTINGS.add(CAMERA_SLEEP_ON_RELEASE);
             PRIVATE_SETTINGS.add(CAMERA_LAUNCH);
+            PRIVATE_SETTINGS.add(SWAP_VOLUME_BUTTONS);
         }
 
         /**

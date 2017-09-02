6e94732c10d2b84d3c19e08f62f45aad4ffffa18
maxwen <max.weninger@gmail.com>
2017-09-02 14:27:50 +0800

    Add WakelockBlocker [1/2]
    ported by faust93
    
    neobuddy89: Moved to global settings.
    
    Change-Id: I93fc64127c63a2d7986dff4f9c2a9941eaa56102

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17282,6 +17282,18 @@ public final class Settings {
          */
         public static final String RINGTONE_FOCUS_MODE_V2 = "ringtone_focus_mode_v2";
 
+        /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+         /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
         /*****************************
          * AICP Global Settings end
          *****************************/

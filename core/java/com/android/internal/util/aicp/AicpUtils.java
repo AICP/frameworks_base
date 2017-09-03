b78c4ae7e726f79c4edb1d1ce8c0f8b2cfe71f99
ezio84 <brabus84@gmail.com>
2017-09-03 15:31:56 +0200

    fixup!: Add api to toggle flashlight
    statusbar service getter borrowed from PhoneWindowManager code
    
    use PackageManager to check if the device has flashlight
    
    use aosp flashcontroller to handle the flash toggle like the flashlight
    tile does
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iafd04cc8c500eff7a1e05b62b4dc0609416de31f

--- core/java/com/android/internal/util/aicp/AicpUtils.java
+++ core/java/com/android/internal/util/aicp/AicpUtils.java
@@ -143,12 +143,30 @@ public class AicpUtils {
 
     // Toggle flashlight
     public static void toggleCameraFlash() {
-        IStatusBarService service = getStatusBarService();
-        if (service != null) {
-            try {
-                service.toggleCameraFlash();
-            } catch (RemoteException e) {
-                // do nothing.
+        FireActions.toggleCameraFlash();
+    }
+
+    private static final class FireActions {
+        private static IStatusBarService mStatusBarService = null;
+
+        private static IStatusBarService getStatusBarService() {
+            synchronized (FireActions.class) {
+                if (mStatusBarService == null) {
+                    mStatusBarService = IStatusBarService.Stub.asInterface(
+                            ServiceManager.getService("statusbar"));
+                }
+                return mStatusBarService;
+            }
+        }
+
+        public static void toggleCameraFlash() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.toggleCameraFlash();
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
             }
         }
     }

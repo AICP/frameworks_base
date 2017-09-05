aa645e481bc04a61208b1c8cfce3df2a1f617a50
Pranav Vashi <neobuddy89@gmail.com>
2017-09-05 13:17:05 +0800

    Fix FAB not disappearing in multiwindow
    Change-Id: I082ab5075cf7cc74ed94923df1f5e9fd0280c4b7

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -295,14 +295,8 @@ public class RecentsView extends FrameLayout {
         // Update the top level view's visibilities
         if (stack.getTaskCount() > 0) {
             hideEmptyView();
-            if (mFloatingButton != null) {
-                mFloatingButton.setVisibility(View.VISIBLE);
-            }
         } else {
             showEmptyView(R.string.recents_empty_message);
-            if (mFloatingButton != null) {
-                mFloatingButton.setVisibility(View.GONE);
-            }
         }
     }
 
@@ -399,6 +393,9 @@ public class RecentsView extends FrameLayout {
         mEmptyView.setVisibility(View.VISIBLE);
         mEmptyView.bringToFront();
         mStackActionButton.bringToFront();
+        if (mFloatingButton != null) {
+            mFloatingButton.setVisibility(View.GONE);
+        }
     }
 
     /**
@@ -409,6 +406,9 @@ public class RecentsView extends FrameLayout {
         mTaskStackView.setVisibility(View.VISIBLE);
         mTaskStackView.bringToFront();
         mStackActionButton.bringToFront();
+        if (mFloatingButton != null) {
+            mFloatingButton.setVisibility(View.VISIBLE);
+        }
     }
 
     /**
@@ -428,7 +428,6 @@ public class RecentsView extends FrameLayout {
     protected void onAttachedToWindow() {
         EventBus.getDefault().register(this, RecentsActivity.EVENT_BUS_PRIORITY + 1);
         EventBus.getDefault().register(mTouchHandler, RecentsActivity.EVENT_BUS_PRIORITY + 2);
-        super.onAttachedToWindow();
         mSettingsObserver.observe();
         mClearRecents.setVisibility(View.VISIBLE);
         mClearRecents.setOnClickListener(new View.OnClickListener() {
@@ -436,6 +435,7 @@ public class RecentsView extends FrameLayout {
             EventBus.getDefault().send(new DismissAllTaskViewsEvent());
             }
         });
+        super.onAttachedToWindow();
     }
 
     @Override

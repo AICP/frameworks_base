b2fff747aff22730b9e1c969542f8925c803dbb9
xyyx <xyyx@mail.ru>
2017-09-09 10:12:48 +0300

    base: Lockscreen Charging info  (3/3)
         based on: [2/2] frameworks: show charging current & voltage on lockscreen by
         yank555-lu [2/2]Base: show more battery info on lockscreen when charging by
         yank555-lu LockscreenCharging: Use double for mChargingVoltage and show as
         4.XV by xyyx Lockscreen charging: Formatting improvements by nathanchance
    
        AICPfy:
        - default set to false
        - Current formatting improvement for devices reporting directly in Amps
    
        Return-logic of computePowerIndication has changed in Q, so adapted
        it to the new logic.
    
        Ported to 11 & 12 by @eyosen: adapt to the move to SettingsLib
    
    Change-Id: Icc08f6f7b0fd31cb5ab31556c6a18ee9bc57bf22

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -27,6 +27,7 @@ import static android.os.BatteryManager.EXTRA_MAX_CHARGING_VOLTAGE;
 import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static android.os.BatteryManager.EXTRA_PRESENT;
 import static android.os.BatteryManager.EXTRA_STATUS;
+import static android.os.BatteryManager.EXTRA_TEMPERATURE;
 
 import android.content.Context;
 import android.content.Intent;
@@ -50,16 +51,23 @@ public class BatteryStatus {
     public final int level;
     public final int plugged;
     public final int health;
+    public final int maxChargingCurrent;
+    public final int maxChargingVoltage;
     public final int maxChargingWattage;
+    public final int temperature;
     public final boolean present;
 
     public BatteryStatus(int status, int level, int plugged, int health,
-            int maxChargingWattage, boolean present) {
+            int maxChargingCurrent, int maxChargingVoltage, int maxChargingWattage,
+            int temperature, boolean present) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
         this.health = health;
+        this.maxChargingCurrent = maxChargingCurrent;
+        this.maxChargingVoltage = maxChargingVoltage;
         this.maxChargingWattage = maxChargingWattage;
+        this.temperature = temperature;
         this.present = present;
     }
 
@@ -68,11 +76,13 @@ public class BatteryStatus {
         plugged = batteryChangedIntent.getIntExtra(EXTRA_PLUGGED, 0);
         level = batteryChangedIntent.getIntExtra(EXTRA_LEVEL, 0);
         health = batteryChangedIntent.getIntExtra(EXTRA_HEALTH, BATTERY_HEALTH_UNKNOWN);
+        maxChargingCurrent = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_CURRENT, -1);
+        maxChargingVoltage = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_VOLTAGE, -1);
+        temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
 
-        final int maxChargingMicroAmp = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_CURRENT,
-                -1);
-        int maxChargingMicroVolt = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_VOLTAGE, -1);
+        final int maxChargingMicroAmp = maxChargingCurrent;
+        int maxChargingMicroVolt = maxChargingVoltage;
 
         if (maxChargingMicroVolt <= 0) {
             maxChargingMicroVolt = DEFAULT_CHARGING_VOLTAGE_MICRO_VOLT;

1880ee219090083ab687f7d29c31960a0ba3685c
xyyx <xyyx@mail.ru>
2017-09-09 10:12:48 +0300

    base: Lockscreen Charging info  (3/3)
         based on: [2/2] frameworks: show charging current & voltage on lockscreen by
         yank555-lu [2/2]Base: show more battery info on lockscreen when charging by
         yank555-lu LockscreenCharging: Use double for mChargingVoltage and show as
         4.XV by xyyx Lockscreen charging: Formatting improvements by nathanchance
    
        AICPfy:
        - default set to false
        - Current formatting improvement for devices reporting directly in Amps
    
        Return-logic of computePowerIndication has changed in Q, so adapted
        it to the new logic.
    
        Ported to 11 by @eyosen: adapt to the move to SettingsLib
    
    Change-Id: Icc08f6f7b0fd31cb5ab31556c6a18ee9bc57bf22

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1686,7 +1686,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();

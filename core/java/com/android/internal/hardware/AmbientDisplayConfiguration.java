ff0cdf4912b0d33389c850352285b651ae9e7256
darjwx <darjwx@gmail.com>
2017-09-14 16:11:36 +0200

    AlwaysOnDisplay: let the maintainer disable or enable it
    * AOD is a nice addition that will work well in devices with OLED displays,
      causing high battery drain in other display panels.
    
    * Give the maintainer the ability to enable or disable it with a device overlay:
        config_enableDozeAlwaysOn
      False by default.
    
    Change-Id: I41d88f8bf1216e337385e669e5a249a0f0cdc882

--- core/java/com/android/internal/hardware/AmbientDisplayConfiguration.java
+++ core/java/com/android/internal/hardware/AmbientDisplayConfiguration.java
@@ -80,7 +80,9 @@ public class AmbientDisplayConfiguration {
     }
 
     public boolean alwaysOnAvailable() {
-        return true;
+        boolean enableDozeAlwaysOn = mContext.getResources().
+                getBoolean(com.android.internal.R.bool.config_enableDozeAlwaysOn);
+        return enableDozeAlwaysOn;
     }
 
     public String ambientDisplayComponent() {

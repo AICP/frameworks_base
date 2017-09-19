fff5c6b7cba7551348e8f02696f6eef61beff166
LorDClockaN <lordclockan@gmail.com>
2017-09-19 19:27:58 +0800

    Powermenu Torch option [1/2]
    Change-Id: Ib65e075bbbc1e3f2ff8d98ab0770f827b9c397bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5829,6 +5829,12 @@ public final class Settings {
          */
         public static final String POWERMENU_EMERGENCY = "powermenu_emergency";
 
+        /**
+         * Whether to display the torch option in the power menu
+         * @hide
+         */
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
         /***************************
          * AICP System Settings end
          ***************************/

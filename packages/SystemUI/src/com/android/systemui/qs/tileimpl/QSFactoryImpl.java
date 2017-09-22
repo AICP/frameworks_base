0158f9e720ebd5ad06043cfd45aac85fd178b31e
fusionjack <dogfight60-fusionjack@yahoo.de>
2017-09-22 19:25:45 +0200

    [QUICK SETTINGS] Add Reboot/Recovery QS Tile
    AICPfy:
    - Bring it to O - @LorDClockaN
    - Include this patch: https://github.com/AICP/frameworks_base/commit/ea602a0cce00fb13885f3a15a676cf0936a13eff
    
    Change-Id: I2c7956b7201e2103420fc7254051141807024fc4

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -35,6 +35,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -69,6 +70,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("night")) return new NightDisplayTile(mHost);
         else if (tileSpec.equals("nfc")) return new NfcTile(mHost);
         else if (tileSpec.equals("aicp_extras")) return new AicpExtrasTile(mHost);
+        else if (tileSpec.equals("reboot")) return new RebootTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);

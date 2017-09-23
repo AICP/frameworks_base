eb57f8d30ab1f85bb232c9585a8c8c6f472ac137
maxwen <max.weninger@gmail.com>
2017-09-23 15:40:18 +0800

    [1/2] base: config to disable power menu on secure lock screen
    Change-Id: I6fb7b6b7d568ffe91a1616bd0ecd267d169bb7f5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7657,6 +7657,13 @@ public final class Settings {
         public static final String QS_AUTO_ADDED_TILES = "qs_auto_tiles";
 
         /**
+         * Disable power menu on secure lock screens
+         *
+         * @hide
+         */
+        public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear

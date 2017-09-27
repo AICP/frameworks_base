0322b8c57065c5b3219e1a3a3cb78fc294bf0b57
Roman Birg <roman@cyngn.com>
2017-09-27 21:01:50 +0200

    Add Compass tile
    YAOSP-NG : port to 7.1.1
    
    AICP:
    - Port to Oreo - @LorDClockAN
    - Port to Pie - @eyosen
    - Port to R - @Claymore1297
    - Port to S - @eyosen
    - Port to T - @semdoc
    - Port to U - @Claymore1297
    - Port to V - @Claymore1297
    - Port to W - @Claymore1297
    
    Change-Id: I64e7ea62e1dd7584fd45bfabfde8364ab82a4c51

--- packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
+++ packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.ProfilesTile
@@ -59,6 +60,12 @@ interface AicpModule {
     @IntoMap
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject CompassTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CompassTile.TILE_SPEC)
+    fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
@@ -106,6 +113,7 @@ interface AicpModule {
         const val AMBIENT_DISPLAY_TILE_SPEC = "ambient_display"
         const val AOD_TILE_SPEC = "aod"
         const val CAFFEINE_TILE_SPEC = "caffeine"
+        const val COMPASS_TILE_SPEC = "compass"
         const val HEADS_UP_TILE_SPEC = "heads_up"
         const val POWERSHARE_TILE_SPEC = "powershare"
         const val PROFILES_TILE_SPEC = "profiles"
@@ -157,6 +165,21 @@ interface AicpModule {
                     ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.DISPLAY,
+            )
+
+        @Provides
+        @IntoMap
+        @StringKey(COMPASS_TILE_SPEC)
+        fun provideCompassTileConfig(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(COMPASS_TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = R.drawable.ic_qs_compass_on,
+                        labelRes = R.string.quick_settings_compass_label
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.UTILITIES,
             )
 
         @Provides

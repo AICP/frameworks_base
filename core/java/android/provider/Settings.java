c5107186975c6fa7951fefa6dd7c7cf437e17e38
SpiritCroc <spiritcroc@gmail.com>
2017-09-28 15:50:54 +0200

    fwb: Add comments to AICP System settings section
    Make it easier to find the place to add new AICP System Settings
    
    Change-Id: Ia6286690dd26dc934560b2f13b491a4cb3d6171d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3918,6 +3918,10 @@ public final class Settings {
         };
 
         /**
+         * --- AICP System settings start
+         */
+
+        /**
          * Setting to determine whether or not to show the battery percentage in the status bar.
          *    0 - Don't show percentage
          *    1 - Show percentage
@@ -4047,6 +4051,11 @@ public final class Settings {
          * @hide
          */
         public static final String VOLUME_DIALOG_STROKE_DASH_GAP = "volume_dialog_stroke_dash_gap";
+
+        /**
+         * --- AICP System settings end
+         *  Add new system settings above this comment
+         */
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings

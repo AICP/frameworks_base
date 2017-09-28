e26f578c5963191225baf05b80d134665a84b78a
SpiritCroc <spiritcroc@gmail.com>
2017-09-28 14:36:02 +0200

    QS: Locale Tile
    Toggle the primary default language
    
    - Bring it to Oreo - @LorDClockaN
    - Bring it to Pie - @eyosen
    
    Change-Id: I3e336eddb360ea1a796727657652ad9301007380

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
+import com.android.systemui.qs.tiles.LocaleTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NfcTile;
@@ -216,6 +217,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new AdbOverNetworkTile(mHost);
             case "aod":
                 return new AODTile(mHost);
+            case "locale":
+                return new LocaleTile(mHost);
         }
 
         // Intent tiles.

35569fa917ed5ab727ef75adf138e94a60e9fdee
ezio84 <brabus84@gmail.com>
2017-09-28 20:47:39 +0300

    Lockscreen bottom shortcuts fixes
    - fix systemui fc
    - fix left custom shortcut always launching Dialer/Contacts
    - fix right custom shortcut breaking double tap on power for camera gesture
    - fix text hint for single tap on custom shortcuts
    - always give default camera intent to bindCameraPrewarmService to ensure
    prewarm for double tap power for camera gesture, KeyguardBottomAreaView.launchCamera
    will unbind it even for non camera right shortcuts after startActivityAsUser
    - fix power camera gesture showing a white splash screen on launch
    when right shorcut is custom
    - fix new O adaptive icons becoming black when launching a shortcut
    
    AICP: removed the final item, O adaptive icon changes
    
    Change-Id: I50b845cd100f2329c66636b82d99b633a307383b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4370,6 +4370,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         mKeyguardIndicationController.showTransientIndication(R.string.phone_hint);
     }
 
+    public void onCustomHintStarted() {
+        mKeyguardIndicationController.showTransientIndication(R.string.custom_hint);
+    }
+
     public void onTrackingStopped(boolean expand) {
         if (mState == StatusBarState.KEYGUARD || mState == StatusBarState.SHADE_LOCKED) {
             if (!expand && !mUnlockMethodCache.canSkipBouncer()) {
@@ -4638,7 +4642,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         if (!mNotificationPanel.canCameraGestureBeLaunched(
                 mStatusBarKeyguardViewManager.isShowing()
-                        && (mExpandedVisible || mBouncerShowing))) {
+                        && (mExpandedVisible || mBouncerShowing), source)) {
             if (DEBUG_CAMERA_LIFT) Slog.d(TAG, "Can't launch camera right now, mExpandedVisible: " +
                     mExpandedVisible);
             return;

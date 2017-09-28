f00f6ab53f28a2093243a5e20383d1152455dbba
BestPig <bestpig82@gmail.com>
2017-09-28 14:23:49 +0200

    QS: Add USB Tether QS Tile
    AICPfy:
    - Bring it to Oreo - @LorDClockaN
    
    - Brint it to Pie - @eyosen
    Combination of
    8d468af7945663439800ffe69e645557f1f4e061
    QS: Add USB Tether QS Tile
    
    fdd6e98198e708db4a27dea89a2f2a0be5c81744
    Refresh USB tile
    
    fa1ae5a3ae4eb9212950fe4447c49c1616ff13b6
    SystemUI: Toggle USB tethering only when USB is connected
    
    Also updated handleState and fixed tile active/inactive states
    
    Change-Id: I271881f40f6ba8e632ea96984b735832025dfc6a

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -48,6 +48,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
+import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -191,6 +192,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mScrTileProvider.get();
             case "reboot":
                 return new RebootTile(mHost);
+            case "usb_tether":
+                return new UsbTetherTile(mHost);
         }
 
         // Intent tiles.

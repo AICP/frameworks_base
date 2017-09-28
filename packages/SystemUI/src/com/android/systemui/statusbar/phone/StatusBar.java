9954f0be750cb61f8806484b07e21df58d2c7a3e
ezio84 <brabus84@gmail.com>
2017-09-28 19:47:39 +0200

    Lockscreen bottom shortcuts fixes
    - fix systemui fc
    - fix left custom shortcut always launching Dialer/Contacts
    - fix right custom shortcut breaking double tap on power for camera gesture
    - fix text hint for single tap on custom shortcuts
    - always give default camera intent to bindCameraPrewarmService to ensure
    prewarm for double tap power for camera gesture, KeyguardBottomAreaView.launchCamera
    will unbind it even for non camera right shortcuts after startActivityAsUser
    - fix power camera gesture showing a white splash screen on launch
    when right shorcut is custom
    - fix new O adaptive icons becoming black when launching a shortcut
    
    Change-Id: I3985ed790ae57ce4e1d006126a6281184468dae4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4847,6 +4847,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         mKeyguardIndicationController.showTransientIndication(R.string.phone_hint);
     }
 
+    public void onCustomHintStarted() {
+        mKeyguardIndicationController.showTransientIndication(R.string.custom_hint);
+    }
+
     public void onTrackingStopped(boolean expand) {
         if (mState == StatusBarState.KEYGUARD || mState == StatusBarState.SHADE_LOCKED) {
             if (!expand && !mUnlockMethodCache.canSkipBouncer()) {
@@ -5255,7 +5259,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             return;
         }
         if (!mNotificationPanel.canCameraGestureBeLaunched(
-                mStatusBarKeyguardViewManager.isShowing() && mExpandedVisible)) {
+                mStatusBarKeyguardViewManager.isShowing() && mExpandedVisible, source)) {
             return;
         }
         if (!mDeviceInteractive) {

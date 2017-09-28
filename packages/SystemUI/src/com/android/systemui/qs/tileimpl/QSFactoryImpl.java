a3bce145443e32d9abb0a6bfcbbb30c0bc42b2d5
Alex Cruz <du.alexcruz@gmail.com>
2017-09-28 14:19:01 +0200

    QS: Add Screenshot QS Tile
    AICPfy:
    - Bring it to Oreo - @LorDClockaN
    - Bring it to Pie - @eyosen
    
    Change-Id: I3392504d18d4c5f191f9f29c81dbe4d9db228890

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -45,6 +45,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -179,6 +180,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CompassTile(mHost);
             case "music":
                 return new MusicTile(mHost);
+            case "screenshot":
+                return new ScreenshotTile(mHost);
         }
 
         // Intent tiles.

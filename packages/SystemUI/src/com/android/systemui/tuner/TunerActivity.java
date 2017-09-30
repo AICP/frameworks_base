6288b382cfe1bf9a628b790e6837a74664de0ed5
Ralf Luther <luther.ralf@gmail.com>
2017-09-30 11:25:47 +0000

    Revert "SystemUI: Revert some Tuner changes"
    This reverts commit 50450dea0a0e5dbe21d5186fcd4028a7156eef79.
    
    Change-Id: Ib9e17e5af555b04756204804b2e79ceff70e545e

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -22,6 +22,7 @@ import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.systemui.R;
@@ -31,6 +32,8 @@ public class TunerActivity extends SettingsDrawerActivity implements
         PreferenceFragment.OnPreferenceStartScreenCallback {
 
     private static final String TAG_TUNER = "tuner";
+
+    private String mInitialTitle;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +53,13 @@ public class TunerActivity extends SettingsDrawerActivity implements
 
             getFragmentManager().beginTransaction().replace(R.id.content_frame,
                     fragment, TAG_TUNER).commit();
+
+            mInitialTitle = String.valueOf(getActionBar().getTitle());
+
+            String extra = getIntent().getStringExtra(TAG_TUNER);
+            if (extra != null) {
+                startPreferenceScreen((PreferenceFragment)fragment, extra, false);
+            }
         }
     }
 
@@ -57,7 +67,18 @@ public class TunerActivity extends SettingsDrawerActivity implements
     public void onBackPressed() {
         if (!getFragmentManager().popBackStackImmediate()) {
             super.onBackPressed();
+        } else {
+            getActionBar().setTitle(mInitialTitle);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -66,7 +87,7 @@ public class TunerActivity extends SettingsDrawerActivity implements
             Class<?> cls = Class.forName(pref.getFragment());
             Fragment fragment = (Fragment) cls.newInstance();
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
-            setTitle(pref.getTitle());
+            getActionBar().setTitle(pref.getTitle());
             transaction.replace(R.id.content_frame, fragment);
             transaction.addToBackStack("PreferenceFragment");
             transaction.commit();
@@ -77,26 +98,34 @@ public class TunerActivity extends SettingsDrawerActivity implements
         }
     }
 
-    @Override
-    public boolean onPreferenceStartScreen(PreferenceFragment caller, PreferenceScreen pref) {
+    private boolean startPreferenceScreen(PreferenceFragment caller, String key, boolean backStack) {
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         SubSettingsFragment fragment = new SubSettingsFragment();
         final Bundle b = new Bundle(1);
-        b.putString(PreferenceFragment.ARG_PREFERENCE_ROOT, pref.getKey());
+        b.putString(PreferenceFragment.ARG_PREFERENCE_ROOT, key);
         fragment.setArguments(b);
         fragment.setTargetFragment(caller, 0);
         transaction.replace(R.id.content_frame, fragment);
-        transaction.addToBackStack("PreferenceFragment");
+        if (backStack) {
+            transaction.addToBackStack("PreferenceFragment");
+        }
         transaction.commit();
 
         return true;
     }
 
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragment caller, PreferenceScreen pref) {
+        return startPreferenceScreen(caller, pref.getKey(), true);
+    }
+
     public static class SubSettingsFragment extends PreferenceFragment {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferenceScreen((PreferenceScreen) ((PreferenceFragment) getTargetFragment())
-                    .getPreferenceScreen().findPreference(rootKey));
+            PreferenceScreen p = (PreferenceScreen) ((PreferenceFragment) getTargetFragment())
+                    .getPreferenceScreen().findPreference(rootKey);
+            setPreferenceScreen(p);
+            getActivity().getActionBar().setTitle(p.getTitle());
         }
     }
 }

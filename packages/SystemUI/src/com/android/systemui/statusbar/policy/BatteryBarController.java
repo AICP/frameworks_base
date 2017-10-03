f3f54627e696cf668b32ce212e87cd9479d3cbc0
tobitege <tobiasteschner@googlemail.com>
2017-10-03 20:06:32 +0200

    Have Battery bar controller obey Hide value
    Change-Id: I49bd20af7607e63a867a72c7693bfbdc7787c28c

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java
@@ -205,7 +205,7 @@ public class BatteryBarController extends LinearLayout {
             mLocation = 0;
         }
 
-        if (isLocationValid(mLocation)) {
+        if (mLocation > 0 && isLocationValid(mLocation)) {
             removeBars();
             addBars();
             // Visibility handled externally

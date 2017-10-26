0f207dcc6d7d9e385b230c0f64270294c74c3bfc
ezio84 <brabus84@gmail.com>
2017-10-26 15:52:52 +0200

    Stop "ticking" when entering doze/ambient display
    * There's no need to tick anymore in this state
      also don't tick if already in Ambient display
      (tick notification wouldn't be shown anyway but
      let's avoid any useless tick operation)
    
    Change-Id: If741e9498768c8c4649a357d3ac3b05126d9a8c8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3467,7 +3467,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mTicker == null || mTickerEnabled == 0) return;
 
         // no ticking on keyguard, we have carrier name in the statusbar
-        if (isKeyguardShowing()) return;
+        if (isKeyguardShowing() || isDozing()) return;
 
         // no ticking in lights-out mode
         if (!areLightsOn()) return;
@@ -4655,6 +4655,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         updateQsExpansionEnabled();
         mDozeScrimController.setDozing(mDozing, animate);
         updateRowStates();
+        if (isDozing()) {
+            haltTicker();
+        }
         Trace.endSection();
     }
 

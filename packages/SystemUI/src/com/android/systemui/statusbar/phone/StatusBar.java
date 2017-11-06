2e118fa7d1938bf471fa28c80899a2e588d98979
LorDClockaN <lordclockan@gmail.com>
2017-11-06 16:17:50 -0500

    disable the notification ticker by default
    Change-Id: I73324329c7004003ea4b96f9d75bb33b67b8e839

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5677,7 +5677,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private void updateTickerSettings() {
         mTickerEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_SHOW_TICKER, 1,
+                Settings.System.STATUS_BAR_SHOW_TICKER, 0,
                 UserHandle.USER_CURRENT);
     }
 

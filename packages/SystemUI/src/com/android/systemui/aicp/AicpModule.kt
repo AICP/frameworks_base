51e5876a20338f039154bf753ab7e49faa2ec082
mydongistiny <jaysonedson@gmail.com>
2017-12-23 22:46:44 -0800

    QS: Add CPUInfo toggle tile
    @neobuddy89: Adapted tile for A13, A14.
    
    Change-Id: I84fcdff1c29c0bcd3f85d60b4ccba25288ebbddd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
+++ packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile;
+import com.android.systemui.qs.tiles.CPUInfoTile;
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.MusicTile
 import com.android.systemui.qs.tiles.OnTheGoTile;
@@ -77,6 +78,12 @@ interface AicpModule {
     @IntoMap
     @StringKey(CompassTile.TILE_SPEC)
     fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
+
+    /** Inject CPUInfoTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CPUInfoTile.TILE_SPEC)
+    fun bindCPUInfoTile(cpuinfoTile: CPUInfoTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds

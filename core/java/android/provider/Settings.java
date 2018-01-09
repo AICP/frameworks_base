8b2f05e62d0e12a086ecfe798b42fb87ae273ab7
beanstown106 <nbenis106@gmail.com>
2018-01-09 18:38:59 +0100

    Fingerprint authentication vibration [1/2]
    AICP:
    - Bring it to o8.1 by @LorDClockaN
    
    Change-Id: I7fc6f7fff7002b39645996bc6791e3a06d501247

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4614,6 +4614,12 @@ public final class Settings {
         public static final String SCREEN_STATE_ON_DELAY = "screen_state_on_delay";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
          * --- AICP System settings end
          *  Add new system settings above this comment
          */

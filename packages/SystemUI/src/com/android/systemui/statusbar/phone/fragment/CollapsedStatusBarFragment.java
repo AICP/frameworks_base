139c23f79f4e5454e0fb0105c04094f02460806b
SpiritCroc <spiritcroc@gmail.com>
2018-01-13 12:02:18 +0100

    Make batterybar play together with other statusbar visibilities
    This fixes battery bar showing in Always On Display.
    
    Change-Id: I3ae2a7453763698ff6344baf1aa8fc7ea8a42cef

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -173,6 +173,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private List<String> mBlockedIcons = new ArrayList<>();
     private Map<Startable, Startable.State> mStartableStates = new ArrayMap<>();
 
+    // AICP additions
+    private View mBatteryBar;
+
     private final OngoingCallListener mOngoingCallListener = new OngoingCallListener() {
         @Override
         public void onOngoingCallStateChanged(boolean animate) {
@@ -408,6 +411,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             showEndSideContent(false);
             showClock(false);
         }
+        mBatteryBar = mStatusBar.findViewById(R.id.battery_bar);
         initOperatorName();
         initNotificationIconArea();
         mSystemEventAnimator = getSystemEventAnimator();
@@ -826,6 +830,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         } else {
             mEndSideAlphaController.animateToAlpha(/*alpha*/ 0f, SOURCE_OTHER, FADE_OUT_DURATION,
                     InterpolatorsAndroidX.ALPHA_OUT, /*startDelay*/ 0);
+            animateHide(mBatteryBar, animate);
         }
     }
 
@@ -834,6 +839,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if (!animate || !mAnimationsEnabled) {
             mEndSideAlphaController.setAlpha(1f, SOURCE_OTHER);
             return;
+        } else {
+            animateShow(mBatteryBar, animate);
         }
         if (mKeyguardStateController.isKeyguardFadingAway()) {
             mEndSideAlphaController.animateToAlpha(/*alpha*/ 1f, SOURCE_OTHER,

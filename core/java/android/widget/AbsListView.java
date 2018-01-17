292bf74b53724e9f2d3c47f98abb4696af08c067
Jun Wang <junwan@codeaurora.org>
2018-01-17 17:54:43 +0800

    Optimize AbsListView to reduce click operation latency
    Separate performClick from delayed message. Then we can do
    performClick in advance of PRESSED_STATE_DURATION(64ms) delay.
    This will benefit for click operation latency. The delayed
    message is still used to clear pressed status.
    
    CRs-Fixed: 2166045
    Change-Id: I6748a6cfaf997cc0e6f6ad35a76d68ae608660ec

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4141,6 +4141,10 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                                     }
                                     mSelector.setHotspot(x, ev.getY());
                                 }
+                                if (!mDataChanged && !mIsDetaching
+                                        && isAttachedToWindow()) {
+                                    performClick.run();
+                                }
                                 if (mTouchModeReset != null) {
                                     removeCallbacks(mTouchModeReset);
                                 }
@@ -4151,10 +4155,6 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                                         mTouchMode = TOUCH_MODE_REST;
                                         child.setPressed(false);
                                         setPressed(false);
-                                        if (!mDataChanged && !mIsDetaching
-                                                && isAttachedToWindow()) {
-                                            performClick.run();
-                                        }
                                     }
                                 };
                                 postDelayed(mTouchModeReset,

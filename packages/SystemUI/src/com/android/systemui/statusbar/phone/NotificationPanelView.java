c3fe4efd9f2b4a2fa4acfd362f9e627d4ffa5cbb
maxwen <max.weninger@gmail.com>
2018-01-21 02:06:31 +0100

    SystemUI: Prevent dummy expansion of status bar
    qs panel gets expanded on double-tap on status bar before screen is
    turned off. So stop all events once a double-tap is detected.
    
    Change-Id: Idd75ae5a9abefecb2b98c9020a34d3065e645018

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1265,7 +1265,9 @@ public class NotificationPanelView extends PanelView implements
         if (!mQsExpanded
                 && mDoubleTapToSleepEnabled
                 && event.getY() < mStatusBarHeaderHeight) {
-            mDoubleTapGesture.onTouchEvent(event);
+            if (mDoubleTapGesture.onTouchEvent(event)) {
+                return false;
+            }
         }
         if (mIsLockscreenDoubleTapEnabled
                 && mBarState == StatusBarState.KEYGUARD) {

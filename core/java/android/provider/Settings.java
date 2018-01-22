9a0e011203954c2f41823dabbaeba60070156733
maxwen <max.weninger@gmail.com>
2018-01-22 04:12:47 +0100

    [1/2] base: custom header image provider
    reads image uri and saves it in the file
    in /data/user_de/0/com.android.systemui/files/custom_file_header_image
    the file must be copied else loading on reboot will fail cause storage
    may not be mounted
    
    Change-Id: I95f7d86f15cf00bb7dfecc3324c00f28e987d013

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4686,6 +4686,10 @@ public final class Settings {
          */
         public static final String STATUS_BAR_CUSTOM_HEADER_IMAGE = "status_bar_custom_header_image";
 
+        /**
+        * @hide
+        */
+       public static final String STATUS_BAR_FILE_HEADER_IMAGE = "status_bar_file_header_image";
 
         /**
          * --- AICP System settings end

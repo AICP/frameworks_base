3b27727d22cf78dc773af4aaa2399ba24df09d52
Michael Bestas <mkbestas@lineageos.org>
2018-01-23 23:03:12 +0200

    base: DeivceUtils: Add deviceSupportsFlashLight util
    Change-Id: I085c435181bbfe961029dca72e060363809c1ce0

--- core/java/com/android/internal/util/aicp/DeviceUtils.java
+++ core/java/com/android/internal/util/aicp/DeviceUtils.java
@@ -124,4 +124,26 @@ public class DeviceUtils {
         String voldState = SystemProperties.get("vold.decrypt");
         return "1".equals(voldState) || "trigger_restart_min_framework".equals(voldState);
     }
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException | AssertionError e) {
+            // Ignore
+        }
+        return false;
+    }
 }

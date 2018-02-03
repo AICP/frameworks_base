d5d49e111efdd05ee28e71521aaf5b20cdd2b7e1
Ethan Chen <intervigil@gmail.com>
2018-02-03 14:02:24 -0800

    Revert "framework/base: fix multithread synchronization"
    This reverts commit c18f02b66499174b2ddd668e9c1219ec41da14c7.
    
    Change-Id: I9414ace898a2237c95ad0506db4f2945ec7151c2

--- core/java/android/content/pm/PackageParser.java
+++ core/java/android/content/pm/PackageParser.java
@@ -1692,21 +1692,19 @@ public class PackageParser {
                             }
                             final Signature[] entrySignatures = convertToSignatures(entryCerts);
 
-                            synchronized (sObjWaitAll) {
-                                if (pkg.mCertificates == null) {
-                                    pkg.mCertificates = entryCerts;
-                                    pkg.mSignatures = entrySignatures;
-                                    pkg.mSigningKeys = new ArraySet<PublicKey>();
-                                    for (int i=0; i < entryCerts.length; i++) {
-                                        pkg.mSigningKeys.add(entryCerts[i][0].getPublicKey());
-                                    }
-                                } else {
-                                    if (!Signature.areExactMatch(pkg.mSignatures, entrySignatures)) {
-                                        throw new PackageParserException(
-                                                INSTALL_PARSE_FAILED_INCONSISTENT_CERTIFICATES, "Package " + apkPath
-                                                + " has mismatched certificates at entry "
-                                                + entry.getName());
-                                    }
+                            if (pkg.mCertificates == null) {
+                                pkg.mCertificates = entryCerts;
+                                pkg.mSignatures = entrySignatures;
+                                pkg.mSigningKeys = new ArraySet<PublicKey>();
+                                for (int i=0; i < entryCerts.length; i++) {
+                                    pkg.mSigningKeys.add(entryCerts[i][0].getPublicKey());
+                                }
+                            } else {
+                                if (!Signature.areExactMatch(pkg.mSignatures, entrySignatures)) {
+                                    throw new PackageParserException(
+                                            INSTALL_PARSE_FAILED_INCONSISTENT_CERTIFICATES, "Package " + apkPath
+                                            + " has mismatched certificates at entry "
+                                            + entry.getName());
                                 }
                             }
                         } catch (GeneralSecurityException e) {

556854e300050c54f5c4935b04eb359159f1dbe7
ezio84 <brabus84@gmail.com>
2018-02-11 01:56:44 +0100

    Fix notification sound for some apps if Smart notification sound enabled
    Change-Id: Ie8a87f0c7b496e423cad25b76b9d3ca7c288a305

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -340,6 +340,7 @@ public class NotificationManagerService extends SystemService {
     // The last key in this list owns the hardware.
     ArrayList<String> mLights = new ArrayList<>();
 
+    @GuardedBy("mNotificationLock")
     private HashMap<String, Long> mAnnoyingNotifications = new HashMap<String, Long>();
     private long mAnnoyingNotificationThreshold = -1;
 
@@ -4002,22 +4003,23 @@ public class NotificationManagerService extends SystemService {
         }
     }
 
-    private boolean notificationIsAnnoying(String pkg) {
-        if (pkg == null
+    @GuardedBy("mNotificationLock")
+    private boolean notificationIsAnnoying(String key, String pkg) {
+        if (key == null
                 || mAnnoyingNotificationThreshold <= 0
-                || "android".equals(pkg)) {
+                || (pkg != null && "android".equals(pkg))) {
             return false;
         }
 
         long currentTime = System.currentTimeMillis();
-        if (mAnnoyingNotifications.containsKey(pkg)
-                && (currentTime - mAnnoyingNotifications.get(pkg)
+        if (mAnnoyingNotifications.containsKey(key)
+                && (currentTime - mAnnoyingNotifications.get(key)
                 < mAnnoyingNotificationThreshold)) {
             // less than threshold; it's an annoying notification!!
             return true;
         } else {
             // not in map or time to re-add
-            mAnnoyingNotifications.put(pkg, currentTime);
+            mAnnoyingNotifications.put(key, currentTime);
             return false;
         }
     }
@@ -4118,11 +4120,12 @@ public class NotificationManagerService extends SystemService {
         }
 
         if (aboveThreshold && isNotificationForCurrentUser(record)) {
-
-            boolean beNoisy = !mScreenOn
-                    || (mScreenOn && mSoundVibScreenOn == 1)
-                    || (mScreenOn && mSoundVibScreenOn == 2 && !mIsMediaPlaying);
-            if (mSystemReady && mAudioManager != null && beNoisy && !notificationIsAnnoying(pkg)) {
+            boolean notificationIsAnnoying = notificationIsAnnoying(key, pkg);
+            boolean beNoisy = (!mScreenOn && !notificationIsAnnoying)
+                    // if mScreenOn && mSoundVibScreenOn == 0 never be noisy
+                    || (mScreenOn && mSoundVibScreenOn == 1 && !notificationIsAnnoying)
+                    || (mScreenOn && mSoundVibScreenOn == 2 && !mIsMediaPlaying && !notificationIsAnnoying);
+            if (mSystemReady && mAudioManager != null && beNoisy) {
                 Uri soundUri = record.getSound();
                 hasValidSound = soundUri != null && !Uri.EMPTY.equals(soundUri);
                 long[] vibration = record.getVibration();

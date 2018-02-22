0f119de50a3bc5b85567d5a7bb71926d78e2192e
mosimchah <mosimchah@gmail.com>
2018-02-22 03:44:27 -0500

    Revert "Reimplement hardware keys custom rebinding"
    This reverts commit 9a5ffb1a1b1666be17fc6ff80bceb4727d0e551d.
    
    Change-Id: I7f11601c3da594a55d402edcd8f65748bb1bda42

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
@@ -509,7 +509,6 @@ public class NavigationBarFragment extends Fragment implements Callbacks, Naviga
         ButtonDispatcher homeButton = mNavigationBarView.getHomeButton();
         if (homeButton != null) {
             homeButton.setOnTouchListener(this::onHomeTouch);
-            homeButton.setLongClickable(true);
             homeButton.setOnLongClickListener(this::onHomeLongClick);
         }
 
@@ -567,9 +566,15 @@ public class NavigationBarFragment extends Fragment implements Callbacks, Naviga
         if (!isUsingStockNav()) {
             return false;
         }
-        KeyButtonView keyButtonView = (KeyButtonView) v;
-        keyButtonView.sendEvent(KeyEvent.ACTION_DOWN, KeyEvent.FLAG_LONG_PRESS);
-        keyButtonView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
+        if (shouldDisableNavbarGestures()) {
+            return false;
+        }
+        MetricsLogger.action(getContext(), MetricsEvent.ACTION_ASSIST_LONG_PRESS);
+        mAssistManager.startAssist(new Bundle() /* args */);
+        mStatusBar.awakenDreams();
+        if (mNavigationBarView != null) {
+            mNavigationBarView.abortCurrentGesture();
+        }
         return true;
     }
 

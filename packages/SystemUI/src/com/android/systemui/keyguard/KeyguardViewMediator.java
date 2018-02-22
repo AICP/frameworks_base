a5f7a14e592ef520010fb85b360e83434484991f
mosimchah <mosimchah@gmail.com>
2018-02-21 20:50:22 -0500

    Revert "Keyguard: Don't apply input restriction if disabled by profile"
    This reverts commit 7a68cee762bf2001c2c89e06f352130335090a18.
    
    Change-Id: Ia2b6f2e74d54e4c090fba023373b0679e3e2d92d

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1007,12 +1007,6 @@ public class KeyguardViewMediator extends SystemUI {
         }
     }
 
-    private boolean isProfileDisablingKeyguard() {
-        Profile profile = mProfileManager.getActiveProfile();
-        return profile != null &&
-                profile.getScreenLockMode().getValue() == Profile.LockMode.DISABLE;
-    }
-
     private boolean isKeyguardDisabled(int userId) {
         if (!mExternallyEnabled) {
             if (DEBUG) Log.d(TAG, "isKeyguardDisabled: keyguard is disabled externally");
@@ -1022,9 +1016,12 @@ public class KeyguardViewMediator extends SystemUI {
             if (DEBUG) Log.d(TAG, "isKeyguardDisabled: keyguard is disabled by setting");
             return true;
         }
-        if (isProfileDisablingKeyguard()) {
-            if (DEBUG) Log.d(TAG, "isKeyguardDisabled: keyguard is disabled by profile");
-            return true;
+        Profile profile = mProfileManager.getActiveProfile();
+        if (profile != null) {
+            if (profile.getScreenLockMode().getValue() == Profile.LockMode.DISABLE) {
+                if (DEBUG) Log.d(TAG, "isKeyguardDisabled: keyguard is disabled by profile");
+                return true;
+            }
         }
         return false;
     }
@@ -1234,7 +1231,7 @@ public class KeyguardViewMediator extends SystemUI {
      * was suppressed by an app that disabled the keyguard or we haven't been provisioned yet.
      */
     public boolean isInputRestricted() {
-        return (mShowing || mNeedToReshowWhenReenabled) && !isProfileDisablingKeyguard();
+        return mShowing || mNeedToReshowWhenReenabled;
     }
 
     private void updateInputRestricted() {

63a51b8bb53dd6800b4e24be2337646582d45061
mosimchah <mosimchah@gmail.com>
2018-02-23 08:25:36 -0500

    Revert "base: Introduce Accidental Touch [1/2]"
    This reverts commit cab4272b6065898973f5294737189beec03dddd1.
    
    Change-Id: Ieebceba9035c15e339840f5aec976684f2a1379d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4701,13 +4701,6 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
-        * Indicates whether ANBI (Accidental navigation button interaction) is enabled.
-        *
-        * @hide
-        */
-        public static final String ANBI_ENABLED = "anbi_enabled";
-
-        /**
         * Whether to mute annoying notifications
         * @hide
         */
--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -461,10 +461,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     BurnInProtectionHelper mBurnInProtectionHelper;
     AppOpsManager mAppOpsManager;
     AlarmManager mAlarmManager;
-    ANBIHandler mANBIHandler;
-
-    private boolean mANBIEnabled;
-
     private boolean mHasFeatureWatch;
     private boolean mHasFeatureLeanback;
 
@@ -1204,9 +1200,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.Secure.getUriFor(
                     LineageSettings.Secure.KILL_APP_LONGPRESS_BACK), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.ANBI_ENABLED), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -2879,20 +2872,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             MSG_ENABLE_POINTER_LOCATION : MSG_DISABLE_POINTER_LOCATION);
                 }
             }
-
-            final boolean ANBIEnabled = Settings.System.getIntForUser(resolver,
-                    Settings.System.ANBI_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-            if (mANBIHandler != null) {
-                if (mANBIEnabled != ANBIEnabled) {
-                    mANBIEnabled = ANBIEnabled;
-                    if (mANBIEnabled) {
-                        mWindowManagerFuncs.registerPointerEventListener(mANBIHandler);
-                    } else {
-                        mWindowManagerFuncs.unregisterPointerEventListener(mANBIHandler);
-                    }
-                }
-            }
-
             // use screen off timeout setting as the timeout for the lockscreen
             mLockScreenTimeout = Settings.System.getIntForUser(resolver,
                     Settings.System.SCREEN_OFF_TIMEOUT, 0, UserHandle.USER_CURRENT);
@@ -6828,12 +6807,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         final boolean isInjected = (policyFlags & WindowManagerPolicy.FLAG_INJECTED) != 0;
 
-        final boolean appSwitchKey = keyCode == KeyEvent.KEYCODE_APP_SWITCH;
-        final boolean homeKey = keyCode == KeyEvent.KEYCODE_HOME;
-        final boolean menuKey = keyCode == KeyEvent.KEYCODE_MENU;
-        final boolean backKey = keyCode == KeyEvent.KEYCODE_BACK;
-//        final boolean navBarKey = event.getSource() == InputDevice.SOURCE_NAVIGATION_BAR;
-
         // If screen is off then we treat the case where the keyguard is open but hidden
         // the same as if it were open and in front.
         // This will prevent any keys other than the power button from waking the screen
@@ -6847,12 +6820,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             Log.d(TAG, "interceptKeyTq keycode=" + keyCode
                     + " interactive=" + interactive + " keyguardActive=" + keyguardActive
                     + " policyFlags=" + Integer.toHexString(policyFlags));
-        }
-
-        if (mANBIHandler != null && mANBIEnabled && mANBIHandler.isScreenTouched()
-/*                && !navBarKey */
-                && (appSwitchKey || homeKey || menuKey || backKey)) {
-            return 0;
         }
 
         // Basic policy based on interactive state.
@@ -8476,10 +8443,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // Ensure observe happens in systemReady() since we need
         // LineageHardwareService to be up and running
         mSettingsObserver.observe();
-
-        if (mDeviceHardwareKeys > 0) {
-            mANBIHandler = new ANBIHandler(mContext);
-        }
 
         readCameraLensCoverState();
         updateUiMode();

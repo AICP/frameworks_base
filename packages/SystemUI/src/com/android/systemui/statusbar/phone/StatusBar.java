0585dfcfbc858e2fef711bb1e440d810bd3134ce
ezio84 <brabus84@gmail.com>
2018-03-07 12:31:44 +0100

    Stock Recents: use Slim Recents icons management [1/2]
    Change-Id: Id5fd224cc21977edc0eed4c27283164324f94145

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -216,7 +216,7 @@ import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
 import com.android.systemui.recents.events.activity.AppTransitionFinishedEvent;
 import com.android.systemui.recents.events.activity.UndockingTaskEvent;
-import com.android.systemui.recents.misc.IconPackHelper;
+import com.android.systemui.slimrecent.icons.IconsHandler;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.slimrecent.RecentController;
 import com.android.systemui.stackdivider.Divider;
@@ -6611,7 +6611,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateFPQuickPulldown();
             updateBatterySaverColor();
             updateTickerAnimation();
-            updateRecentsIconPack();
 
             // Update slim recents
             updateRecentsMode();
@@ -6701,7 +6700,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private void updateRecentsIconPack() {
         String currentIconPack = Settings.System.getStringForUser(mContext.getContentResolver(),
             Settings.System.RECENTS_ICON_PACK, mCurrentUserId);
-        IconPackHelper.getInstance(mContext).updatePrefs(currentIconPack);
+        IconsHandler.getInstance(mContext).updatePrefs(currentIconPack);
         mRecents.evictAllCaches();
     }
 
@@ -8512,6 +8511,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mSlimRecents = null;
             }
         }
+
+        IconsHandler.getInstance(mContext).resetIconNormalizer();
+        updateRecentsIconPack();
     }
 
     private void rebuildRecentsScreen() {

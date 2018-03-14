bdb89a9cf35a6a21158bd5d6a249c4142ac8c38f
beanstown106 <nbenis106@gmail.com>
2018-03-14 19:11:39 +0300

    Long-press power while display is off for torch
        Squash of:
    
        Author: beanstown106 <nbenis106@gmail.com>
        Date:   Sun Jan 17 09:14:19 2016 -0500
            policy: Long-press power while display is off for torch
            Long-press the power button while the display is off
            to turn the torchlight on and off.
            Credits:
             - Lion0738: The main hooks here:
               https://github.com/lion0738/android_frameworks_base/commit/9af2b7844a4d973c8b6c542d7937f56a24a7e5f1
             - Atlantis: The logic on where to hook into this for power button only
             - Alex Cruz: Helping and giving me some pointers along the way
            Change-Id: I14365389990eb06daaa127f5db66df45abf6c064
            (cherry picked from commit 604f00ce176463bc3e60b99195fc7a757f49f957)
    
        Author: Sam Mortimer <sam@mortimer.me.uk>
        Date:   Sat Dec 24 13:22:53 2016 -0800
            [1/3] Torch long press power: add auto-off function
            Change-Id: Icdf50082324f8292859f0df8b271e730b02c84e7
    
    Change-Id: I49222892c8fbc8a63af580c763e8987b225b11d3

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -96,5 +96,7 @@ public class SystemSettings {
         Settings.System.QQS_SHOW_BRIGHTNESS,
         Settings.System.SHOW_AUTO_BRIGHTNESS_BUTTON,
         Settings.System.BRIGHTNESS_SLIDER_POSITION,
+        Settings.System.TORCH_LONG_PRESS_POWER_GESTURE,
+        Settings.System.TORCH_LONG_PRESS_POWER_TIMEOUT,
     };
 }

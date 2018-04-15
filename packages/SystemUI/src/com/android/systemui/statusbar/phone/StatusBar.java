dc4af51798344efb92c0aac35af70e5f20e7c353
ezio84 <brabus84@gmail.com>
2018-04-15 11:02:35 +0200

    Ambient and statusbar media tickers: code fixes and improvements
    - fix glitches (text disappearing then showing again) on track changes
    when ambient ticker is already showing
    - skip useless visibility calls
    - be sure to get the latest updated track infos after
    StatusBar.onAsyncInflationFinished() is finished for the
    media notification
    - be sure to remove track infos if the media notification is removed
    - cleanup code
    
    Change-Id: I888779951a1c9634b20da3dde1c6663051b8b197

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -729,6 +729,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mNavigationBar.setMediaPlaying(true);
             }
         } else {
+            mEntryToRefresh = null;
             if (isAmbientContainerAvailable()) {
                 ((AmbientIndicationContainer)mAmbientIndicationContainer).hideIndication();
             }
@@ -752,15 +753,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         for (int i = 0; i < N; i++) {
             final Entry entry = activeNotifications.get(i);
             if (entry.notification.getPackageName().equals(pkg)) {
-                if (mTickerEnabled == 2) {
-                    tick(entry.notification, true, true, mMediaMetadata);
-                }
-                if (isAmbientContainerAvailable()) {
-                    ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata);
-                }
-                if (mSlimRecents != null) {
-                    mSlimRecents.setMediaInfo(mMediaMetadata);
-                }
                 // NotificationInflater calls async MediaNotificationProcessoron to create notification
                 // colors and when finished will trigger AsyncInflationFinished for all registered callbacks
                 // like StatusBar. From there we'll send updated colors to Pulse
@@ -2016,6 +2008,15 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         if (mEntryToRefresh == entry) {
             final Notification n = entry.notification.getNotification();
+            if (mTickerEnabled == 2) {
+                tick(entry.notification, true, true, mMediaMetadata);
+            }
+            if (isAmbientContainerAvailable()) {
+                ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata);
+            }
+            if (mSlimRecents != null) {
+                mSlimRecents.setMediaInfo(mMediaMetadata);
+            }
             final int[] colors = {n.backgroundColor, n.foregroundColor,
                     n.primaryTextColor, n.secondaryTextColor};
             if (mNavigationBar != null) {
@@ -2710,9 +2711,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                         + mMediaController.getPackageName());
             }
             mMediaController.unregisterCallback(mMediaListener);
-            setMediaPlaying();
         }
         mMediaController = null;
+        setMediaPlaying();
     }
 
     private boolean sameSessions(MediaController a, MediaController b) {
@@ -6266,7 +6267,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     }
                     setCleanLayout(mAmbientMediaPlaying == 3 ? reason : -1);
                     if (isAmbientContainerAvailable()) {
-                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setTickerMarquee(true);
+                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setPulsing(true);
                     }
                 }
 
@@ -6276,7 +6277,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     setPulsing(null);
                     setCleanLayout(-1);
                     if (isAmbientContainerAvailable()) {
-                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setTickerMarquee(false);
+                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setPulsing(false);
                     }
                 }
 

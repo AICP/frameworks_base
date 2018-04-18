e399036444a185b7fd5b4b1e0bd34165612f625b
Ali B <abittin@gmail.com>
2018-04-18 23:06:35 +0300

    SystemUI: Ticker: play nice with center clock - Part deux
    It's the clock itself dummy
    
    Change-Id: Ieff8d1d46658c092fe9ca4e0d009c06993cd988c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -498,7 +498,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     // viewgroup containing the normal contents of the statusbar
     LinearLayout mStatusBarContent;
     // Other views that need hiding for the notification ticker
-    View mCenterClockLayout;
+    View mCenterClock;
 
     // expanded notifications
     protected NotificationPanelView mNotificationPanel; // the sliding/resizing panel within the notification window
@@ -1225,7 +1225,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mStatusBarView.setScrimController(mScrimController);
                     mStatusBarView.setBouncerShowing(mBouncerShowing);
                     mStatusBarContent = (LinearLayout) mStatusBarView.findViewById(R.id.status_bar_contents);
-                    mCenterClockLayout = mStatusBarView.findViewById(R.id.center_clock_layout);
+                    mCenterClock = mStatusBarView.findViewById(R.id.center_clock);
                     setAreThereNotifications();
                     checkBarModes();
                 }).getFragmentManager()
@@ -4010,8 +4010,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
             mStatusBarContent.setVisibility(View.GONE);
             mStatusBarContent.startAnimation(outAnim);
-            mCenterClockLayout.setVisibility(View.GONE);
-            mCenterClockLayout.startAnimation(outAnim);
+            mCenterClock.setVisibility(View.GONE);
+            mCenterClock.startAnimation(outAnim);
             if (mTickerView != null) {
                 mTickerView.setVisibility(View.VISIBLE);
                 mTickerView.startAnimation(inAnim);
@@ -4030,8 +4030,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
             mStatusBarContent.setVisibility(View.VISIBLE);
             mStatusBarContent.startAnimation(inAnim);
-            mCenterClockLayout.setVisibility(View.VISIBLE);
-            mCenterClockLayout.startAnimation(inAnim);
+            mCenterClock.setVisibility(View.VISIBLE);
+            mCenterClock.startAnimation(inAnim);
             if (mTickerView != null) {
                 mTickerView.setVisibility(View.GONE);
                 mTickerView.startAnimation(outAnim);
@@ -4043,8 +4043,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             if (mStatusBarContent.getVisibility() != View.VISIBLE) {
                 mStatusBarContent.setVisibility(View.VISIBLE);
                 mStatusBarContent.startAnimation(loadAnim(false, null));
-                mCenterClockLayout.setVisibility(View.VISIBLE);
-                mCenterClockLayout.startAnimation(loadAnim(false, null));
+                mCenterClock.setVisibility(View.VISIBLE);
+                mCenterClock.startAnimation(loadAnim(false, null));
             }
             if (mTickerView != null) {
                 mTickerView.setVisibility(View.GONE);

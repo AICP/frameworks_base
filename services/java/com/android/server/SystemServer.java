6d5a91ea8275ebd176857223e7f2079025ec841a
Adin Kwok <adin.kwok@carbonrom.org>
2018-04-18 01:05:27 -0700

    Smart Pixels: Switch to registered receiver
    Switching to a registered receiver allows to properly handle updates
    on enabling of battery saver mode and switching of users.
    
    Also only update screen filter with burn-in protection when the
    device is in an interactive state.
    
    Test: Service starts after rebooting with it enabled
          Service starts on battery saver mode (user toggle)
          Service starts on battery saver mode (auto-enabled)
          Service re-adjusts on user switch to current user settings
          Filter updates after selected timeout
    
    Change-Id: Iced17fd5cc49e0163754bf75782f8465b54e859b
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -108,6 +108,7 @@ import com.android.server.power.ShutdownThread;
 import com.android.server.restrictions.RestrictionsManagerService;
 import com.android.server.security.KeyAttestationApplicationIdProviderService;
 import com.android.server.security.KeyChainSystemService;
+import com.android.server.smartpixels.SmartPixelsReceiver;
 import com.android.server.soundtrigger.SoundTriggerService;
 import com.android.server.stats.StatsCompanionService;
 import com.android.server.statusbar.StatusBarManagerService;
@@ -264,6 +265,7 @@ public final class SystemServer {
     private PackageManager mPackageManager;
     private ContentResolver mContentResolver;
     private EntropyMixer mEntropyMixer;
+    private SmartPixelsReceiver mSmartPixelsReceiver;
 
     private boolean mOnlyCore;
     private boolean mFirstBoot;
@@ -1935,6 +1937,7 @@ public final class SystemServer {
                 reportWtf("Notifying incident daemon running", e);
             }
             traceEnd();
+            mSmartPixelsReceiver = new SmartPixelsReceiver(context);
         }, BOOT_TIMINGS_TRACE_LOG);
     }
 

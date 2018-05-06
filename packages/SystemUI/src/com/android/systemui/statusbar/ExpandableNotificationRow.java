936946829ec63eb5e23c46cd34886435f1274283
dongwan0605.kim <dongwan0605.kim@lge.com>
2018-05-06 17:40:23 +0900

    Fix intercepting touch events for guts
    When guts are showing in keyguard, user's touch events
    could be intercepted by ActivatableNotificationView
    especially in notification header area.
    And then, user had to touch buttons in guts twice to operate.
    So, fix not to intercept touch events when guts are showing.
    
    Test: Lockscreen > Swipe EXPANDABLE notification and
    click snooze button > Try to click UNDO button or
    expand button in snooze layout.
    
    Change-Id: I7f36ca8f38c974561ea7585b4560fff8c2d634fb

--- packages/SystemUI/src/com/android/systemui/statusbar/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/ExpandableNotificationRow.java
@@ -2181,6 +2181,9 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
 
     @Override
     protected boolean disallowSingleClick(MotionEvent event) {
+        if (areGutsExposed()) {
+            return false;
+        }
         float x = event.getX();
         float y = event.getY();
         NotificationHeaderView header = getVisibleNotificationHeader();

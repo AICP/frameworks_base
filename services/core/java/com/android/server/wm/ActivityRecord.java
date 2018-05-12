38fdeeaf8752e1b1527ca5f7b1bbffdfcf9102be
Jesse Chan <jc@lineageos.org>
2018-05-12 00:14:26 -0500

    Add an option to force pre-O apps to use full screen aspect ratio
    When an app target pre-O releases, the default max aspect ratio
    is 1.86:1 which leads to ugly black areas on devices that have
    screens with higher aspect ratio (for example Galaxy S8/S9).
    
    This change adds an option to allow users to change default
    aspect ratio for pre-O apps to 2.1 which would fit recent devices.
    
    Wight554:
    Simplify the code, we don't need the Settings for that, just bool
    
    Change-Id: I88035fb079bad9a66b36fe5e861017af568b8f4c

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -785,6 +785,10 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
                 }
             });
 
+    // Full screen aspect ratio
+    private final float mFullScreenAspectRatio = Resources.getSystem().getFloat(
+                    com.android.internal.R.dimen.config_screenAspectRatio);
+
     /**
      * Current sequencing integer of the configuration, for skipping old activity configurations.
      */
@@ -7829,7 +7833,9 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
     // TODO(b/36505427): Consider moving this method and similar ones to ConfigurationContainer.
     private boolean applyAspectRatio(Rect outBounds, Rect containingAppBounds,
             Rect containingBounds, float desiredAspectRatio, boolean fixedOrientationLetterboxed) {
-        final float maxAspectRatio = info.getMaxAspectRatio();
+        final boolean higherAspectRatio = Resources.getSystem().getBoolean(
+                com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+        final float maxAspectRatio = higherAspectRatio ? mFullScreenAspectRatio : info.getMaxAspectRatio();
         final Task rootTask = getRootTask();
         final float minAspectRatio = info.getMinAspectRatio();
 

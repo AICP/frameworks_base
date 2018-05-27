403865dbe5aa6217dc0af4692c68ff7b52c91c65
Adarsh-MR <adarshmr1998@gmail.com>
2018-05-27 08:04:03 +0000

    Themes: Expose QS tiles and label colors
    * also exposed expand indicator and underline colors
    
    * also disabling grayscale tint if config_enable_qs_tile_tinting enabled.
      because switching tiles state (enable/disable) will apply grayscale tint and removes
      themed tint color,  big thanks to Gurpreet ( SNN Infinity) for headsup on this.
      this bool also prevents black on black issue if a themer doesn't change tiles tint colors.
    
    * forward-port and adapt to Android Pie
    * Android Pie changes:
      Added resource for QS secondary label, qs tile backgrounds.
      Updated exposed colors with Android Pie changes.
    
    * Android Q changes [SpiritCroc]:
      Disable qs_tiles_label_color, exposure not needed anymore.
      Move all exposed colors to res/colors to allow following attrs as
      before to make it work with dark and light themes
    
    Change-Id: Iba15ad4013378a815607894a36ee18ea30bfcc3b
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    Signed-off-by: SpiritCroc <dev@spiritcroc.de>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -73,6 +73,8 @@ import com.android.systemui.qs.QuickStatusBarHeader;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.tuner.TunerService;
 
+import com.android.systemui.R;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -538,14 +540,32 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
     public abstract CharSequence getTileLabel();
 
     public static int getColorForState(Context context, int state) {
+
+        boolean enableQsTileTinting = context.getResources()
+                .getBoolean(R.bool.config_enable_qs_tile_tinting);
         switch (state) {
             case Tile.STATE_UNAVAILABLE:
-                return Utils.getDisabled(context,
-                        Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary));
+                if (!enableQsTileTinting) {
+                    return Utils.getDisabled(context,
+                            Utils.getColorAttrDefaultColor(context,
+                                    android.R.attr.textColorSecondary));
+                } else {
+                    return Utils.getDisabled(context,
+                            context.getColor(R.color.qs_tiles_unavailable_tint));
+                }
             case Tile.STATE_INACTIVE:
-                return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary);
+                if (!enableQsTileTinting) {
+                    return Utils.getColorAttrDefaultColor(context,
+                            android.R.attr.textColorSecondary);
+                } else {
+                    return context.getColor(R.color.qs_tiles_inactive_tint);
+                }
             case Tile.STATE_ACTIVE:
-                return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
+                if (!enableQsTileTinting) {
+                    return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
+                } else {
+                    return context.getColor(R.color.qs_tiles_active_tint);
+                }
             default:
                 Log.e("QSTile", "Invalid state " + state);
                 return 0;

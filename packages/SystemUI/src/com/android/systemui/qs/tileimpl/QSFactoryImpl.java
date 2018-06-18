9ea493e2c90383da359703a76c14e37c9928e088
mydongistiny <jaysonedson@gmail.com>
2018-06-17 17:44:52 -0700

    QS: SystemUI: Add On-The-Go Tile
    Change-Id: Ic345f83723a48e3a7d610926fb9b47fab7ef878f

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -48,6 +48,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.OnTheGoTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -145,6 +146,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CompassTile(mHost);
             case "reboot":
                 return new RebootTile(mHost);
+            case "onthego":
+                return new OnTheGoTile(mHost);
         }
         // Intent tiles.
         if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);

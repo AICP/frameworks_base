ROM-FIXES/omnirom_frameworks_base

Proper supplementary service notification handling.
Change-Id: I4fa94d4ba68a1570d3f822be569ae124882c0e66

telecomm/java/android/telecom/Call.java
@@ -320,12 +320,42 @@ public final class Call {
         public static final int PROPERTY_IS_EXTERNAL_CALL = 0x00000040;
 
         /**
+         * Whether the call was forwarded from another party (GSM only)
+         * @hide
+         */
+        public static final int PROPERTY_WAS_FORWARDED = 0x00000080;
+
+        /**
+         * Whether the call is held remotely
+         * @hide
+         */
+        public static final int PROPERTY_HELD_REMOTELY = 0x00000100;
+
+        /**
+         * Whether the dialing state is waiting for the busy remote side
+         * @hide
+         */
+        public static final int PROPERTY_DIALING_IS_WAITING = 0x00000200;
+
+        /**
+         * Whether an additional call came in and was forwarded while the call was active
+         * @hide
+         */
+        public static final int PROPERTY_ADDITIONAL_CALL_FORWARDED = 0x00000400;
+
+        /**
+         * Whether incoming calls are barred at the remote side
+         * @hide
+         */
+        public static final int PROPERTY_REMOTE_INCOMING_CALLS_BARRED = 0x00000800;
+
+        /**
          * Indicates that the call has CDMA Enhanced Voice Privacy enabled.
          */
         public static final int PROPERTY_HAS_CDMA_VOICE_PRIVACY = 0x00000080;
 
         //******************************************************************************************
-        // Next PROPERTY value: 0x00000100
+        // Next PROPERTY value: 0x00001000
         //******************************************************************************************
 
         private final String mTelecomCallId;
@@ -487,7 +517,22 @@ public final class Call {
             if (hasProperty(properties, PROPERTY_IS_EXTERNAL_CALL)) {
                 builder.append(" PROPERTY_IS_EXTERNAL_CALL");
             }
-            if(hasProperty(properties, PROPERTY_HAS_CDMA_VOICE_PRIVACY)) {
+            if (hasProperty(properties, PROPERTY_WAS_FORWARDED)) {
+                builder.append(" PROPERTY_WAS_FORWARDED");
+            }
+            if (hasProperty(properties, PROPERTY_HELD_REMOTELY)) {
+                builder.append(" PROPERTY_HELD_REMOTELY");
+            }
+            if (hasProperty(properties, PROPERTY_DIALING_IS_WAITING)) {
+                builder.append(" PROPERTY_DIALING_IS_WAITING");
+            }
+            if (hasProperty(properties, PROPERTY_ADDITIONAL_CALL_FORWARDED)) {
+                builder.append(" PROPERTY_ADDITIONAL_CALL_FORWARDED");
+            }
+            if (hasProperty(properties, PROPERTY_REMOTE_INCOMING_CALLS_BARRED)) {
+                builder.append(" PROPERTY_REMOTE_INCOMING_CALLS_BARRED");
+            }
+            if (hasProperty(properties, PROPERTY_HAS_CDMA_VOICE_PRIVACY)) {
                 builder.append(" PROPERTY_HAS_CDMA_VOICE_PRIVACY");
             }
             builder.append("]");

AICP/frameworks_base

telecomm/java/android/telecom/Call.java
@@ -423,8 +423,20 @@ public final class Call {
          */
         public static final int PROPERTY_ASSISTED_DIALING_USED = 0x00000200;
 
+        /**
+         * Whether the call was forwarded from another party (GSM only)
+         * @hide
+         */
+        public static final int PROPERTY_WAS_FORWARDED = 0x00000400;
+
+        /**
+         * Whether incoming calls are barred at the remote side
+         * @hide
+         */
+        public static final int PROPERTY_REMOTE_INCOMING_CALLS_BARRED = 0x0000800;
+
         //******************************************************************************************
-        // Next PROPERTY value: 0x00000400
+        // Next PROPERTY value: 0x00001000
         //******************************************************************************************
 
         private final String mTelecomCallId;
@@ -586,6 +598,12 @@ public final class Call {
             }
             if(hasProperty(properties, PROPERTY_ASSISTED_DIALING_USED)) {
                 builder.append(" PROPERTY_ASSISTED_DIALING_USED");
+            }
+            if (hasProperty(properties, PROPERTY_WAS_FORWARDED)) {
+                builder.append(" PROPERTY_WAS_FORWARDED");
+            }
+            if (hasProperty(properties, PROPERTY_REMOTE_INCOMING_CALLS_BARRED)) {
+                builder.append(" PROPERTY_REMOTE_INCOMING_CALLS_BARRED");
             }
             builder.append("]");
             return builder.toString();
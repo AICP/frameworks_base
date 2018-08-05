7963234aeff713addd2fd0971b8ca36a99063704
maxwen <max.weninger@gmail.com>
2018-08-05 14:27:23 +0200

    base: cleanup and fix a few nav gestures issues
    make sure we dont do anything while on keyguard
    long press should only be active in home location
    make threshold values resource values
    make sure if gestures are disabled no edge events are lost
    
    Change-Id: Ifb36c9b0c868257e73988f09dc11c991a0b3b9bb

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7771,6 +7771,7 @@ public class WindowManagerService extends IWindowManager.Stub
         mPolicy.screenRecordAction(mode);
     }
 
+    @Override
     public boolean isGestureButtonEnabled() {
         return this.mPolicy.isGestureButtonEnabled();
     }

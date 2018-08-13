6ba2124e6617e2967edeb2ed0a40b7be06f66a67
camcory <camcory1@gmail.com>
2018-08-13 14:03:14 -0400

    Add the powersave hint to the circle battery
    Change-Id: I6319725959d922ef608814047594c9f2808dcd6a

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -637,6 +637,12 @@ public class BatteryMeterDrawableBase extends Drawable {
                 c.drawText(mWarningString, x, y, mWarningTextPaint);
             }
         }
+        // Draw the powersave outline last
+        if (!mCharging && mPowerSaveEnabled && mPowerSaveAsColorError) {
+            if (level > 0) {
+                c.drawArc(mFrame, 270, 3.6f * level, false, mPowersavePaint);
+            }
+        }
     }
 
     // Some stuff required by Drawable.

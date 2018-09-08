f4d4f6bbb6e365aaa097895d307c4f1ec1e5f17c
Pranav Vashi <neobuddy89@gmail.com>
2018-09-08 22:32:30 +0530

    Separate double tap to sleep on lockscreen [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ibbf33f7b5dc1cbb74cb0f40076bcf60d332d2f81

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5741,6 +5741,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
+
+        /**
          * Wheter to show network traffic indicator in statusbar
          * @hide
          */

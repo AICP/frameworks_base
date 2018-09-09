22ad9ccddac9e1563b748dceb4bae82bc35dc27b
faust93 <monumentum@gmail.com>
2018-09-09 21:10:06 +0800

    Add AlarmsBlocker [1/2]
    Made by faust93. Based on WakelockBlocker by maxwen
    
    neobuddy89: Moved to global settings.
    
    Change-Id: Ida3075aa251443463b86b4bbf2c523d036fba65d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16047,6 +16047,18 @@ public final class Settings {
          */
         public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
 
+        /**
+         * List of alarms blocks selected
+         * @hide
+         */
+        public static final String ALARM_BLOCKING_LIST = "alarm_blocking_list";
+
+        /**
+         * Enable blocking alarm
+         * @hide
+         */
+        public static final String ALARM_BLOCKING_ENABLED = "alarm_blocking_enabled";
+
         /***************************
          * AICP Global Settings end
          ***************************/

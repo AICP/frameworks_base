f168cf4a72dd74007cf4da0ea09f4ec120fca4cd
PMS22 <prathams99@rediff.com>
2018-10-15 23:55:57 +0300

    CompassTile: Add a device supports compass check
    Change-Id: I32f9bb66d53a45b91f935232c0a09e180a7e6f93

--- core/java/com/android/internal/util/aicp/DeviceUtils.java
+++ core/java/com/android/internal/util/aicp/DeviceUtils.java
@@ -24,6 +24,7 @@ import android.content.res.Resources;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.net.ConnectivityManager;
 import android.nfc.NfcAdapter;
@@ -145,5 +146,11 @@ public class DeviceUtils {
             // Ignore
         }
         return false;
+    }
+
+    public static boolean deviceSupportsCompass(Context ctx) {
+        SensorManager sm = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
+        return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
+                && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
     }
 }

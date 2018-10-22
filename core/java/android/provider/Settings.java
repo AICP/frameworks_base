29d03e5d0ab631d2d1eb6dc46b5fbbd6e02b0e77
SagarMakhar <sagarmakhar@gmail.com>
2018-10-22 18:50:29 +0000

    fwb: SystemUI: Add VOLTE icon toggle [1/2]
    Change-Id: I28a981517213509c0a40a815c6e40c695fea6c68

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5622,6 +5622,14 @@ public final class Settings {
         /** @hide */
         private static final Validator SLIM_RECENT_ENTER_EXIT_ANIMATION_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
+        /**
+        * Whether to show VoLTE icon or not
+        * @hide
+        */
+        public static final String SHOW_VOLTE_ICON = "volte_icon";
+
+        /** @hide */
+        private static final Validator SHOW_VOLTE_ICON_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /***************************
          * AICP System Settings end
@@ -5805,6 +5813,7 @@ public final class Settings {
             SLIM_RECENTS_CORNER_RADIUS,
             SLIM_RECENTS_BLACKLIST_VALUES,
             SLIM_RECENT_ENTER_EXIT_ANIMATION,
+            SHOW_VOLTE_ICON,
             // AICP Settings end
         };
 
@@ -6035,6 +6044,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SLIM_RECENTS_CORNER_RADIUS);
             PRIVATE_SETTINGS.add(SLIM_RECENTS_BLACKLIST_VALUES);
             PRIVATE_SETTINGS.add(SLIM_RECENT_ENTER_EXIT_ANIMATION);
+            PRIVATE_SETTINGS.add(SHOW_VOLTE_ICON);
             // AICP Settings end
         }
 
@@ -6255,6 +6265,7 @@ public final class Settings {
             VALIDATORS.put(SLIM_RECENTS_CORNER_RADIUS, SLIM_RECENTS_CORNER_RADIUS_VALIDATOR);
             VALIDATORS.put(SLIM_RECENTS_BLACKLIST_VALUES, SLIM_RECENTS_BLACKLIST_VALUES_VALIDATOR);
             VALIDATORS.put(SLIM_RECENT_ENTER_EXIT_ANIMATION, SLIM_RECENT_ENTER_EXIT_ANIMATION_VALIDATOR);
+            VALIDATORS.put(SHOW_VOLTE_ICON,SHOW_VOLTE_ICON_VALIDATOR);
             // AICP Settings end
         }
 

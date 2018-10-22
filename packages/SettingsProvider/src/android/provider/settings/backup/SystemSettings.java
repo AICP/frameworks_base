79f9bdccbde0dcedcba59fbe9cfbe0bd67dffcc6
SagarMakhar <sagarmakhar@gmail.com>
2018-10-22 18:50:29 +0000

    Add VOLTE icon toggle [1/2]
    Change-Id: I28a981517213509c0a40a815c6e40c695fea6c68

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -249,5 +249,6 @@ public class SystemSettings {
         Settings.System.ROAMING_INDICATOR_ICON,
         Settings.System.SHOW_LTE_FOURGEE,
         Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL,
+        Settings.System.SHOW_VOLTE_ICON,
     };
 }

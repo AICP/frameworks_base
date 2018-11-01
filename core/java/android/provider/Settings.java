09a4a42bb3baa37261588719e21e9540547908ca
doc HD <doc.divxm@gmail.com>
2018-11-01 08:06:11 +0300

    fw_base: Remove duplicate declaration
    Change-Id: Id3df68e43ba0a78bd90dbc56216922289b77f199

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4395,12 +4395,6 @@ public final class Settings {
          */
 
         /**
-         * Whether to show the battery info on the lockscreen while charging
-         * @hide
-         */
-        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
-
-        /**
          * Three Finger Gesture from Oppo
          * @hide
          */

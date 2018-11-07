0f4fc608fa522cf00ea18274114311adc8881730
SpiritCroc <dev@spiritcroc.de>
2018-11-07 15:02:52 +0100

    Add back more battery bar locations [1/2]
    Based on orignal battery bar commit by
    cphelps76 <cphelps76@droidconcepts.com>
    
    - Bring to Pie
    - Add bottom of statusbar location
    - Use extra preference for location so we can use our masterswitch
        preference in AE for en-/disabling
    - Leave away rotated navigation bar for now
        depending whether rotated to left or right, battery bar would show
        up on other edge, so would need extra logic
    
    Change-Id: I9b323e025d04a3a16b5557f96f0cd5ad08dce0dc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6627,6 +6627,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO_TEMP_UNIT = "lockscreen_charge_temp_unit";
 
         /**
+         * Location of the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_LOCATION = "statusbar_battery_bar_location";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6829,6 +6835,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_THICKNESS);
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_STYLE);
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_ANIMATE);
+            PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_LOCATION);
         }
 
         /**

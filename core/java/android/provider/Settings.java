f79de2f56b76dbd2faef4e08a373d3408b7420e7
Ali B <abittin@gmail.com>
2018-11-10 10:57:41 +0300

    Add faster qs tile animation speeds (1/2)
    Change-Id: I4b0245b8afdc87a4873bb4c596a2b017670218f0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4974,7 +4974,7 @@ public final class Settings {
 
         /** @hide */
         private static final Validator ANIM_TILE_DURATION_VALIDATOR =
-              new SettingsValidators.InclusiveIntegerRangeValidator(1500, 2500);
+              new SettingsValidators.InclusiveIntegerRangeValidator(500, 2500);
 
         /**
          * Change quick settings tiles interpolator

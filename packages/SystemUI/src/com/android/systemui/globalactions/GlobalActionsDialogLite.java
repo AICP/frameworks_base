e547624e346e5da449792a7b1cb25ada096cd709
Alex Cruz <alex@dirtyunicorns.com>
2018-11-17 19:41:05 +0530

    SystemUI: Power menu animations [1/2]
    SDB: Bring it back from the Dead & adapt for A12-A14
    
    Change-Id: I2915561792c4f2b0c5d72a2ead9a64dee8cbd065
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -90,6 +90,7 @@ import android.view.Display;
 import android.view.GestureDetector;
 import android.view.IWindowManager;
 import android.view.LayoutInflater;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
@@ -629,6 +630,25 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         prepareDialog();
 
         WindowManager.LayoutParams attrs = mDialog.getWindow().getAttributes();
+
+        boolean isPrimary = UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
+
+        int powermenuAnimations = isPrimary ? getPowermenuAnimations() : 0;
+        switch (powermenuAnimations) {
+           case 0:
+              attrs.windowAnimations = R.style.GlobalActionsAnimationEnter;
+              attrs.gravity = Gravity.CENTER|Gravity.CENTER_HORIZONTAL;
+           break;
+           case 1:
+              attrs.windowAnimations = R.style.GlobalActionsAnimation;
+              attrs.gravity = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
+           break;
+           case 2:
+              attrs.windowAnimations = R.style.GlobalActionsAnimationTop;
+              attrs.gravity = Gravity.TOP|Gravity.CENTER_HORIZONTAL;
+           break;
+        }
+
         attrs.setTitle("GlobalActionsDialogLite");
         attrs.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
         attrs.alpha = setPowerMenuAlpha();
@@ -692,6 +712,11 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         double dDim = mPowerMenuDialogDim / 100.0;
         float dim = (float) dDim;
         return dim;
+    }
+
+    private int getPowermenuAnimations() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.POWER_MENU_ANIMATIONS, 0);
     }
 
     /**

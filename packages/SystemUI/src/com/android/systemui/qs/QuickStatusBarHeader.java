52cd6e7ec8c14ef7c6fbf6c155d4c5129bd59da2
Lars Greiss <kufikugel@googlemail.com>
2018-11-25 17:53:21 +0100

    Status bar clock customizations [1/2]
    ezio84 & jchasky edits:
    - Adapt to Pie
    - Strip down and drop features we don't need anymore
    - Cleanup/improve code
    - Make it play nice with Pie's expanded status bar date/clock view
    - Make it play nice with Heads Up
    
    - Ability to hide clock
    - AM/PM
    - Date format
    - Date style
    - Date position
    
    @eyosen's edits:
    - Adapt to 10
    - Do not remove clock seconds
    
    Change-Id: Ia294f0d64ab1f07bc95c98efce285925a2d35bfc

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -161,6 +161,7 @@ public class QuickStatusBarHeader extends FrameLayout implements
         mClockView.setOnClickListener(this);
         mClockView.setOnLongClickListener(this);
         mDatePrivacySeparator = findViewById(R.id.space);
+        mClockView.setQsHeader();
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
 
@@ -445,6 +446,7 @@ public class QuickStatusBarHeader extends FrameLayout implements
         if (mExpanded == expanded) return;
         mExpanded = expanded;
         quickQSPanelController.setExpanded(expanded);
+        mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateEverything();
     }
 

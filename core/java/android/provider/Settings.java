303ba853ce3b50b5c3ab93d3a53694ec5a718636
Lars Greiss <kufikugel@googlemail.com>
2018-11-25 17:53:21 +0100

    Status bar clock customizations [1/2]
    ezio84 & jchasky edits:
    - Adapt to Pie
    - Strip down and drop features we don't need anymore
    - Cleanup/improve code
    - Make it play nice with Pie's expanded status bar date/clock view
    - Make it play nice with Heads Up
    
    - Ability to hide clock
    - AM/PM
    - Date format
    - Date style
    - Date position
    
    @eyosen's edits:
    - Adapt to 10
    - Do not remove clock seconds
    
    Change-Id: Ia294f0d64ab1f07bc95c98efce285925a2d35bfc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10041,6 +10041,16 @@ public final class Settings {
          */
         public static final int ACCESSIBILITY_MAGNIFICATION_MODE_WINDOW = 0x2;
 
+        /*****************************
+         * AICP Secure Settings start
+         *****************************/
+
+         /**
+         Add aicp settings like e.g.
+         @hide
+         public static final String AICP_FOO_BAR_BALABALA = "foo_bar_balabala";
+         */
+
         /*
          * Boolean value whether to display volume panel on the display left
          * @hide
@@ -10070,6 +10080,67 @@ public final class Settings {
          * @hide
          */
         public static final String DEVICE_HOSTNAME = "device_hostname";
+
+        /**
+         * Show or hide clock seconds
+         * 0 - hide
+         * 1 - show (default)
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_SECONDS = "status_bar_clock_seconds";
+
+        /**
+         * Show or hide clock
+         * 0 - hide
+         * 1 - show (default)
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK = "status_bar_clock";
+
+        /**
+         * AM/PM Style for clock options
+         * 0 - Normal AM/PM
+         * 1 - Small AM/PM
+         * 2 - No AM/PM  (default)
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_AM_PM_STYLE = "statusbar_clock_am_pm_style";
+
+        /**
+         * Shows custom date before clock time
+         * 0 - No Date
+         * 1 - Small Date
+         * 2 - Normal Date
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_DATE_DISPLAY = "statusbar_clock_date_display";
+
+        /**
+         * Sets the date string style
+         * 0 - Regular style
+         * 1 - Lowercase
+         * 2 - Uppercase
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_DATE_STYLE = "statusbar_clock_date_style";
+
+        /**
+         * Stores the java DateFormat string for the date
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
+
+        /**
+         * Position of date
+         * 0 - Left of clock
+         * 1 - Right of clock
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
+
+        /*****************************
+         * AICP Secure Settings end
+         *****************************/
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -37,6 +37,7 @@ import com.android.systemui.statusbar.notification.NotificationWakeUpCoordinator
 import com.android.systemui.statusbar.notification.collection.NotificationEntry;
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
 import com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayout;
+import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.OnHeadsUpChangedListener;
 
@@ -280,7 +281,9 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                 updateParentClipping(false /* shouldClip */);
                 mHeadsUpStatusBarView.setVisibility(View.VISIBLE);
                 show(mHeadsUpStatusBarView);
-                hide(mClockView, View.INVISIBLE);
+                if (((Clock)mClockView).isClockVisible()) {
+                    hide(mClockView, View.INVISIBLE);
+                }
                 if (mCenteredIconView.getVisibility() != View.GONE) {
                     hide(mCenteredIconView, View.INVISIBLE);
                 }
@@ -288,7 +291,9 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                     hide(mOperatorNameView, View.INVISIBLE);
                 }
             } else {
-                show(mClockView);
+                if (((Clock)mClockView).isClockVisible()) {
+                    show(mClockView);
+                }
                 if (mCenteredIconView.getVisibility() != View.GONE) {
                     show(mCenteredIconView);
                 }

caf5ee213cffabad20792b87e8faeaa3b216a92f
Ali B <abittin@gmail.com>
2018-12-01 09:44:58 +0300

    Revert "Pulse: add back auto color based on albumart"
    This reverts commit 2582b527768743d1bc2a924af615f37d9f3ed630.
    
    Change-Id: Ice7498cf47c1b8ff4325cbe4bf47dcfcade58a68

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -1066,12 +1066,6 @@ public class NavigationBarView extends FrameLayout implements PluginListener<Nav
         }
     }
 
-    public void setPulseColors(boolean colorizedMedia, int[] colors) {
-        if (mPulse != null) {
-            mPulse.setPulseColors(colorizedMedia, colors);
-        }
-    }
-
     @Override
     public boolean onStartPulse(Animation animatePulseIn) {
         // TODO add buttons alpha animation

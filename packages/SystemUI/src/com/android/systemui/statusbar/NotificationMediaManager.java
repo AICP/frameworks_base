a8e5501919c9beeb0a78e0f04e78e369f20407f2
ezio84 <brabus84@gmail.com>
2018-12-01 18:48:47 +0100

    Ambient music: be sure to clean track info on media notification drop
    a just in case protection if the player doesn't correctly close the
    session
    
    issue tracked with Spotify
    
    Change-Id: Ie4cde412bdd8a5ea0e2bc44b2b0ee15dd748bbfc

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -221,6 +221,7 @@ public class NotificationMediaManager implements Dumpable {
 
     public void clearCurrentMediaNotification() {
         mMediaNotificationKey = null;
+        mPresenter.setAmbientMusicInfo(null, null);
         clearCurrentMediaNotificationSession();
     }
 

e8134fc5f25afeb2e84108fd7648111654c05b41
ezio84 <brabus84@gmail.com>
2018-12-09 20:17:58 +0100

    Move net monitor to expanded statusbar header
    Preview: https://i.ibb.co/1TCCwV8/455010636-14778.jpg
    
    Actually, with notch or low dpi and more statusbar icons we have not so
    much space there to show the net monitor.
    A notch can hide it and the system can now handle only a fixed max number of
    notifications and status icons, so when net monitor shows itself it can force
    the system to drop some icons to show the infamous dot.
    
    Let's also consider we usually check the monitor to see if the data
    transfer is working as expected and the speed is proper, so it's not
    an info we want to see always (it's also distracting when reading
    something).
    
    So, let's move it to expanded statusbar.
    There we have also some room to add line spacing and more info.
    
    [SpiritCroc]
    Update to work with our normal status bar network traffic and allow user
    selection
    
    @eyosen: updated to 10 & 11
    
    Change-Id: I3fca786aec4f27c763e70d404448bd71dfaecceb

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -49,7 +49,7 @@ import com.android.systemui.statusbar.StatusIconDisplayable;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.CallIndicatorIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.MobileIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.WifiIconState;
-import com.android.systemui.statusbar.policy.NetworkTraffic;
+import com.android.systemui.statusbar.policy.StatusBarNetworkTraffic;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -331,8 +331,8 @@ public interface StatusBarIconController {
             return view;
         }
 
-        protected NetworkTraffic addNetworkTraffic(int index, String slot) {
-            NetworkTraffic view = onCreateNetworkTraffic(slot);
+        protected StatusBarNetworkTraffic addNetworkTraffic(int index, String slot) {
+            StatusBarNetworkTraffic view = onCreateNetworkTraffic(slot);
             mGroup.addView(view, index, onCreateLayoutParams());
             return view;
         }
@@ -364,8 +364,8 @@ public interface StatusBarIconController {
             return view;
         }
 
-        private NetworkTraffic onCreateNetworkTraffic(String slot) {
-            NetworkTraffic view = new NetworkTraffic(mContext);
+        private StatusBarNetworkTraffic onCreateNetworkTraffic(String slot) {
+            StatusBarNetworkTraffic view = new StatusBarNetworkTraffic(mContext);
             view.setPadding(4, 0, 4, 0);
             return view;
         }

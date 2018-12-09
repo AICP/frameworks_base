a783ed87780817d0f927ce0f8b2e1d8b4c94867e
ezio84 <brabus84@gmail.com>
2018-12-09 20:17:58 +0100

    Move net monitor to expanded statusbar header
    Preview: https://i.ibb.co/1TCCwV8/455010636-14778.jpg
    
    Actually, with notch or low dpi and more statusbar icons we have not so
    much space there to show the net monitor.
    A notch can hide it and the system can now handle only a fixed max number of
    notifications and status icons, so when net monitor shows itself it can force
    the system to drop some icons to show the infamous dot.
    
    Let's also consider we usually check the monitor to see if the data
    transfer is working as expected and the speed is proper, so it's not
    an info we want to see always (it's also distracting when reading
    something).
    
    So, let's move it to expanded statusbar.
    There we have also some room to add line spacing and more info.
    
    [SpiritCroc]
    Update to work with our normal status bar network traffic and allow user
    selection
    
    @eyosen: updated to 10 & 11
    
    Change-Id: I3fca786aec4f27c763e70d404448bd71dfaecceb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5760,6 +5760,13 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
+        /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_EXPANDED_STATUS_BAR_STATE =
+                "network_traffic_expanded_status_bar_state";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -5898,6 +5905,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_EXPANDED_STATUS_BAR_STATE);
         }
 
         /**

360715820f8f3b2eb867e4c1ade4bc075237be9a
Jon Evans <jon@craftyjon.com>
2018-12-10 11:57:12 -0500

    Port: Wake on plug (1/2)
    AICPfy:
    - Oreo bringup and move from CMSettings
    
    Change-Id: Ifc27f7f7d8b7b43af680011cb86e60f10391886c

--- packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -76,6 +76,7 @@ public class GlobalSettings {
         Settings.Global.ARE_USER_DISABLED_HDR_FORMATS_ALLOWED,
         Settings.Global.DEVICE_CONFIG_SYNC_DISABLED,
         Settings.Global.POWER_BUTTON_LONG_PRESS,
-        Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS
+        Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS,
+        Settings.Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
     };
 }

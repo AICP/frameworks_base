9cd43f60a8876554714d9d08c47e8786c5e25abe
a.derendyaev <a.derendyaev@magdv.com>
2018-12-17 16:28:44 +0700

    extended audio panel [1/2]
    Change-Id: Ieb85137ee73f7ed032b9bbdd4087be2c72a4d1f5
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4819,6 +4819,24 @@ public final class Settings {
         private static final Validator SCREEN_OFF_ANIMATION_VALIDATOR =
                 ANY_INTEGER_VALIDATOR;
 
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
+
         /**
          * @hide
          */

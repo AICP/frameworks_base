368125548b525e56b079a973a53fd0c11d624619
ezio84 <brabus84@gmail.com>
2019-01-03 19:26:55 +0100

    Always use standard small clock when pulsing on new notifications
    this is a quick fix for custom clock glitches when the device pulses
    on a new notification and at the same time it keeps what Google wanted
    in this case: show a small clock and give more importance to the
    notification itself
    
    Change-Id: I838f8deeb252a9f493131606fc79475a89b1a40c
    TODO: handle custom clocks in onLayoutChange shouldAnimate part

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -94,6 +94,8 @@ public class KeyguardStatusView extends GridLayout implements
 
     private int mClockSelection;
 
+    private boolean mWasLatestViewSmall;
+
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
 
         @Override
@@ -235,6 +237,7 @@ public class KeyguardStatusView extends GridLayout implements
      */
     private void onSliceContentChanged() {
         boolean smallClock = mKeyguardSlice.hasHeader() || mPulsing;
+        prepareSmallView(smallClock);
         float clockScale = smallClock ? mSmallClockScale : 1;
 
         RelativeLayout.LayoutParams layoutParams =
@@ -265,6 +268,7 @@ public class KeyguardStatusView extends GridLayout implements
         int heightOffset = mPulsing || mWasPulsing ? 0 : getHeight() - mLastLayoutHeight;
         boolean hasHeader = mKeyguardSlice.hasHeader();
         boolean smallClock = hasHeader || mPulsing;
+        prepareSmallView(smallClock);
         long duration = KeyguardSliceView.DEFAULT_ANIM_DURATION;
         long delay = smallClock || mWasPulsing ? 0 : duration / 4;
         mWasPulsing = false;
@@ -354,7 +358,7 @@ public class KeyguardStatusView extends GridLayout implements
     private void refreshTime() {
         mClockView.refresh();
 
-        if (mClockSelection == 0) {
+        if (mClockSelection == 0 || mWasLatestViewSmall) {
             mClockView.setFormat12Hour(Patterns.clockView12);
             mClockView.setFormat24Hour(Patterns.clockView24);
         } else if (mClockSelection == 1) {
@@ -457,6 +461,23 @@ public class KeyguardStatusView extends GridLayout implements
                 mClockView.setVisibility(View.VISIBLE);
                 mCustomClockView.setVisibility(View.GONE);
                 break;
+        }
+    }
+
+    private void prepareSmallView(boolean small) {
+        if (mWasLatestViewSmall == small) return;
+        mWasLatestViewSmall = small;
+        if (small) {
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)
+                    mKeyguardSlice.getLayoutParams();
+            params.addRule(RelativeLayout.BELOW, R.id.clock_view);
+            mClockView.setSingleLine(true);
+            mClockView.setGravity(Gravity.CENTER);
+            mClockView.setVisibility(View.VISIBLE);
+            mCustomClockView.setVisibility(View.GONE);
+        } else {
+            setStyle();
+            refreshTime();
         }
     }
 
@@ -620,6 +641,11 @@ public class KeyguardStatusView extends GridLayout implements
 
         mClockSelection = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.LOCKSCREEN_CLOCK_SELECTION, 0, UserHandle.USER_CURRENT);
+
+        setStyle();
+    }
+
+    private void setStyle() {
 
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)
                mKeyguardSlice.getLayoutParams();

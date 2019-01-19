8e1f7a0a2c71fae754b51ebe64c035de7dd59214
Ali B <abittin@gmail.com>
2019-01-19 19:02:05 +0300

    base: tuner: remove clock from blacklist reset
    We dont have the clock in the tuner so no need to
    have it on reset
    
    Change-Id: Ibe0a44208a712a36b47147aff4768dbf62afdee3

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -36,7 +36,6 @@ import com.android.systemui.DemoMode;
 import com.android.systemui.Dependency;
 import com.android.systemui.qs.QSTileHost;
 import com.android.systemui.settings.CurrentUserTracker;
-import com.android.systemui.statusbar.phone.ClockController;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.util.leak.LeakDetector;
 
@@ -54,7 +53,6 @@ public class TunerServiceImpl extends TunerService {
     // Things that use the tunable infrastructure but are now real user settings and
     // shouldn't be reset with tuner settings.
     private static final String[] RESET_BLACKLIST = new String[] {
-            ClockController.CLOCK_POSITION,
             QSTileHost.TILES_SETTING,
             Settings.Secure.DOZE_ALWAYS_ON
     };

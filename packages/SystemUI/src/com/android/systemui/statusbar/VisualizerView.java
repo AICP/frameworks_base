a856631cc8a1584c34deeb771355df0b42d95402
Pranav Vashi <neobuddy89@gmail.com>
2019-01-19 21:32:45 +0300

    LockscreenVisualizer: Use offload thread
    Change-Id: Id5be2e939e403040d3c233a09b9b662d1b7ed0e0

--- packages/SystemUI/src/com/android/systemui/statusbar/VisualizerView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/VisualizerView.java
@@ -27,7 +27,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.media.audiofx.Visualizer;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -36,6 +35,9 @@ import android.support.v7.graphics.Palette;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.UiOffloadThread;
 
 public class VisualizerView extends View
         implements Palette.PaletteAsyncListener {
@@ -68,6 +70,8 @@ public class VisualizerView extends View
     private int mCustomColor;
     private Bitmap mCurrentBitmap;
 
+    private final UiOffloadThread mUiOffloadThread;
+
     private Visualizer.OnDataCaptureListener mVisualizerListener =
             new Visualizer.OnDataCaptureListener() {
         byte rfk, ifk;
@@ -94,13 +98,9 @@ public class VisualizerView extends View
         }
     };
 
-    private final Runnable mLinkVisualizer = new Runnable() {
-        @Override
-        public void run() {
-            if (DEBUG) {
-                Log.w(TAG, "+++ mLinkVisualizer run()");
-            }
-
+    public void dolink() {
+        mUiOffloadThread.submit(() -> {
+            if (mVisualizer != null) return;
             try {
                 mVisualizer = new Visualizer(0);
             } catch (Exception e) {
@@ -113,12 +113,8 @@ public class VisualizerView extends View
             mVisualizer.setDataCaptureListener(mVisualizerListener,Visualizer.getMaxCaptureRate(),
                     false, true);
             mVisualizer.setEnabled(true);
-
-            if (DEBUG) {
-                Log.w(TAG, "--- mLinkVisualizer run()");
-            }
-        }
-    };
+        });
+    }
 
     private void unlink() {
         if (DEBUG) {
@@ -160,6 +156,8 @@ public class VisualizerView extends View
                 }
             });
         }
+
+        mUiOffloadThread = Dependency.get(UiOffloadThread.class);
     }
 
     public VisualizerView(Context context, AttributeSet attrs) {
@@ -351,7 +349,7 @@ public class VisualizerView extends View
                 && mVisualizerEnabled && !mOccluded) {
             if (!mDisplaying) {
                 mDisplaying = true;
-                AsyncTask.execute(mLinkVisualizer);
+                dolink();
                 animate()
                         .alpha(1f)
                         .withEndAction(null)

516606933bda9f09a7204f064bda600748d7e9e7
Ali B <abittin@gmail.com>
2019-01-31 20:05:33 +0300

    SystemUI: update omnijawsclient to use PreferenceActivity
    Change-Id: I616f8b9f6ef1d89dd4db967dab6d73d4a556c001

--- packages/SystemUI/src/com/android/systemui/omni/OmniJawsClient.java
+++ packages/SystemUI/src/com/android/systemui/omni/OmniJawsClient.java
@@ -38,6 +38,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.UserHandle;
+import android.preference.PreferenceActivity;
 import android.provider.Settings;
 import android.util.Log;
 
@@ -86,7 +87,6 @@ public class OmniJawsClient {
     private static final String WEATHER_UPDATE = "org.omnirom.omnijaws.WEATHER_UPDATE";
     private static final String WEATHER_ERROR = "org.omnirom.omnijaws.WEATHER_ERROR";
 
-    private static final String AE_EXTRA_FRAGMENT_CLASS = "com.aicp.extras.extra.preference_fragment";
     private static final String AE_SETTINGSACTIVITY = "com.aicp.extras.SettingsActivity";
     private static final String SETTINGS_PACKAGE_NAME = "com.aicp.extras";
     private static final String WEATHER_SETTINGS = "com.aicp.extras.fragments.Weather";
@@ -249,7 +249,7 @@ public class OmniJawsClient {
         if (isOmniJawsServiceInstalled()) {
             Intent settings = new Intent(Intent.ACTION_MAIN);
             settings.setClassName(SETTINGS_PACKAGE_NAME, AE_SETTINGSACTIVITY);
-            settings.putExtra(AE_EXTRA_FRAGMENT_CLASS, WEATHER_SETTINGS);
+            settings.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, WEATHER_SETTINGS);
             return settings;
         }
         return null;

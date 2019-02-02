9bfb1aee5c07b770b40ac267de52749927303243
Alex <du.alexcruz@gmail.com>
2019-02-02 00:53:08 -0600

    Carrier Label: Bring back the scroll (marquee) effect
    The AOSP carrier label class adds this effect, so this just keeps
    that behavior in place.
    
    https://github.com/DirtyUnicorns/android_frameworks_base/blob/p9x/packages/SystemUI/src/com/android/keyguard/CarrierText.java#L296-L301
    
    Change-Id: I2352314341f441f9b8f53a8478a6f151551288b0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -222,8 +222,10 @@ public class KeyguardStatusBarView extends RelativeLayout
         if (mCarrierLabel != null) {
             if (mShowCarrierLabel == 1 || mShowCarrierLabel == 3) {
                 mCarrierLabel.setVisibility(mHideContents ? View.INVISIBLE : View.VISIBLE);
+                mCarrierLabel.setSelected(mHideContents ? false : true);
             } else {
                 mCarrierLabel.setVisibility(View.GONE);
+                mCarrierLabel.setSelected(false);
             }
         }
     }

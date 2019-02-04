b4b09560b28f6fec37db4224809e9f7f3988b85b
mosimchah <mosimchah@gmail.com>
2019-02-04 17:36:59 -0500

    Revert "HW buttons: add screen off and power menu actions [1/2]"
    This reverts commit adbbbe26faa4014c2d52f233c79a8686a70e38c3.
    
    Change-Id: I87dc18cc2d681ae6ab940cfa45d4edea725732d1

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -405,8 +405,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int KEY_ACTION_SPLIT_SCREEN = 8;
     private static final int KEY_ACTION_FORCE_CLOSE_APP = 9;
     private static final int KEY_ACTION_SCREENSHOT = 10;
-    private static final int KEY_ACTION_SCREEN_OFF = 11;
-    private static final int KEY_ACTION_POWER_MENU = 12;
 
     // Special values, used internal only.
     private static final int KEY_ACTION_HOME = 100;
@@ -4340,9 +4338,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             KEY_ACTION_LAST_APP,
             KEY_ACTION_SPLIT_SCREEN,
             KEY_ACTION_FORCE_CLOSE_APP,
-            KEY_ACTION_SCREENSHOT,
-            KEY_ACTION_SCREEN_OFF,
-            KEY_ACTION_POWER_MENU,
+            KEY_ACTION_SCREENSHOT
         };
 
     /**
@@ -4563,13 +4559,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case KEY_ACTION_SCREENSHOT:
                 takeScreenshot();
-                break;
-            case KEY_ACTION_SCREEN_OFF:
-                goToSleep(SystemClock.uptimeMillis(), PowerManager.GO_TO_SLEEP_REASON_POWER_BUTTON,
-                        0);
-                break;
-            case KEY_ACTION_POWER_MENU:
-                showGlobalActions();
                 break;
         }
     }

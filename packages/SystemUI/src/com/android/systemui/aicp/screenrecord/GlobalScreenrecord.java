e7a02493aba2ec953027d7e432e88818b87e6f63
Ali B <abittin@gmail.com>
2019-02-15 11:14:26 +0300

    base: screenrecord: update notif text for high aspect ratio devices
    Change-Id: Id0769ad7214992713a7e2cc930641b322ec4277c

--- packages/SystemUI/src/com/android/systemui/aicp/screenrecord/GlobalScreenrecord.java
+++ packages/SystemUI/src/com/android/systemui/aicp/screenrecord/GlobalScreenrecord.java
@@ -258,15 +258,18 @@ class GlobalScreenrecord {
         final String base = r.getString(R.string.screenrecord_notif_title);
         switch (mode) {
             case WindowManager.SCREEN_RECORD_LOW_QUALITY:
-                mNotifContent = base + " - 480x800 @1.5Mbps";
+                mNotifContent = base +
+                    (mHigherAspectRatio ? " - 480x960" : " - 480x800") + " @1.5Mbps";
                 mRecordingStartTime = System.currentTimeMillis();
                 break;
             case WindowManager.SCREEN_RECORD_MID_QUALITY:
-                mNotifContent = base + " - 720x1280 @4Mbps";
+                mNotifContent = base +
+                    (mHigherAspectRatio ? " - 720x1440" : " - 720x1280") + " @4Mbps";
                 mRecordingStartTime = System.currentTimeMillis();
                 break;
             case WindowManager.SCREEN_RECORD_HIGH_QUALITY:
-                mNotifContent = base + " - 720x1280 @8Mbps";
+                mNotifContent = base +
+                    (mHigherAspectRatio ? " - 720x1440" : " - 720x1280") + " @8Mbps";
                 mRecordingStartTime = System.currentTimeMillis();
                 break;
             case -1:

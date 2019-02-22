ea0266aec573079fb495e4de498eb9a4cfb7e62c
Anushek Prasal <anushekprasal@gmail.com>
2019-02-22 23:15:31 +0530

    SystemUI: Always show date in QS header
    Change-Id: I234e87473c7d8c8e746c62992707e975cbc95e46

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -334,7 +334,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         if (mExpanded == expanded) return;
         mExpanded = expanded;
         mHeaderQsPanel.setExpanded(expanded);
-        mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateEverything();
     }
 

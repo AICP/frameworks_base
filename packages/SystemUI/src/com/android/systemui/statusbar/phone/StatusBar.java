fd1d28fbc71977d6af15108cd91a9f374852919c
ezio84 <brabus84@gmail.com>
2019-02-28 06:13:00 +0300

    ActionUtils: Reverse actions for QS and notification panel
    This just makes sense, so lets do it!
    
    Change-Id: Ic08bee12c90dc55265dea3c3972f4087b93d2826

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2582,6 +2582,15 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         setInteracting(StatusBarManager.WINDOW_STATUS_BAR, true);
     }
 
+    @Override
+    public void toggleSettingsPanel() {
+        if (mPanelExpanded) {
+            animateCollapsePanels();
+        } else {
+            animateExpandSettingsPanel(null);
+        }
+    }
+
     public void animateCollapsePanels() {
         animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
     }

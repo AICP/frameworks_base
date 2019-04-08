4fda54f0f0f0c22e27e0ea280a5eea0c82ad9971
Chris Crump <chris@cypheros.co>
2019-04-08 04:14:09 -0400

    SystemUI: Ensure battery estimates update on callback
    Battery estimates weren't updating is the view was not null, and
    only updated when the battery level change. Force an update of the
    BatteryMeterView on every callback change
    
    Change-Id: If2c5ad1a2ec314abb41e1b192bc7022c502e86c0

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -394,7 +394,9 @@ public class BatteryMeterView extends LinearLayout implements
                     new ViewGroup.LayoutParams(
                             LayoutParams.WRAP_CONTENT,
                             LayoutParams.MATCH_PARENT));
+            return;
         }
+        updatePercentText();
     }
 
     @Override

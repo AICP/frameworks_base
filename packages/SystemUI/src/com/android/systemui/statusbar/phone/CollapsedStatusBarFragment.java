1f3ff67e38743327447f6a6105b716fe93a1c74c
Pranav Vashi <neobuddy89@gmail.com>
2019-04-22 13:55:45 +0300

    Use alphaoptimized layout for custom icons on left
    @SKULSHADY: Use a stripped down version of the original commit just to handle carrier label and logo.
    
    AICP: Adapted to our statusbar items and 10
    
    Change-Id: Ia4fc519f60266ddee2b7360a6f076e8aea7f53c1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -66,6 +66,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private KeyguardMonitor mKeyguardMonitor;
     private NetworkController mNetworkController;
     private LinearLayout mSystemIconArea;
+    private LinearLayout mCustomIconArea;
     private View mStatusBarLogo, mStatusBarLogoRight;
     private View mNotificationIconAreaInner;
     private View mCenteredIconArea;
@@ -141,6 +142,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mDarkIconManager.setShouldLog(true);
         Dependency.get(StatusBarIconController.class).addIconGroup(mDarkIconManager);
         mSystemIconArea = mStatusBar.findViewById(R.id.system_icon_area);
+        mCustomIconArea = mStatusBar.findViewById(R.id.left_icon_area);
         mClockController = new ClockController(mStatusBar);
         mBatteryBars[0] = mStatusBar.findViewById(R.id.battery_bar);
         mBatteryBars[1] = mStatusBar.findViewById(R.id.battery_bar_1);
@@ -338,14 +340,14 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     public void hideNotificationIconArea(boolean animate) {
         animateHide(mNotificationIconAreaInner, animate);
         animateHide(mCenteredIconArea, animate);
-        animateHide(mStatusBarLogo, animate);
+        animateHide(mCustomIconArea, animate);
         animateHide(mClockController.getClockLayout(), animate);
     }
 
     public void showNotificationIconArea(boolean animate) {
         animateShow(mNotificationIconAreaInner, animate);
         animateShow(mCenteredIconArea, animate);
-        animateShow(mStatusBarLogo, animate);
+        animateShow(mCustomIconArea, animate);
         animateShow(mClockController.getClockLayout(), animate);
     }
 

e0ce4447dedba4a1d9bbb9796dc9ea58beb97732
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    Add toggle to disable charging animation [1/2]
    @neobuddy89: Use same toggle for wireless charging animation too.
    @semdoc: add backup & settings validators
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I23ce1b9db22c30844c90f01c08053f1933f6fd8f

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -225,5 +225,6 @@ public class SystemSettings {
         Settings.System.NAVIGATION_BAR_HINT,
         Settings.System.FORCE_FULLSCREEN_CUTOUT_APPS,
         Settings.System.QS_DATA_USAGE_PANEL,
+        Settings.System.CHARGING_ANIMATION,
     };
 }

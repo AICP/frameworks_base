b342b8c5c73022996167305239dd9ef428103a72
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    Add toggle to disable charging animation [1/2]
    @neobuddy89: Use same toggle for wireless charging animation too.
    @semdoc: add backup & settings validators
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I23ce1b9db22c30844c90f01c08053f1933f6fd8f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6759,6 +6759,12 @@ public final class Settings {
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6916,6 +6922,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ON_THE_GO_ALPHA);
             PRIVATE_SETTINGS.add(ON_THE_GO_SERVICE_RESTART);
             PRIVATE_SETTINGS.add(ON_THE_GO_CAMERA);
+            PRIVATE_SETTINGS.add(CHARGING_ANIMATION);
         }
 
         /**

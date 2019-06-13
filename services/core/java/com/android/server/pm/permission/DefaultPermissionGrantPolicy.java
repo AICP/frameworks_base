364394dfbe0e3db282047de6a2368045f6b15ed7
Ali B <abittin@gmail.com>
2019-06-13 16:39:15 +0300

    core: move OmniJaws client to its new home
    Also includes
    	Grant default location permissions to OmniJaws
    	Author: Ali B.
    	Change-Id: I2988eea61414dafc94e52729b997d07f4673a9ad
    
    Change-Id: I50349a910aa604959def70377da135c6d94c7774

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -33,6 +33,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManagerInternal;
+import android.content.pm.PackageParser;
 import android.content.pm.PermissionInfo;
 import android.content.pm.ProviderInfo;
 import android.content.pm.ResolveInfo;
@@ -64,6 +65,7 @@ import android.util.Xml;
 
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.XmlUtils;
+import com.android.internal.util.aicp.OmniJawsClient;
 import com.android.server.LocalServices;
 import com.android.server.pm.permission.PermissionManagerServiceInternal.PackagesProvider;
 import com.android.server.pm.permission.PermissionManagerServiceInternal.SyncAdapterPackagesProvider;
@@ -871,7 +873,8 @@ public final class DefaultPermissionGrantPolicy {
         grantSystemFixedPermissionsToSystemPackage(pm,"com.aicp.updater3", userId, STORAGE_PERMISSIONS);
 
         // OmniJaws
-        grantSystemFixedPermissionsToSystemPackage(pm, "org.omnirom.omnijaws", userId, ALWAYS_LOCATION_PERMISSIONS);
+        String omnijawsServicePackageName = "org.omnirom.omnijaws";
+        grantSystemFixedPermissionsToSystemPackage(pm, omnijawsServicePackageName, userId, ALWAYS_LOCATION_PERMISSIONS);
 
         // Google Account
         grantSystemFixedPermissionsToSystemPackage(pm, "com.google.android.gsf.login", userId, CONTACTS_PERMISSIONS,

2827457c3275174bd678aa1a68dfd5b808a2d2bb
Ali B <abittin@gmail.com>
2019-06-13 16:39:15 +0300

    core: move OmniJaws client to its new home
    Also includes
    	Grant default location permissions to OmniJaws
    	Author: Ali B.
    	Change-Id: I2988eea61414dafc94e52729b997d07f4673a9ad
    
    Change-Id: I50349a910aa604959def70377da135c6d94c7774

--- packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
+++ packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
@@ -46,8 +46,8 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.aicp.OmniJawsClient;
 import com.android.systemui.R;
-import com.android.systemui.omni.OmniJawsClient;
 import com.android.systemui.statusbar.phone.SettingsButton;
 import com.android.systemui.plugins.ActivityStarter;
 

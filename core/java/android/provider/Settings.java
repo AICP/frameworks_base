b482527bec935efbd7db7cb829ee1b7d89315210
Marko Man <darkobas@gmail.com>
2019-07-10 12:03:24 +0200

    base: Op gestures: add swipe start height setting [1/2]
    AICPfied: no omni, aicp
    
    Change-Id: I9e0b9db44f402be106806b1c45a50a75432f8cf1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6300,6 +6300,15 @@ public final class Settings {
         private static final Validator LESS_BORING_HEADS_UP_VALIDATOR =
                 BOOLEAN_VALIDATOR;
 
+        /**
+         * @hide
+         */
+        public static final String BOTTOM_GESTURE_SWIPE_START =
+                "bottom_gesture_navigation_swipe_start";
+
+        private static final Validator BOTTOM_GESTURE_SWIPE_START_VALIDATOR =
+                ANY_INTEGER_VALIDATOR;
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6499,6 +6508,7 @@ public final class Settings {
             OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT,
             OMNI_BOTTOM_GESTURE_SWIPE_LIMIT,
             BOTTOM_GESTURE_FEEDBACK_DURATION,
+            BOTTOM_GESTURE_SWIPE_START,
             AICP_LOCKSCREEN_WEATHER_STYLE,
             LOCK_HIDE_STATUS_BAR,
             ENABLE_APP_CIRCLE_BAR,
@@ -6799,6 +6809,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT);
             PRIVATE_SETTINGS.add(OMNI_BOTTOM_GESTURE_SWIPE_LIMIT);
             PRIVATE_SETTINGS.add(BOTTOM_GESTURE_FEEDBACK_DURATION);
+            PRIVATE_SETTINGS.add(BOTTOM_GESTURE_SWIPE_START);
             PRIVATE_SETTINGS.add(AICP_LOCKSCREEN_WEATHER_STYLE);
             PRIVATE_SETTINGS.add(LOCK_HIDE_STATUS_BAR);
             PRIVATE_SETTINGS.add(ENABLE_APP_CIRCLE_BAR);
@@ -7109,6 +7120,7 @@ public final class Settings {
             VALIDATORS.put(OMNI_BOTTOM_GESTURE_SWIPE_LIMIT,
                     OMNI_BOTTOM_GESTURE_SWIPE_LIMIT_VALIDATOR);
             VALIDATORS.put(BOTTOM_GESTURE_FEEDBACK_DURATION, BOTTOM_GESTURE_FEEDBACK_DURATION_VALIDATOR);
+            VALIDATORS.put(BOTTOM_GESTURE_SWIPE_START, BOTTOM_GESTURE_SWIPE_START_VALIDATOR);
             VALIDATORS.put(AICP_LOCKSCREEN_WEATHER_STYLE,AICP_LOCKSCREEN_WEATHER_STYLE_VALIDATOR);
             VALIDATORS.put(LOCK_HIDE_STATUS_BAR,LOCK_HIDE_STATUS_BAR_VALIDATOR);
             VALIDATORS.put(ENABLE_APP_CIRCLE_BAR,ENABLE_APP_CIRCLE_BAR_VALIDATOR);
--- services/core/java/com/android/server/policy/GestureButton.java
+++ services/core/java/com/android/server/policy/GestureButton.java
@@ -71,6 +71,7 @@ public class GestureButton implements PointerEventListener {
     private int mScreenHeight = -1;
     private int mScreenWidth = -1;
     private boolean mSwipeStartFromEdge;
+    private int mSwipeStartThreshold;
     private boolean mKeyEventHandled;
     private boolean mRecentsTriggered;
     private boolean mLongSwipePossible;
@@ -92,9 +93,6 @@ public class GestureButton implements PointerEventListener {
             vibrator.vibrate(VibrationEffect.createOneShot(mHapticDuration, VibrationEffect.DEFAULT_AMPLITUDE));
        }
     };
-
-    private final int mSwipeStartThreshold =
-                    SystemProperties.getInt("ro.bottom_gesture.swipe_start.threshold", 20);
 
     private OnTouchListener mTouchListener = new OnTouchListener() {
         public boolean onTouch(View v, MotionEvent event) {
@@ -143,6 +141,7 @@ public class GestureButton implements PointerEventListener {
         windowManager.getDefaultDisplay().getRealMetrics(displayMetrics);
         mScreenHeight = Math.max(displayMetrics.widthPixels, displayMetrics.heightPixels);
         mScreenWidth = Math.min(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        mSwipeStartThreshold = context.getResources().getInteger(R.integer.config_navgestureswipestart);
         mSwipeMinLength = getSwipeLengthInPixel(context.getResources().getInteger(R.integer.config_navgestureswipeminlength));
         mMoveTolerance = context.getResources().getInteger(R.integer.config_navgesturemovethreshold);
         mSwipeTriggerTimeout  = context.getResources().getInteger(R.integer.config_navgestureswipetimout);
@@ -349,6 +348,11 @@ public class GestureButton implements PointerEventListener {
         mHapticDuration = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.BOTTOM_GESTURE_FEEDBACK_DURATION, 50,
                 UserHandle.USER_CURRENT);
+        mSwipeStartThreshold = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_SWIPE_START,
+                getSwipeLengthInPixel(mContext.getResources().getInteger(R.integer.config_navgestureswipestart)),
+                UserHandle.USER_CURRENT);
+
         if (DEBUG) Slog.i(TAG, "updateSettings mSwipeTriggerTimeout = " + mSwipeTriggerTimeout + " mSwipeMinLength = " + mSwipeMinLength);
     }
 

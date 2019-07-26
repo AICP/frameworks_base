3e6cbeac743fdbb51ecc61e359d991f3f4cb7d29
ezio84 <brabus84@gmail.com>
2019-07-26 12:45:22 +0300

    Less boring heads up option [1/2]
    Q: don't skip the new Bubbles (the circular popping up notifications
    like the Dialer one when a call is active and you press the home button)
    Reference: https://github.com/ezio84/abc_frameworks_base/commit/c3d6f7d386286a4e55a3e0644782827705db7168#diff-c38ee79f64d8dc57e7757863d741be46
    
    Allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    @neobuddy89:
    * Added clock app too in whitelist.
    * Adapted and squashed for A12.
    
    @Stallix:
    * Adapted logging for A13.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I6d619e1d32e44a6b8ee2f3c488394ece7bf2e08d

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
@@ -306,7 +306,8 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 Handler.createAsync(Looper.myLooper()));
 
         mNotificationInterruptStateProvider =
-                new TestableNotificationInterruptStateProviderImpl(mContext.getContentResolver(),
+                new TestableNotificationInterruptStateProviderImpl(mContext,
+                        mContext.getContentResolver(),
                         mPowerManager,
                         mDreamManager, mAmbientDisplayConfiguration, mNotificationFilter,
                         mStatusBarStateController, mBatteryController, mHeadsUpManager,
@@ -1026,6 +1027,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
             NotificationInterruptStateProviderImpl {
 
         TestableNotificationInterruptStateProviderImpl(
+                Context context,
                 ContentResolver contentResolver,
                 PowerManager powerManager,
                 IDreamManager dreamManager,
@@ -1038,7 +1040,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 Handler mainHandler,
                 NotifPipelineFlags flags,
                 KeyguardNotificationVisibilityProvider keyguardNotificationVisibilityProvider) {
-            super(contentResolver, powerManager, dreamManager, ambientDisplayConfiguration, filter,
+            super(context, contentResolver, powerManager, dreamManager, ambientDisplayConfiguration, filter,
                     batteryController, controller, headsUpManager, logger, mainHandler,
                     flags, keyguardNotificationVisibilityProvider);
             mUseHeadsUp = true;

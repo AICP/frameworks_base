fd1ed9471b0a3f3dd0027dc0caefdb1535ecee39
ezio84 <brabus84@gmail.com>
2019-07-26 12:45:22 +0300

    Add less boring heads up option [SQUASHED]
    Q: don't skip the new Bubbles (the circular popping up notifications
    like the Dialer one when a call is active and you press the home button)
    Reference: https://github.com/ezio84/abc_frameworks_base/commit/c3d6f7d386286a4e55a3e0644782827705db7168#diff-c38ee79f64d8dc57e7757863d741be46
    
    Allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    neobuddy89:
    * Added clock app too in whitelist.
    * Adapted and squashed for A12.
    
    Stallix:
    * Adapted logging for A13.
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Wed, 14 Dec 2022 08:22:21 +0530
        Subject: Slightly optimize less boring apps check for heads up
    
        * We do not want to keep checking packages everytime
          when this option is disabled or device is dozing.
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Thu, 9 Mar 2023 12:06:30 +0800
        Subject: Less boring heads up: Don't skip heads up for most messaging apps
    
        From: Rushab Shah <rushabshah32@gmail.com>
        Date: Sun, 30 Apr 2023 00:22:42 +0530
        Subject: LessBoring: fixup LessBoring preventing bubbles
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Tue, 26 Sep 2023 11:40:59 +0800
        Subject: SystemUI: Less boring headsup enhancements and fixes
    
        * cleanup methods and use AOSP's headsup observer
    
        test: call/messaging while toggling reticker/less boring headsup feature on/off
    
        attempt to fix: whitelisted notification apps skipped when using reticker/less boring headsup feature
    
    Change-Id: I26ecd6ca36db09ee662210b386c031f90016ddc4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    
    SystemUI: fixup! Add less boring heads up option to 14 QPR2
    
    Change-Id: If808875f145a765eecfa7944b148cd5690251804
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    
    SystemUI: Fix systemui crash when checking headups whitelist packages
    
    Change-Id: I1413eb05757773dcd9804c64fafa1d951ea7b854
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
@@ -426,6 +426,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
 
         mVisualInterruptionDecisionProvider =
                 VisualInterruptionDecisionProviderTestUtil.INSTANCE.createProviderByFlag(
+                        mContext,
                         mAmbientDisplayConfiguration,
                         mBatteryController,
                         mDeviceProvisionedController,

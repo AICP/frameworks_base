e36a21433349490929ab8de9c8a83f13696f0270
Sam Mortimer <sam@mortimer.me.uk>
2019-08-15 19:40:55 -0700

    base: Add hotspot setting to allow VPN upstreams [1/3]
    Change-Id: I4c6de1a2ac44f997da399d98c003bd21fe0cd0be

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11734,6 +11734,12 @@ public final class Settings {
         public static final String MONET_ENGINE_WHITE_LUMINANCE = "monet_engine_white_luminance_user";
 
 
+        /**
+         * Whether tethering is allowed to use VPN upstreams
+         */
+        @SuppressLint("NoSettingsProvider")
+        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
+
         /*****************************
          * AICP Secure Settings end
          *****************************/

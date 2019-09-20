6f4aa20df3b2af49ba28940403d0c06c780aaba0
LuK1337 <priv.luk@gmail.com>
2019-09-20 18:30:03 +0200

    base: Add navbar layout inversion tuning [1/2]
    * Originally implemented by jesec but since
      then has been reworked by me to use RTL
      instead of replacing left / right buttons.
    
    idoybh:
    * Use existing settings observer instead of tunables
    * Properly add the setting to Settings.java So we can back it up
    * Get rid of useless global var
    
    @jhonboy121:
    * use NavigationBarModeController to update layout on settings change
    
    Change-Id: I6159f0232ba2ee9f45d2afbba7d11072d902d9ac
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -103,6 +103,7 @@ public class NavigationBarInflaterView extends FrameLayout
     private int mNavBarMode = NAV_BAR_MODE_3BUTTON;
 
     private int mHomeHandleWidthMode = 0;
+    private boolean mNavBarLayoutInverse = false;
 
     public NavigationBarInflaterView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -111,6 +112,8 @@ public class NavigationBarInflaterView extends FrameLayout
         final NavigationModeController controller = Dependency.get(NavigationModeController.class);
         mNavBarMode = controller.addListener(this);
         mHomeHandleWidthMode = controller.getNavigationHandleWidthMode();
+        mNavBarLayoutInverse = controller.shouldInvertNavBarLayout();
+        updateLayoutInversion();
     }
 
     @VisibleForTesting
@@ -167,9 +170,35 @@ public class NavigationBarInflaterView extends FrameLayout
     }
 
     @Override
+    public void onNavBarLayoutInverseChanged(boolean inverse) {
+        if (mNavBarLayoutInverse == inverse) return;
+        mNavBarLayoutInverse = inverse;
+        if (mNavBarMode != NAV_BAR_MODE_3BUTTON) return;
+        updateLayoutInversion();
+    }
+
+    private void updateLayoutInversion() {
+        if (mNavBarLayoutInverse) {
+            final int layoutDirection = mContext.getResources()
+                .getConfiguration().getLayoutDirection();
+            setLayoutDirection(layoutDirection == LAYOUT_DIRECTION_RTL
+                ? LAYOUT_DIRECTION_LTR
+                : LAYOUT_DIRECTION_RTL);
+        } else {
+            setLayoutDirection(LAYOUT_DIRECTION_INHERIT);
+        }
+    }
+
+    @Override
     protected void onDetachedFromWindow() {
         Dependency.get(NavigationModeController.class).removeListener(this);
         super.onDetachedFromWindow();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        updateLayoutInversion();
     }
 
     public void onLikelyDefaultLayoutChange() {

c1eaced52f3c1f6b3c1588b93163fe556bf2c780
Kshitij Gupta <kshitijgm@gmail.com>
2019-09-21 09:42:53 +0530

    Position "At a Glance" on type clock below status bar.
    Change summary:
     - Position below status bar on AOD, and account for burn in prevention
       movement.
     - Position below lock icon on lock screen.
    
    Change-Id: Ie575764143887a11ede3b85fa109db651814a639

--- packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -52,6 +52,11 @@ public class TypeClockController implements ClockPlugin {
     private final SysuiColorExtractor mColorExtractor;
 
     /**
+     * Computes preferred position of clock.
+     */
+    private final SmallClockPosition mClockPosition;
+
+    /**
      * Renders preview from clock view.
      */
     private final ViewPreviewer mRenderer = new ViewPreviewer();
@@ -84,6 +89,7 @@ public class TypeClockController implements ClockPlugin {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mClockPosition = new SmallClockPosition(res);
     }
 
     private void createViews() {
@@ -156,7 +162,7 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public int getPreferredY(int totalHeight) {
-        return totalHeight / 2;
+        return mClockPosition.getPreferredY();
     }
 
     @Override
@@ -189,6 +195,7 @@ public class TypeClockController implements ClockPlugin {
         if (mDarkController != null) {
             mDarkController.setDarkAmount(darkAmount);
         }
+        mClockPosition.setDarkAmount(darkAmount);
     }
 
     @Override

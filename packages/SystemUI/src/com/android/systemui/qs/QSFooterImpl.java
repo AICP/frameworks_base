953371eca2ab324575f3083842b3039b614eddc7
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2019-09-28 14:52:55 +0200

    SystemUI: Show something usefull in the QS footer
     * Show #AICP instead of that useless build version.
    
    Change-Id: I012b70d5b1ad247f7ee34b545c043a6d4d556475

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -169,10 +169,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         /*TextView v = findViewById(R.id.build);
         if (v == null) return;
         if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE,
-                    Build.ID));
+            v.setText("#AICP");
             v.setVisibility(View.VISIBLE);
         } else {
             v.setVisibility(View.GONE);

9a1f1db0286b50c17bb44d203ed5f3efff7a5f12
Ali B <abittin@gmail.com>
2019-10-06 16:43:49 +0300

    base: core: bring back more aicp utils
    Change-Id: I0a404a981ef1682cbb71170442eb818cde39224b

--- core/java/com/android/internal/util/aicp/DeviceUtils.java
+++ core/java/com/android/internal/util/aicp/DeviceUtils.java
@@ -24,22 +24,25 @@ import android.content.res.Resources;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.net.ConnectivityManager;
 import android.nfc.NfcAdapter;
 import android.os.SystemProperties;
 import android.os.Vibrator;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.DisplayInfo;
 import android.view.WindowManager;
-import android.provider.Settings;
+import android.util.Log;
 
 import com.android.internal.telephony.PhoneConstants;
 import static android.hardware.Sensor.TYPE_LIGHT;
 import static android.hardware.Sensor.TYPE_PROXIMITY;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DeviceUtils {
@@ -48,6 +51,27 @@ public class DeviceUtils {
     public static final int DEVICE_PHONE  = 0;
     public static final int DEVICE_HYBRID = 1;
     public static final int DEVICE_TABLET = 2;
+
+/*    public static boolean deviceSupportsMobileData(Context context) {
+        ConnectivityManager cm =
+            (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
+    }
+*/
+    public static boolean deviceSupportsBluetooth() {
+        return (BluetoothAdapter.getDefaultAdapter() != null);
+    }
+
+/*    public static boolean deviceSupportsLte(Context context) {
+        final TelephonyManager tm =
+            (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE);
+                    // || tm.getLteOnGsmMode() != 0; // add back if when we have support on LP for it
+    }
+*/
+    public static boolean deviceSupportsGps(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+    }
 
     public static boolean deviceSupportsNfc(Context context) {
         return (NfcAdapter.getDefaultAdapter(context) != null) ||
@@ -123,5 +147,73 @@ public class DeviceUtils {
     public static boolean isDataEncrypted() {
         String voldState = SystemProperties.get("vold.decrypt");
         return "1".equals(voldState) || "trigger_restart_min_framework".equals(voldState);
+    }
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException | AssertionError e) {
+            // Ignore
+        }
+        return false;
+    }
+
+    public static boolean deviceSupportsCompass(Context ctx) {
+        SensorManager sm = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
+        return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
+                && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
+    }
+
+    public static FilteredDeviceFeaturesArray filterUnsupportedDeviceFeatures(Context context,
+            String[] valuesArray, String[] entriesArray) {
+        if (valuesArray == null || entriesArray == null || context == null) {
+            return null;
+        }
+        List<String> finalEntries = new ArrayList<String>();
+        List<String> finalValues = new ArrayList<String>();
+        FilteredDeviceFeaturesArray filteredDeviceFeaturesArray =
+            new FilteredDeviceFeaturesArray();
+
+        for (int i = 0; i < valuesArray.length; i++) {
+            if (isSupportedFeature(context, valuesArray[i])) {
+                finalEntries.add(entriesArray[i]);
+                finalValues.add(valuesArray[i]);
+            }
+        }
+        filteredDeviceFeaturesArray.entries =
+            finalEntries.toArray(new String[finalEntries.size()]);
+        filteredDeviceFeaturesArray.values =
+            finalValues.toArray(new String[finalValues.size()]);
+        return filteredDeviceFeaturesArray;
+    }
+
+    private static boolean isSupportedFeature(Context context, String action) {
+        if (action.equals(ActionConstants.ACTION_TORCH)
+                        && !deviceSupportsFlashLight(context)
+                || action.equals(ActionConstants.ACTION_VIB)
+                        && !deviceSupportsVibrator(context)
+                || action.equals(ActionConstants.ACTION_VIB_SILENT)
+                        && !deviceSupportsVibrator(context)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static class FilteredDeviceFeaturesArray {
+        public String[] entries;
+        public String[] values;
     }
 }

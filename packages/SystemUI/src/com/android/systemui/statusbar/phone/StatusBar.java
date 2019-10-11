fd39b324c015b90f71509034322c4d44870bb26d
Ali B <abittin@gmail.com>
2019-10-11 20:04:08 +0300

    qs and qqs layout fixes
    Change-Id: Ifc94a997ccebda435b06175a6a35c8354196b451

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -668,6 +668,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS),
                     false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.OMNI_QS_LAYOUT_COLUMNS),
+                    false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4505,6 +4511,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         if (mNotificationShadeWindowViewController != null) {
             mNotificationShadeWindowViewController.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+        }
+
+        if (mQSPanel != null) {
+            mQSPanel.updateSettings();
+        }
+        if (mQuickQSPanel != null) {
+            mQuickQSPanel.updateSettings();
         }
     }
 }

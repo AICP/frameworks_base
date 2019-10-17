0ab91333e957f279e357cf7c32d750d421216988
Julian Veit <claymore1298@gmail.com>
2019-10-17 04:15:53 +0300

    base: Allow toggling QS footer text [1/2]
    Adopted logic for A16 QPR1.
    Also added a new Property to scale font weight.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    SystemUI: Allow customizing footer text [1/2]
    
    * @jhonboy121: simplified conditionals
    * @semdoc    : AICPfied
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Change-Id: I06f062b8d180ed381d47fe51081adf88131a1291
    Signed-off-by: Julian Veit <claymore1298@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6954,6 +6954,24 @@ public final class Settings {
         public static final String SMART_PIXELS_ON_POWER_SAVE = "smart_pixels_on_power_save";
 
         /**
+         * Whether to show qs footer text
+         * @hide
+         */
+        public static final String QS_FOOTER_TEXT_SHOW = "qs_footer_text_show";
+
+        /**
+         * QS footer text
+         * @hide
+         */
+        public static final String QS_FOOTER_TEXT_STRING = "qs_footer_text_string";
+
+	/**
+         * QS footer text font weight
+         * @hide
+         */
+        public static final String QS_FOOTER_TEXT_FONT_WEIGHT = "qs_footer_text_font_weight";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -7130,6 +7148,9 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_BLEND_COLORS_REVERSE);
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_BLEND_DARK_COLORS_REVERSE);
             PRIVATE_SETTINGS.add(SENSOR_BLOCK);
+            PRIVATE_SETTINGS.add(QS_FOOTER_TEXT_SHOW);
+            PRIVATE_SETTINGS.add(QS_FOOTER_TEXT_STRING);
+            PRIVATE_SETTINGS.add(QS_FOOTER_TEXT_FONT_WEIGHT);
         }
 
         /**
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -160,7 +160,10 @@ public class SystemSettings {
                 Settings.System.STATUS_BAR_LOGO_POSITION,
                 Settings.System.STATUS_BAR_LOGO_STYLE,
                 Settings.System.STATUS_BAR_LOGO_COLOR_ACCENT,
-                Settings.System.SENSOR_BLOCK
+                Settings.System.SENSOR_BLOCK,
+                Settings.System.QS_FOOTER_TEXT_SHOW,
+                Settings.System.QS_FOOTER_TEXT_STRING,
+                Settings.System.QS_FOOTER_TEXT_FONT_WEIGHT
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);

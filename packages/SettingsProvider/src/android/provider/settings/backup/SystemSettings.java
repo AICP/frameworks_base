23aeaad02c84b666c823b916692057c5a98ae395
ZeNiXxX <zenixxx.havoc@gmail.com>
2019-10-27 20:43:00 +0100

    SystemUI: Add QS System Info [1/2]
    Lets use the empty space to show some information
    
    thx to @neobuddy89 for his help
    
    Change-Id: Ib99a8cf3e829d349e9bccd02ebdf70783f4e4dea
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -296,5 +296,6 @@ public class SystemSettings {
         Settings.System.HEADS_UP_NOTIFICATION_SNOOZE,
         Settings.System.HEADS_UP_TIMEOUT,
         Settings.System.HEADS_UP_BLACKLIST_VALUES,
+        Settings.System.QS_SYSTEM_INFO,
     };
 }

bc5965e728d81a638ef31d1ae2cc89f81324547f
SpiritCroc <dev@spiritcroc.de>
2019-10-28 09:31:48 +0100

    fwb: Slim recents W / 16.1
    Since Android 15, not much left to do in fwb anymore, as the launcher
    does recents these days.
    
    Partly includes:
    
    commit fbbe5de6640ac0b8a52e948d246b19b4487ce521
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sat Oct 21 14:46:24 2017 +0200
    
        Slim Recents 3.0 [1/3]
    
        Refactored/improved everything.
        Slim Recents won't never die.
        http://i.imgur.com/PO3RSeg.gif
    
        [SpiritCroc]
        Bring to P
    
        Change-Id: I9deb0412e2421df2ac2755f2111a3cec70a7d2c4
    
    commit c14325728a9d87d35e2137bb6f0dba77769592df
    Author: SpiritCroc <dev@spiritcroc.de>
    Date:   Tue Apr 2 11:06:14 2019 +0200
    
        Slim recents old enter/exit animation [1/3]
    
        Change-Id: Ib10f54d56de9c4dc6cab827c21cca9363ffaa820
    
    Change-Id: I3c3486fa94a35f17e48c7a2943cad7fea5c69e78

--- services/core/java/com/android/server/wm/RecentTasks.java
+++ services/core/java/com/android/server/wm/RecentTasks.java
@@ -18,6 +18,7 @@ package com.android.server.wm;
 
 import static android.app.ActivityManager.RECENT_IGNORE_UNAVAILABLE;
 import static android.app.ActivityManager.RECENT_WITH_EXCLUDED;
+import static android.app.ActivityManager.SLIM_RECENTS;
 import static android.app.ActivityTaskManager.INVALID_TASK_ID;
 import static android.app.WindowConfiguration.ACTIVITY_TYPE_ASSISTANT;
 import static android.app.WindowConfiguration.ACTIVITY_TYPE_DREAM;
@@ -978,6 +979,7 @@ class RecentTasks {
     private ArrayList<ActivityManager.RecentTaskInfo> getRecentTasksImpl(int maxNum, int flags,
             boolean getTasksAllowed, int userId, int callingUid) {
         final boolean withExcluded = (flags & RECENT_WITH_EXCLUDED) != 0;
+        final boolean slimRecents = (flags & SLIM_RECENTS) != 0;
         final Set<Integer> includedUsers = getProfileIds(userId);
         includedUsers.add(Integer.valueOf(userId));
 
@@ -987,7 +989,7 @@ class RecentTasks {
         for (int i = 0; i < size; i++) {
             final Task task = mTasks.get(i);
 
-            if (isVisibleRecentTask(task)) {
+            if (isVisibleRecentTask(task, slimRecents)) {
                 numVisibleTasks++;
                 if (isInVisibleRange(task, i, numVisibleTasks, withExcluded)) {
                     // Fall through
@@ -1453,6 +1455,10 @@ class RecentTasks {
      */
     @VisibleForTesting
     boolean isVisibleRecentTask(@NonNull Task task) {
+        return isVisibleRecentTask(task, false);
+    }
+
+    private boolean isVisibleRecentTask(Task task, boolean slimRecents) {
         if (DEBUG_RECENTS_TRIM_TASKS) {
             Slog.d(TAG, "isVisibleRecentTask: task=" + task
                     + " isForceExcludedFromRecents=" + task.isForceExcludedFromRecents()

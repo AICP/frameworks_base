dfe39d0b15d28fa50d26b3ba4414f4ada70e19cf
Daniel Norman <danielnorman@google.com>
2019-11-01 15:59:48 -0700

    ImsManager.Connector became FeatureConnector.
    See https://r.android.com/1132314
    
    Bug: 144056746
    Change-Id: I63ac8620390963583bedbd1bcd469890bdb0883a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -50,6 +50,7 @@ import android.util.Log;
 
 import com.android.ims.ImsException;
 import com.android.ims.ImsManager;
+import com.android.ims.FeatureConnector;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.settingslib.Utils;
 import com.android.settingslib.graph.SignalDrawable;
@@ -104,7 +105,7 @@ public class MobileSignalController extends SignalController<
     private boolean mDataDisabledIcon;
 
     private ImsManager mImsManager;
-    private ImsManager.Connector mImsManagerConnector;
+    private FeatureConnector<ImsManager> mFeatureConnector;
     private int mCallState = TelephonyManager.CALL_STATE_IDLE;
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
@@ -139,8 +140,18 @@ public class MobileSignalController extends SignalController<
         updateDataSim();
 
         int phoneId = mSubscriptionInfo.getSimSlotIndex();
-        mImsManagerConnector = new ImsManager.Connector(mContext, phoneId,
-                new ImsManager.Connector.Listener() {
+        mFeatureConnector = new FeatureConnector(mContext, phoneId,
+                new FeatureConnector.Listener<ImsManager> () {
+                    @Override
+                    public boolean isSupported() {
+                        return true;
+                    }
+
+                    @Override
+                    public ImsManager getFeatureManager() {
+                        return ImsManager.getInstance(mContext, phoneId);
+                    }
+
                     @Override
                     public void connectionReady(ImsManager manager) throws ImsException {
                         Log.d(mTag, "ImsManager: connection ready.");
@@ -266,7 +277,7 @@ public class MobileSignalController extends SignalController<
                 true, mObserver);
         mContext.registerReceiver(mVolteSwitchObserver,
                 new IntentFilter("org.codeaurora.intent.action.ACTION_ENHANCE_4G_SWITCH"));
-        mImsManagerConnector.connect();
+        mFeatureConnector.connect();
     }
 
     /**
@@ -276,7 +287,7 @@ public class MobileSignalController extends SignalController<
         mPhone.listen(mPhoneStateListener, 0);
         mContext.getContentResolver().unregisterContentObserver(mObserver);
         mContext.unregisterReceiver(mVolteSwitchObserver);
-        mImsManagerConnector.disconnect();
+        mFeatureConnector.disconnect();
     }
 
     /**

7f1f06644f1e1b0c0a4074213484e48737987a51
ezio84 <brabus84@gmail.com>
2019-11-14 00:56:59 +0300

    PhoneWindowManager: Allow torch during ambient display
    This allows long press power button for torch to work during ambient display.
    
    @eyosen: bring back isDozeMode for convenience
    
    shagbag913: partial pick from:
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Fri Feb 2 01:24:34 2018 -0500
        base: Support binding the power button to flashlight
        Thanks to beanstown106 for the initial longpress action calls in
        PhoneWindowManager (improved by lineage guys)
        [cut]
        Allow torch action also on ambient display
    
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1163,7 +1163,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mPowerKeyHandled = true;
             mHandler.removeMessages(MSG_POWER_LONG_PRESS);
             // See if we deferred screen wake because long press power for torch is enabled
-            if (mResolvedLongPressOnPowerBehavior == LONG_PRESS_POWER_TORCH && !isScreenOn()) {
+            if (mResolvedLongPressOnPowerBehavior == LONG_PRESS_POWER_TORCH &&
+                    (!isScreenOn() || isDozeMode())) {
                 wakeUpFromPowerKey(SystemClock.uptimeMillis());
             }
         }
@@ -1433,7 +1434,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (FactoryTest.isLongPressOnPowerOffEnabled()) {
             return LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM;
         }
-        if (mTorchLongPressPowerEnabled && !isScreenOn()) {
+        if (mTorchLongPressPowerEnabled && (!isScreenOn() || isDozeMode())) {
             return LONG_PRESS_POWER_TORCH;
         }
         return mLongPressOnPowerBehavior;

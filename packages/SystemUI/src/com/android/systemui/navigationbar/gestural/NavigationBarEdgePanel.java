c6ca225a29f0ef15f241a56aa84689e06fc9cc09
Ali B <abittin@gmail.com>
2019-11-18 23:37:39 +0300

    Allow to hide arrow for back gesture [1/2]
    Thanks to Nicolas Dhouailly <ndhouailly@gmail.com>
    for the original commit for 10 in
    ChangeID Ia1465c7ce47ffa6da719e40140857b94646ac402
    
    Complete rewrite for 11 taking advantage of existing
    	 GestureNavigationSettingsObserver
    
    @eyosen: Adapted to 12
    
    Change-Id: Ibf0a8083845ca1a171cb8bb8a8569765fc96e99b

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
@@ -207,6 +207,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
      * The current translation of the arrow
      */
     private float mCurrentTranslation;
+    private boolean mBackArrowVisibility;
     /**
      * Where the arrow will be in the resting position.
      */
@@ -448,6 +449,11 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         mWindowManager.addView(this, mLayoutParams);
     }
 
+    @Override
+    public void setBackArrowVisibility(boolean backArrowVisibility) {
+        mBackArrowVisibility = backArrowVisibility;
+    }
+
     /**
      * Adjusts the sampling rect to conform to the actual visible bounding box of the arrow.
      */
@@ -500,7 +506,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
                 resetOnDown();
                 mStartX = event.getX();
                 mStartY = event.getY();
-                setVisibility(VISIBLE);
+                setVisibility(mBackArrowVisibility ? VISIBLE : INVISIBLE);
                 updatePosition(event.getY());
                 mRegionSamplingHelper.start(mSamplingRect);
                 mWindowManager.updateViewLayout(this, mLayoutParams);

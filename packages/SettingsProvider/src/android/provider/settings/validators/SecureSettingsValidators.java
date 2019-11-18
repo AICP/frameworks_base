3206c26ee9c64117e609e791e7fe11b0040b369d
Ali B <abittin@gmail.com>
2019-11-18 23:37:39 +0300

    Allow to hide arrow for back gesture [1/2]
    Thanks to Nicolas Dhouailly <ndhouailly@gmail.com>
    for the original commit for 10 in
    ChangeID Ia1465c7ce47ffa6da719e40140857b94646ac402
    
    Complete rewrite for 11 taking advantage of existing
    	 GestureNavigationSettingsObserver
    
    @eyosen: Adapted to 12
    
    Change-Id: Ibf0a8083845ca1a171cb8bb8a8569765fc96e99b

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -352,5 +352,6 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.SHOW_WIFI_STANDARD_ICON, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.VOLUME_LINK_NOTIFICATION, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.GESTURE_NAVBAR_LENGTH_MODE, new InclusiveIntegerRangeValidator(0, 2));
+        VALIDATORS.put(Secure.SHOW_BACK_ARROW_GESTURE, BOOLEAN_VALIDATOR);
     }
 }

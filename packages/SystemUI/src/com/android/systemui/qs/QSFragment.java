d1a9b04b52b7473368925481e1806424e51b7ffc
Ali B <abittin@gmail.com>
2019-11-27 15:25:28 +0300

    SysUI: System Info: observe in statusbar
    There already is a central observer in our statusbar
    so no need to have a second.
    
    Change-Id: I6938d678ab1604e5d55257e7078a070b7c46728a

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -431,6 +431,10 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         mQuickQSPanelController.setBrightnessMirror(brightnessMirrorController);
     }
 
+    public QuickStatusBarHeader getQuickStatusBarHeader() {
+        return mHeader;
+    }
+
     @Override
     public boolean isShowingDetail() {
         return mQSCustomizerController.isCustomizing() || mQSDetail.isShowingDetail();

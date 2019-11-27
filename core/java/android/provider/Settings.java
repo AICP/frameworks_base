27b9535c9e041feb9937c4f0b856207e1b378107
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: old style mobile data indicators [1/2]
    neobuddy89: Use Tuner API.
    Change-Id: I492c4007272037983f0da5fd9e972c2784fc19fa

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5965,6 +5965,11 @@ public final class Settings {
          */
         public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
 
+        /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6140,6 +6145,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_QS_DATE);
             PRIVATE_SETTINGS.add(STATUS_BAR_CLOCK_SIZE);
             PRIVATE_SETTINGS.add(QS_HEADER_CLOCK_SIZE);
+            PRIVATE_SETTINGS.add(USE_OLD_MOBILETYPE);
         }
 
         /**

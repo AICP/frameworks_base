0c3a7dfda004bd2da800714b2d39846f0979445a
ezio84 <brabus84@gmail.com>
2019-11-29 23:46:55 +0100

    Screenrecord: Remember last recording options
    Change-Id: Iaeb4dab6c15c57e91767c3741018ef88fb02c604

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6178,6 +6178,30 @@ public final class Settings {
          */
         public static final String FLASHLIGHT_ON_CALL_WAITING = "flashlight_on_call_waiting";
 
+        /**
+         * Screenrecord: enable mic
+         * @hide
+         */
+        public static final String SCREENRECORD_ENABLE_MIC = "screenrecord_enable_mic";
+
+        /**
+         * Screenrecord: show taps
+         * @hide
+         */
+        public static final String SCREENRECORD_SHOW_TAPS = "screenrecord_show_taps";
+
+        /**
+         * Screenrecord: show stop dot
+         * @hide
+         */
+        public static final String SCREENRECORD_STOP_DOT = "screenrecord_stop_dot";
+
+        /**
+         * Screenrecord: low quality
+         * @hide
+         */
+        public static final String SCREENRECORD_LOW_QUALITY = "screenrecord_low_quality";
+
         /***************************
          * AICP System Settings end
          ***************************/

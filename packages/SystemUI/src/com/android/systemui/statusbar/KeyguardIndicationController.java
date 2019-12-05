5bd4cf1b59af884bbe8dc2bc1b7464056f1cccca
Anushek Prasal <anushekprasal@gmail.com>
2019-12-05 21:16:13 +0530

    BatteryService: Add Warp charging support
    Stallix: Port to R
    
    Eyosen: Port to S
    Not really needed but feels good to have it. Based on https://github.com/Havoc-OS/android_frameworks_base/commit/9fcb3663e315d2c2884a66d951b1dd30a9c034bb
    
    Change-Id: I6bdc72a0654bed75bfcf595049378e61736d28d8

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -851,6 +851,11 @@ public class KeyguardIndicationController {
                             ? R.string.keyguard_indication_charging_time_fast
                             : R.string.keyguard_plugged_in_charging_fast;
                     break;
+                case BatteryStatus.CHARGING_WARP:
+                    chargingId = hasChargingTime
+                            ? R.string.keyguard_indication_warp_charging_time
+                            : R.string.keyguard_plugged_in_warp_charging;
+                    break;
                 case BatteryStatus.CHARGING_SLOWLY:
                     chargingId = hasChargingTime
                             ? R.string.keyguard_indication_charging_time_slowly

omnirom/android_frameworks_base_old

[1/2] Add a whitelist for location providers outside of /system
Based on a patch by danielegobbetti@github:
https://github.com/microg/android_packages_apps_UnifiedNlp/commit/f5a4569e9145d45678b76dc8cb5f4aa582c0ebdb
Additionally, a whitelist for packages is added based on a prop.
With the whitelist, it is possible to selectively disable PackageManager.MATCH_SYSTEM_ONLY when needed.

Change-Id: Ibe99a1ef6ab8c88945f47de95615e1fb869f0cdb

services/core/java/com/android/server/ServiceWatcher.java
@@ -31,6 +31,7 @@ import android.content.pm.Signature;
 import android.content.res.Resources;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.util.Log;
 import android.util.Slog;
@@ -52,6 +53,7 @@ public class ServiceWatcher implements ServiceConnection {
     private static final boolean D = false;
     public static final String EXTRA_SERVICE_VERSION = "serviceVersion";
     public static final String EXTRA_SERVICE_IS_MULTIUSER = "serviceIsMultiuser";
+    private static final String WHITELIST_PACKAGELIST_PROPERTY = "ro.services.whitelist.packagelist";
 
     private final String mTag;
     private final Context mContext;
@@ -88,12 +90,22 @@ public class ServiceWatcher implements ServiceConnection {
             List<String> initialPackageNames) {
         PackageManager pm = context.getPackageManager();
         ArrayList<HashSet<Signature>> sigSets = new ArrayList<HashSet<Signature>>();
+
+        String whitelistPackagesValue = SystemProperties.get(WHITELIST_PACKAGELIST_PROPERTY,"");
+        String[] whitelistPackagesArray = whitelistPackagesValue.split(",");
+        HashSet<String> whitelistPackagesSet = new HashSet<String>(Arrays.asList(whitelistPackagesArray));
+
         for (int i = 0, size = initialPackageNames.size(); i < size; i++) {
             String pkg = initialPackageNames.get(i);
             try {
                 HashSet<Signature> set = new HashSet<Signature>();
-                Signature[] sigs = pm.getPackageInfo(pkg, PackageManager.MATCH_SYSTEM_ONLY
-                        | PackageManager.GET_SIGNATURES).signatures;
+                Signature[] sigs = null;
+                if(whitelistPackagesSet != null && whitelistPackagesSet.contains(pkg)) {
+                    sigs = pm.getPackageInfo(pkg, PackageManager.GET_SIGNATURES).signatures;
+                } else {
+                    sigs = pm.getPackageInfo(pkg, PackageManager.MATCH_SYSTEM_ONLY
+                            | PackageManager.GET_SIGNATURES).signatures;
+                }
                 set.addAll(Arrays.asList(sigs));
                 sigSets.add(set);
             } catch (NameNotFoundException e) {

AICP/frameworks_base

services/core/java/com/android/server/ServiceWatcher.java
@@ -35,6 +35,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.RemoteException;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.util.Log;
 import android.util.Slog;
@@ -64,6 +65,8 @@ public class ServiceWatcher implements ServiceConnection {
     public static final String EXTRA_SERVICE_VERSION = "serviceVersion";
     public static final String EXTRA_SERVICE_IS_MULTIUSER = "serviceIsMultiuser";
 
+    private static final String WHITELIST_PACKAGELIST_PROPERTY = "ro.services.whitelist.packagelist";
+
 
     /** Function to run on binder interface. */
     public interface BinderRunner {
@@ -85,11 +88,22 @@ public class ServiceWatcher implements ServiceConnection {
         PackageManager pm = context.getPackageManager();
 
         ArrayList<HashSet<Signature>> signatureSets = new ArrayList<>(packageNames.length);
+
+        String whitelistPackagesValue = SystemProperties.get(WHITELIST_PACKAGELIST_PROPERTY,"");
+        String[] whitelistPackagesArray = whitelistPackagesValue.split(",");
+        HashSet<String> whitelistPackagesSet = new HashSet<String>(Arrays.asList(whitelistPackagesArray));
+
         for (String packageName : packageNames) {
             try {
-                Signature[] signatures = pm.getPackageInfo(packageName,
-                        PackageManager.MATCH_SYSTEM_ONLY
-                                | PackageManager.GET_SIGNATURES).signatures;
+                Signature[] signatures = null;
+                if(whitelistPackagesSet != null && whitelistPackagesSet.contains(packageName)) {
+                    signatures = pm.getPackageInfo(packageName, PackageManager.GET_SIGNATURES).signatures;
+                    Log.i("ServiceWatcher", packageName + " is whitelisted, ignored PackageManager.MATCH_SYSTEM_ONLY");
+                } else {
+                    signatures = pm.getPackageInfo(packageName,
+                            PackageManager.MATCH_SYSTEM_ONLY
+                                    | PackageManager.GET_SIGNATURES).signatures;
+                }
 
                 HashSet<Signature> set = new HashSet<>();
                 Collections.addAll(set, signatures);
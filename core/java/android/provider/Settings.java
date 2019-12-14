09c4b5934d6e46e4a4ebdadd23cc06e3f313b636
Ido Ben-Hur <idoybh2@gmail.com>
2019-12-14 18:50:43 +0200

    SystemUI: Add auto color option for edge lightning [1/2]
    Change-Id: Ia2070b777f44c59cd65179e979c802de384ce73e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11127,6 +11127,12 @@ public final class Settings {
          */
         public static final String PULSE_AMBIENT_LIGHT_DURATION = "pulse_ambient_light_duration";
 
+        /**
+         * Whether to use color from wallpaper for Ambient edge light
+         * @hide
+         */
+        public static final String PULSE_AMBIENT_LIGHT_AUTO_COLOR = "pulse_ambient_light_auto_color";
+
         /*****************************
          * AICP Secure Settings end
          *****************************/

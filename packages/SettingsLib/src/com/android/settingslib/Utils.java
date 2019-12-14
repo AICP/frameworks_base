178414b68f442b2594bd71ad022a29094858a6c3
Anushek Prasal <anushekprasal@gmail.com>
2019-12-15 02:55:43 +0530

    Display actual charge tech in battery settings [1/2]
    VOOC charge, Warp charge, dash charge whichever one applies
    
    Change-Id: I1e82661d697b922a57349a8d7abd3ef742874adc

--- packages/SettingsLib/src/com/android/settingslib/Utils.java
+++ packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -206,6 +206,15 @@ public class Utils {
             if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
                 if (batteryStatus.isPluggedInWired()) {
                     switch (batteryStatus.getChargingSpeed(context)) {
+                        case BatteryStatus.CHARGING_VOOC:
+                            statusString = res.getString(R.string.battery_info_status_vooc_charging);
+                            break;
+                        case BatteryStatus.CHARGING_DASH:
+                            statusString = res.getString(R.string.battery_info_status_dash_charging);
+                            break;
+                        case BatteryStatus.CHARGING_WARP:
+                            statusString = res.getString(R.string.battery_info_status_warp_charging);
+                            break;
                         case BatteryStatus.CHARGING_FAST:
                             statusString = res.getString(
                                     R.string.battery_info_status_charging_fast);

8ac96d9f3d9f861537459c6ad73a7580acd60a4f
PMS22 <pratham220399@gmail.com>
2019-12-16 00:16:34 -0700

    Add Flashlight blink on incoming calls [1/3]
    - There are tons of apps out there on playstore which replicate the same
      iOS behaviour. Let's just add it in system keeping it disabled by default.
    
    Change-Id: I9d873f4b145416fc13220d3b4da98da1ce638e9e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6033,6 +6033,16 @@ public final class Settings {
          */
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
+        /**
+         * Whether to blink flashlight for incoming calls
+         * 0 = Disabled (Default)
+         * 1 = Blink flashlight only in Ringer mode
+         * 2 = Blink flashlight only in DND mode
+         * 3 = Blink flashlight always regardless of ringer mode
+         * @hide
+         */
+        public static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6217,6 +6227,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(INCREASING_RING);
             PRIVATE_SETTINGS.add(INCREASING_RING_RAMP_UP_TIME);
             PRIVATE_SETTINGS.add(INCREASING_RING_START_VOLUME);
+            PRIVATE_SETTINGS.add(FLASHLIGHT_ON_CALL);
         }
 
         /**

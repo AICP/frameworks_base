5cfb37cab996cccdc612294b15403a1bcc40822c
Ido Ben-Hur <idoybh2@gmail.com>
2019-12-19 15:27:43 +0200

    SystemUI: Allow changing duration of ambient edge light [1/2]
    Change-Id: Iaa592ea09a5653e76446c8dc6c05bb536c77ec3f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11121,6 +11121,12 @@ public final class Settings {
          */
         public static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
 
+        /**
+         * Duration of Ambient edge light in seconds
+         * @hide
+         */
+        public static final String PULSE_AMBIENT_LIGHT_DURATION = "pulse_ambient_light_duration";
+
         /*****************************
          * AICP Secure Settings end
          *****************************/

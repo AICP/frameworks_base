a5542ebf3b31ca9d805811da6b14a11d34a7f16e
LuK1337 <priv.luk@gmail.com>
2020-01-09 18:20:10 +0200

    SystemUI: Add navbar layout inversion tuning (1/2)
    * Originally implemented by jesec but since
      then has been reworked by me to use RTL
      instead of replacing left / right buttons.
    
    Change-Id: I5428ff2e25012ae8cc534982b32fe0aa79e2ef4a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
@@ -53,6 +53,7 @@ public class NavigationBarInflaterView extends FrameLayout
     public static final String NAV_BAR_VIEWS = "sysui_nav_bar";
     public static final String NAV_BAR_LEFT = "sysui_nav_bar_left";
     public static final String NAV_BAR_RIGHT = "sysui_nav_bar_right";
+    public static final String NAV_BAR_INVERSE = "sysui_nav_bar_inverse";
 
     public static final String MENU_IME_ROTATE = "menu_ime";
     public static final String BACK = "back";
@@ -99,6 +100,8 @@ public class NavigationBarInflaterView extends FrameLayout
 
     private OverviewProxyService mOverviewProxyService;
     private int mNavBarMode = NAV_BAR_MODE_3BUTTON;
+
+    private boolean mInverseLayout;
 
     public NavigationBarInflaterView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -150,16 +153,18 @@ public class NavigationBarInflaterView extends FrameLayout
     }
 
     @Override
-    protected void onDetachedFromWindow() {
-        Dependency.get(TunerService.class).removeTunable(this);
-        Dependency.get(NavigationModeController.class).removeListener(this);
-        super.onDetachedFromWindow();
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Dependency.get(TunerService.class).addTunable(this, NAV_BAR_INVERSE);
+	Dependency.get(TunerService.class).addTunable(this, NAV_BAR_VIEWS);
     }
 
     @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        Dependency.get(TunerService.class).addTunable(this, NAV_BAR_VIEWS);
+    protected void onDetachedFromWindow() {
+        Dependency.get(TunerService.class).removeTunable(this);
+        Dependency.get(NavigationModeController.class).removeListener(this);
+        Dependency.get(TunerService.class).removeTunable(this);
+        super.onDetachedFromWindow();
     }
 
     @Override
@@ -167,6 +172,16 @@ public class NavigationBarInflaterView extends FrameLayout
         if (NAV_BAR_VIEWS.equals(key)) {
             setNavigationBarLayout(newValue);
         }
+        if (NAV_BAR_INVERSE.equals(key)) {
+            mInverseLayout = TunerService.parseIntegerSwitch(newValue, false);
+            updateLayoutInversion();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        updateLayoutInversion();
     }
 
     public void setNavigationBarLayout(String layoutValue) {
@@ -281,6 +296,19 @@ public class NavigationBarInflaterView extends FrameLayout
                 true /* landscape */, false /* start */);
 
         updateButtonDispatchersCurrentView();
+    }
+
+    private void updateLayoutInversion() {
+        if (mInverseLayout) {
+            Configuration config = mContext.getResources().getConfiguration();
+            if (config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+                setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
+            } else {
+                setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+            }
+        } else {
+            setLayoutDirection(View.LAYOUT_DIRECTION_INHERIT);
+        }
     }
 
     private void addGravitySpacer(LinearLayout layout) {

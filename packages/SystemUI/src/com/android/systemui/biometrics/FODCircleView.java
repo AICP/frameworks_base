257cd20f7212d3c89a96e5aa8c3268917fc1e11f
XiNGRZ <chenxingyu92@gmail.com>
2020-01-14 19:42:55 +0800

    FODCircleView: Dispatch onPress to HAL only after dimming is applied
    REAPPLY with different ChangeID.
    OrgChange-Id: Iecdbe3bb38f003f5a7cda86b6bf3082691fd33f5
    
    Change-Id: I1efd224993021214521501d0d8ea44ef7427f224

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -96,6 +96,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private boolean mIsAuthenticated;
     private boolean mCanUnlockWithFp;
 
+    private float mCurrentDimAmount = 0.0f;
+
     private Handler mHandler;
 
     private LockPatternUtils mLockPatternUtils;
@@ -279,6 +281,16 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         mFODAnimation = new FODAnimation(context, mPositionX, mPositionY);
 
         mFodPressedImage = res.getBoolean(R.bool.config_fodPressedImage);
+
+        getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            float drawingDimAmount = mParams.dimAmount;
+            if (mCurrentDimAmount == 0.0f && drawingDimAmount > 0.0f) {
+                dispatchPress();
+                mCurrentDimAmount = drawingDimAmount;
+            } else if (mCurrentDimAmount > 0.0f && drawingDimAmount == 0.0f) {
+                mCurrentDimAmount = drawingDimAmount;
+            }
+        });
     }
 
     @Override
@@ -383,7 +395,6 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         setDim(true);
         updateAlpha();
-        dispatchPress();
 
         if (mFodPressedImage) {
             setImageResource(R.drawable.fod_icon_pressed);

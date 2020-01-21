c5b34d3fa78d290433da1110a37760f9d43bb258
00day0 <therandomuser11@gmail.com>
2020-01-21 17:07:42 +0100

    [SQUASHED] SystemUI: QS Header data usage info [1/2]
    Includes:
    
    Author: Ali B <abittin@gmail.com>
    Date:   Tue Jun 23 20:46:35 2020 +0300
    
        SystemUI: DataUsage: use proper connection icons
    
        Change-Id: I54e4eb3be07c64d7758f7010578f3d96b66b55c2
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Thu Jun 18 23:38:13 2020 +0300
    
        SystemUI: QSHeaderDataUsage: hide usage if device is disconnected
    
        - also clean-up DataUsageView
    
        Change-Id: I0b76d46c1ab2ec4b5c9d851ee74aa649bf60491d
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Thu Jun 18 16:09:04 2020 +0300
    
        SystemUI: QS Header data usage info: don't show used
    
        - also remove space between unit and value
    
        Change-Id: I2878b5831539f3ddc568add7a04d9dcb8e9f613b
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Thu May 28 06:24:17 2020 +0530
    
        SystemUI: QSHeaderDataUsage: show Wi-Fi usage when connected to Wi-Fi
    
        Change-Id: I7e3b04c4d20e2bcfeb659457a4f6639250fa55f3
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Thu May 28 06:21:10 2020 +0530
    
        AicpUtils: add method to check if device is connected to Wi-Fi
    
        Extracted from 085cb7424f5b8b451f2c92ef2d2fc81b5f103e0d
    
        Change-Id: I1d0fd663b123c0da66e976ac485f9411d5429756
    
    Author: Alex Cruz <du.alexcruz@gmail.com>
    Date:   Fri Dec 20 10:51:39 2019 -0500
    
        AicpUtils: Add method to check for both wifi and mobile connections
    
        Change-Id: If8f50e519778c03cb249a2ea8c7a7893c5b9f52f
    
    Author: Ali B <abittin@gmail.com>
    Date:   Thu Jun 11 14:59:35 2020 +0300
    
        DataUsageView: move it to QQS Status Icons area
    
        And properly size it to fit with the rest of
        the content there.
        Add some padding as well while making it
        overlayable in device trees
    
        Change-Id: Ic58df39f628e6baf9f407d0b94f4416d6185c38f
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Tue May 26 19:47:35 2020 +0530
    
        QSHeaderDataUsage: add icon and improve view
    
        - also improve system info layout
        - improve layout visibility
        - reduce icon size to match battery icon
    
        Change-Id: I94d3af42d2f907ed3ba11193458f45af0e02bc24
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Sun May 24 11:26:38 2020 +0530
    
        SystemUI: DataUsageDetailView: add ability to show daily data usage
    
        Change-Id: I7b34cc1f8f28428dfb3c335d9f39e18050a6690b
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Sat May 23 18:44:36 2020 +0530
    
        QS header datausage: update view immediately after change
    
        Change-Id: Id8f18c49372e4ca94dec4a230bee0d38648badfe
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Sat May 23 18:31:09 2020 +0530
    
        QS header datausage: add ability to show daily/monthly data usage [1/2]
    
        Change-Id: I9df6ee6e988de3156e4bf2304bb5e2bba0ab3964
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Sat May 23 07:31:36 2020 +0530
    
        SystemUI: QS Header data usage info: improve layout and view
    
        Change-Id: I1ba5e94ba98febe71cac4731e952e312b4060a98
    
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Sat May 23 09:14:37 2020 +0530
    
        AicpUtils: add method to get today's passed time
    
        Change-Id: I27523238f09ef9ed94621f68a668b0bb6d4d68d7
    
    Author: 00day0 <therandomuser11@gmail.com>
    Date:   Tue Jan 21 17:07:42 2020 +0100
    
        SystemUI: QS Header data usage info [1/2]
    
        Change-Id: I05ccd2e48f7533138e8da3a47ae73b618631828c
    
    Change-Id: I05ccd2e48f7533138e8da3a47ae73b618631828c

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -59,6 +59,7 @@ import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LifecycleRegistry;
 
+import com.android.internal.util.aicp.AicpUtils;
 import com.android.internal.util.aicp.FileUtils;
 import com.android.settingslib.Utils;
 import com.android.systemui.BatteryMeterView;
@@ -71,6 +72,7 @@ import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
 import com.android.systemui.qs.QSDetail.Callback;
 import com.android.systemui.qs.carrier.QSCarrierGroup;
 import com.android.systemui.statusbar.CommandQueue;
+import com.android.systemui.statusbar.info.DataUsageView;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.phone.StatusBarIconController.TintedIconManager;
 import com.android.systemui.statusbar.phone.StatusBarWindowView;
@@ -134,6 +136,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
     private int mRingerMode = AudioManager.RINGER_MODE_NORMAL;
     private AlarmManager.AlarmClockInfo mNextAlarm;
+
+    // Data Usage
+    private View mDataUsageLayout;
+    private ImageView mDataUsageImage;
+    private DataUsageView mDataUsageView;
 
     private ImageView mNextAlarmIcon;
     /** {@link TextView} containing the actual text indicating when the next alarm will go off. */
@@ -243,6 +250,13 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mClockView.setQsHeader();
         mDateView = findViewById(R.id.date);
         mDateView.setOnClickListener(this);
+        mDataUsageLayout = findViewById(R.id.daily_data_usage_layout);
+        mDataUsageImage = findViewById(R.id.daily_data_usage_icon);
+        mDataUsageView = findViewById(R.id.data_sim_usage);
+
+        updateDataUsageImage();
+        // Set the correct tint for the data usage icons so they contrast
+        mDataUsageImage.setImageTintList(ColorStateList.valueOf(fillColor));
 
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
@@ -470,6 +484,40 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         updateHeaderImage();
         updateResources();
         setBatteryPercentMode();
+        updateDataUsageView();
+        updateDataUsageImage();
+    }
+
+    private void updateDataUsageView() {
+        if (mDataUsageView.isDataUsageEnabled() != 0) {
+            if (AicpUtils.isConnected(mContext)) {
+                DataUsageView.updateUsage();
+                mDataUsageLayout.setVisibility(View.VISIBLE);
+                mDataUsageImage.setVisibility(View.VISIBLE);
+                mDataUsageView.setVisibility(View.VISIBLE);
+            } else {
+                mDataUsageView.setVisibility(View.GONE);
+                mDataUsageImage.setVisibility(View.GONE);
+                mDataUsageLayout.setVisibility(View.GONE);
+            }
+        } else {
+            mDataUsageView.setVisibility(View.GONE);
+            mDataUsageImage.setVisibility(View.GONE);
+            mDataUsageLayout.setVisibility(View.GONE);
+        }
+    }
+
+    public void updateDataUsageImage() {
+        if (mDataUsageView.isDataUsageEnabled() == 0) {
+            mDataUsageImage.setVisibility(View.GONE);
+        } else {
+            if (AicpUtils.isWiFiConnected(mContext)) {
+                mDataUsageImage.setImageDrawable(mContext.getDrawable(R.drawable.ic_data_usage_wifi));
+            } else {
+                mDataUsageImage.setImageDrawable(mContext.getDrawable(R.drawable.ic_data_usage_cellular));
+            }
+            mDataUsageImage.setVisibility(View.VISIBLE);
+        }
     }
 
     private void updateStatusIconAlphaAnimator() {
@@ -505,6 +553,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateSystemInfoText();
         updateEverything();
+        updateDataUsageView();
     }
 
     /**

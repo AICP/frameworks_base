e0db594a2dabdc5bfc25a695d6f466419cd28b44
Steven Laver <lavers@google.com>
2020-02-18 12:56:05 -0800

    MobileSignalController fix corresponding to upstream FeatureConnector change
    FeatureConnector was changed upstream, in
    platform/frameworks/opt/net/ims:c514e52ea6b0c89ae8f5e2ab8666585ed815900a%5E%21/#F0.
    Adjust code in MobileSignalController to match.
    
    Change-Id: Ie21bf189b70e4e16eb9f31e8b85eb6bc0c9f5f20
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -143,11 +143,6 @@ public class MobileSignalController extends SignalController<
         mFeatureConnector = new FeatureConnector(mContext, phoneId,
                 new FeatureConnector.Listener<ImsManager> () {
                     @Override
-                    public boolean isSupported() {
-                        return true;
-                    }
-
-                    @Override
                     public ImsManager getFeatureManager() {
                         return ImsManager.getInstance(mContext, phoneId);
                     }
@@ -164,7 +159,7 @@ public class MobileSignalController extends SignalController<
                         Log.d(mTag, "ImsManager: connection unavailable.");
                         removeListeners();
                     }
-        });
+        }, "?");
 
 
         mObserver = new ContentObserver(new Handler(receiverLooper)) {

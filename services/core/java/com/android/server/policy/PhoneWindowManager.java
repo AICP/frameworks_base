c9f23998b8d4067437021b04c48822fad49ba31e
ezio84 <brabus84@gmail.com>
2020-02-28 07:20:04 +0300

    Navbar: Add Expand notifications action
    Change-Id: I5340875e1cd9a558705fc01046f3243d0de62057

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6894,6 +6894,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_SCREENSHOT:
                 toggleScreenshot();
                 break;
+            case NavbarUtilities.KEY_ACTION_NOTIFICATIONS:
+                toggleNotifications();
+                break;
         }
     }
 
@@ -6991,5 +6994,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void launchCamera() {
         performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Launch camera");
         ActionHandler.performTask(mContext, ActionHandler.SYSTEMUI_TASK_CAMERA);
+    }
+
+    // Toggle notifications panel
+    private void toggleNotifications() {
+        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Expand notifications");
+        ActionHandler.performTask(mContext, ActionHandler.SYSTEMUI_TASK_NOTIFICATION_PANEL);
     }
 }

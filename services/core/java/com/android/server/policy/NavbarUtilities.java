7453dd8537a74bfb1e45311612914419fafa14f0
Muhammed Nazim <mnm9994u@gmail.com>
2020-02-28 07:20:04 +0300

    Navbar: Add Power menu action
    *DU Edits*
    - Added support for longpress action to existing method for triggering keypress
    - Adapted to our navbar implementation (Original commit below)
    
    iGio90/android_frameworks_base@0e07620
    
    Change-Id: Ifb63254300b1200292ce30e1a819fe87c449c64a

--- services/core/java/com/android/server/policy/NavbarUtilities.java
+++ services/core/java/com/android/server/policy/NavbarUtilities.java
@@ -43,6 +43,7 @@ public class NavbarUtilities {
     public static final int KEY_ACTION_SCREEN_OFF = 12;
     public static final int KEY_ACTION_SCREENSHOT = 13;
     public static final int KEY_ACTION_NOTIFICATIONS = 14;
+    public static final int KEY_ACTION_POWER_MENU = 15;
 
     // Special values, used internal only.
     public static final int KEY_ACTION_HOME = 100;
@@ -98,6 +99,7 @@ public class NavbarUtilities {
             KEY_ACTION_SCREEN_OFF,
             KEY_ACTION_SCREENSHOT,
             KEY_ACTION_NOTIFICATIONS,
+            KEY_ACTION_POWER_MENU,
     };
 
     /**

cbcac3be1c1f7eb2602069674cd3f27739c1f86d
Alex <du.alexcruz@gmail.com>
2020-03-01 16:15:13 -0500

    Navbar: Add kill app action
    Change-Id: Ic865539cc731ade9458d0d30dc5688797cb6a59d

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6922,6 +6922,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_RINGER_MODES:
                 toggleRingerModes();
                 break;
+            case NavbarUtilities.KEY_ACTION_KILL_APP:
+                killForegroundApp();
+                break;
         }
     }
 
@@ -7000,6 +7003,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case NavbarUtilities.KEY_ACTION_RINGER_MODES:
                 toggleRingerModes();
+                break;
+            case NavbarUtilities.KEY_ACTION_KILL_APP:
+                killForegroundApp();
                 break;
         }
     }
@@ -7116,6 +7122,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void toggleRingerModes() {
         performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Toggle ringer modes");
         ActionHandler.performTask(mContext, ActionHandler.SYSTEMUI_TASK_SOUNDMODE_VIB_SILENT);
+    }
+
+    // Kill foreground app
+    private void killForegroundApp() {
+        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Kill foreground app");
+//        ActionHandler.performTask(mContext, ActionHandler.SYSTEMUI_TASK_KILL_PROCESS);
+        ActionHandler.killForegroundApp(mContext, mCurrentUserId);
     }
 
     private static void customAppLaunch(Context context, String action) {

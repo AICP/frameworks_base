41a1a6b50ab169d826814580419737bbfc7bab7a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-09 11:10:34 -0300

    SystemUI: Allow enabling compact layout of navigation bar
    Includes also:
    
    commit 4a73c8ace697c7cb78dca4fe83b6a0b2bfc6bd2d
    Author: Alex <du.alexcruz@gmail.com>
    Date:   Fri Feb 28 04:12:52 2020 -0500
    
        Navbar: Ensure 2/3 button layout options play nice with gesture navbar
    
        The 2 and 3 button navbar layout options were causing the gesture 'pill' to
        disappear when enabled. This commit ensures that the pill survives when switching
        and keeps the user's layout for 2 and 3 button navbar in play when they return.
    
        Test:
        - Enable 2 or 3 button navbar
        - Go into layout options
        - Set the layout to compact or any other option but 'Normal'
        - Enable the gesture navbar
    
        Change-Id: Id4f0bc8c7c155090bb9f7ce24edd5bdbbd7553d5
    
    Change-Id: I68ecb01e7c8d74462be55bab9ad395dd7d3a2d90
    
    Co-authored-by: Alex <du.alexcruz@gmail.com>
    Change-Id: I98441bf5c77f8d3eff9260d546d9c3ed61a0ab1b

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -163,5 +163,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_MATERIAL_DISMISS_STYLE,
         Settings.System.NOTIFICATION_MATERIAL_DISMISS_BGSTYLE,
         Settings.System.NAVIGATION_BAR_INVERSE,
+        Settings.System.NAV_BAR_COMPACT_LAYOUT,
     };
 }

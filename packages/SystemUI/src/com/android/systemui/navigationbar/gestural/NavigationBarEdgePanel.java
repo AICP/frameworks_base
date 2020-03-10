cba91f9a7c08bf431577ddf638a2d793da3b4e81
Nico <nicorg2529@gmail.com>
2020-03-10 21:53:40 +0100

    Extended swipe: show a double arrow when almost triggered
    abcdu: adapted to our code
    Change-Id: If8905cb231e5ea0e993a590ce7f02669b46b8136

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
@@ -150,6 +150,9 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
     private final float mBaseTranslation;
     private final float mArrowLength;
     private final float mArrowThickness;
+    private float mLongSwipeThreshold;
+    private boolean mAlmostLongSwipe;
+    private boolean mIsExtendedSwipe;
 
     /**
      * The minimum delta needed in movement for the arrow to change direction / stop triggering back
@@ -471,6 +474,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         mVelocityTracker.addMovement(event);
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
+                mAlmostLongSwipe = false;
                 mDragSlopPassed = false;
                 resetOnDown();
                 mStartX = event.getX();
@@ -528,6 +532,9 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         float x = (polarToCartX(mCurrentAngle) * mArrowLength);
         float y = (polarToCartY(mCurrentAngle) * mArrowLength);
         Path arrowPath = calculatePath(x,y);
+        if (mAlmostLongSwipe) {
+            arrowPath.addPath(calculatePath(x,y), mArrowThickness * 2.0f * (mIsLeftPanel ? 1 : -1), 0.0f);
+        }
         if (mShowProtection) {
             canvas.drawPath(arrowPath, mProtectionPaint);
         }
@@ -548,6 +555,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         mArrowPaddingEnd = res.getDimensionPixelSize(R.dimen.navigation_edge_panel_padding);
         mMinArrowPosition = res.getDimensionPixelSize(R.dimen.navigation_edge_arrow_min_y);
         mFingerOffset = res.getDimensionPixelSize(R.dimen.navigation_edge_finger_offset);
+        mLongSwipeThreshold = mDisplaySize.x * 0.45f;
     }
 
     private void updateArrowDirection() {
@@ -715,6 +723,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         float x = event.getX();
         float y = event.getY();
         float touchTranslation = MathUtils.abs(x - mStartX);
+        mAlmostLongSwipe = mIsExtendedSwipe && (touchTranslation > mLongSwipeThreshold);
         float yOffset = y - mStartY;
         float delta = touchTranslation - mPreviousTouchTranslation;
         if (Math.abs(delta) > 0) {
@@ -911,6 +920,11 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
 
     private float dp(float dp) {
         return mDensity * dp;
+    }
+
+    @Override
+    public void setLongSwipeEnabled(boolean enabled) {
+        mIsExtendedSwipe = enabled;
     }
 
     @Override

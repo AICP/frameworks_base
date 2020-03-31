50360f40532c0ef568c34c9697c20f8f55496c2c
doc HD <doc.divxm@gmail.com>
2020-03-31 21:35:54 +0200

    Revert "DisplayPolicy: Fix watchdog when adding new display"
    This reverts commit cc40f0674b4c5d0cd9d1f911fd7ba4466e9ef2d1.
    
    Reason for revert: build fails.
    
    Change-Id: Ie09b85d004def6de4eaf73ed32380095e3c603ea

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -603,9 +603,6 @@ public class DisplayPolicy {
             } else if ("0".equals(navBarOverride)) {
                 mHasNavigationBar = true;
             }
-
-            // Register content observer only for main display
-            mSettingsObserver = new SettingsObserver(mHandler);
         } else {
             mHasStatusBar = false;
             mHasNavigationBar = mDisplayContent.supportsSystemDecorations();

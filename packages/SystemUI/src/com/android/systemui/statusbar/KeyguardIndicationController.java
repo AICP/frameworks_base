190d361b394405a42f2ff1b4cca498d61b97c524
calebcabob <calphonic@gmail.com>
2020-04-21 05:15:59 -0400

    LockscreenCharging: Fix up jank and bring in °F/°C toggle [1/2]
    This does the following:
    - Neatly seperates the stats into 2 lines (both centered),
      rather than depending on text wrapping - ultimately looking
      janky.
    
      Replaced some measurement dividers.
    
    - Included a way for users to view battery temps in Fahrenheit
      now, with the option to still use Celsius if wanted.
    
    Change-Id: Ic5187f72cc780512abe15491e1607e1df7dc1a5c
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: calebcabob <calphonic@gmail.com>
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1272,9 +1272,9 @@ public class KeyguardIndicationController {
 
         String batteryInfo = "";
         boolean showbatteryInfo = Settings.System.getIntForUser(mContext.getContentResolver(),
-            Settings.System.LOCKSCREEN_BATTERY_INFO, 0, UserHandle.USER_CURRENT) == 1;
+            Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT) == 1;
         boolean batteryTempUnit = Settings.System.getIntForUser(mContext.getContentResolver(),
-            Settings.System.LOCKSCREEN_BATTERY_INFO_TEMP_UNIT, 1, UserHandle.USER_CURRENT) == 0;
+            Settings.System.LOCKSCREEN_BATTERY_INFO_TEMP_UNIT, 0, UserHandle.USER_CURRENT) == 0;
          if (showbatteryInfo) {
             if (mChargingCurrent >= mCurrentDivider * 1000) {
                 batteryInfo = String.format("%.1f" , (mChargingCurrent / mCurrentDivider / 1000)) + "A";
@@ -1286,16 +1286,16 @@ public class KeyguardIndicationController {
                         String.format("%.1f" , (mChargingWattage / mCurrentDivider / 1000)) + "W";
             }
             if (mChargingVoltage > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
+                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " • ") +
                         String.format("%.1f", (mChargingVoltage / 1000 / 1000)) + "V";
             }
             if (mTemperature > 0) {
                 if (batteryTempUnit) {
-                    batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                            Math.round(mTemperature / 10 * 9f / 5f + 32) + "°F";
-                } else {
-                    batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                            mTemperature / 10 + "°C";
+                    batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " • ") +
+                            Math.round(mTemperature / 10 * 9f / 5f + 32) + "°F" + "\n";
+                    } else {
+                    batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " • ") +
+                        mTemperature / 10 + "°C" + "\n";
                 }
             }
             if (batteryInfo != "") {

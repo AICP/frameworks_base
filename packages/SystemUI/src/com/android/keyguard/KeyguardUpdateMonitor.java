1b7c4821644121f504af162ab5f840b007e0b4fc
karthick111 <karthickrko61@gmail.com>
2020-04-23 09:27:03 +0000

    BatteryService: Add VOOC charging support
    * Based on: https://github.com/ForkLineageOS/android_frameworks_base/commit/81e2ed461f5d3a206570bd748e353932d7cf4bc9
    
    Stallix: Port to R
    
    Eyosen: Ported to S
    
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I3823d67bb256ac6a44a5f93e584a2302e10c9997

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2039,7 +2039,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, false, false, true);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, false, false, false, true);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -3351,6 +3351,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         // change in warp charging while plugged in
         if (nowPluggedIn && current.warpChargeStatus != old.warpChargeStatus) {
+            return true;
+        }
+
+        // change in VOOC charging while plugged in
+        if (nowPluggedIn && current.voocChargeStatus != old.voocChargeStatus) {
             return true;
         }
 

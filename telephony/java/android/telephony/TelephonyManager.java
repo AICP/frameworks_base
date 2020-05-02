cb47d7563a17fa22ccbc3e1579acd996d0503677
maxwen <max.weninger@gmail.com>
2020-05-02 20:58:05 +0300

    base: Extended suspend actions (1/3)
    Add 3G toggle to the suspend actions mix as
    it is currently one step below the maximum G
    available (4, 4.5).
    
    Change-Id: I961ce8c755261f18c3c482a29c201069083640ba

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -6114,6 +6114,16 @@ public class TelephonyManager {
     }
 
     /**
+     * @hide
+     */
+    public void toggle3G(boolean threeGees) {
+        try {
+            getITelephony().toggle3G(threeGees);
+        } catch (RemoteException e) {
+        }
+    }
+
+    /**
      * Returns the MMS user agent.
      */
     public String getMmsUserAgent() {

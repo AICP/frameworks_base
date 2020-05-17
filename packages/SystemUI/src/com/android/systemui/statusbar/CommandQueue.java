a1db0df3a07d7a9cb1a70018e2b89e5380c39a16
bigrushdog <randall.rushing@gmail.com>
2020-05-17 09:17:32 -0700

    Pulse navbar audio gfx visualizer for Android 11 [1/4]
    2 of 4 is in external/pulse
    
    Change-Id: I046f37e0a50fd8569d309d5bb48c6c5ebfbafd8f

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -132,6 +132,8 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     private static final int MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW  = 60 << MSG_SHIFT;
     private static final int MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW  = 61 << MSG_SHIFT;
     private static final int MSG_KILL_FOREGROUND_APP               = 62 << MSG_SHIFT;
+    private static final int MSG_SCREEN_PINNING_STATE_CHANGED      = 63 << MSG_SHIFT;
+    private static final int MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED   = 64 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -355,6 +357,10 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         default void toggleCameraFlashState(boolean enable) { }
 
         default void killForegroundApp() { }
+
+        default void screenPinningStateChanged(boolean enabled) {}
+
+        default void leftInLandscapeChanged(boolean isLeft) {}
     }
 
     public CommandQueue(Context context) {
@@ -1029,6 +1035,24 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         }
     }
 
+    @Override
+    public void leftInLandscapeChanged(boolean isLeft) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED);
+            mHandler.obtainMessage(MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED,
+                    isLeft ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
+    @Override
+    public void screenPinningStateChanged(boolean enabled) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_SCREEN_PINNING_STATE_CHANGED);
+            mHandler.obtainMessage(MSG_SCREEN_PINNING_STATE_CHANGED,
+                    enabled ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -1397,6 +1421,16 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                 case MSG_KILL_FOREGROUND_APP:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).killForegroundApp();
+                    }
+                    break;
+                case MSG_SCREEN_PINNING_STATE_CHANGED:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).screenPinningStateChanged(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).leftInLandscapeChanged(msg.arg1 != 0);
                     }
                     break;
             }

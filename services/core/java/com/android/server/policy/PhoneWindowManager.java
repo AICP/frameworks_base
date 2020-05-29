ba8ac48b8f07e6eb516afc426ece621742c0e846
bigrushdog <randall.rushing@gmail.com>
2020-05-29 20:25:39 +0530

    Gestures nav: Add Show power menu function
    Picked up from https://github.com/AICP/frameworks_base/commit/b61fe76abf62cbede90fc4ccbdcfd4bfd9c70929
     
    Trimmed for AICP's ActionHandler
    
    Change-Id: I553df6f2b50cfb72590dbafc65ce378d92549a3e

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6581,6 +6581,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mHandler.removeCallbacks(mScreenshotRunnable);
                 mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_SELECTED_REGION);
                 mHandler.post(mScreenshotRunnable);
+            } else if (ActionHandler.INTENT_SHOW_POWER_MENU.equals(action)) {
+                showGlobalActions();
             }
         }
     }

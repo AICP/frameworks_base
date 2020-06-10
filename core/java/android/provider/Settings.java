0a9600a5fea55ae5e95021ef96057ead24839029
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-10 21:37:59 +0300

    base: Allow to flash on call only when entirely silent [1/3]
    Change-Id: I43b4fc6842931d22c6b1bc0753b24288f9565771

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6037,8 +6037,9 @@ public final class Settings {
          * Whether to blink flashlight for incoming calls
          * 0 = Disabled (Default)
          * 1 = Blink flashlight only in Ringer mode
-         * 2 = Blink flashlight only in DND mode
-         * 3 = Blink flashlight always regardless of ringer mode
+         * 2 = Blink flashlight only when ringer is not audible
+         * 3 = Blink flahslight only when entirely silent
+         * 4 = Blink flashlight always regardless of ringer mode
          * @hide
          */
         public static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";

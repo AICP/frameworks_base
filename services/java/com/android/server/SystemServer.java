03a2bc3b7b5b10b305ff849eaf535ea52bcd857d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-15 15:48:07 -0300

    Initial import of lineagehw
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_lineage-sdk
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: Ief90ce8c20115d6a1c17c84a9874af6246dc1dcd

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -137,6 +137,7 @@ import com.android.server.input.InputManagerService;
 import com.android.server.inputmethod.InputMethodManagerService;
 import com.android.server.integrity.AppIntegrityManagerService;
 import com.android.server.lights.LightsService;
+import com.android.server.lineage.LineageHardwareService;
 import com.android.server.locales.LocaleManagerService;
 import com.android.server.location.LocationManagerService;
 import com.android.server.logcat.LogcatManagerService;
@@ -2520,6 +2521,14 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartMediaMetricsManager");
             mSystemServiceManager.startService(MediaMetricsManagerService.class);
             t.traceEnd();
+
+            // LineageHardware
+            if (!mOnlyCore){
+                t.traceBegin("StartLineageHardwareService");
+                mSystemServiceManager.startService(LineageHardwareService.class);
+                t.traceEnd();
+            }
+
         }
 
         t.traceBegin("StartMediaProjectionManager");

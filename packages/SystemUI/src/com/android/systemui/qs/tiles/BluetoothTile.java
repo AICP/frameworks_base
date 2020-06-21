466cfd950f7f221348a4225ff6e3657223ba0ec1
Michael W <baddaemon87@gmail.com>
2020-06-21 17:22:39 +0200

    SystemUI: Fix issue with Bluetooth tile
    * Currently: Pair a device
    * Open BT tile details ("details"), device is shown
    * Disable bt via the switchbar, close the details via the "done" button
    * Reopen the details -> the list is empty, neither "empty" text nor
      actual devices are shown
    
    After this patch: Reopening the details turns on bt which in turn makes
     devices visible again
    
    Change-Id: I1a0a5ebdcaca50bc821b0180f2109d00091eaa1c

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -291,6 +291,7 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
             refreshState();
             if (isShowingDetail()) {
                 mDetailAdapter.updateItems();
+                mDetailAdapter.setItemsVisible(true);
                 fireToggleStateChanged(mDetailAdapter.getToggleState());
             }
         }

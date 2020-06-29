Singhaarav059/frameworks_base-2

SystemUI:QS Header data usage: Backup settings with proper validator
Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

core/java/android/provider/Settings.java
@@ -6740,6 +6740,9 @@ public final class Settings {
          */
         public static final String QS_DATAUSAGE = "qs_datausage";
 
+        /** @hide */
+        private static final Validator QS_DATAUSAGE_VALIDATOR = ANY_INTEGER_VALIDATOR;
+
         /**
          * Controls whether to show R style notification headers
          * @hide
@@ -7058,6 +7061,7 @@ public final class Settings {
             NOTIFICATION_HEADERS,
             HIDE_SMART_REPLIES,
             STATUS_BAR_CUSTOM_HEADER_HEIGHT,
+            QS_DATAUSAGE,
         };
 
         /**
@@ -7296,6 +7300,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_NEW_TINT);
             PRIVATE_SETTINGS.add(HIDE_SMART_REPLIES);
             PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_HEIGHT);
+            PRIVATE_SETTINGS.add(QS_DATAUSAGE);
         }
 
         /**
@@ -7506,6 +7511,7 @@ public final class Settings {
             VALIDATORS.put(NOTIFICATION_HEADERS, NOTIFICATION_HEADERS_VALIDATOR);
             VALIDATORS.put(HIDE_SMART_REPLIES, HIDE_SMART_REPLIES_VALIDATOR);
             VALIDATORS.put(STATUS_BAR_CUSTOM_HEADER_HEIGHT, STATUS_BAR_CUSTOM_HEADER_HEIGHT_VALIDATOR);
+            VALIDATORS.put(QS_DATAUSAGE, QS_DATAUSAGE_VALIDATOR);
         }
 
         /**

AICP/frameworks_base

Change-Id: I675f3678d199d8d9e0f4dab109e1f48016269dc4

core/java/android/provider/Settings.java
@@ -7112,6 +7112,9 @@ public final class Settings {
          */
         public static final String QS_DATAUSAGE = "qs_datausage";
 
+        /** @hide */
+        private static final Validator QS_DATAUSAGE_VALIDATOR = BOOLEAN_VALIDATOR;
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -7393,6 +7396,7 @@ public final class Settings {
             SCREEN_STATE_TWOG,
             SCREEN_STATE_THREEG,
             GESTURE_NAVBAR_SHOW,
+            QS_DATAUSAGE,
             // AICP Settings end
         };
 
@@ -7745,6 +7749,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SCREEN_STATE_TWOG);
             PRIVATE_SETTINGS.add(SCREEN_STATE_THREEG);
             PRIVATE_SETTINGS.add(GESTURE_NAVBAR_SHOW);
+            PRIVATE_SETTINGS.add(QS_DATAUSAGE);
             // AICP Settings end
         }
 
@@ -8086,6 +8091,7 @@ public final class Settings {
             VALIDATORS.put(SCREEN_STATE_TWOG, SCREEN_STATE_TWOG_VALIDATOR);
             VALIDATORS.put(SCREEN_STATE_THREEG, SCREEN_STATE_THREEG_VALIDATOR);
             VALIDATORS.put(GESTURE_NAVBAR_SHOW,GESTURE_NAVBAR_SHOW_VALIDATOR);
+            VALIDATORS.put(QS_DATAUSAGE, QS_DATAUSAGE_VALIDATOR);
             // AICP Settings end
         }
 
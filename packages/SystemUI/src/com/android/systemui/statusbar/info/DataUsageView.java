DerpLab/platform_frameworks_base

SystemUI: QSHeaderDataUsage: show Wi-Fi usage when connected to Wi-Fi
Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/info/DataUsageView.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.provider.Settings;
 import android.view.View;
 
+import com.android.internal.util.aosip.aosipUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.NetworkController;
@@ -58,8 +59,12 @@ public class DataUsageView extends TextView {
         mobileDataController.setSubscriptionId(
             SubscriptionManager.getDefaultDataSubscriptionId());
         final DataUsageController.DataUsageInfo info = isDataUsageEnabled() == 1 ?
-                mobileDataController.getDailyDataUsageInfo()
-                : mobileDataController.getDataUsageInfo();
+                (aosipUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getDailyWifiDataUsageInfo()
+                        : mobileDataController.getDailyDataUsageInfo())
+                : (aosipUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getWifiDataUsageInfo()
+                        : mobileDataController.getDataUsageInfo());
 
         formatedinfo = formatDataUsage(info.usageLevel);
     }

AICP/frameworks_base

Change-Id: I7e3b04c4d20e2bcfeb659457a4f6639250fa55f3

packages/SystemUI/src/com/android/systemui/statusbar/info/DataUsageView.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.provider.Settings;
 import android.view.View;
 
+import com.android.internal.util.aicp.AicpUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.NetworkController;
@@ -58,9 +59,14 @@ public class DataUsageView extends TextView {
         mobileDataController.setSubscriptionId(
             SubscriptionManager.getDefaultDataSubscriptionId());
         final DataUsageController.DataUsageInfo info = isDataUsageEnabled() == 1 ?
-                mobileDataController.getDailyDataUsageInfo()
-                : mobileDataController.getDataUsageInfo();
-        formatedinfo = formatDataUsage(info.usageLevel) + " ";
+                (AicpUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getDailyWifiDataUsageInfo()
+                        : mobileDataController.getDailyDataUsageInfo())
+                : (AicpUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getWifiDataUsageInfo()
+                        : mobileDataController.getDataUsageInfo());
+
+        formatedinfo = formatDataUsage(info.usageLevel);
     }
 
     public int isDataUsageEnabled() {
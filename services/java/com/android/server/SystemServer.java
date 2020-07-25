6deeef2c82fa2e80fc3f277e8a3d7d3439451e91
rituj <ritujbeniwal@gmail.com>
2020-07-25 15:18:03 +0530

    base: Introduce automated DC Dimming
    * Schedule based on sunset/sunrise
    * Automate based on the average brightness
    * Automate based on both of the above options
    * Add QS Tile to control DC Dimming
    * Disable the service on devices with no DC dimming support
    * Set config_deviceDcDimmingSysfsNode to point to the relevant sysfs kernel node
    
    Co-authored-by: Andrew Fluck <andrew@aospa.co>
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Iaebf6624a8311722de3443c0ff5b033381749d20

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -111,6 +111,7 @@ import com.android.server.contentcapture.ContentCaptureManagerInternal;
 import com.android.server.coverage.CoverageService;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.display.AutoAODService;
+import com.android.server.display.DcDimmingService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
@@ -1302,6 +1303,15 @@ public final class SystemServer {
             t.traceBegin("IorapForwardingService");
             mSystemServiceManager.startService(IorapForwardingService.class);
             t.traceEnd();
+
+            if (!context.getResources().getString(R.string.config_deviceDcDimmingSysfsNode)
+                    .isEmpty()) {
+                t.traceBegin("StartDcDimmingService");
+                mSystemServiceManager.startService(DcDimmingService.class);
+                t.traceEnd();
+            } else {
+                Slog.i(TAG, "Dc Dimming not supported");
+            }
 
             t.traceBegin("SignedConfigService");
             SignedConfigService.registerUpdateReceiver(mSystemContext);

068236513cc11914d8e93b84ae7e6975d60eac4e
spezi77 <spezi7713@gmx.net>
2020-08-02 17:13:00 +0200

    Extend Pulse to Ambient Screen [1/3]
    Many thanks to @neobuddy89 for his help
    
    AOSiP edit:
    *redo commit based on changes made by du 41cc3c16d0c2811f54182a935695278a8875ead0
    *move related code back to external/pulse
    
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I20f3fc6d15bc0cee9b9b7f1b72e47ae9b069d496

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3974,6 +3974,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         return mVisualizerView;
     }
 
+    public VisualizerView getAmVisualizer() {
+        return mVisualizerView;
+    }
+
     /**
      * TODO: Remove this method. Views should not be passed forward. Will cause theme issues.
      * @return bottom area view

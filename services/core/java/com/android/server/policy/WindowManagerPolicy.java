5b1753604eac3ce01509521070eb594af71e5fad
Cosmin Tanislav <demonsingur@gmail.com>
2020-08-16 19:45:00 +0200

    services: unlock fingerprint only on power button press
    Change-Id: I2f569a8ab4d1dd2c118cfb2d1c1072ce6b4e3584

--- services/core/java/com/android/server/policy/WindowManagerPolicy.java
+++ services/core/java/com/android/server/policy/WindowManagerPolicy.java
@@ -1479,4 +1479,11 @@ public interface WindowManagerPolicy extends WindowManagerPolicyConstants {
      * @return whether the value was changed.
      */
     boolean setAodShowing(boolean aodShowing);
+
+    public interface FingerListener {
+        void powerDown(boolean z);
+    }
+    void registerFingerListener(FingerListener fingerListener);
+    void interceptPowerKeyByFinger(long j);
+    void notifySideFpAuthenOrEnroll(boolean z);
 }

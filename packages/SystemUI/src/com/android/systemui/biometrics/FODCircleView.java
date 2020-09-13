bad2ca9a97a0d2710fbf4dcd798ef781080d73b1
Ido Ben-Hur <idoybh2@gmail.com>
2020-09-14 01:35:40 +0300

    base: Allow choosing a custom color for pressed FOD [1/2]
    Change-Id: I135934cbe445365778853b7d1fdc8b1a64bb2c94

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -271,7 +271,6 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
 
         Resources res = context.getResources();
 
-        mPaintFingerprint.setColor(res.getColor(R.color.config_fodColor));
         mPaintFingerprint.setAntiAlias(true);
 
         mTargetUsesInKernelDimming = res.getBoolean(com.android.internal.R.bool.config_targetUsesInKernelDimming);
@@ -467,6 +466,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
         if (mIsAuthenticated) {
             return;
         }
+
+        mPaintFingerprint.setColor(getFODColor());
 
         mIsCircleShowing = true;
 
@@ -787,6 +788,15 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
         } else {
             this.setColorFilter(null);
         }
+    }
+
+    private int getFODColor() {
+       int defaultColor = mContext.getResources().getColor(R.color.config_fodColor);
+       int fodColor = Settings.System.getInt(mContext.getContentResolver(), Settings.System.FOD_COLOR, defaultColor);
+       if ( fodColor != 0)
+           return fodColor;
+       else
+           return defaultColor;
     }
 
     private static int lighter(int color, int factor) {

c44f3b4e8e827183a825c85756cdd4cfbef84cbc
maxwen <max.weninger@gmail.com>
2020-10-04 18:08:25 +0200

    base: add hooks to backup and restore aicp
     settings
    
    for heavens sake google...
    dont force us to change both Settings and SettingProvider for
    every setting we add and want to be backed up
    
    *** AICPfied
    
    Change-Id: Id6c8a7183d5a008ca7fd38ea4042046ab3168ec5
    Signed-off-by: doc HD <doc.divxm@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5217,6 +5217,45 @@ public final class Settings {
             return isCallingPackageAllowedToWriteSettings(context, Process.myUid(),
                     context.getOpPackageName(), false);
         }
+
+
+        /*****************************
+         * AICP System Settings start
+         *****************************/
+
+-        /**
+-         Add aicp settings like e.g.
+-         @hide
+-         public static final String AICP_FOO_BAR_BALABALA = "foo_bar_balabala";
+-        */
+
+
+        /***************************
+         * AICP System Settings end
+         ***************************/
+
+        /**
+         * SettingsBackupAgent will combine its list with this so we dont need
+         * to add new things into SettingsProvider SystemSettings
+         * @hide
+         */
+        public static final String[] AICP_SETTINGS_TO_BACKUP = {
+            // AICP_FOO_BAR_BALABALA
+        };
+
+        /**
+         * SettingsBackupAgent will combine its list with this so we dont need
+         * to add new things into SettingsProvider SystemSettingsValidators
+         * we cant use Validators interface so use a simple integer mapping
+         * BOOLEAN_VALIDATOR == 0
+         * ANY_INTEGER_VALIDATOR == 1
+         * ANY_STRING_VALIDATOR == 2
+         * @hide
+         */
+        public static final Map<String, Integer> AICP_SETTINGS_VALIDATORS = new ArrayMap<>();
+        static {
+            //AICP_SETTINGS_VALIDATORS.put(AICP_FOO_BAR_BALABALA, 1);
+        }
     }
 
     /**

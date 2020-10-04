4d957afe251403260faa317bc77fc6f8db7207c9
jhonboy121 <alfredmathew05@gmail.com>
2020-10-04 12:49:28 -0700

    SystemUI: allow changing the length of gesture navbar [1/2]
    * forward ported from 11 and improved a lot by delegating
      all the dirty work to controllers and using LayoutParams
      for changing the width instead of screwing around in onDraw.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Icba8d7cb8b295c5dfebcc8c1a9886b746b8ce4b4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11488,6 +11488,14 @@ public final class Settings {
          */
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
+        /**
+         * Gesture navbar length mode.
+         * Supported modes: 0 for normal length, 1 for medium and 2 for long.
+         * Default 0.
+         * @hide
+         */
+        public static final String GESTURE_NAVBAR_LENGTH_MODE = "gesture_navbar_length_mode";
+
         /*****************************
          * AICP Secure Settings end
          *****************************/

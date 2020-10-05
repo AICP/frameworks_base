c229baaa680a06c95d11be972c163baa0a805f5a
DennySPb <dennyspb@gmail.com>
2020-10-05 12:52:54 +0300

    SystemUI: Optional haptic feedback on back gesture [1/2]
    Change-Id: I3bd949cde08ac9a0513e05f2356e84fde07ed767

--- core/java/com/android/internal/policy/GestureNavigationSettingsObserver.java
+++ core/java/com/android/internal/policy/GestureNavigationSettingsObserver.java
@@ -67,6 +67,9 @@ public class GestureNavigationSettingsObserver extends ContentObserver {
         r.registerContentObserver(
                 Settings.Secure.getUriFor(Settings.Secure.USER_SETUP_COMPLETE),
                 false, this, UserHandle.USER_ALL);
+        r.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.BACK_GESTURE_HAPTIC),
+                false, this, UserHandle.USER_ALL);
         DeviceConfig.addOnPropertiesChangedListener(
                 DeviceConfig.NAMESPACE_SYSTEMUI,
                 runnable -> mMainHandler.post(runnable),
@@ -94,6 +97,12 @@ public class GestureNavigationSettingsObserver extends ContentObserver {
         return getSensitivity(userRes, Settings.Secure.BACK_GESTURE_INSET_SCALE_RIGHT);
     }
 
+    public boolean getEdgeHaptic() {
+        return Settings.System.getIntForUser(
+               mContext.getContentResolver(), Settings.System.BACK_GESTURE_HAPTIC, 0,
+               UserHandle.USER_CURRENT) == 1;
+    }
+
     public boolean areNavigationButtonForcedVisible() {
         return Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.USER_SETUP_COMPLETE, 0, UserHandle.USER_CURRENT) == 0;
@@ -114,4 +123,6 @@ public class GestureNavigationSettingsObserver extends ContentObserver {
                 mContext.getContentResolver(), side, 1.0f, UserHandle.USER_CURRENT);
         return (int) (inset * scale);
     }
+
+
 }

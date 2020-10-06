8a906f409812ac68b472e2e7c282a74bdcf4d75c
Bruno Martins <bgcngm@gmail.com>
2020-10-06 16:01:35 +0100

    fwb: Re-introduce keyboard backlight brightness control
    Picked remaining support from commit bbd00b7 and lid state
    handling from commit 68771fe.
    
    Change-Id: I310dfd026b2dc5e7bc47dd4a2c1ae029d58a1e8e

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -227,5 +227,6 @@ public class SecureSettings {
         Settings.Secure.SHOW_MEDIA_SQUIGGLE_ANIMATION,
         Settings.Secure.BUTTON_BACKLIGHT_TIMEOUT,
         Settings.Secure.BUTTON_BRIGHTNESS,
+        Settings.Secure.KEYBOARD_BRIGHTNESS,
     };
 }

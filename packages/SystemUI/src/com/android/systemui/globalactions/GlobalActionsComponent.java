99fe0796c06087f1f7a4bd450cee20c516692a5b
ezio84 <brabus84@gmail.com>
2020-10-18 19:14:32 +0200

    Powermenu: advanced reboot toggle - android 13
    base: PowerMenu advanced reboot toggle
    
    Now R already provides an additional dialog when there are more
    than the max allowed buttons in the first row (3 by default),
    to put there both Power and Restart button.
    
    We can use that existing logic to put together Restart and our
    custom buttons (recovery-bootloader-systemui), without breaking
    the possibility for Power to be moved there if needed.
    
    Q reference: https://github.com/ezio84/abc_frameworks_base/commit/749ba7b9e0821d755d8775b018af266083569ffc
    
    Change-Id: I8273ecf9ecf2ee7e8bbdc035f33479e3dac80577
    
    Update power menu icons
    
    Change-Id: I42a9a6ac88ec4bac052a409c1c8b6385d22092f3
    
    Power menu: don't allow device trees to override the config
    
    Advanced power: fix Power showing twice if lockdown button is available
    
    base: expose advancedReboot for OpenDelta
    
    Change-Id: I5cb73f76bc04edaa8ec59177e0a270cb5b651eda
    Co-authored-by: Alex Crux <du.alexcruz@gmail.com>
    Co-authored-by: maxwen <max.weninger@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsComponent.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsComponent.java
@@ -80,8 +80,8 @@ public class GlobalActionsComponent extends CoreStartable
     }
 
     @Override
-    public void handleShowShutdownUi(boolean isReboot, String reason) {
-        mExtension.get().showShutdownUi(isReboot, reason);
+    public void handleShowShutdownUi(boolean isReboot, String reason, boolean advancedReboot) {
+        mExtension.get().showShutdownUi(isReboot, reason, advancedReboot);
     }
 
     @Override
@@ -119,6 +119,14 @@ public class GlobalActionsComponent extends CoreStartable
     public void reboot(boolean safeMode) {
         try {
             mBarService.reboot(safeMode);
+        } catch (RemoteException e) {
+        }
+    }
+
+    @Override
+    public void advancedReboot(String mode) {
+        try {
+            mBarService.advancedReboot(mode);
         } catch (RemoteException e) {
         }
     }

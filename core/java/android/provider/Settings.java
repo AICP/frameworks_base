0efc7dd18af038e6f8dcc5de996797e7280d0c13
Ali B <abittin@gmail.com>
2020-10-25 18:22:47 +0300

    base: add missing HARDWARE_KEYS_DISABLE string
    Needed for PartsBin
    
    Change-Id: I136c92110d9fc233b2a8e0f2654a66a7dc1aa7e7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9200,6 +9200,12 @@ public final class Settings {
         public static final String ADAPTIVE_CONNECTIVITY_ENABLED = "adaptive_connectivity_enabled";
 
         /**
+         * Controls if hardware keys are disabled.
+         * @hide
+         */
+        public static final String HARDWARE_KEYS_DISABLE = "hardware_keys_disable";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

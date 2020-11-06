afb27929701916f9c2bd255907c89fcf9b36d940
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2020-11-06 09:55:35 +0300

    fw_base: PowerMenu: avoid duplicate power button if Lockdown button shows up
    Change-Id: I8c6962a5ac44b302566f6156c16cb213d0130590

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -748,7 +748,6 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                 /*tempActions.size gets in count already PowerOptionsAction if added*/
                 && tempActions.size() > getMaxShownPowerItems()) {
             tempActions.remove(shutdownAction);
-            mPowerItems.add(shutdownAction);
         } else {
             mPowerItems.remove(shutdownAction);
         }

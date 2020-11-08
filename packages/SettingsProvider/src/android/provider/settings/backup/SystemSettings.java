694ad42943f8b3b6956fd30c2a50fb2ae0b1c753
ezio84 <brabus84@gmail.com>
2020-11-08 10:17:01 +0100

    Ambient music: pulse on new music tracks and Now Playing info [1/2]
    Change-Id: If6933471507573a09c6cb02bcb9ed02f0fc33cec

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -254,5 +254,6 @@ public class SystemSettings {
         Settings.System.ROAMING_INDICATOR_ICON,
         Settings.System.SHOW_LTE_FOURGEE,
         Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL,
+        Settings.System.PULSE_ON_NEW_TRACKS,
     };
 }

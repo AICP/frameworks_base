27d0f3ae77ec9f5ff8d44cd431dd01514b607d83
maxwen <max.weninger@gmail.com>
2020-11-09 21:10:53 +0300

    base: SystemUI: add back QS hide label
    if enabled label click (secondary) will be reassigned to long click
    for dual target tiles
    
    @idoybh edits: Adapt to A11, improve some code while at it
    
    AICP'fied by @eyosen: left out dual target parts so no secondary
    click assigments
    
    Change-Id: I440362ab38057cbfba7750d75825179bd830c4e8

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -395,8 +395,10 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
      * {@link QSTileImpl#getLongClickIntent}
      */
     protected void handleLongClick() {
-        Dependency.get(ActivityStarter.class).postStartActivityDismissingKeyguard(
-                getLongClickIntent(), 0);
+        if (getLongClickIntent() != null) {
+            Dependency.get(ActivityStarter.class).postStartActivityDismissingKeyguard(
+                    getLongClickIntent(), 0);
+        }
     }
 
     /**

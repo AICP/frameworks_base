bc8e1d6d703fadd8d4aa157f3ac594a072dc8874
LuK1337 <priv.luk@gmail.com>
2020-11-12 14:49:51 +0100

    fixup! SystemUI: implement better partial screenshot
    * On R sys ui flags no longer disable edge swipe...
    
    Change-Id: I767997a82ee0fc819bbaa7bc906e89127e53a17c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -180,6 +180,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
     private static final int HAPTIC_DURATION = 20;
 
     private final Vibrator mVibrator;
+    private boolean mBlockedGesturalNavigation;
 
     private final GestureNavigationSettingsObserver mGestureNavigationSettingsObserver;
 
@@ -668,6 +669,10 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         return topActivity != null && mGestureBlockingActivities.contains(topActivity);
     }
 
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        mBlockedGesturalNavigation = blocked;
+    }
+
     @Override
     public void writeToProto(SystemUiTraceProto proto) {
         if (proto.edgeBackGestureHandler == null) {
@@ -682,7 +687,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         }
 
         public void onInputEvent(InputEvent event) {
-            EdgeBackGestureHandler.this.onInputEvent(event);
+            if (!mBlockedGesturalNavigation) {
+                EdgeBackGestureHandler.this.onInputEvent(event);
+            }
             finishInputEvent(event, true);
         }
     }

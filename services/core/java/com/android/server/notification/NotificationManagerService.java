2953d1e9307368161f4849b3678a2b3102cbebe4
ezio84 <brabus84@gmail.com>
2020-11-18 22:45:18 +0100

    Led lights: don't skip dialer and other ones
    https://github.com/ezio84/abc_frameworks_base/commit/543692372b0bcfba1f04bd650cfd69a3df8ceb34#diff-d7b00843a7d21e20fe1ee38e433c02c6
    made the check just too restrictive, it doesn't work well
    with a custom lights feature like the one we have.
    
    Change-Id: I000cd4eb93458bd598c2207a0e9fddbb07003f68

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -7450,7 +7450,7 @@ public class NotificationManagerService extends SystemService {
         final int buzzBeepBlink = (buzz ? 1 : 0) | (beep ? 2 : 0) | (blink ? 4 : 0);
         if (buzzBeepBlink > 0) {
             // Ignore summary updates because we don't display most of the information.
-            if (record.getSbn().isGroup() && record.getSbn().getNotification().isGroupSummary()) {
+            if (!blink && record.getSbn().isGroup() && record.getSbn().getNotification().isGroupSummary()) {
                 if (DEBUG_INTERRUPTIVENESS) {
                     Slog.v(TAG, "INTERRUPTIVENESS: "
                             + record.getKey() + " is not interruptive: summary");
@@ -7518,7 +7518,7 @@ public class NotificationManagerService extends SystemService {
             return false;
         }
         // Suppressed because it's a silent update
-        final Notification notification = record.getNotification();
+        /*final Notification notification = record.getNotification();
         if (record.isUpdate && (notification.flags & FLAG_ONLY_ALERT_ONCE) != 0) {
             return false;
         }
@@ -7529,7 +7529,7 @@ public class NotificationManagerService extends SystemService {
         // not if in call
         if (isInCall()) {
             return false;
-        }
+        }*/
         // check current user
         if (!isNotificationForCurrentUser(record)) {
             return false;

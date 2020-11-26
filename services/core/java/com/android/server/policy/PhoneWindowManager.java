96014845b27d5d7fef6dbfdc50a051b230401f00
Ali B <abittin@gmail.com>
2020-11-26 17:04:09 +0300

    base: do hwkey checks before handing it over to keyhandlers
    Change-Id: I2763180b4a75d04b3c5225fff9af921d220b01e5

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3919,6 +3919,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
 
+        // Disable hw keys in Ambient and when screen off
+        if (mANBIHandler != null && mANBIEnabled && mANBIHandler.isScreenTouched()
+                && !navBarKey && (appSwitchKey || homeKey || menuKey || backKey)) {
+            return 0;
+        }
+
+        // Disable hw keys in Ambient and when screen off
+        if ((isDozeMode() || !isScreenOn()) && (appSwitchKey || homeKey || menuKey || backKey)) {
+            return 0;
+        }
+
         // Specific custom key handling
         if (mCustomKeyHandler != null) {
             try {
@@ -3965,17 +3976,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         // Specific device key handling
         if (dispatchKeyToKeyHandlers(event)) {
-            return 0;
-        }
-
-        // Disable hw keys in Ambient and when screen off
-        if (mANBIHandler != null && mANBIEnabled && mANBIHandler.isScreenTouched()
-                && !navBarKey && (appSwitchKey || homeKey || menuKey || backKey)) {
-            return 0;
-        }
-
-        // Disable hw keys in Ambient and when screen off
-        if ((isDozeMode() || !isScreenOn()) && (appSwitchKey || homeKey || menuKey || backKey)) {
             return 0;
         }
 

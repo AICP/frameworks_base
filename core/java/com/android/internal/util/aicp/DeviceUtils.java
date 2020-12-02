33ec894d1a1f4df69635695f42c5abb99bf4a4a4
Ali B <abittin@gmail.com>
2020-12-02 11:40:45 +0300

    base: utils: bring in DeviceKeysConstants from Lineage
    Change-Id: Ib755418230c67669f6474ba1885ee38d6eef0750

--- core/java/com/android/internal/util/aicp/DeviceUtils.java
+++ core/java/com/android/internal/util/aicp/DeviceUtils.java
@@ -35,12 +35,16 @@ import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.DisplayInfo;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.util.Log;
 
 import com.android.internal.telephony.PhoneConstants;
 import static android.hardware.Sensor.TYPE_LIGHT;
 import static android.hardware.Sensor.TYPE_PROXIMITY;
+
+import static com.android.internal.util.aicp.DeviceKeysConstants.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -185,6 +189,109 @@ public class DeviceUtils {
         return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
                 && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
     }
+
+    public static int getDeviceKeys(Context context) {
+        return context.getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+    }
+
+    public static int getDeviceWakeKeys(Context context) {
+        return context.getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareWakeKeys);
+    }
+
+    /* returns whether the device has power key or not. */
+    public static boolean hasPowerKey() {
+        return KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_POWER);
+    }
+
+    /* returns whether the device has home key or not. */
+    public static boolean hasHomeKey(Context context) {
+        return (getDeviceKeys(context) & KEY_MASK_HOME) != 0;
+    }
+
+    /* returns whether the device has back key or not. */
+    public static boolean hasBackKey(Context context) {
+        return (getDeviceKeys(context) & KEY_MASK_BACK) != 0;
+    }
+
+    /* returns whether the device has menu key or not. */
+    public static boolean hasMenuKey(Context context) {
+        return (getDeviceKeys(context) & KEY_MASK_MENU) != 0;
+    }
+
+    /* returns whether the device has assist key or not. */
+    public static boolean hasAssistKey(Context context) {
+        return (getDeviceKeys(context) & KEY_MASK_ASSIST) != 0;
+    }
+
+    /* returns whether the device has app switch key or not. */
+    public static boolean hasAppSwitchKey(Context context) {
+        return (getDeviceKeys(context) & KEY_MASK_APP_SWITCH) != 0;
+    }
+
+    /* returns whether the device has camera key or not. */
+    public static boolean hasCameraKey(Context context) {
+        return (getDeviceKeys(context) & KEY_MASK_CAMERA) != 0;
+    }
+
+    /* returns whether the device has volume rocker or not. */
+    public static boolean hasVolumeKeys(Context context) {
+        return (getDeviceKeys(context) & KEY_MASK_VOLUME) != 0;
+    }
+
+    /* returns whether the device can be waken using the home key or not. */
+    public static boolean canWakeUsingHomeKey(Context context) {
+        return (getDeviceWakeKeys(context) & KEY_MASK_HOME) != 0;
+    }
+
+    /* returns whether the device can be waken using the back key or not. */
+    public static boolean canWakeUsingBackKey(Context context) {
+        return (getDeviceWakeKeys(context) & KEY_MASK_BACK) != 0;
+    }
+
+    /* returns whether the device can be waken using the menu key or not. */
+    public static boolean canWakeUsingMenuKey(Context context) {
+        return (getDeviceWakeKeys(context) & KEY_MASK_MENU) != 0;
+    }
+
+    /* returns whether the device can be waken using the assist key or not. */
+    public static boolean canWakeUsingAssistKey(Context context) {
+        return (getDeviceWakeKeys(context) & KEY_MASK_ASSIST) != 0;
+    }
+
+    /* returns whether the device can be waken using the app switch key or not. */
+    public static boolean canWakeUsingAppSwitchKey(Context context) {
+        return (getDeviceWakeKeys(context) & KEY_MASK_APP_SWITCH) != 0;
+    }
+
+    /* returns whether the device can be waken using the camera key or not. */
+    public static boolean canWakeUsingCameraKey(Context context) {
+        return (getDeviceWakeKeys(context) & KEY_MASK_CAMERA) != 0;
+    }
+
+    /* returns whether the device can be waken using the volume rocker or not. */
+    public static boolean canWakeUsingVolumeKeys(Context context) {
+        return (getDeviceWakeKeys(context) & KEY_MASK_VOLUME) != 0;
+    }
+
+    /* returns whether the device supports button backlight adjusment or not. */
+    public static boolean hasButtonBacklightSupport(Context context) {
+        final boolean buttonBrightnessControlSupported = context.getResources().getInteger(
+                com.android.internal.R.integer
+                        .config_deviceSupportsButtonBrightnessControl) != 0;
+
+        // All hardware keys besides volume and camera can possibly have a backlight
+        return buttonBrightnessControlSupported
+                && (hasHomeKey(context) || hasBackKey(context) || hasMenuKey(context)
+                || hasAssistKey(context) || hasAppSwitchKey(context));
+    }
+
+    /* returns whether the device supports keyboard backlight adjusment or not.
+    public static boolean hasKeyboardBacklightSupport(Context context) {
+        return context.getResources().getInteger(com.android.internal.R.integer
+                .config_deviceSupportsKeyboardBrightnessControl) != 0;
+    } */
 
     public static FilteredDeviceFeaturesArray filterUnsupportedDeviceFeatures(Context context,
             String[] valuesArray, String[] entriesArray) {

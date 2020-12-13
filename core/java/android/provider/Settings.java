c9da04784c64d22772d12156625a2c124dc01092
ezio84 <brabus84@gmail.com>
2020-12-13 16:55:34 +0530

    Allow extended/"L" back swipe to trigger actions/app/activities
    - screen off on long swipe is pretty nice :P
    
    Allow a wider back swipe to trigger actions (instead of timeout)
    
    - swipe for more than 3/4 of X axys display size to trigger action
    instead of long swipe timeout
    
    Allow a "L" back swipe to trigger more actions
    
    - even a fucked up "L" is good to trigger the action :D
    
    - also don't break normal back swipe if no action is set as extended swipe
    action
    
    Change-Id: I82d9bb672eb3aadf68438401e96ec69bed2e6fd8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6489,6 +6489,122 @@ public final class Settings {
          */
         public static final String OMNI_USE_OLD_MOBILETYPE = "use_old_mobiletype";
 
+        /**
+         * Gestures nav: left long back swipe action
+         * @hide
+         */
+        public static final String LEFT_LONG_BACK_SWIPE_ACTION = "left_long_back_swipe_action";
+
+        /**
+         * Gestures nav: left long back swipe app action
+         * @hide
+         */
+        public static final String LEFT_LONG_BACK_SWIPE_APP_ACTION = "left_long_back_swipe_app_action";
+
+        /**
+         * Display friendly name of custom application launched when
+         * using the long left back swipe app action
+         * @hide
+         */
+        public static final String LEFT_LONG_BACK_SWIPE_APP_FR_ACTION = "left_long_back_swipe_app_fr_action";
+
+        /**
+         * Launch custom activity when using the long left back swipe app action
+         *
+         * @hide
+         */
+        public static final String LEFT_LONG_BACK_SWIPE_APP_ACTIVITY_ACTION = "left_long_back_swipe_app_activity_action";
+
+        /**
+         * Gestures nav: right long back swipe action
+         * @hide
+         */
+        public static final String RIGHT_LONG_BACK_SWIPE_ACTION = "right_long_back_swipe_action";
+
+        /**
+         * Gestures nav: right long back swipe app action
+         * @hide
+         */
+        public static final String RIGHT_LONG_BACK_SWIPE_APP_ACTION = "right_long_back_swipe_app_action";
+
+        /**
+         * Display friendly name of custom application launched when
+         * using the long right back swipe app action
+         * @hide
+         */
+        public static final String RIGHT_LONG_BACK_SWIPE_APP_FR_ACTION = "right_long_back_swipe_app_fr_action";
+
+        /**
+         * Launch custom activity when using the long right back swipe app action
+         *
+         * @hide
+         */
+        public static final String RIGHT_LONG_BACK_SWIPE_APP_ACTIVITY_ACTION = "right_long_back_swipe_app_activity_action";
+
+        /**
+         * Gestures nav: long back swipe timeout
+         * @hide
+         */
+        public static final String LONG_BACK_SWIPE_TIMEOUT = "long_back_swipe_timeout";
+
+        /**
+         * Gestures nav: whether to use extended swipe instead of timeout to trigger custom actions
+         * @hide
+         */
+        public static final String BACK_SWIPE_EXTENDED = "back_swipe_extended";
+
+        /**
+         * Gestures nav: left long back swipe action
+         * @hide
+         */
+        public static final String LEFT_VERTICAL_BACK_SWIPE_ACTION = "left_vertical_back_swipe_action";
+
+        /**
+         * Gestures nav: left long back swipe app action
+         * @hide
+         */
+        public static final String LEFT_VERTICAL_BACK_SWIPE_APP_ACTION = "left_vertical_back_swipe_app_action";
+
+        /**
+         * Display friendly name of custom application launched when
+         * using the long left back swipe app action
+         * @hide
+         */
+        public static final String LEFT_VERTICAL_BACK_SWIPE_APP_FR_ACTION = "left_vertical_back_swipe_app_fr_action";
+
+        /**
+         * Launch custom activity when using the long left back swipe app action
+         *
+         * @hide
+         */
+        public static final String LEFT_VERTICAL_BACK_SWIPE_APP_ACTIVITY_ACTION = "left_vertical_back_swipe_app_activity_action";
+
+        /**
+         * Gestures nav: right long back swipe action
+         * @hide
+         */
+        public static final String RIGHT_VERTICAL_BACK_SWIPE_ACTION = "right_vertical_back_swipe_action";
+
+        /**
+         * Gestures nav: right long back swipe app action
+         * @hide
+         */
+        public static final String RIGHT_VERTICAL_BACK_SWIPE_APP_ACTION = "right_vertical_back_swipe_app_action";
+
+        /**
+         * Display friendly name of custom application launched when
+         * using the long right back swipe app action
+         * @hide
+         */
+        public static final String RIGHT_VERTICAL_BACK_SWIPE_APP_FR_ACTION = "right_vertical_back_swipe_app_fr_action";
+
+        /**
+         * Launch custom activity when using the long right back swipe app action
+         *
+         * @hide
+         */
+        public static final String RIGHT_VERTICAL_BACK_SWIPE_APP_ACTIVITY_ACTION = "right_vertical_back_swipe_app_activity_action";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -6800,6 +6916,24 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VOLTE_VOWIFI_OVERRIDE);
             PRIVATE_SETTINGS.add(COMBINED_STATUS_BAR_SIGNAL_ICONS);
             PRIVATE_SETTINGS.add(OMNI_USE_OLD_MOBILETYPE);
+            PRIVATE_SETTINGS.add(LEFT_LONG_BACK_SWIPE_ACTION);
+            PRIVATE_SETTINGS.add(LEFT_LONG_BACK_SWIPE_APP_ACTION);
+            PRIVATE_SETTINGS.add(LEFT_LONG_BACK_SWIPE_APP_FR_ACTION);
+            PRIVATE_SETTINGS.add(LEFT_LONG_BACK_SWIPE_APP_ACTIVITY_ACTION);
+            PRIVATE_SETTINGS.add(RIGHT_LONG_BACK_SWIPE_ACTION);
+            PRIVATE_SETTINGS.add(RIGHT_LONG_BACK_SWIPE_APP_ACTION);
+            PRIVATE_SETTINGS.add(RIGHT_LONG_BACK_SWIPE_APP_FR_ACTION);
+            PRIVATE_SETTINGS.add(RIGHT_LONG_BACK_SWIPE_APP_ACTIVITY_ACTION);
+            PRIVATE_SETTINGS.add(LONG_BACK_SWIPE_TIMEOUT);
+            PRIVATE_SETTINGS.add(BACK_SWIPE_EXTENDED);
+            PRIVATE_SETTINGS.add(LEFT_VERTICAL_BACK_SWIPE_ACTION);
+            PRIVATE_SETTINGS.add(LEFT_VERTICAL_BACK_SWIPE_APP_ACTION);
+            PRIVATE_SETTINGS.add(LEFT_VERTICAL_BACK_SWIPE_APP_FR_ACTION);
+            PRIVATE_SETTINGS.add(LEFT_VERTICAL_BACK_SWIPE_APP_ACTIVITY_ACTION);
+            PRIVATE_SETTINGS.add(RIGHT_VERTICAL_BACK_SWIPE_ACTION);
+            PRIVATE_SETTINGS.add(RIGHT_VERTICAL_BACK_SWIPE_APP_ACTION);
+            PRIVATE_SETTINGS.add(RIGHT_VERTICAL_BACK_SWIPE_APP_FR_ACTION);
+            PRIVATE_SETTINGS.add(RIGHT_VERTICAL_BACK_SWIPE_APP_ACTIVITY_ACTION);
         }
 
         /**

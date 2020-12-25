d7d767c37afb101d822bec04cbe78f483d819f10
ezio84 <brabus84@gmail.com>
2020-12-25 21:47:24 +0100

    Hidden gestural bar: fix visual glitches when switching states
    hacky way but it works (TODO: find a proper fix in the new year,
    i don't have motivation to dig into navbar/keyguard code fuckery now lol)
    
    setting the height to 1px keeps the navbar almost invisible
    but fixes the annoying visual glitches when going from screen
    off to ambient pulsing or lockscreen (more noticeable on some devices
    like bonito)
    to replicate the issue without this commit:
    - screen ON, then screen off, then double tap to go to ambient,
    then double tap to go to lockscreen, then double tap to switch screen off,
    then switch screen on again
    - or just switch screen off/on a few times with the power button
    
    Also sync the hide pill code with Pulse hide pill feature
    
    Change-Id: Ib1cc83492f8a091be5cac4563d844010cef69dbc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
@@ -159,9 +159,6 @@ public class NavigationBarInflaterView extends FrameLayout
                 : mOverviewProxyService.shouldShowSwipeUpUI()
                         ? R.string.config_navBarLayoutQuickstep
                         : R.string.config_navBarLayout;
-        if (!mIsHintEnabled && defaultResource == R.string.config_navBarLayoutHandle) {
-            return getContext().getString(defaultResource).replace(HOME_HANDLE, "");
-        }
         return getContext().getString(defaultResource);
     }
 
@@ -215,6 +212,10 @@ public class NavigationBarInflaterView extends FrameLayout
             clearViews();
             inflateLayout(layoutValue);
         }
+    }
+
+    public boolean isHintEnabled() {
+        return mIsHintEnabled;
     }
 
     public void onLikelyDefaultLayoutChange() {

fecc797ce59db9e022401d5200d869b31d31dbc6
Ali B <abittin@gmail.com>
2020-12-27 13:48:19 +0300

    base: navigation handle length fixes
        With adaptations, includes:
        Commit: base: wrap the size of the navigation handle
        Author: Stylogey <stylogey@gmail.com>
        Date: 2020-03-25 04:00:25
        ChangeID: Ie48cb462219f179eff1611a5cd5f35837c3be962
    
    Change-Id: I72876748c612e33936319d41f3c3f1d7d7891062

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10954,11 +10954,6 @@ public final class Settings {
          */
         public static final String SHOW_BACK_ARROW_GESTURE = "show_back_arrow_gesture";
 
-        /** Width of the navigation handle.
-         *  @hide
-         */
-        public static final String NAVIGATION_HANDLE_WIDTH = "navigation_handle_width";
-
         /**
          * Whether tethering is allowed to use VPN upstreams
          * 0 = false, 1 = true
@@ -11072,6 +11067,12 @@ public final class Settings {
          * @hide
          */
         public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
+
+        /**
+         * Width of the navigation handle.
+         * @hide
+         */
+        public static final String NAVIGATION_HANDLE_WIDTH = "navigation_handle_width";
 
         /*****************************
          * AICP Secure Settings end

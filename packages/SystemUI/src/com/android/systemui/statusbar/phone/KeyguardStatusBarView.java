f0734a32f1b16ae9a2e52844fd2bee603016f716
Ali B <abittin@gmail.com>
2020-12-31 00:05:00 +0300

    Keyguard: fix the mystery of disappearing battery percent
    Whenever an overlay is changed, battery percent will
    disappear from statusbar, if enabled. Remove unneeded update
    view call as BatteryMeterView already handles this
    
    Change-Id: I2037f265aca19513d5f4cb73b8c4857d1318dce4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -479,7 +479,6 @@ public class KeyguardStatusBarView extends RelativeLayout
         mCarrierLabel.setTextAppearance(
                 Utils.getThemeAttr(mContext, com.android.internal.R.attr.textAppearanceSmall));
         onThemeChanged();
-        mBatteryView.updatePercentView();
     }
 
     private void updateIconsAndTextColors() {

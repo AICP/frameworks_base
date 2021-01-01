e20754586b52ec5298ab3edb0b891894401b7044
diegocr@gmail.com <diegocr@gmail.com>
2021-01-01 21:43:17 +0100

    base: FOD add aicp logos [1/2]
    Aicp icons by @eboye
    
    Change-Id: I720ecb7bf774d2f147462d12a1ddb1c92cd3dd9d

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -409,46 +409,52 @@ public class FODCircleView extends ImageView {
         int fodicon = getFODIcon();
 
         if (fodicon == 0) {
-            this.setImageResource(R.drawable.fod_icon_default);
+            this.setImageResource(R.drawable.fod_icon_aicp);
         } else if (fodicon == 1) {
-            this.setImageResource(R.drawable.fod_icon_default_1);
+            this.setImageResource(R.drawable.fod_icon_aicp_1);
         } else if (fodicon == 2) {
-            this.setImageResource(R.drawable.fod_icon_default_2);
+            this.setImageResource(R.drawable.fod_icon_aicp_2);
         } else if (fodicon == 3) {
-            this.setImageResource(R.drawable.fod_icon_default_3);
+            this.setImageResource(R.drawable.fod_icon_default);
         } else if (fodicon == 4) {
-            this.setImageResource(R.drawable.fod_icon_default_4);
+            this.setImageResource(R.drawable.fod_icon_default_1);
         } else if (fodicon == 5) {
-            this.setImageResource(R.drawable.fod_icon_default_5);
+            this.setImageResource(R.drawable.fod_icon_default_2);
         } else if (fodicon == 6) {
-            this.setImageResource(R.drawable.fod_icon_arc_reactor);
+            this.setImageResource(R.drawable.fod_icon_default_3);
         } else if (fodicon == 7) {
-            this.setImageResource(R.drawable.fod_icon_cpt_america_flat);
+            this.setImageResource(R.drawable.fod_icon_default_4);
         } else if (fodicon == 8) {
-            this.setImageResource(R.drawable.fod_icon_cpt_america_flat_gray);
+            this.setImageResource(R.drawable.fod_icon_default_5);
         } else if (fodicon == 9) {
-            this.setImageResource(R.drawable.fod_icon_dragon_black_flat);
+            this.setImageResource(R.drawable.fod_icon_arc_reactor);
         } else if (fodicon == 10) {
-            this.setImageResource(R.drawable.fod_icon_future);
+            this.setImageResource(R.drawable.fod_icon_cpt_america_flat);
         } else if (fodicon == 11) {
-            this.setImageResource(R.drawable.fod_icon_glow_circle);
+            this.setImageResource(R.drawable.fod_icon_cpt_america_flat_gray);
         } else if (fodicon == 12) {
-            this.setImageResource(R.drawable.fod_icon_neon_arc);
+            this.setImageResource(R.drawable.fod_icon_dragon_black_flat);
         } else if (fodicon == 13) {
-            this.setImageResource(R.drawable.fod_icon_neon_arc_gray);
+            this.setImageResource(R.drawable.fod_icon_future);
         } else if (fodicon == 14) {
-            this.setImageResource(R.drawable.fod_icon_neon_circle_pink);
+            this.setImageResource(R.drawable.fod_icon_glow_circle);
         } else if (fodicon == 15) {
-            this.setImageResource(R.drawable.fod_icon_neon_triangle);
+            this.setImageResource(R.drawable.fod_icon_neon_arc);
         } else if (fodicon == 16) {
-            this.setImageResource(R.drawable.fod_icon_paint_splash_circle);
+            this.setImageResource(R.drawable.fod_icon_neon_arc_gray);
         } else if (fodicon == 17) {
-            this.setImageResource(R.drawable.fod_icon_rainbow_horn);
+            this.setImageResource(R.drawable.fod_icon_neon_circle_pink);
         } else if (fodicon == 18) {
-            this.setImageResource(R.drawable.fod_icon_shooky);
+            this.setImageResource(R.drawable.fod_icon_neon_triangle);
         } else if (fodicon == 19) {
-            this.setImageResource(R.drawable.fod_icon_spiral_blue);
+            this.setImageResource(R.drawable.fod_icon_paint_splash_circle);
         } else if (fodicon == 20) {
+            this.setImageResource(R.drawable.fod_icon_rainbow_horn);
+        } else if (fodicon == 21) {
+            this.setImageResource(R.drawable.fod_icon_shooky);
+        } else if (fodicon == 22) {
+            this.setImageResource(R.drawable.fod_icon_spiral_blue);
+        } else if (fodicon == 23) {
             this.setImageResource(R.drawable.fod_icon_sun_metro);
         }
         this.setScaleType(ImageView.ScaleType.CENTER_INSIDE);

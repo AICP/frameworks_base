a835d48274d4780025bfda8b8c1b2b68af166176
calebcabob <calphonic@gmail.com>
2021-01-06 15:50:57 +0300

    base: LockscreenCharging: Bring in °F/°C toggle [1/2]
    Extracted from
    https://review.aosip.dev/c/AOSIP/platform_frameworks_base/+/16594
    
    Default updated to celcius
    
    Change-Id: If618c5235d4864c8e68d6f266d1012f3ba64ba3b

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -571,6 +571,8 @@ public class KeyguardIndicationController implements StateListener,
         String batteryInfo = "";
         boolean showbatteryInfo = Settings.System.getIntForUser(mContext.getContentResolver(),
             Settings.System.LOCKSCREEN_BATTERY_INFO, 0, UserHandle.USER_CURRENT) == 1;
+        boolean batteryTempUnit = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.LOCKSCREEN_BATTERY_INFO_TEMP_UNIT, 1, UserHandle.USER_CURRENT) == 0;
 
         if (showbatteryInfo) {
             if (mChargingCurrent > 0) {
@@ -579,12 +581,17 @@ public class KeyguardIndicationController implements StateListener,
                           mChargingCurrent : (mChargingCurrent / 1000))) + "mA" ;
             }
             if (mChargingVoltage > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
+                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " • ") +
                         String.format("%.1f", (mChargingVoltage / 1000 / 1000)) + "V";
             }
             if (mTemperature > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                        mTemperature / 10 + "°C";
+                if (batteryTempUnit) {
+                    batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " • ") +
+                            Math.round(mTemperature / 10 * 9f / 5f + 32) + "°F";
+                } else {
+                    batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " • ") +
+                            mTemperature / 10 + "°C";
+                }
             }
             if (batteryInfo != "") {
                 batteryInfo = "\n" + batteryInfo;

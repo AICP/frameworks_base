3d4cbfa2a2a535c6808f4ce858d55074f106a624
calebcabob <calphonic@gmail.com>
2021-01-06 15:50:57 +0300

    base: LockscreenCharging: Bring in °F/°C toggle [1/2]
    Extracted from
    https://review.aosip.dev/c/AOSIP/platform_frameworks_base/+/16594
    
    Default updated to celcius
    
    Change-Id: If618c5235d4864c8e68d6f266d1012f3ba64ba3b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6765,6 +6765,14 @@ public final class Settings {
         public static final String CHARGING_ANIMATION = "charging_animation";
 
         /**
+         * Whether to show Fahrenheit or Celsius with battery info on the
+         * lockscreen while charging
+         *
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO_TEMP_UNIT = "lockscreen_charge_temp_unit";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6958,6 +6966,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(TRANSPARENT_POWER_DIALOG_DIM);
             PRIVATE_SETTINGS.add(POWER_MENU_ANIMATIONS);
             PRIVATE_SETTINGS.add(LOCKSCREEN_BATTERY_INFO);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_BATTERY_INFO_TEMP_UNIT);
         }
 
         /**

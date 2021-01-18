46bd3a31292f0ae72bfe817e88400f76aff11b92
Ali B <abittin@gmail.com>
2021-01-18 19:22:17 +0300

    SystemUI: PowerMenu: always add Power off
    Irrespective of the state of Advanced power menu, power
    off option should be available to the user
    
    Change-Id: Ife175af6d3d9d1720c3dc6124b772013fdc6dbf5

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -735,7 +735,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             addedKeys.add(actionKey);
         }
 
-        if (tempActions.contains(restartAction) && (advancedRebootEnabled(mContext))) {
+        if (tempActions.contains(restartAction) && advancedRebootEnabled(mContext)) {
             // transfer restart and advanced restart to their own list of power actions
             // and position it where Reset button was supposed to be
             int powerOptionsIndex = tempActions.indexOf(restartAction);
@@ -771,7 +771,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                 /*tempActions.size gets in count already PowerOptionsAction if added*/
                 && tempActions.size() > getMaxShownPowerItems()) {
             tempActions.remove(shutdownAction);
-        } else {
+        } else if (tempActions.contains(shutdownAction) && mPowerItems.contains(shutdownAction)) {
             mPowerItems.remove(shutdownAction);
         }
         for (Action action : tempActions) {

702a7e585cceb852ef87c8939d1b5517a8db5b11
TheScarastic <warabhishek@gmail.com>
2021-01-21 20:32:22 +0200

    fwb: Add screen off fod [1/2]
    Change-Id: I6406795b80029ffc877e0b6ec8e40e5322ada4f9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4890,6 +4890,12 @@ public final class Settings {
         public static final String FOD_ICON_WALLPAPER_COLOR = "fod_icon_wallpaper_color";
 
         /**
+         * Screen off fod
+         * @hide
+         */
+        public static final String FOD_GESTURE = "fod_gesture";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate

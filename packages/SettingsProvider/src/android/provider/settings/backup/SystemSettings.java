71d656c44516e888884916a819e97e4814438194
Marko Man <darkobas@gmail.com>
2021-01-25 22:56:41 +0530

    base: Allow to disable Battery light
    Change-Id: I64957fc89d8bd6c68f18149e4801397f1e7aeade

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -236,5 +236,6 @@ public class SystemSettings {
         Settings.System.BATTERY_LIGHT_MEDIUM_COLOR,
         Settings.System.BATTERY_LIGHT_FULL_COLOR,
         Settings.System.BATTERY_LIGHT_REALLYFULL_COLOR,
+        Settings.System.BATTERY_LIGHT_ENABLED,
     };
 }

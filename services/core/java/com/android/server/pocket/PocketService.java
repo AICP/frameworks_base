c6b4a68dfd40d1be03b0655f1c9856cfb97370ef
Laker <lakerman1987@gmail.com>
2021-02-05 19:43:44 +0300

    PocketLock: Make using light sensor optional
    * On some devices using light sensor may break autobrightness
    
    Change-Id: I2bb4aad1ecc6808a4546dfdcada4335074d0d40e

--- services/core/java/com/android/server/pocket/PocketService.java
+++ services/core/java/com/android/server/pocket/PocketService.java
@@ -581,6 +581,8 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
     }
 
     private void startListeningForLight() {
+        boolean mUseLightSensor = mContext.getResources().getBoolean(
+                                    com.android.internal.R.bool.config_pocketUseLightSensor);
 
         if (mVendorSensor != null) {
             return;
@@ -590,7 +592,7 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
             Log.d(TAG, "startListeningForLight()");
         }
 
-        if (!PocketConstants.ENABLE_LIGHT_JUDGE) {
+        if (!mUseLightSensor) {
             return;
         }
 

d477186b14e5f5b17cd2506dfdaaab07e1fb62c8
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-06 21:54:25 +0300

    Screenrecord: Allow limiting the max framerate
    Some devices seem to have problems when running the screen recorder at
    native resolution @ max fps (I'm looking at you surya). The symptoms may
    include severe lagging in the recording or stuff like crashes when
    stopping in a landscape orientation. Allow limiting the recorder fps to
    fix these issues.
    
    Test: record screen on surya with config_screenRecorderMaxFramerate==90
    while screen is running @ 120Hz, observe a smooth video. Stop the
    recording while in full screen landscape youtube video, observe no crash
    
    Change-Id: If2d499ed13ed7eebb981dad700daf50cdc66afb5

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -55,6 +55,7 @@ import android.view.WindowManager;
 
 import com.android.internal.R;
 import com.android.systemui.mediaprojection.MediaProjectionCaptureTarget;
+import com.android.systemui.res.R;
 
 import java.io.Closeable;
 import java.io.File;
@@ -92,6 +93,7 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
     private ScreenRecordingAudioSource mAudioSource;
     private final MediaProjectionCaptureTarget mCaptureRegion;
     private final Handler mHandler;
+    private int mMaxRefreshRate;
 
     private Context mContext;
     ScreenMediaRecorderListener mListener;
@@ -106,6 +108,8 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
         mCaptureRegion = captureRegion;
         mListener = listener;
         mAudioSource = audioSource;
+        mMaxRefreshRate = mContext.getResources().getInteger(
+                R.integer.config_screenRecorderMaxFramerate);
     }
 
     private void prepare() throws IOException, RemoteException, RuntimeException {
@@ -144,6 +148,7 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
         WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         wm.getDefaultDisplay().getRealMetrics(metrics);
         int refreshRate = (int) wm.getDefaultDisplay().getRefreshRate();
+        if (mMaxRefreshRate != 0 && refreshRate > mMaxRefreshRate) refreshRate = mMaxRefreshRate;
         int[] dimens = getSupportedSize(metrics.widthPixels, metrics.heightPixels, refreshRate);
         int width = dimens[0];
         int height = dimens[1];

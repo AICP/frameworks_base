b569ff0ef396c84b63e83ccaf0ea9068a03640ea
Alexander Martinz <alex@amartinz.at>
2021-06-29 16:09:30 +0300

    BatteryService: Add support for oem fast charger detection
    Based on AOSPA fw/base 0af31949c69f7c306b49ecf2c25579300973fdc9
      - https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9
    
    Allows to indicate, whether a device is charged using a
    proprietary oem fast charge solution, which write their status
    to sysfs.
    
    The oem fast charge detection tries to be as generic as possible and is
    configured via overlays.
    
    Paths to sysfs to read status
      - core/res/res/values/aicp_config.xml
        - config_oemFastChargerStatusPaths
    
    Change-Id: Ia91a9c3bb20967c5aac18c24ac7c2be10a0b8bf1
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -32,6 +32,7 @@ import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
 import static android.os.BatteryManager.EXTRA_WARP_CHARGER;
 import static android.os.BatteryManager.EXTRA_VOOC_CHARGER;
 import static android.os.BatteryManager.EXTRA_TURBO_POWER;
+import static android.os.BatteryManager.EXTRA_OEM_FAST_CHARGER;
 
 import android.content.Context;
 import android.content.Intent;
@@ -54,6 +55,7 @@ public class BatteryStatus {
     public static final int CHARGING_WARP = 4;
     public static final int CHARGING_VOOC = 5;
     public static final int CHARGING_TURBO_POWER = 6;
+    public static final int CHARGING_OEM_FAST_CHARGER = 7;
 
     public final int status;
     public final int level;
@@ -69,11 +71,13 @@ public class BatteryStatus {
     public final boolean warpChargeStatus;
     public final boolean voocChargeStatus;
     public final boolean turboPowerStatus;
+    public final boolean oemChargeStatus;
 
     public BatteryStatus(int status, int level, int plugged, int health,
             int maxChargingCurrent, int maxChargingVoltage, int maxChargingWattage,
             float temperature, boolean dashChargeStatus, boolean warpChargeStatus,
-            boolean voocChargeStatus, boolean turboPowerStatus, boolean present) {
+            boolean voocChargeStatus, boolean turboPowerStatus,
+            boolean oemChargeStatus, boolean present) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
@@ -86,6 +90,7 @@ public class BatteryStatus {
         this.warpChargeStatus = warpChargeStatus;
         this.voocChargeStatus = voocChargeStatus;
         this.turboPowerStatus = turboPowerStatus;
+        this.oemChargeStatus = oemChargeStatus;
         this.present = present;
     }
 
@@ -101,6 +106,7 @@ public class BatteryStatus {
         warpChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_WARP_CHARGER, false);
         voocChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_VOOC_CHARGER, false);
         turboPowerStatus = batteryChangedIntent.getBooleanExtra(EXTRA_TURBO_POWER, false);
+        oemChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_OEM_FAST_CHARGER, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
 
         final int maxChargingMicroAmp = maxChargingCurrent;
@@ -202,6 +208,7 @@ public class BatteryStatus {
                 warpChargeStatus ? CHARGING_WARP :
                 voocChargeStatus ? CHARGING_VOOC :
                 turboPowerStatus ? CHARGING_TURBO_POWER :
+                oemChargeStatus ? CHARGING_OEM_FAST_CHARGER :
                 maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                 maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                         maxChargingWattage > fastThreshold ? CHARGING_FAST :

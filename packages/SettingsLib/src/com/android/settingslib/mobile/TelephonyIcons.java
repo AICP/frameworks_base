17621e030132d4a388e2ec710bf617cf8de9bf85
spkal01 <kalligeross@gmail.com>
2021-07-06 00:45:37 +0530

    Add Vivo Vowifi Icon [1/2]
    Files taken from here https://github.com/ShapeShiftOS/android_frameworks_base/tree/android_11/packages/SystemUI/res/drawable
    
    Change-Id: Ie68bd49f25dfb109db75452807648d989dfc6203

--- packages/SettingsLib/src/com/android/settingslib/mobile/TelephonyIcons.java
+++ packages/SettingsLib/src/com/android/settingslib/mobile/TelephonyIcons.java
@@ -54,6 +54,7 @@ public class TelephonyIcons {
     static final int ICON_VOWIFI_Simple1 = R.drawable.ic_vowifi_simple1;
     static final int ICON_VOWIFI_Simple2 = R.drawable.ic_vowifi_simple2;
     static final int ICON_VOWIFI_Simple3 = R.drawable.ic_vowifi_simple3;
+    static final int ICON_VOWIFI_VIVO = R.drawable.ic_vowifi_vivo;
 
 
     public static final MobileIconGroup CARRIER_NETWORK_CHANGE = new MobileIconGroup(
@@ -417,6 +418,19 @@ public class TelephonyIcons {
             AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
             0,
             TelephonyIcons.ICON_VOWIFI_Simple3,
+            false);
+
+    static final MobileIconGroup VOWIFI_VIVO = new MobileIconGroup(
+            "VoWIFI_VIVO",
+            null,
+            null,
+            AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH,
+            0, 0,
+            0,
+            0,
+            AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH[0],
+            0,
+            TelephonyIcons.ICON_VOWIFI_VIVO,
             false);
 
 

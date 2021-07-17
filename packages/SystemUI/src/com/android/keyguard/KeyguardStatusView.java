3f5db1498d626f9e20f99e6c4704bcd06aeecded
Ali B <abittin@gmail.com>
2021-07-17 16:26:11 +0300

    SystemUI: show weatherView during pulsing
    Change-Id: Ibe328e3dd86a734824b2d459b1d23d877b3f59a0

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -509,9 +509,6 @@ public class KeyguardStatusView extends GridLayout implements
             return;
         }
         mPulsing = pulsing;
-        if (mWeatherView != null) {
-            mWeatherView.setVisibility((mShowWeather && mOmniStyle && !mPulsing) ? View.VISIBLE : View.GONE);
-        }
     }
 
     private boolean shouldShowLogout() {

5e481d10861e251342dc2dd4d6c56a5f9f84b0aa
GtrCraft <nickvanbokhorst@hotmail.com>
2021-09-08 12:56:15 +0200

    Revert "StatusBarIconView: use app icons instead of provided notif one"
    This reverts commit b5e7c81fb95fd37863f1c729ebfb4cd4a9bc1616.
    
    Change-Id: Ifa856807c23edd1503785659b204bfe746a82514

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -516,12 +516,8 @@ public class NotificationIconAreaController implements DarkReceiver,
         if (colorize) {
             color = DarkIconDispatcher.getTint(mTintArea, v, tint);
         }
-        if (v.getStatusBarIcon().pkg.contains("systemui")) {
-            v.setStaticDrawableColor(color);
-            v.setDecorColor(tint);
-        } else {
-            return;
-        }
+        v.setStaticDrawableColor(color);
+        v.setDecorColor(tint);
     }
 
     /**

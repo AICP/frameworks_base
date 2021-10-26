9263032edc3ffb7fa7f269ce8b2ffd0d9180c0e2
Daniel Micay <danielmicay@gmail.com>
2021-10-26 01:31:20 -0400

    add missing display cutout overlays
    Change-Id: Ia7d1c44392390e455733ca4ce3cca322931a84d8

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -20,11 +20,13 @@ LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
 LOCAL_LICENSE_CONDITIONS := notice
 LOCAL_NOTICE_FILE := $(LOCAL_PATH)/../../NOTICE
 LOCAL_REQUIRED_MODULES := \
+	AvoidAppsInCutoutOverlay \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
 	DisplayCutoutEmulationHoleOverlay \
 	DisplayCutoutEmulationTallOverlay \
 	DisplayCutoutEmulationWaterfallOverlay \
+	DisplayCutoutEmulationWideOverlay \
 	FontNotoSerifSourceOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarMode3ButtonOverlay \
@@ -33,6 +35,7 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayWideBack \
 	NavigationBarModeGesturalOverlayExtraWideBack \
 	NavigationBarModeGesturalOverlayFS \
+	NoCutoutOverlay \
 	NotchBarKillerLeftrOverlay \
 	NotchBarKillerOverlay \
 	preinstalled-packages-platform-overlays.xml

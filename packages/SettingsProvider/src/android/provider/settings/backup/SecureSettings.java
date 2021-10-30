b2de076732c897cbb7ef4952ca98bf6309327385
jhonboy121 <alfredmathew05@gmail.com>
2021-10-30 17:17:29 +0530

    base: add public setting keys for monet tunables
    * also allow subclasses of ThemeOverlayController to override reevaluateSystemTheme
    * added protected methods to get generated colors, so as to override in kdrag0n's impl
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
    Change-Id: I2f9bb4c096591cc1f92424d4097e80aada15896c

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -207,5 +207,11 @@ public class SecureSettings {
         Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON,
         Settings.Secure.VOLUME_PANEL_ON_LEFT,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
+        Settings.Secure.MONET_ENGINE_ACCURATE_SHADES,
+        Settings.Secure.MONET_ENGINE_COLOR_OVERRIDE,
+        Settings.Secure.MONET_ENGINE_CUSTOM_COLOR,
+        Settings.Secure.MONET_ENGINE_CHROMA_FACTOR,
+        Settings.Secure.MONET_ENGINE_LINEAR_LIGHTNESS,
+        Settings.Secure.MONET_ENGINE_WHITE_LUMINANCE,
     };
 }

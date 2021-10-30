f2d5c80f85b55b6b831a4421ed171a04dd1d0d52
jhonboy121 <alfredmathew05@gmail.com>
2021-10-30 17:17:29 +0530

    base: add public setting keys for monet tunables
    * also allow subclasses of ThemeOverlayController to override reevaluateSystemTheme
    * added protected methods to get generated colors, so as to override in kdrag0n's impl
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
    Change-Id: I2f9bb4c096591cc1f92424d4097e80aada15896c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11014,6 +11014,48 @@ public final class Settings {
         @SuppressLint("NoSettingsProvider")
         public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
 
+        /**
+         * Accurate shades (default 1)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_ACCURATE_SHADES = "monet_engine_accurate_shades";
+
+        /**
+         * Color override (default null)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_COLOR_OVERRIDE = "monet_engine_color_override";
+
+        /**
+         * Custom Color override (default null)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_CUSTOM_COLOR = "monet_engine_custom_color";
+
+        /**
+         * Colorfulness (default 1.0)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_CHROMA_FACTOR = "monet_engine_chroma_factor";
+
+        /**
+         * Custom lightness scale (default 0)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_LINEAR_LIGHTNESS = "monet_engine_linear_lightness";
+
+        /**
+         * Brightness (default 425, range 0-1000; depends on custom lightness)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_WHITE_LUMINANCE = "monet_engine_white_luminance_user";
+
         /*****************************
          * AICP Secure Settings end
          *****************************/

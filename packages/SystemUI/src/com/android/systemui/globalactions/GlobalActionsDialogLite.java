b71d683d0ad0a03d00a3ed46827da784480e339b
spezi77 <spezi7713@gmx.net>
2021-11-10 00:45:35 +0100

    Powermenu Torch: Allow simultaneous access
    Use a callback to retrieve the torch state. This is necessary
    because the flashlight can also be switched by any other triggers
    outside of the power menu, e.g. by a QS Tile or Active Edge Squeezes.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I28fa2dbe74e7310fcef057c9a9c4b8c1c0a8aee5
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -418,6 +418,10 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         mScreenshotHelper = new ScreenshotHelper(context);
 
         mConfigurationController.addCallback(this);
+
+        // get notified of torch state changes
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerTorchCallback(torchCallback, null);
     }
 
     /**
@@ -1065,20 +1069,32 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         return new ScreenshotAction();
     }
 
+    private CameraManager mCameraManager;
+    CameraManager.TorchCallback torchCallback = new CameraManager.TorchCallback() {
+        @Override
+        public void onTorchModeUnavailable(String cameraId) {
+            super.onTorchModeUnavailable(cameraId);
+        }
+
+        @Override
+        public void onTorchModeChanged(String cameraId, boolean enabled) {
+            super.onTorchModeChanged(cameraId, enabled);
+            mTorchEnabled = enabled;
+        }
+    };
+
     private Action getTorchToggleAction() {
         return new SinglePressAction(com.android.systemui.R.drawable.ic_lock_torch,
                 com.android.systemui.R.string.quick_settings_flashlight_label) {
 
             public void onPress() {
                 try {
-                    CameraManager cameraManager = (CameraManager)
-                            mContext.getSystemService(Context.CAMERA_SERVICE);
-                    for (final String cameraId : cameraManager.getCameraIdList()) {
+                    for (final String cameraId : mCameraManager.getCameraIdList()) {
                         CameraCharacteristics characteristics =
-                            cameraManager.getCameraCharacteristics(cameraId);
+                            mCameraManager.getCameraCharacteristics(cameraId);
                         int orient = characteristics.get(CameraCharacteristics.LENS_FACING);
                         if (orient == CameraCharacteristics.LENS_FACING_BACK) {
-                            cameraManager.setTorchMode(cameraId, !mTorchEnabled);
+                            mCameraManager.setTorchMode(cameraId, !mTorchEnabled);
                             mTorchEnabled = !mTorchEnabled;
                         }
                     }

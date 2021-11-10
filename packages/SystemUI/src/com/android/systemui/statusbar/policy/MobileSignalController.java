d8ba66b7e9e1fa1ed60386dc1239ef21e5eaf1eb
DennySPb <dennyspb@gmail.com>
2021-11-10 09:46:09 +0300

    SystemUI: Hide mobile data indicator when VoLTE icon enabled
    Change-Id: Id67c3c67853e1235cc439c8d9e16c4e294cd66d3

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -715,7 +715,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                     && !mCurrentState.carrierNetworkChangeMode
                     && mCurrentState.activityOut;
             showDataIcon &= mCurrentState.isDefault || dataDisabled;
-            int typeIcon = (showDataIcon || mConfig.alwaysShowDataRatIcon) ? icons.dataType : 0;
+            int typeIcon = ((showDataIcon || mConfig.alwaysShowDataRatIcon) && getVolteResId() == 0) ? icons.dataType : 0;
             MobileIconGroup vowifiIconGroup = getVowifiIconGroup();
             if (vowifiIconGroup != null && (mVoWiFiIcon >= 1)) {
                 typeIcon = vowifiIconGroup.dataType;

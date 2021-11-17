9790d3b19e929d27e68bcc6d7d2ee80136155659
Ido Ben-Hur <idoybh2@gmail.com>
2021-11-17 15:18:51 +0200

    SystemUI: Fix NetworkTraffic UI on expanded QS
    Views are always there, they just animate alpha.
    Let's include netwrok traffic so it'll only be visible when expanded
    Correctly handle text tint while at it
    
    P.S.: Ignore gradle generated files
    
    Change-Id: I78b65db1aaf5302c38ea5fb598be9c5a33b3d1f1

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -43,6 +43,7 @@ import com.android.systemui.statusbar.phone.StatusBarContentInsetsProvider;
 import com.android.systemui.statusbar.phone.StatusBarIconController.TintedIconManager;
 import com.android.systemui.statusbar.phone.StatusIconContainer;
 import com.android.systemui.statusbar.policy.Clock;
+import com.android.systemui.statusbar.policy.NetworkTraffic;
 import com.android.systemui.statusbar.policy.VariableDateView;
 
 import java.util.List;
@@ -69,6 +70,7 @@ public class QuickStatusBarHeader extends FrameLayout {
     private View mSecurityHeaderView;
     private View mStatusIconsView;
     private View mContainer;
+    private NetworkTraffic mNetworkTraffic;
 
     private View mQSCarriers;
     private ViewGroup mClockContainer;
@@ -136,6 +138,7 @@ public class QuickStatusBarHeader extends FrameLayout {
         mRightLayout = findViewById(R.id.rightLayout);
         mDateContainer = findViewById(R.id.date_container);
         mPrivacyContainer = findViewById(R.id.privacy_container);
+        mNetworkTraffic = findViewById(R.id.networkTraffic);
 
         mClockContainer = findViewById(R.id.clock_container);
         mClockView = findViewById(R.id.clock);
@@ -274,6 +277,7 @@ public class QuickStatusBarHeader extends FrameLayout {
                     android.R.attr.textColorSecondary);
             mTextColorPrimary = textColor;
             mClockView.setTextColor(textColor);
+            mNetworkTraffic.setTintColor(textColor);
             if (mTintedIconManager != null) {
                 mTintedIconManager.setTint(textColor);
             }
@@ -337,6 +341,7 @@ public class QuickStatusBarHeader extends FrameLayout {
                 .addFloat(mDateView, "alpha", 0, 0, 1)
                 .addFloat(mClockDateView, "alpha", 1, 0, 0)
                 .addFloat(mQSCarriers, "alpha", 0, 1)
+                .addFloat(mNetworkTraffic, "alpha", 0, 1)
                 .setListener(new TouchAnimator.ListenerAdapter() {
                     @Override
                     public void onAnimationAtEnd() {

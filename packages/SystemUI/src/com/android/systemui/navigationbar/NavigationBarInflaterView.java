e6a1ceb65774772f021d62eac7d3200da032ef0b
cjh1249131356 <cjh1249131356@gmail.com>
2021-12-08 09:34:01 +0800

    SystemUI: Improve gesture bar length [1/2]
    * Merge hidden into length settings.
    * Cleanup useless configs in overlay.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: I84dbe643cc63df205864158e6c453fad69af0adc

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -20,9 +20,13 @@ import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON;
 
 import android.annotation.Nullable;
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.om.IOverlayManager;
 import android.content.res.Configuration;
 import android.graphics.drawable.Icon;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -86,6 +90,8 @@ public class NavigationBarInflaterView extends FrameLayout
     private static final String ABSOLUTE_SUFFIX = "A";
     private static final String ABSOLUTE_VERTICAL_CENTERED_SUFFIX = "C";
 
+    private static final String OVERLAY_NAVIGATION_FULL_SCREEN = "com.aicp.overlay.systemui.immnav.gestural";
+
     protected LayoutInflater mLayoutInflater;
     protected LayoutInflater mLandscapeInflater;
 
@@ -105,7 +111,7 @@ public class NavigationBarInflaterView extends FrameLayout
     private OverviewProxyService mOverviewProxyService;
     private int mNavBarMode = NAV_BAR_MODE_3BUTTON;
 
-    private int mHomeHandleWidthMode = 0;
+    private int mHomeHandleWidthMode = 1;
     private boolean mNavBarLayoutInverse = false;
     private boolean mCompactLayout;
 
@@ -160,6 +166,9 @@ public class NavigationBarInflaterView extends FrameLayout
                 defaultResource == R.string.config_navBarLayoutQuickstep) && mCompactLayout){
             return "left;back,home,recent;right";
         }
+        if (mHomeHandleWidthMode == 0 && defaultResource == R.string.config_navBarLayoutHandle) {
+            return getContext().getString(defaultResource).replace(HOME_HANDLE, "");
+        }
         return getContext().getString(defaultResource);
     }
 
@@ -176,6 +185,7 @@ public class NavigationBarInflaterView extends FrameLayout
             if (QuickStepContract.isGesturalMode(mNavBarMode)) {
                 clearViews();
                 inflateLayout(getDefaultLayout());
+                updateNavbar(mHomeHandleWidthMode);
             }
         }
     }
@@ -288,6 +298,21 @@ public class NavigationBarInflaterView extends FrameLayout
     private void updateAlternativeOrder(View v) {
         if (v instanceof ReverseLinearLayout) {
             ((ReverseLinearLayout) v).setAlternativeOrder(mAlternativeOrder);
+        }
+    }
+
+    private void updateNavbar(int mode) {
+        final IOverlayManager iom = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+        final boolean state = mode == 0;
+        final int userId = ActivityManager.getCurrentUser();
+        try {
+            iom.setEnabled(OVERLAY_NAVIGATION_FULL_SCREEN, state, userId);
+            if (state) {
+                iom.setHighestPriority(OVERLAY_NAVIGATION_FULL_SCREEN, userId);
+            }
+        } catch (RemoteException re) {
+            throw re.rethrowFromSystemServer();
         }
     }
 
@@ -467,11 +492,11 @@ public class NavigationBarInflaterView extends FrameLayout
         } else if (HOME_HANDLE.equals(button)) {
             v = inflater.inflate(R.layout.home_handle, parent, false);
             final ViewGroup.LayoutParams lp = v.getLayoutParams();
-            if (mHomeHandleWidthMode == 1) {
+            if (mHomeHandleWidthMode == 2) {
                 lp.width = getResources().getDimensionPixelSize(
                     R.dimen.navigation_home_handle_width_medium);
                 v.setLayoutParams(lp);
-            } else if (mHomeHandleWidthMode == 2) {
+            } else if (mHomeHandleWidthMode == 3) {
                 lp.width = getResources().getDimensionPixelSize(
                     R.dimen.navigation_home_handle_width_long);
                 v.setLayoutParams(lp);

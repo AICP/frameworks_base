be9f51c8df70ac0d237d32bdd36347fc4fe4f2c1
maxwen <max.weninger@gmail.com>
2021-12-23 00:12:37 +0100

    SystemUI: qs columns count settings
    and vertical qs tile layout settings (and resource config)
    
    Change-Id: I5b519ad62d17fec16400229264c66cbbde2a2031
    
    Change-Id: I5b519ad62d17fec16400229464d66cbbde2a2035

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -60,6 +60,8 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tileimpl.QSTileViewImpl;
 import com.android.systemui.res.R;
 
+import com.aicp.gear.util.AicpUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -143,6 +145,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
                         R.integer.small_land_lockscreen_quick_settings_num_columns)
                 : context.getResources().getInteger(NUM_COLUMNS_ID);
         mAccessibilityDelegate = new TileAdapterDelegate();
+        mNumColumns = AicpUtils.getQSColumnsCount(mContext, mNumColumns);
         mSizeLookup.setSpanIndexCacheEnabled(true);
         mTempTextView = new TextView(context);
         mMinTileViewHeight = context.getResources().getDimensionPixelSize(R.dimen.qs_tile_height);
@@ -168,6 +171,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
                 ? mContext.getResources().getInteger(
                         R.integer.small_land_lockscreen_quick_settings_num_columns)
                 : mContext.getResources().getInteger(NUM_COLUMNS_ID);
+        numColumns = AicpUtils.getQSColumnsCount(mContext, numColumns);
         if (numColumns != mNumColumns) {
             mNumColumns = numColumns;
             return true;

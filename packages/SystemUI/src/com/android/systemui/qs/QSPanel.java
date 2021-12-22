0459df223e28a71c605ac66d06feffa3eb0830e9
maxwen <max.weninger@gmail.com>
2021-12-23 00:12:37 +0100

    SystemUI: qs columns count settings
    and vertical qs tile layout settings (and resource config)
    
    Change-Id: I5b519ad62d17fec16400229264c66cbbde2a2031
    
    Change-Id: I5b519ad62d17fec16400229464d66cbbde2a2035

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -699,7 +699,7 @@ public class QSPanel extends LinearLayout implements Tunable {
 
     void setColumnRowLayout(boolean withMedia) {
         mTileLayout.setMinRows(withMedia ? 2 : 1);
-        mTileLayout.setMaxColumns(withMedia ? 2 : 4);
+        mTileLayout.setMaxColumns(withMedia ? mTileLayout.getResourceColumns() : mTileLayout.getResourceColumns() * 2);
         placeTileLayoutForScene(withMedia);
     }
 
@@ -860,6 +860,10 @@ public class QSPanel extends LinearLayout implements Tunable {
         int getNumVisibleTiles();
 
         default void setLogger(QSLogger qsLogger) { }
+
+        int getResourceColumns();
+
+        void updateSettings();
     }
 
     interface OnConfigurationChangedListener {

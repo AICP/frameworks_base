0a61fd03e0db161f50a522480dc439e4104b54c7
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-27 17:05:57 +0700

    SystemUI: Add support for GameSpace
    This is an addon for GameSpace for broadcasting about game start/stop.
    Additionally, it also has special option for suppressing fullscreen
    intent like incoming call.
    
    This also squash commits:
    - SystemUI: Improve GameSpace lifecycle and broadcast handling
    - GameSpaceManager: Handle various case against "locking screen" scenario
    
    Change-Id: I1affbd1a0e806a08807d4fa27195210afbff6d56

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6646,6 +6646,20 @@ public final class Settings {
          */
         public static final String SECONDARY_COLOR_CLOCK = "use_secondary_color_clock";
 
+        /**
+         * GameSpace: List of added games by user
+         * @hide
+         */
+        @Readable
+        public static final String GAMESPACE_GAME_LIST = "gamespace_game_list";
+
+        /**
+         * GameSpace: Whether fullscreen intent will be suppressed while in game session
+         * @hide
+         */
+        @Readable
+        public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
+
         /***************************
          * AICP System Settings end
          ***************************/

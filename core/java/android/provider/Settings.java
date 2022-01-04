b548c16415859e31a3e4d21abc1bddb2ea19cc9c
Kshitij Gupta <kshitijgm@gmail.com>
2022-01-04 16:10:15 +0530

    Screen off animations [1/2]
    Fade (default), CRT, and Scale
    
    Credit and respect to @xplodwild for paving the way
    back in the KitKat days!
    
    History of the ElectronBeam class can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/kitkat/services/java/com/android/server/power/ElectronBeam.java
    
    *** Changes for Android 10 by @bigrushdog ***
    Various updates needed for Surface related API in the ElectronBeam animation class
    
    [@AgentFabulous | POSP]
    - Rebase and rewrite ElectronBeam class on top of ColorFade
    - Add usage of class-common Transaction instance
    - Add support for respecting wide-color and protected-content
    - Fixup for new API
    
    Change-Id: I58d269d44c901a8c0471807e3cf05c2054205d28
    Co-Authored-By: Kshitij Gupta <kshitijgm@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5516,6 +5516,12 @@ public final class Settings {
          */
         public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
 
+        /**
+         * Defines the screen-off animation to display
+         * @hide
+         */
+        public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -5703,6 +5709,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DOZE_ON_CHARGE_NOW);
             PRIVATE_SETTINGS.add(STATUS_BAR_BRIGHTNESS_CONTROL);
             PRIVATE_SETTINGS.add(FINGERPRINT_SUCCESS_VIB);
+            PRIVATE_SETTINGS.add(SCREEN_OFF_ANIMATION);
         }
 
         /**

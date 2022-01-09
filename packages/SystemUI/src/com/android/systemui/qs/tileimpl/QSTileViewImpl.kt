e62775a31214d19f9c51f800cf2dc3b07446c963
maxwen <max.weninger@gmail.com>
2022-01-09 17:39:28 +0100

    [1/2] SystemUI: qs hide label setting
    Change-Id: Icf697b7251aed37a77630cdca906a9912f1d8ea2

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -74,6 +74,8 @@ import com.android.systemui.qs.tileimpl.QSIconViewImpl.QS_ANIM_LENGTH
 import com.android.systemui.res.R
 import java.util.Objects
 
+import com.aicp.gear.util.AicpUtils;
+
 private const val TAG = "QSTileViewImpl"
 
 open class QSTileViewImpl
@@ -197,6 +199,7 @@ constructor(
     private val locInScreen = IntArray(2)
     private var vertical = false
     private val forceHideCheveron = true;
+    private var labelHide = false
 
     /** Visuo-haptic long-press effects */
     private var longPressEffectAnimator: ValueAnimator? = null
@@ -224,10 +227,7 @@ constructor(
         setId(generateViewId())
 
         vertical = resources.getBoolean(R.bool.qs_tile_vertical_layout)
-        vertical = Settings.System.getIntForUser(context.getContentResolver(),
-                Settings.System.QS_TILE_VERTICAL_LAYOUT,
-                if (vertical) 1 else 0, UserHandle.USER_CURRENT) != 0;
-
+        vertical = AicpUtils.getQSTileVerticalLayout(context, if (vertical) 1 else 0)
         if (vertical) {
             orientation = LinearLayout.VERTICAL
             gravity = Gravity.CENTER_HORIZONTAL
@@ -235,6 +235,12 @@ constructor(
             orientation = LinearLayout.HORIZONTAL
             gravity = Gravity.CENTER_VERTICAL or Gravity.START
         }
+
+        labelHide = AicpUtils.getQSTileLabelHide(context)
+
+        if (labelHide)
+            gravity = Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL
+
         importantForAccessibility = IMPORTANT_FOR_ACCESSIBILITY_YES
         clipChildren = false
         clipToPadding = false
@@ -284,10 +290,7 @@ constructor(
         }
 
         vertical = resources.getBoolean(R.bool.qs_tile_vertical_layout)
-        vertical = Settings.System.getIntForUser(context.getContentResolver(),
-                Settings.System.QS_TILE_VERTICAL_LAYOUT,
-                if (vertical) 1 else 0, UserHandle.USER_CURRENT) != 0;
-
+        vertical = AicpUtils.getQSTileVerticalLayout(context, if (vertical) 1 else 0)
         if (vertical) {
             orientation = LinearLayout.VERTICAL
             gravity = Gravity.CENTER_HORIZONTAL
@@ -295,6 +298,9 @@ constructor(
             orientation = LinearLayout.HORIZONTAL
             gravity = Gravity.CENTER_VERTICAL or Gravity.START
         }
+
+        if (labelHide)
+            gravity = Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL
 
         val padding = resources.getDimensionPixelSize(R.dimen.qs_tile_padding)
         val startPadding = if (vertical) padding else resources.getDimensionPixelSize(R.dimen.qs_tile_start_padding)
@@ -337,7 +343,8 @@ constructor(
         }
         setLabelColor(getLabelColorForState(QSTile.State.DEFAULT_STATE))
         setSecondaryLabelColor(getSecondaryLabelColorForState(QSTile.State.DEFAULT_STATE))
-        addView(labelContainer)
+        if (!labelHide)
+            addView(labelContainer)
     }
 
     private fun createAndAddSideView() {

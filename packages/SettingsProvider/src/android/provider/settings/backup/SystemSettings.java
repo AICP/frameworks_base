796c929a59d9138fca5da320312212ad55a40503
maxwen <max.weninger@gmail.com>
2022-01-09 17:39:28 +0100

    [1/2] SystemUI: qs hide label setting
    Change-Id: Icf697b7251aed37a77630cdca906a9912f1d8ea2

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -166,7 +166,8 @@ public class SystemSettings {
                 Settings.System.QS_FOOTER_TEXT_STRING,
                 Settings.System.QS_TILE_VERTICAL_LAYOUT,
                 Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE,
-                Settings.System.QS_LAYOUT_COLUMNS
+                Settings.System.QS_LAYOUT_COLUMNS,
+                Settings.System.QS_TILE_LABEL_HIDE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);

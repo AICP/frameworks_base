4e756ad7a4e64590cdce53ebaa344bb31b041eda
Pranav Vashi <neobuddy89@gmail.com>
2022-01-15 17:09:09 +0530

    SystemUI: Refactor colored statusbar icons
    * Remove painful systemui checks everywhere in StatusBariconView
    * Apply NO_COLOR when colored icons are enabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iff53377b1fa51f4dba048d6fcbf9a837732bd227

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -23,6 +23,8 @@ import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_IMS;
 import android.annotation.Nullable;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
 import android.view.Gravity;
@@ -262,6 +264,8 @@ public interface StatusBarIconController {
 
         protected ArrayList<String> mBlockList = new ArrayList<>();
 
+        private final boolean mNewIconStyle;
+
         private boolean mIsOldSignalStyle = false;
 
         public IconManager(ViewGroup group, FeatureFlags featureFlags) {
@@ -270,6 +274,8 @@ public interface StatusBarIconController {
             mContext = group.getContext();
             mIconSize = mContext.getResources().getDimensionPixelSize(
                     com.android.internal.R.dimen.status_bar_icon_size);
+            mNewIconStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
         }
 
         public boolean isDemoable() {
@@ -337,6 +343,7 @@ public interface StatusBarIconController {
         protected StatusBarIconView addIcon(int index, String slot, boolean blocked,
                 StatusBarIcon icon) {
             StatusBarIconView view = onCreateStatusBarIconView(slot, blocked);
+            view.setIconStyle(mNewIconStyle);
             view.set(icon);
             mGroup.addView(view, index, onCreateLayoutParams());
             return view;

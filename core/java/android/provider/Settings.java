1fba25eace59e3e1e4b5391edeef093d4c6e83b4
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:20:04 +0530

    SystemUI: Allow toggling privacy indicators [1/2]
     **@semdoc add settings backup validators
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
    Change-Id: I55fae9c85f69ebd72c60b11c2946c3bd94af10f5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11768,6 +11768,24 @@ public final class Settings {
          */
         public static final String EXTENDED_MONET_THEMES = "monet_themes_style";
 
+        /**
+         * Whether to show privacy indicator for location
+         * @hide
+         */
+        public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
+
+        /**
+         * Whether to show privacy indicator for camera
+         * @hide
+         */
+        public static final String ENABLE_CAMERA_PRIVACY_INDICATOR = "enable_camera_privacy_indicator";
+
+        /**
+         * Whether to show privacy indicator for media projection
+         * @hide
+         */
+        public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
+
         /*****************************
          * AICP Secure Settings end
          *****************************/

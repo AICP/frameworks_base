3872fa9dd785631f182cd92bb6589bc28795cc65
jhenrique09 <jhenrique09@users.noreply.github.com>
2022-01-18 20:46:03 +0800

    base: Implement cutout force full screen [1/2]
    Inspired by MIUI and Essential
    
    CutoutFullscreenController: Adapted from https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    
    [cjh1249131356]: Adapt to A12
    
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Sat, 21 Apr 2018 23:45:57 -0700
    
        Add an option to force pre-O apps to use full screen aspect ratio
    
        When an app target pre-O releases, the default max aspect ratio
        is 1.86:1 which leads to ugly black areas on devices that have
        screens with higher aspect ratio (for example Galaxy S8/S9).
    
        This change adds an option to allow users to change aspect ratio
        for pre-O apps to full screen aspect ratio.
    
        Change-Id: I41d5408841593a12443be885e11959bffaebb67b
    
    Change-Id: Ia93a550cc952962de4fa270aae37a7b8c9ba45ac

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -223,5 +223,6 @@ public class SystemSettings {
         Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE,
         Settings.System.QS_LAYOUT_COLUMNS,
         Settings.System.HIDE_IME_SPACE_ENABLE,
+        Settings.System.FORCE_FULLSCREEN_CUTOUT_APPS,
     };
 }

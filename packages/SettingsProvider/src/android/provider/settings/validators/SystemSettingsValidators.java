6f3b51d5de67b8b1f80dfaca27b98fe648e4ae2c
Ali B <abittin@gmail.com>
2022-01-25 16:25:52 +0300

    base: fix Batterybar color validator
    From NON_NEGATIVE integer to ANY as color values
    are negative
    
    Change-Id: I9d6c894d64fb74b58756e140b09a17e4fa31bb3a

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -21,6 +21,7 @@ import static android.provider.settings.validators.SettingsValidators.BOOLEAN_VA
 import static android.provider.settings.validators.SettingsValidators.COMPONENT_NAME_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.LENIENT_IP_ADDRESS_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.NON_NEGATIVE_INTEGER_VALIDATOR;
+import static android.provider.settings.validators.SettingsValidators.ANY_INTEGER_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.URI_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.VIBRATION_INTENSITY_VALIDATOR;
 
@@ -229,7 +230,7 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.ALERT_SLIDER_NOTIFICATIONS, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.SHOW_BATTERY_IMAGE, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.STATUSBAR_BATTERY_BAR, NON_NEGATIVE_INTEGER_VALIDATOR);
-        VALIDATORS.put(System.STATUSBAR_BATTERY_BAR_COLOR, NON_NEGATIVE_INTEGER_VALIDATOR);
+        VALIDATORS.put(System.STATUSBAR_BATTERY_BAR_COLOR, ANY_INTEGER_VALIDATOR);
         VALIDATORS.put(System.STATUSBAR_BATTERY_BAR_THICKNESS, NON_NEGATIVE_INTEGER_VALIDATOR);
         VALIDATORS.put(System.STATUSBAR_BATTERY_BAR_STYLE, NON_NEGATIVE_INTEGER_VALIDATOR);
         VALIDATORS.put(System.STATUSBAR_BATTERY_BAR_ANIMATE, NON_NEGATIVE_INTEGER_VALIDATOR);

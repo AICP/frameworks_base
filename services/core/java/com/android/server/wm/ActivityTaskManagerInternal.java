18f6d90dd19f14f4e69d9af6b2e7dafcccb33aab
jhonboy121 <alfredmathew05@gmail.com>
2022-02-01 19:54:15 +0530

    base: introduce app lock [1/4]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
    Change-Id: I02de488013a6b5d438754b9dd8619cd15c3a8340

--- services/core/java/com/android/server/wm/ActivityTaskManagerInternal.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerInternal.java
@@ -713,4 +713,6 @@ public abstract class ActivityTaskManagerInternal {
      */
     public abstract void restartTaskActivityProcessIfVisible(
             int taskId, @NonNull String packageName);
+
+    public abstract boolean isVisibleActivity(IBinder activityToken);
 }

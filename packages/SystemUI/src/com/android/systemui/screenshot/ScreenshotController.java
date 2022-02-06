dbbdc24fe81c939e8dce66c180eb515efa5a77d5
AshutoshSundresh <ashutoshsundresh@gmail.com>
2022-02-06 19:05:02 +0530

    SystemUI: Integrate Google Lens into Screenshot UI
    - shares the screenshot taken to Google Lens
    - informs the user to install Google Lens if it is not installed
    
    Squashed:
    
        From: cjh1249131356 <cjh1249131356@gmail.com>
        Date: Wed, 6 Jul 2022 15:35:31 +0800
        Subject: SystemUI: Make Lens work without independent package installed
    
        Reference: https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/35169a68be308b16aa833dfc68ec3f25b6fbf191
    
        Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 26 Feb 2023 00:16:39 +0530
        Subject: SystemUI: Do not add lens screenshot without google package enabled
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I55d104319b8ee6ac320afead78ae79759aa882cd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -168,6 +168,7 @@ public class ScreenshotController {
     static class SavedImageData {
         public Uri uri;
         public Notification.Action deleteAction;
+        public Notification.Action lensAction;
         public List<Notification.Action> smartActions;
         public Notification.Action deleteAction;
         public Notification.Action quickShareAction;
@@ -180,6 +181,7 @@ public class ScreenshotController {
         public void reset() {
             uri = null;
             deleteAction = null;
+            lensAction = null;
             smartActions = null;
             quickShareAction = null;
             deleteAction = null;
@@ -223,6 +225,7 @@ public class ScreenshotController {
     static final String EXTRA_ACTION_TYPE = "android:screenshot_action_type";
     static final String EXTRA_ID = "android:screenshot_id";
     static final String ACTION_TYPE_DELETE = "Delete";
+    static final String ACTION_TYPE_LENS = "Lens";
     static final String ACTION_TYPE_SHARE = "Share";
     static final String ACTION_TYPE_EDIT = "Edit";
     static final String EXTRA_SMART_ACTIONS_ENABLED = "android:smart_actions_enabled";

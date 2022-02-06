338df8fe1fd09b49517043dd806f302548891ab8
Volodymyr Zhdanov <wight554@gmail.com>
2022-02-06 18:55:51 +0300

    fwb: provider: mark SENSOR_BLOCK as readable
    From S+, settings keys annotated with @hide are restricted
    to system_server and system apps only,
    unless they are annotated with @Readable.
    
    Fix FC's in apps Telegram, Maps etc...
    log: https://haste.aicp-rom.com/egigakanuy
    
    Signed-off-by: George Zacharia <george.zcharia@gmail.com>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
    Change-Id: Ifaa88ac49496e6721d86cd7ef3cb2fde5c0d91d0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5582,6 +5582,7 @@ public final class Settings {
          * Sensor block per-package
          * @hide
          */
+        @Readable
         public static final String SENSOR_BLOCK = "sensor_block";
 
         /**

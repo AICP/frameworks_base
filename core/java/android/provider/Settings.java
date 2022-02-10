9171df5bae96e03aaabd8e4266263e5d7b09724a
jhonboy121 <alfredmathew05@gmail.com>
2022-02-10 19:45:35 +0300

    SystemUI: Add public setting keys for monet tunables
    and support for reloading theme without systemui restart
    
    AEX Edits -
     Add keys for our custom color
    
    ARROW Edits -
     Add keys into backup too
    
    Change-Id: I2f9bb4c096591cc1f92424d4097e80aada15896c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11641,6 +11641,49 @@ public final class Settings {
          */
         public static final String PULSE_SOLID_UNITS_ROUNDED = "pulse_solid_units_rounded";
 
+        /**
+         * Accurate shades (default 1)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_ACCURATE_SHADES = "monet_engine_accurate_shades";
+
+        /**
+         * Color override (default null)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_COLOR_OVERRIDE = "monet_engine_color_override";
+
+        /**
+         * Custom Color override (default null)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_CUSTOM_COLOR = "monet_engine_custom_color";
+
+        /**
+         * Colorfulness (default 1.0)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_CHROMA_FACTOR = "monet_engine_chroma_factor";
+
+        /**
+         * Custom lightness scale (default 0)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_LINEAR_LIGHTNESS = "monet_engine_linear_lightness";
+
+        /**
+         * Brightness (default 425, range 0-1000; depends on custom lightness)
+         * @hide
+         */
+        @Readable
+        public static final String MONET_ENGINE_WHITE_LUMINANCE = "monet_engine_white_luminance_user";
+
+
         /*****************************
          * AICP Secure Settings end
          *****************************/

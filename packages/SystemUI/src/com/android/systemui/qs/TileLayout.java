8d8e4147596779bee9cbcce1f97af5f238d5cb70
Ali B <abittin@gmail.com>
2022-02-18 16:31:08 +0300

    QSPanel: tile layout fixes and cleanup
    @eyosen:
    - Properly update qs and qqs tile layout after selection change
    by using existing tunerservice in the controllers instead of
    using a dedicated SettingsObserver.
    
    - Cleanup tunable imports
    
    Change-Id: I859841d1171a307fed3b4d885a45d34138cbf58d

--- packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
@@ -306,7 +306,8 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
 
     @Override
     public void updateSettings() {
-        setMaxColumns(getResourceColumns());
-        requestLayout();
+        if (setMaxColumns(getResourceColumns())) {
+            requestLayout();
+        }
     }
 }

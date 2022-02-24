cb2bccc3e7282a408eb8535dcd69e325de501b9d
Semavi Ulusoy <doc.divxm@gmail.com>
2022-02-24 17:33:34 +0300

    Revert "udfps: Implement UdfpsHbmProvider"
    This reverts commit 1b48cf74f3a03fe88d18ed46998516281935368d.
    
    Change-Id: Id139c5e0b54fea057f507e61c19d12eb2ef20802

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -59,7 +59,6 @@ import android.view.accessibility.AccessibilityManager;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.R;
-import com.android.systemui.biometrics.UdfpsHbmTypes.HbmType;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.doze.DozeReceiver;
@@ -99,7 +98,7 @@ import kotlin.Unit;
  */
 @SuppressWarnings("deprecation")
 @SysUISingleton
-public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
+public class UdfpsController implements DozeReceiver {
     private static final String TAG = "UdfpsController";
     private static final String PULSE_ACTION = "com.android.systemui.doze.pulse";
     private static final long AOD_INTERRUPT_TIMEOUT_MILLIS = 1000;
@@ -120,7 +119,6 @@ public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
     @NonNull private final DumpManager mDumpManager;
     @NonNull private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
     @Nullable private final Vibrator mVibrator;
-    @NonNull private final Handler mMainHandler;
     @NonNull private final FalsingManager mFalsingManager;
     @NonNull private final PowerManager mPowerManager;
     @NonNull private final AccessibilityManager mAccessibilityManager;
@@ -562,7 +560,6 @@ public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
         mContext = context;
         mExecution = execution;
         // TODO (b/185124905): inject main handler and vibrator once done prototyping
-        mMainHandler = mainHandler;
         mVibrator = vibrator;
         mInflater = inflater;
         // The fingerprint manager is queried for UDFPS before this class is constructed, so the
@@ -786,7 +783,7 @@ public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
                 mView = (UdfpsView) mInflater.inflate(R.layout.udfps_view, null, false);
                 mOnFingerDown = false;
                 mView.setSensorProperties(mSensorProps);
-                mView.setHbmProvider(this);
+                mView.setHbmProvider(mHbmProvider);
                 UdfpsAnimationViewController animation = inflateUdfpsAnimation(reason);
                 mAttemptedToDismissKeyguard = false;
                 animation.init();
@@ -1051,22 +1048,5 @@ public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
          * Called onFingerDown events.
          */
         void onFingerDown();
-    }
-
-    @Override
-    public void enableHbm(@HbmType int hbmType, @Nullable Surface surface,
-            @Nullable Runnable onHbmEnabled) {
-        // TO-DO send call to lineage biometric hal and/or add dummy jni that device could override
-        if (onHbmEnabled != null) {
-            mMainHandler.post(onHbmEnabled);
-        }
-    }
-
-    @Override
-    public void disableHbm(@Nullable Runnable onHbmDisabled) {
-        // TO-DO send call to lineage biometric hal and/or add dummy jni that device could override
-        if (onHbmDisabled != null) {
-            mMainHandler.post(onHbmDisabled);
-        }
     }
 }

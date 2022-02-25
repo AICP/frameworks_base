8486d15f69cec9fb0b74dba5d02597347bda9ace
Semavi Ulusoy <doc.divxm@gmail.com>
2022-02-25 14:48:21 +0300

    Revert "keystore: Block key attestation for SafetyNet"
    This commiyt srelies on PixelPropsUtils, which we are trying to avoid.
    This reverts commit f1942d36dcbe08a741bc685f61156ef14f8ec18f.
    
    Change-Id: I718d6e3f6eebf83b8537717c9ce10b05bbac276e

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -77,8 +77,6 @@ import java.util.Set;
 
 import javax.crypto.SecretKey;
 
-import com.android.internal.util.custom.PixelPropsUtils;
-
 /**
  * A java.security.KeyStore interface for the Android KeyStore. An instance of
  * it can be created via the {@link java.security.KeyStore#getInstance(String)
@@ -166,8 +164,6 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
-        PixelPropsUtils.onEngineGetCertificateChain();
-
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {

5bcf11d277fdbfd98d79b3507a744927ef39899f
Ali B <abittin@gmail.com>
2022-02-26 07:30:56 +0100

    Revert "DefaultPermissions: grant SUSPEND_APP permission to systemui"
    This reverts commit 5b32bf417b87e9bb4d9e099c682d49f23973bd6d.
    
    Reason for revert: Stops devices from booting
    
    Change-Id: I589b39f37397e6c501cf7c9eb01d61488e61c3db

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -958,9 +958,6 @@ final class DefaultPermissionGrantPolicy {
 
         // Google Markup
         grantSystemFixedPermissionsToSystemPackage(pm, "com.google.android.markup", userId, STORAGE_PERMISSIONS);
-
-       // SystemUI
-        grantSystemFixedPermissionsToSystemPackage(pm, "com.android.systemui", userId, SUSPEND_APP_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,

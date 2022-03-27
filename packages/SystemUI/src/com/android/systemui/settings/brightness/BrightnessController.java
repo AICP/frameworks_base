bf1b3b4be4076095806c0c0e4b6a308702fa437b
jhonboy121 <alfredmathew05@gmail.com>
2022-03-27 23:06:14 +0530

    SystemUI: brightness slider customisations [1/2]
    * Includes:
        * QQS slider visibility control
        * Auto brightness icon and visibility control
        * Slider position control (applies to both qs and qqs)
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I84c4556920b702cbfebed21140be04d1f26a874c

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -41,6 +41,7 @@ import android.service.vr.IVrManager;
 import android.service.vr.IVrStateCallbacks;
 import android.util.Log;
 import android.util.MathUtils;
+import android.view.View;
 
 import com.android.internal.display.BrightnessSynchronizer;
 import com.android.internal.logging.MetricsLogger;
@@ -79,6 +80,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
 
     private final Handler mBackgroundHandler;
     private final BrightnessObserver mBrightnessObserver;
+    private final AutoBrightnessController mAutoBrightnessController;
 
     private final DisplayListener mDisplayListener = new DisplayListener() {
         @Override
@@ -106,6 +108,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     @Override
     public void setMirror(BrightnessMirrorController controller) {
         mControl.setMirrorControllerAndMirror(controller);
+        mAutoBrightnessController.setMirror(controller);
     }
 
     /** ContentObserver to watch brightness */
@@ -279,7 +282,8 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
             Context context,
             ToggleSlider control,
             BroadcastDispatcher broadcastDispatcher,
-            @Background Handler bgHandler) {
+            @Background Handler bgHandler,
+            AutoBrightnessController autoBrightnessController) {
         mContext = context;
         mControl = control;
         mControl.setMax(GAMMA_SPACE_MAX);
@@ -292,6 +296,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
             }
         };
         mBrightnessObserver = new BrightnessObserver(mHandler);
+        mAutoBrightnessController = autoBrightnessController;
 
         mDisplayId = mContext.getDisplayId();
         PowerManager pm = context.getSystemService(PowerManager.class);
@@ -307,10 +312,12 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
 
     public void registerCallbacks() {
         mBackgroundHandler.post(mStartListeningRunnable);
+        mAutoBrightnessController.registerCallbacks();
     }
 
     /** Unregister all call backs, both to and from the controller */
     public void unregisterCallbacks() {
+        mAutoBrightnessController.unregisterCallbacks();
         mBackgroundHandler.post(mStopListeningRunnable);
         mControlValueInitialized = false;
     }
@@ -442,24 +449,29 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         private final Context mContext;
         private final BroadcastDispatcher mBroadcastDispatcher;
         private final Handler mBackgroundHandler;
+        private final AutoBrightnessController.Factory mAutoBrightnessControllerFactory;
 
         @Inject
         public Factory(
                 Context context,
                 BroadcastDispatcher broadcastDispatcher,
-                @Background Handler bgHandler) {
+                @Background Handler bgHandler,
+                AutoBrightnessController.Factory autoBrightnessControllerFactory) {
             mContext = context;
             mBroadcastDispatcher = broadcastDispatcher;
             mBackgroundHandler = bgHandler;
+            mAutoBrightnessControllerFactory = autoBrightnessControllerFactory;
         }
 
         /** Create a {@link BrightnessController} */
-        public BrightnessController create(ToggleSlider toggleSlider) {
+        public BrightnessController create(ToggleSlider toggleSlider,
+                View brightnessSliderView) {
             return new BrightnessController(
                     mContext,
                     toggleSlider,
                     mBroadcastDispatcher,
-                    mBackgroundHandler);
+                    mBackgroundHandler,
+                    mAutoBrightnessControllerFactory.create(brightnessSliderView));
         }
     }
 

AOSP-whatever/platform_frameworks_base

Screenshot tile: improve code
Change-Id: Ie130e84971cf8f42a52350d7bbabfb1d9feb52ad

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -70,10 +70,9 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         mHost.collapsePanels();
 
         //finish collapsing the panel
-        try {
-             Thread.sleep(1000); //1s
-        } catch (InterruptedException ie) {}
-        Utils.takeScreenshot(mRegion ? false : true);
+        mUiHandler.postDelayed(() -> {
+            Utils.takeScreenshot(mRegion ? false : true);
+        }, 1000/*timeout*/);
     }
 
     @Override

AICP/frameworks_base

Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -90,10 +90,9 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         mHost.collapsePanels();
 
         //finish collapsing the panel
-        try {
-             Thread.sleep(1000); //1s
-        } catch (InterruptedException ie) {}
-        AicpUtils.takeScreenshot(mRegion ? false : true);
+        mUiHandler.postDelayed(() -> {
+            AicpUtils.takeScreenshot(mRegion ? false : true);
+        }, 1000/*timeout*/);
     }
 
     @Override
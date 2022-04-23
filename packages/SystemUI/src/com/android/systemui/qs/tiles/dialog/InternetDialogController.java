7f4ea41cea5887c80c318709107935edaa53bf13
LibXZR <i@xzr.moe>
2022-04-23 14:28:13 +0800

    SystemUI: Introduce 5G toggle in internet connectivity dialog
    Most of time, 5G is still considered too power hungry compared
    to the improvement of network speed. Provide users with a quick
    and convenient way to toggle this function so that it can only
    be used when needed.
    
    Change-Id: Ieee71904220e2d472d31045ca926b5c854299eed
    Signed-off-by: LibXZR <i@xzr.moe>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogController.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogController.java
@@ -39,6 +39,7 @@ import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.sysprop.TelephonyProperties;
 import android.telephony.AccessNetworkConstants;
 import android.telephony.NetworkRegistrationInfo;
 import android.telephony.ServiceState;
@@ -756,6 +757,41 @@ public class InternetDialogController implements AccessPointController.AccessPoi
             }
         }
         mWorkerHandler.post(() -> setMergedCarrierWifiEnabledIfNeed(subId, enabled));
+    }
+
+    boolean isFivegSupported() {
+        List<Integer> list = TelephonyProperties.default_network();
+        for (int type : list) {
+            if (type > 22)
+                return true;
+        }
+        return false;
+    }
+
+    boolean isFivegEnabled() {
+        if (mTelephonyManager == null
+            || (mTelephonyManager.getAllowedNetworkTypesForReason(
+                    TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER)
+                & TelephonyManager.NETWORK_TYPE_BITMASK_NR) == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    void setFivegEnabled(boolean enabled) {
+        if (mTelephonyManager == null)
+            return;
+
+        long newType = mTelephonyManager.getAllowedNetworkTypesForReason(
+            TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER);
+        if (enabled)
+            newType |= TelephonyManager.NETWORK_TYPE_BITMASK_NR;
+        else
+            newType &= ~TelephonyManager.NETWORK_TYPE_BITMASK_NR;
+
+        mTelephonyManager.setAllowedNetworkTypesForReason(
+                TelephonyManager.ALLOWED_NETWORK_TYPES_REASON_USER,
+                newType);
     }
 
     boolean isDataStateInService() {

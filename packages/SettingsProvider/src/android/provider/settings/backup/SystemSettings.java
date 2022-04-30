284ff59331a441594d299220dcedb864ea8ae3ce
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    SystemUI: Transparent QS customization
    Inspired by dotOS
    
    ** AICP Edits: add backup validators
    
    @neobuddy89: Clean up unused code and convert to 1-100 scale.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I484be7c8bdbec70801ac913e7fdaf7d2735d8ab3

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -318,5 +318,6 @@ public class SystemSettings {
         Settings.System.ANIM_TILE_STYLE,
         Settings.System.ANIM_TILE_DURATION,
         Settings.System.ANIM_TILE_INTERPOLATOR,
+        Settings.System.QS_TRANSPARENCY,
     };
 }

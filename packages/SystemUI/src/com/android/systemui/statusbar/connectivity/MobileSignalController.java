a6ae975f25afc2d7ecaf9e6b6b28fd310b5dcd86
R15Hi <rk.8969009@gmail.com>
2022-05-02 13:21:59 +0530

    SystemUI: Add 14 New VoLTE icon [1/2]
    * Thankful to:
    1. Flowder: https://t.me/flowdor
    2. Iconic icon: https://t.me/IconicIcons
    3. Acherus: https://t.me/acherussubstratum
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Change-Id: I3939cb590ec446566ac110186c4a3b915e10099a

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -625,6 +625,48 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 case 10:
                     resId = R.drawable.ic_volte_vivo;
                     break;
+                case 11:
+                    resId = R.drawable.ic_volte_aris;
+                    break;
+                case 12:
+                    resId = R.drawable.ic_volte_beast;
+                    break;
+                case 13:
+                    resId = R.drawable.ic_volte_ios;
+                    break;
+                case 14:
+                    resId = R.drawable.ic_volte_lr;
+                    break;
+                case 15:
+                    resId = R.drawable.ic_volte_realme;
+                    break;
+                case 16:
+                    resId = R.drawable.ic_volte_typeA;
+                    break;
+                case 17:
+                    resId = R.drawable.ic_volte_typeB;
+                    break;
+                case 18:
+                    resId = R.drawable.ic_volte_typeC;
+                    break;
+                case 19:
+                    resId = R.drawable.ic_volte_typeD;
+                    break;
+                case 20:
+                    resId = R.drawable.ic_volte_typeE;
+                    break;
+                case 21:
+                    resId = R.drawable.ic_volte_vcircle;
+                    break;
+                case 22:
+                    resId = R.drawable.ic_volte_vimeo;
+                    break;
+                case 23:
+                    resId = R.drawable.ic_volte_volit;
+                    break;
+                case 24:
+                    resId = R.drawable.ic_volte_zirco;
+                    break;
                 case 0:
                 default:
                     resId = R.drawable.ic_volte;

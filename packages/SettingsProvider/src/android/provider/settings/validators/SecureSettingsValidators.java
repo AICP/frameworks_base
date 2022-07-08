069e6dc871b39a73798618eb22f14c2a558e0b0e
ReallySnow <reallysnow233@gmail.com>
2022-07-08 17:36:35 +0800

    fw/b: Import Wi-Fi standard icon [1/2]
    * Show Wi-Fi 4/5/6 standard on statusbar
    
    AICP Edits: add backup validators
    
    Co-authored-by: Col_or <col_or@qq.com>
    Co-authored-by: Kujou Yuko <i@779.moe>
    Change-Id: I3658cda5b303bd03cf83e174e4dfd6a55d33e860

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -349,5 +349,6 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.MONET_ENGINE_CHROMA_FACTOR, ANY_FLOAT_VALIDATOR);
         VALIDATORS.put(Secure.MONET_ENGINE_LINEAR_LIGHTNESS, new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(Secure.MONET_ENGINE_WHITE_LUMINANCE, new InclusiveIntegerRangeValidator(0, 1000));
+        VALIDATORS.put(Secure.SHOW_WIFI_STANDARD_ICON, BOOLEAN_VALIDATOR);
     }
 }

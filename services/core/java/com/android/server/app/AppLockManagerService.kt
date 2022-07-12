7c4fd6f7c283869da559a82788cb2c8c52d789d3
jhonboy121 <alfredmathew05@gmail.com>
2022-07-12 20:13:47 +0530

    services: AppLockManagerService: fix an oob exception
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I2d89a2f79a3476fac09d6375d6bb8e914f6c7087

--- services/core/java/com/android/server/app/AppLockManagerService.kt
+++ services/core/java/com/android/server/app/AppLockManagerService.kt
@@ -197,12 +197,12 @@ class AppLockManagerService(
                 }.map {
                     atmInternal.getActivityName(it)?.packageName
                 }.filterNotNull().toSet()
-                logD("topPackages = $topPackages",
-                    "currentTopPackages = $currentTopPackages")
+                logD("currentTopPackages = $currentTopPackages")
                 // We should return early if current top packages
                 // are empty to avoid doing anything absurd.
                 if (currentTopPackages.isEmpty()) return@launch
                 val packagesToLock = mutex.withLock {
+                    logD("topPackages = $topPackages")
                     val packages = topPackages.filter {
                         !currentTopPackages.contains(it) && unlockedPackages.contains(it)
                     }.toSet()

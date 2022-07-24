322602770295d3a74e678965bbc290b734ad3d77
Josh Fox <joshfox87@gmail.com>
2022-07-25 07:50:06 +0800

    overlays: Add notch bar killer
    Change-Id: I67056fb23d8b086e9db167c988bcf39a55320c83

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -32,6 +32,7 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
 	NavigationBarModeGesturalOverlayExtraWideBack \
+	NotchBarKillerOverlay \
 	preinstalled-packages-platform-overlays.xml
 
 include $(BUILD_PHONY_PACKAGE)

62e8c4abe3a340c364f60d1c9178b1161aa1d10b
minaripenguin <alexfinhart@gmail.com>
2022-07-26 19:15:04 +0800

    SystemUI: Secondary color for lockscreen clock toggle [1/2]
     ** AICP edits: add settings / backup validators
    
    Change-Id: I50d0c2050a4f0d68d2caa7a4a157a8622dcc091d
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>
    Signed-off-by: str143 <alexfinhart@gmail.com>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -232,5 +232,6 @@ public class SystemSettings {
         Settings.System.QS_DATA_USAGE_PANEL,
         Settings.System.FORCE_FULLSCREEN_CUTOUT_APPS,
         Settings.System.CHARGING_ANIMATION,
+        Settings.System.SECONDARY_COLOR_CLOCK,
     };
 }

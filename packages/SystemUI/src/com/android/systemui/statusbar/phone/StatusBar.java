9767141189e4fe540916d1b79c9260e19444d3ff
Semavi Ulusoy <doc.divxm@gmail.com>
2022-08-21 13:19:29 +0300

    Revert "SystemUI: Port brightness slider changes"
    This reverts commit d7a26217dc0946b403799c6380baa1d981802a07.
    
    Change-Id: Id263d95cd1c633b8808e4baf645b7c39139af075

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1384,7 +1384,6 @@ public class StatusBar extends SystemUI implements
                             .withDefault(this::createDefaultQSFragment)
                             .build());
             mBrightnessMirrorController = new BrightnessMirrorController(
-                    mContext,
                     mNotificationShadeWindowView,
                     mNotificationPanelViewController,
                     mNotificationShadeDepthControllerLazy.get(),

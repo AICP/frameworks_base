2505b4747997b40f8f5dc8fd35f8dbb1585b49f7
Joey Huab <joey@evolution-x.org>
2022-08-22 22:14:40 +0000

    SystemUI: VoLTE changes
    StatusBarImsView: Adjust onDarkChanged
    
    * After 6c5ebb3.
    
    MobileSignalController: Adapt connectionReady changes
    
    * After https://android.googlesource.com/platform/frameworks/opt/net/ims/+/a93a7c4931a6da026d3c369297f3803ababfebaf.
    
    Change-Id: I9b1d328016f5b917898c4e5a2229950797822e18

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/SignalCallback.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/SignalCallback.kt
@@ -157,6 +157,7 @@ data class MobileDataIndicators(
     @JvmField val subId: Int,
     @JvmField val roaming: Boolean,
     @JvmField val showTriangle: Boolean,
+    @JvmField val isDefault: Boolean,
     @JvmField val volteId: Int
 ) {
     override fun toString(): String {
@@ -173,6 +174,7 @@ data class MobileDataIndicators(
                 .append(",subId=").append(subId)
                 .append(",roaming=").append(roaming)
                 .append(",showTriangle=").append(showTriangle)
+                .append(",isDefault=").append(isDefault)
                 .append(",volteId=").append(volteId)
                 .append(']').toString()
     }

b37b69dec961ef3aad58047898e987ab22447d23
Semavi Ulusoy <doc.divxm@gmail.com>
2022-08-31 16:36:22 +0300

    fw_base: core: Settings: Add AICP placeholders
    Change-Id: I4fb3fe40fd009ea11d845958bf41849dcec4c5dc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5481,6 +5481,14 @@ public final class Settings {
         @Readable
         public static final String MULTI_AUDIO_FOCUS_ENABLED = "multi_audio_focus_enabled";
 
+        /*****************************
+         * AICP System Settings start
+         *****************************/
+
+        /***************************
+         * AICP System Settings end
+         ***************************/
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
@@ -10671,6 +10679,14 @@ public final class Settings {
          * @hide
          */
         public static final String ADAPTIVE_CONNECTIVITY_ENABLED = "adaptive_connectivity_enabled";
+
+        /*****************************
+         * AICP Secure Settings start
+         *****************************/
+
+        /*****************************
+         * AICP Secure Settings end
+         *****************************/
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to

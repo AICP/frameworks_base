b47819edb8c854bb8711e3c37f76055590d5b48b
Ido Ben-Hur <idoybh2@gmail.com>
2022-09-10 23:11:44 +0300

    SystemUI: Allow customizing footer text [1/2]
    * @jhonboy121: simplified conditionals
    * @semdoc    : AICPfied
    
    Change-Id: I87d5197f37ae86808bafa71d62dae8cf5ca4f8f3
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6878,6 +6878,12 @@ public final class Settings {
         public static final String QS_FOOTER_TEXT_SHOW = "qs_footer_text_show";
 
         /**
+         * QS footer text
+         * @hide
+         */
+        public static final String QS_FOOTER_TEXT_STRING = "qs_footer_text_string";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -7057,6 +7063,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_BLEND_COLORS_REVERSE);
             PRIVATE_SETTINGS.add(STATUSBAR_BATTERY_BAR_BLEND_DARK_COLORS_REVERSE);
             PRIVATE_SETTINGS.add(QS_FOOTER_TEXT_SHOW);
+            PRIVATE_SETTINGS.add(QS_FOOTER_TEXT_STRING);
         }
 
         /**

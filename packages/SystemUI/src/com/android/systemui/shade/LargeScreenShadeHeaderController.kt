14744bd722394f0af4e868ec8e82549000bc368f
ReallySnow <reallysnow233@gmail.com>
2022-09-15 13:38:48 +0800

    Re-apply "SystemUI: Follow light/dark theme in SplitShade Header"
    * Google's default implementation is dark, which means
      it doesn't need to follow the light/dark color change,
      but we broke it, so add it to complete the SplitShade
      Header color change
    
    Co-authored-by: Col_or <col_or@qq.com>
    Change-Id: I84c37b563b003c07694858d3ad0975562f8cc2eb

--- packages/SystemUI/src/com/android/systemui/shade/LargeScreenShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/LargeScreenShadeHeaderController.kt
@@ -22,6 +22,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Trace
 import android.os.Trace.TRACE_TAG_APP
@@ -89,6 +90,7 @@ class LargeScreenShadeHeaderController @Inject constructor(
     private val privacyIconsController: HeaderPrivacyIconsController,
     private val insetsProvider: StatusBarContentInsetsProvider,
     private val configurationController: ConfigurationController,
+    private val context: Context,
     private val variableDateViewControllerFactory: VariableDateViewController.Factory,
     @Named(LARGE_SCREEN_BATTERY_CONTROLLER)
     private val batteryMeterViewController: BatteryMeterViewController,
@@ -139,6 +141,7 @@ class LargeScreenShadeHeaderController @Inject constructor(
     private var cutoutRight = 0
     private var roundedCorners = 0
     private var lastInsets: WindowInsets? = null
+    private var textColorPrimary = Color.TRANSPARENT
 
     private var qsDisabled = false
     private var visible = false
@@ -265,6 +268,10 @@ class LargeScreenShadeHeaderController @Inject constructor(
             }
             updateResources()
         }
+
+        override fun onUiModeChanged() {
+            updateResources()
+        }
     }
 
     override fun onInit() {
@@ -325,6 +332,7 @@ class LargeScreenShadeHeaderController @Inject constructor(
 
         updateVisibility()
         updateTransition()
+        updateResources()
     }
 
     override fun onViewDetached() {
@@ -501,6 +509,23 @@ class LargeScreenShadeHeaderController @Inject constructor(
         val padding = resources.getDimensionPixelSize(R.dimen.qs_panel_padding)
         header.setPadding(padding, header.paddingTop, padding, header.paddingBottom)
         updateQQSPaddings()
+
+        val fillColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        iconManager.setTint(fillColor)
+        val textColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        val colorStateList = Utils.getColorAttr(context, android.R.attr.textColorPrimary)
+        if (textColor != textColorPrimary) {
+            val textColorSecondary = Utils.getColorAttrDefaultColor(context,
+                    android.R.attr.textColorSecondary)
+            textColorPrimary = textColor
+            if (iconManager != null) {
+                iconManager.setTint(textColor)
+            }
+            clock.setTextColor(textColorPrimary)
+            date.setTextColor(textColorPrimary)
+            qsCarrierGroup.updateColors(textColorPrimary, colorStateList)
+            batteryIcon.updateColors(textColorPrimary, textColorSecondary, textColorPrimary)
+        }
     }
 
     private fun updateQQSPaddings() {

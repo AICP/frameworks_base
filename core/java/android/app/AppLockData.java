f20f4266b28f85223d8d7c0e2ca70d9b71221b2d
jhonboy121 <alfredmathew05@gmail.com>
2022-09-16 20:27:47 +0530

    base: applock: refactor
    * encapsulate config data sources and add a data class for per app config
    * added support for migrating between different versions of data
    * fixed notification content not being redacted.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Idbfab4d3347e715b9ee5bfe9848c4060f3236d69

---
+++ core/java/android/app/AppLockData.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.app;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Class to hold package level information about an
+ * application protected with app lock.
+ *
+ * @hide
+ */
+public final class AppLockData implements Parcelable {
+
+    public static final Parcelable.Creator<AppLockData> CREATOR =
+            new Parcelable.Creator<AppLockData>() {
+
+        @Override
+        public AppLockData createFromParcel(Parcel in) {
+            return new AppLockData(in);
+        }
+
+        @Override
+        public AppLockData[] newArray(int size) {
+            return new AppLockData[size];
+        }
+    };
+
+    private final String mPackageName;
+    private final boolean mShouldRedactNotification;
+
+    /** @hide */
+    public AppLockData(
+        @NonNull final String packageName,
+        final boolean shouldRedactNotification
+    ) {
+        mPackageName = packageName;
+        mShouldRedactNotification = shouldRedactNotification;
+    }
+
+    private AppLockData(final Parcel in) {
+        mPackageName = in.readString();
+        mShouldRedactNotification = in.readBoolean();
+    }
+
+    @NonNull
+    public String getPackageName() {
+        return mPackageName;
+    }
+
+    public boolean getShouldRedactNotification() {
+        return mShouldRedactNotification;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel parcel, final int flags) {
+        parcel.writeString(mPackageName);
+        parcel.writeBoolean(mShouldRedactNotification);
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+        return "AppLockData[ packageName = " + mPackageName +
+            ", shouldRedactNotification = " + mShouldRedactNotification + " ]";
+    }
+}

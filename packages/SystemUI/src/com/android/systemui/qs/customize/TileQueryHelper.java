omnirom/android_frameworks_base_old

base: SystemUI: add custom qs tiles overlay
allow adding a list of custom tiles

Change-Id: I73791c0e75005d741c8443e8fbbc797500906423

packages/SystemUI/src/com/android/systemui/qs/customize/TileQueryHelper.java
@@ -78,7 +78,8 @@ public class TileQueryHelper {
     }
 
     private void addStockTiles(QSTileHost host) {
-        String possible = mContext.getString(R.string.quick_settings_tiles_stock);
+        String possible = mContext.getString(R.string.quick_settings_tiles_stock)
+                + "," + mContext.getString(R.string.quick_settings_tiles_extra);
         final ArrayList<String> possibleTiles = new ArrayList<>();
         possibleTiles.addAll(Arrays.asList(possible.split(",")));
         if (Build.IS_DEBUGGABLE) {

AICP/frameworks_base

packages/SystemUI/src/com/android/systemui/qs/customize/TileQueryHelper.java
@@ -98,7 +98,8 @@ public class TileQueryHelper {
     }
 
     private void addCurrentAndStockTiles(QSTileHost host) {
-        String stock = mContext.getString(R.string.quick_settings_tiles_stock);
+        String stock = mContext.getString(R.string.quick_settings_tiles_stock)
+                + "," + mContext.getString(R.string.quick_settings_tiles_extra);
         String current = Settings.Secure.getString(mContext.getContentResolver(),
                 Settings.Secure.QS_TILES);
         final ArrayList<String> possibleTiles = new ArrayList<>();
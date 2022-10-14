f37660b20874d57dd5a0abd48be3c8b47dba54fe
Vjs Pranavasri <pranavasri@live.in>
2022-10-14 23:29:04 +0300

    AicpUtils: Add support for changing dpi
    * Taken from https://github.com/StagOS/android_frameworks_base/commit/982fadaf9dc0118de426fb386191d4f28de5c4d6
    
    * Although google made it's implementation at least right now dpi is a mess, this should work temporarily.
    * Needed for clean switching between FHD+ & QHD+
    
    Change-Id: I9cc4e5ab353fa316fa0f2c58ebefd7edd84b7cd7

--- core/java/com/android/internal/util/aicp/AicpUtils.java
+++ core/java/com/android/internal/util/aicp/AicpUtils.java
@@ -47,6 +47,8 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.statusbar.IStatusBarService;
 
+import java.io.IOException;
+import java.lang.InterruptedException;
 import java.util.List;
 import java.util.Locale;
 
@@ -361,6 +363,22 @@ public class AicpUtils {
                         ServiceManager.getService("statusbar"));
             }
             return mStatusBarService;
+        }
+    }
+
+    // Google now has a change screen reolution option but it fails to update dpi properly
+    // This function takes screen diagonal and updates dpi
+    public static void changeScreenDPI(int resolutionWidth, int resolutionHeight, float diagonalLength) {
+        int dpi = (int) (Math.sqrt((resolutionHeight*resolutionHeight) + (resolutionWidth*resolutionWidth)) / diagonalLength);
+        String command = String.format("wm density %s", Integer.toString(dpi));
+        try {
+            Runtime.getRuntime().exec(command).waitFor();
+        } catch (IOException e) {
+            System.err.println("AicpUtils: Error changing dpi");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.err.println("AicpUtils: Error changing dpi");
+            e.printStackTrace();
         }
     }
 

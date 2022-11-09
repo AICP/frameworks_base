4d81d1517d14ffcd63b703d743e6bdd49d538cd4
minaripenguin37 <alexfinhart@gmail.com>
2022-11-10 06:01:01 +0800

    SystemUI: Make combined signal icons configurable at runtime [1/2]
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Change-Id: I83b6ca7eda2e1f8c2ceb430021084965a66a42bc

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -488,6 +488,18 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
                 },
                 UserHandle.USER_ALL);
 
+        mSecureSettings.registerContentObserverForUser(
+                Settings.Secure.getUriFor(Settings.Secure.ENABLE_COMBINED_SIGNAL_ICONS),
+                false,
+                new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                            int userId) {
+                        restartSystemUI();
+                    }
+                },
+                UserHandle.USER_ALL);
+
         if (!mIsMonetEnabled) {
             return;
         }
@@ -542,6 +554,10 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
                 }
             }
         });
+    }
+
+    private void restartSystemUI() {
+        android.os.Process.killProcess(android.os.Process.myPid());
     }
 
     protected void reevaluateSystemTheme(boolean forceReload) {

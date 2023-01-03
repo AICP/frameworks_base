8eb1aed4efbb58bf46a08334486dcca444fad1a9
Pranav Vashi <neobuddy89@gmail.com>
2023-01-04 03:07:54 +0530

    SystemUI: Update IMS states and update icon when necessary
    Change-Id: Idc1806d580e49f3e83f698e967df59a1c4f71a4e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -264,8 +264,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
     @Override
     void notifyListenersIfNecessary() {
-        mNetworkController.updateImsIcon();
         super.notifyListenersIfNecessary();
+        mNetworkController.updateImsIcon();
     }
 
     void setCarrierNetworkChangeMode(boolean carrierNetworkChangeMode) {
@@ -305,6 +305,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 mImsMmTelManager.registerImsRegistrationCallback(
                         mReceiverHandler::post, mRegistrationCallback);
                 Log.d(mTag, "registerImsRegistrationCallback succeeded");
+                queryImsState();
             } catch (RuntimeException | ImsException e) {
                 if (mRetryCount < MAX_RETRY) {
                     Log.e(mTag, mRetryCount + " registerImsRegistrationCallback failed", e);
@@ -417,6 +418,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         } catch (Exception e) {
             Log.e(mTag, "removeListeners", e);
         }
+        queryImsState();
     }
 
     @Override

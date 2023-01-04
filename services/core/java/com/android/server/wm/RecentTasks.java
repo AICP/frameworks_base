675e32e7d9f2b776471883260dd8cebed99f7604
Louis Chang <louischang@google.com>
2023-01-04 05:01:39 +0000

    Revert "[RESTRICT AUTOMERGE] Trim the activity info of another uid if no privilege"
    This reverts commit fa8d6362348738284b3f33a13e1fa5cdd0af67b2.
    
    Reason for revert: apps crashed due to the top activity info trimmed
    
    Bug: 264269392 263434196 263438172
    Change-Id: I57d37649acb31bd93bd5aa10507f548cd77fc8f2
    (cherry picked from commit b37e4e7e6f465c4b6a291be6c65587dbd75b4ae4)
    Merged-In: I57d37649acb31bd93bd5aa10507f548cd77fc8f2

--- services/core/java/com/android/server/wm/RecentTasks.java
+++ services/core/java/com/android/server/wm/RecentTasks.java
@@ -976,7 +976,7 @@ class RecentTasks {
                 continue;
             }
 
-            res.add(createRecentTaskInfo(task, true /* stripExtras */, getTasksAllowed));
+            res.add(createRecentTaskInfo(task, true /* stripExtras */));
         }
         return res;
     }
@@ -1896,8 +1896,7 @@ class RecentTasks {
     /**
      * Creates a new RecentTaskInfo from a Task.
      */
-    ActivityManager.RecentTaskInfo createRecentTaskInfo(Task tr, boolean stripExtras,
-            boolean getTasksAllowed) {
+    ActivityManager.RecentTaskInfo createRecentTaskInfo(Task tr, boolean stripExtras) {
         final ActivityManager.RecentTaskInfo rti = new ActivityManager.RecentTaskInfo();
         // If the recent Task is detached, we consider it will be re-attached to the default
         // TaskDisplayArea because we currently only support recent overview in the default TDA.
@@ -1909,9 +1908,6 @@ class RecentTasks {
         rti.id = rti.isRunning ? rti.taskId : INVALID_TASK_ID;
         rti.persistentId = rti.taskId;
         rti.lastSnapshotData.set(tr.mLastTaskSnapshotData);
-        if (!getTasksAllowed) {
-            Task.trimIneffectiveInfo(tr, rti);
-        }
 
         // Fill in organized child task info for the task created by organizer.
         if (tr.mCreatedByOrganizer) {

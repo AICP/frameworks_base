d4fdbe8fcef95de9476b50fd9d5ef5078251230f
Semavi Ulusoy <doc.divxm@gmail.com>
2023-01-04 20:58:40 +0300

    Merge tag 'android-13.0.0_r20' into t13.0
    Android 13.0.0 release 20
    
    Change-Id: If4abc6a25843dbccba1fc3cc4250eb10b193f27c

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -24,6 +24,7 @@ import android.annotation.Nullable;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.res.Configuration;
+import android.content.res.Configuration.Orientation;
 import android.database.ContentObserver;
 import android.metrics.LogMaker;
 import android.net.Uri;
@@ -83,6 +84,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
 
     @Nullable
     private Consumer<Boolean> mMediaVisibilityChangedListener;
+    @Orientation
     private int mLastOrientation;
     private String mCachedSpecs = "";
     @Nullable
@@ -96,21 +98,16 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
             new QSPanel.OnConfigurationChangedListener() {
                 @Override
                 public void onConfigurationChange(Configuration newConfig) {
+                    mQSLogger.logOnConfigurationChanged(
+                        /* lastOrientation= */ mLastOrientation,
+                        /* newOrientation= */ newConfig.orientation,
+                        /* containerName= */ mView.getDumpableTag());
+
                     mShouldUseSplitNotificationShade =
-                            LargeScreenUtils.shouldUseSplitNotificationShade(getResources());
-                    // Logging to aid the investigation of b/216244185.
-                    Log.d(TAG,
-                            "onConfigurationChange: "
-                                    + "mShouldUseSplitNotificationShade="
-                                    + mShouldUseSplitNotificationShade + ", "
-                                    + "newConfig.windowConfiguration="
-                                    + newConfig.windowConfiguration);
-                    mQSLogger.logOnConfigurationChanged(mLastOrientation, newConfig.orientation,
-                            mView.getDumpableTag());
-                    if (newConfig.orientation != mLastOrientation) {
-                        mLastOrientation = newConfig.orientation;
-                        switchTileLayout(false);
-                    }
+                        LargeScreenUtils.shouldUseSplitNotificationShade(getResources());
+                    mLastOrientation = newConfig.orientation;
+
+                    switchTileLayoutIfNeeded();
                     onConfigurationChanged();
                 }
             };
@@ -403,6 +400,10 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         if (mView.isListening()) {
             refreshAllTiles();
         }
+    }
+
+    private void switchTileLayoutIfNeeded() {
+        switchTileLayout(/* force= */ false);
     }
 
     boolean switchTileLayout(boolean force) {

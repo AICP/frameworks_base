a9a365a76be84a91572f17dd8240944fc49d45e2
Michael Bestas <mkbestas@lineageos.org>
2023-03-15 04:01:31 +0200

    fixup! SystemUI: Add API for runtime taskbar config
    Change-Id: I43aa54895caaa1e55bb2e794bc12567b93ed77de

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarController.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarController.java
@@ -496,7 +496,7 @@ public class NavigationBarController implements
 
     @Override
     public void dump(@NonNull PrintWriter pw, @NonNull String[] args) {
-        pw.println("mIsTablet=" + mIsTablet);
+        pw.println("mTaskbarShowing=" + mTaskbarShowing);
         pw.println("mNavMode=" + mNavMode);
         for (int i = 0; i < mNavigationBars.size(); i++) {
             if (i > 0) {

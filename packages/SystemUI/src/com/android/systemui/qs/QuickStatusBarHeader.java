47aa1ad126197db1969d9ef04af08ec2406616c1
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-30 22:33:05 +0800

    SystemUI: QS Header Image [1/2]
    * reverse engineered from ancient os with [1] as reference for broken codes from jadx
    * Optimized qs banner resources for systemui file size
    * all credits goes to ancientOS
    
    [1]: https://github.com/Hikari-no-Tenshi/crdroid_android_frameworks_base/commit/354d981d3f79783e284c8aced755e90496324982
    
    mnri: rewrite and refactored
    
    Co-authored-by: Idc <59904122+IDontCare-05@users.noreply.github.com>
    Change-Id: I6200b9dbacf51b920979226653a7e3d68fea854a
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -22,6 +22,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.VibrationEffect;
@@ -37,12 +38,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Space;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.android.internal.graphics.ColorUtils;
 import com.android.internal.policy.SystemBarUtils;
 import com.android.settingslib.Utils;
 import com.android.systemui.Dependency;
@@ -72,6 +75,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
             "system:" + Settings.System.SHOW_QS_CLOCK;
     private static final String SHOW_QS_DATE =
             "system:" + Settings.System.SHOW_QS_DATE;
+    private static final String QS_HEADER_IMAGE =
+            "system:" + Settings.System.QS_HEADER_IMAGE;
 
     private boolean mExpanded;
     private boolean mQsDisabled;
@@ -106,6 +111,12 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     private BatteryMeterView mBatteryRemainingIcon;
     private StatusIconContainer mIconContainer;
     private View mPrivacyChip;
+
+    // QS Header
+    private ImageView mQsHeaderImageView;
+    private View mQsHeaderLayout;
+    private boolean mHeaderImageEnabled;
+    private int mHeaderImageValue;
 
     @Nullable
     private TintedIconManager mTintedIconManager;
@@ -181,6 +192,10 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         mBatteryRemainingIcon.setOnClickListener(this);
         mBatteryRemainingIcon.setOnLongClickListener(this);
 
+        mQsHeaderLayout = findViewById(R.id.layout_header);
+        mQsHeaderImageView = findViewById(R.id.qs_header_image_view);
+        mQsHeaderImageView.setClipToOutline(true);
+
         updateResources();
         Configuration config = mContext.getResources().getConfiguration();
         setDatePrivacyContainersWidth(config.orientation == Configuration.ORIENTATION_LANDSCAPE);
@@ -192,7 +207,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
 
         Dependency.get(TunerService.class).addTunable(this,
                 SHOW_QS_CLOCK,
-                SHOW_QS_DATE);
+                SHOW_QS_DATE,
+                QS_HEADER_IMAGE);
     }
 
     void onAttach(TintedIconManager iconManager,
@@ -228,8 +244,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         if (mDatePrivacyView.getMeasuredHeight() != mTopViewMeasureHeight) {
             mTopViewMeasureHeight = mDatePrivacyView.getMeasuredHeight();
-            updateAnimators();
         }
+        updateAnimators();
     }
 
     @Override
@@ -365,6 +381,27 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         updateAnimators();
 
         updateClockDatePadding();
+        updateQSHeaderImage();
+    }
+
+    private void updateQSHeaderImage() {
+        boolean mIsNightMode = (mContext.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+        int orientation = getResources().getConfiguration().orientation;
+        if (mHeaderImageEnabled && orientation != Configuration.ORIENTATION_LANDSCAPE) {
+            int fadeFilter = ColorUtils.blendARGB(Color.TRANSPARENT, mIsNightMode ? Color.BLACK : Color.WHITE, 30 / 100f);
+            int resId = getResources().getIdentifier("qs_header_image_" + String.valueOf(mHeaderImageValue), "drawable", "com.android.systemui");
+            mQsHeaderImageView.setImageResource(resId);
+            mQsHeaderImageView.setColorFilter(fadeFilter, PorterDuff.Mode.SRC_ATOP);
+            mQsHeaderImageView.setVisibility(View.VISIBLE);
+        } else {
+            mQsHeaderImageView.setVisibility(View.GONE);
+        }
+
+        ViewGroup.MarginLayoutParams qsHeaderLayout = (ViewGroup.MarginLayoutParams) mQsHeaderLayout.getLayoutParams();
+        qsHeaderLayout.height = mHeaderImageEnabled && orientation != Configuration.ORIENTATION_LANDSCAPE ?
+            mContext.getResources().getDimensionPixelSize(R.dimen.qs_header_height_full) : 0;
+        qsHeaderLayout.setMargins(-50, 0, -50, 0);
+        mQsHeaderLayout.setLayoutParams(qsHeaderLayout);
     }
 
     private void updateClockDatePadding() {
@@ -668,6 +705,12 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
                         TunerService.parseIntegerSwitch(newValue, true);
                 mDateContainer.setVisibility(mShowDate ? View.VISIBLE : View.GONE);
                 mClockDateView.setVisibility(mShowClock && mShowDate ? View.VISIBLE : View.GONE);
+                break;
+            case QS_HEADER_IMAGE:
+                mHeaderImageValue =
+                       TunerService.parseInteger(newValue, 0);
+                mHeaderImageEnabled = mHeaderImageValue != 0;
+                updateResources();
                 break;
             default:
                 break;

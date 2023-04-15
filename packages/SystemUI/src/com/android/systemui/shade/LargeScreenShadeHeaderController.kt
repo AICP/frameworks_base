d4aa190f30ac0eeb3e428d609e1687b64ee06351
Ali B <abittin@gmail.com>
2023-04-15 21:06:46 +0300

    NetworkTraffic: fix color issues on combined QS header
     ** it was white in light theme
     ** inspired from https://gerrit.aicp-rom.com/c/AICP/frameworks_base/+/114470
    
    Change-Id: I1383b2a518e700eca727fafa1130610b60a0efcd
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/LargeScreenShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/LargeScreenShadeHeaderController.kt
@@ -533,6 +533,7 @@ class LargeScreenShadeHeaderController @Inject constructor(
             qsCarrierGroup.updateColors(textColorPrimary, colorStateList)
             batteryIcon.updateColors(textColorPrimary, textColorSecondary, textColorPrimary)
             networkTraffic.setTintColor(textColorPrimary)
+            networkTraffic.setTextColor(textColorPrimary);
         }
     }
 

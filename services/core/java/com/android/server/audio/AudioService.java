18856010bc798445f72f3f65023059c8d30f0335
Pranav Vashi <neobuddy89@gmail.com>
2023-04-18 23:24:48 +0530

    base: Add switch to unlink ringtone and notifications volume [1/2]
    * Make use of AOSP config to set default initial value
    
    Ref:
    https://github.com/crdroidandroid/android_frameworks_base/commit/345ff7b8618bd57f6c6ccbf875cf860338deb513
    https://github.com/crdroidandroid/android_frameworks_base/commit/ab1aad9aa76391d675ca38a93f23120032c1d957
    
    Change-Id: Ib0adde055d91527a57b203cb488bce6f62974322
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -846,6 +846,9 @@ public class AudioService extends IAudioService.Stub
         0.9f,   // Pre-scale for index 3
     };
 
+    private boolean mLinkNotificationWithVolume;
+    private final boolean mVoiceCapable;
+
     private NotificationManager mNm;
     private AudioManagerInternal.RingerModeDelegate mRingerModeDelegate;
     private VolumePolicy mVolumePolicy = VolumePolicy.DEFAULT;
@@ -1031,6 +1034,9 @@ public class AudioService extends IAudioService.Stub
         mNotifAliasRing = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_alias_ring_notif_stream_types);
 
+        mVoiceCapable = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_voice_capable);
+
         // Initialize volume
         // Priority 1 - Android Property
         // Priority 2 - Audio Policy Service
@@ -1196,6 +1202,10 @@ public class AudioService extends IAudioService.Stub
                 com.android.internal.R.integer.config_safe_media_volume_index) * 10;
 
         mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+
+        // read this in before readPersistedSettings() because updateStreamVolumeAlias needs it
+        mLinkNotificationWithVolume = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.VOLUME_LINK_NOTIFICATION, mNotifAliasRing ? 1 : 0) == 1;
 
         mUseFixedVolume = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_useFixedVolume);
@@ -2236,7 +2246,7 @@ public class AudioService extends IAudioService.Stub
                     mStreamVolumeAlias = STREAM_VOLUME_ALIAS_DEFAULT;
                     dtmfStreamAlias = AudioSystem.STREAM_MUSIC;
             }
-            if (!mNotifAliasRing) {
+            if (!mLinkNotificationWithVolume && mVoiceCapable) {
                 mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] =
                         AudioSystem.STREAM_NOTIFICATION;
             }
@@ -2712,6 +2722,9 @@ public class AudioService extends IAudioService.Stub
             AudioSystem.setRttEnabled(mRttEnabled);
             setVolumeKeysControlRingTone();
         }
+
+        mLinkNotificationWithVolume = Settings.Secure.getInt(cr,
+                Settings.Secure.VOLUME_LINK_NOTIFICATION, mNotifAliasRing ? 1 : 0) == 1;
 
         mMuteAffectedStreams = mSettings.getSystemIntForUser(cr,
                 System.MUTE_STREAMS_AFFECTED, AudioSystem.DEFAULT_MUTE_STREAMS_AFFECTED,
@@ -8794,6 +8807,8 @@ public class AudioService extends IAudioService.Stub
                     Settings.Secure.VOICE_INTERACTION_SERVICE), false, this);
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.VOLUME_KEYS_CONTROL_RING_TONE), false, this);
+            mContentResolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.VOLUME_LINK_NOTIFICATION), false, this);
         }
 
         @Override
@@ -8818,6 +8833,13 @@ public class AudioService extends IAudioService.Stub
                 sendEnabledSurroundFormats(mContentResolver, mSurroundModeChanged);
                 setVolumeKeysControlRingTone();
                 updateAssistantUIdLocked(/* forceUpdate= */ false);
+            }
+            boolean linkNotificationWithVolume = Settings.Secure.getInt(mContentResolver,
+                    Settings.Secure.VOLUME_LINK_NOTIFICATION, mNotifAliasRing ? 1 : 0) == 1;
+            if (linkNotificationWithVolume != mLinkNotificationWithVolume) {
+                mLinkNotificationWithVolume = linkNotificationWithVolume;
+                onInitStreamsAndVolumes();
+                updateStreamVolumeAlias(true, TAG);
             }
         }
 

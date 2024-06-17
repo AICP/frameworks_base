e5a4ddfea3eda87d2521f3e40072683c6dbea15e
someone5678 <someone5678@users.noreply.github.com>
2024-06-17 23:58:17 +0900

    fixup! Screenshot: Add delete action chip intent
    * Restore and adapt code for delete action
    
    Test: Build, boot, take screenshot, check delete action chip works.
    
    Change-Id: I3157169963065720d6b3ce9b5ad98a9d6bb74e82

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -167,8 +167,8 @@ public class ScreenshotController {
      */
     static class SavedImageData {
         public Uri uri;
-        public List<Notification.Action> smartActions;
         public Notification.Action deleteAction;
+        public List<Notification.Action> smartActions;
         public Notification.Action quickShareAction;
         public UserHandle owner;
         public String subject;  // Title for sharing
@@ -178,9 +178,9 @@ public class ScreenshotController {
          */
         public void reset() {
             uri = null;
+            deleteAction = null;
             smartActions = null;
             quickShareAction = null;
-            deleteAction = null;
             subject = null;
         }
     }

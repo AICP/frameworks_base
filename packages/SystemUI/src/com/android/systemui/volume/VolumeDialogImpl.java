dfa34f2c342bdeb747bc33677f80bd33c40b26ce
Pranav Vashi <neobuddy89@gmail.com>
2024-07-06 10:09:27 +0530

    SystemUI: VolumeDialog: Fix volume panel on left on secondary users
    Change-Id: I51b8790fc5ac9293ff65864b30a18ba646563b96
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -434,9 +434,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             ContentObserver volumePanelOnLeftObserver = new ContentObserver(null) {
                 @Override
                 public void onChange(boolean selfChange) {
-                    final boolean volumePanelOnLeft = Settings.Secure.getInt(
+                    final boolean volumePanelOnLeft = Settings.Secure.getIntForUser(
                             mContext.getContentResolver(),
-                            Settings.Secure.VOLUME_PANEL_ON_LEFT, 0) != 0;
+                            Settings.Secure.VOLUME_PANEL_ON_LEFT, 0,
+                            UserHandle.USER_CURRENT) != 0;
                     if (mVolumePanelOnLeft != volumePanelOnLeft) {
                         mVolumePanelOnLeft = volumePanelOnLeft;
                         mHandler.post(mControllerCallbackH::onConfigurationChanged);

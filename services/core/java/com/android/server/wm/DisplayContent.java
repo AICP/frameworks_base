d5d59971e649ae24cb1a2a42a4a4aa3e41ce8ba1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-31 06:18:29 +0530

    wm: Add API to listen for secure content in display
    Required by LMOFreeform.
    
    Change-Id: I97143a8b35af1432b9b12319bab5c1dfec9f9b34

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -796,6 +796,8 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
     /** Last window to hold the screen locked. */
     private WindowState mLastWakeLockHoldingWindow;
 
+    private boolean mHasSecureContent;
+
     /**
      * Whether display is allowed to ignore all activity size restrictions.
      * @see #isDisplayIgnoreActivitySizeRestrictions
@@ -5158,6 +5160,11 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
         return imeLayeringTarget.mSession.mUid == uid && imeLayeringTarget.mSession.mPid == pid;
     }
 
+    boolean hasSecureWindowOnScreen() {
+        final WindowState win = getWindow(w -> w.isOnScreen() && w.isSecureLocked());
+        return win != null;
+    }
+
     // TODO: Super unexpected long method that should be broken down...
     void applySurfaceChangesTransaction() {
         final WindowSurfacePlacer surfacePlacer = mWmService.mWindowPlacerLocked;
@@ -5292,6 +5299,13 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
         mInputMonitor.setUpdateInputWindowsNeededLw();
         if (updateInputWindows) {
             mInputMonitor.updateInputWindowsLw(false /*force*/);
+        }
+
+        // Notify if display added or removed a secure window
+        final boolean hasSecureContent = hasSecureWindowOnScreen();
+        if (hasSecureContent != mHasSecureContent) {
+            mHasSecureContent = hasSecureContent;
+            mWmService.notifyDisplaySecureContentChange(mDisplayId, hasSecureContent);
         }
     }
 

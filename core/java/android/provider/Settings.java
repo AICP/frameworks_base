3074bcd5a0b2fcc8b61e5a08683c38cc63b32183
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-14 12:28:40 +0800

    SystemUI: Introduce Clock face feature
    Change-Id: I71f5d4577ba3d38dfcf988da2768617cd1821db7
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14268,6 +14268,12 @@ public final class Settings {
         public static final String HIDE_DEVELOPER_STATUS = "hide_developer_status";
 
         /**
+         * Lockscreen custom clock face
+         * @hide
+         */
+        public static final String LOCK_SCREEN_CUSTOM_CLOCK_STYLE = "lock_screen_custom_clock_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

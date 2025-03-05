1766a22c143832a1e55d39da50eee63ea04a49d5
Sumedh Sen <sumedhsen@google.com>
2025-03-05 14:42:42 -0800

    Remove invalid null check for callingPackage
    getLaunchedFromPackage() will never return null. As such, remove the null check while processing a session based install
    
    Bug: 399881067
    Bug: 364604008
    Test: atest CtsPackageInstallTestCases CtsAppOps2TestCases
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:d733423089a4006c2cdd2357be6cf176e84ea150)
    Merged-In: I4dc5fc8108c1ffffde82a5d774d4eb3805fbf0a3
    Change-Id: I4dc5fc8108c1ffffde82a5d774d4eb3805fbf0a3

--- packages/PackageInstaller/src/com/android/packageinstaller/InstallStart.java
+++ packages/PackageInstaller/src/com/android/packageinstaller/InstallStart.java
@@ -27,6 +27,7 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageInstaller;
+import android.content.pm.PackageInstaller.SessionInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.net.Uri;
@@ -67,14 +68,15 @@ public class InstallStart extends Activity {
         // If the activity was started via a PackageInstaller session, we retrieve the calling
         // package from that session
         final int sessionId = (isSessionInstall
-                ? intent.getIntExtra(PackageInstaller.EXTRA_SESSION_ID, -1)
-                : -1);
-        if (callingPackage == null && sessionId != -1) {
-            PackageInstaller packageInstaller = getPackageManager().getPackageInstaller();
+                ? intent.getIntExtra(PackageInstaller.EXTRA_SESSION_ID, SessionInfo.INVALID_ID)
+                : SessionInfo.INVALID_ID);
+        if (sessionId != SessionInfo.INVALID_ID) {
+            PackageInstaller packageInstaller = mPackageManager.getPackageInstaller();
             PackageInstaller.SessionInfo sessionInfo = packageInstaller.getSessionInfo(sessionId);
-            callingPackage = (sessionInfo != null) ? sessionInfo.getInstallerPackageName() : null;
-            callingAttributionTag =
-                    (sessionInfo != null) ? sessionInfo.getInstallerAttributionTag() : null;
+            if (sessionInfo != null) {
+                callingPackage = sessionInfo.getInstallerPackageName();
+                callingAttributionTag = sessionInfo.getInstallerAttributionTag();
+            }
         }
 
         final ApplicationInfo sourceInfo = getSourceInfo(callingPackage);
@@ -191,7 +193,7 @@ public class InstallStart extends Activity {
     private ApplicationInfo getSourceInfo(@Nullable String callingPackage) {
         if (callingPackage != null) {
             try {
-                return getPackageManager().getApplicationInfo(callingPackage, 0);
+                return mPackageManager.getApplicationInfo(callingPackage, 0);
             } catch (PackageManager.NameNotFoundException ex) {
                 // ignore
             }
@@ -238,7 +240,7 @@ public class InstallStart extends Activity {
     }
 
     private boolean isSystemDownloadsProvider(int uid) {
-        final ProviderInfo downloadProviderPackage = getPackageManager().resolveContentProvider(
+        final ProviderInfo downloadProviderPackage = mPackageManager.resolveContentProvider(
                 DOWNLOADS_AUTHORITY, 0);
         if (downloadProviderPackage == null) {
             // There seems to be no currently enabled downloads provider on the system.

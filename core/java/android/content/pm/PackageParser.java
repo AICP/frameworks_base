59d1b948584f802ada01055b6508ca2e33751fc9
Steven Moreland <smoreland@google.com>
2025-03-19 16:13:37 -0700

    PM: Restrict subclass construction.
    Don't construct a random class just because you were asked to.
    
    Bug: 373467684
    Test: atest PackageParsingPerfTest
    Flag: EXEMPT bugfix
    (cherry picked from commit 65c1a90bf4af54f555ded29ec2384072b1c962b8)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b6af52a75354b862781c7b46466c3356791c5599)
    Merged-In: I328eca7b8de1eab637e3991adeb2e1d147c30268
    Change-Id: I328eca7b8de1eab637e3991adeb2e1d147c30268

--- core/java/android/content/pm/PackageParser.java
+++ core/java/android/content/pm/PackageParser.java
@@ -7772,6 +7772,10 @@ public class PackageParser {
             final ArrayList<T> intentsList;
             try {
                 final Class<T> cls = (Class<T>) Class.forName(componentName);
+                if (!IntentInfo.class.isAssignableFrom(cls)) {
+                    throw new AssertionError("Intent list requires subclass of IntentInfo, not: "
+                            + componentName);
+                }
                 final Constructor<T> cons = cls.getConstructor(Parcel.class);
 
                 intentsList = new ArrayList<>(N);

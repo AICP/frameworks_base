9be2c04c8b2d75a6785fdf0b3cf562163d5bf357
Ibrahim Yilmaz <iyz@google.com>
2025-05-21 13:16:05 +0000

    [ENR] Only skip hide sensitive anim for showing public
    We don't want to play hide sensitive animation when layout is not changed and mShowingPublic is true. Current code is too aggressive and can cause 0 content alpha.
    
    Bug: 414782247
    Bug: 392614656
    Test: Check hideSensitive animation play and appear animations.
    Flag: EXEMPT trivial bugfix
    (cherry picked from commit e93d4b015c283e55cb08d68a499aab41f03e1272)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:5b2f020dbcf952529ce8386d8a297d205544ee8d
    Merged-In: Ia417a27dcb7d26609df5629bae9bbba1785983a0
    Change-Id: Ia417a27dcb7d26609df5629bae9bbba1785983a0

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
@@ -2817,11 +2817,15 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
         }
         boolean oldShowingPublic = mShowingPublic;
         mShowingPublic = mSensitive && hideSensitive;
-        if (mShowingPublicInitialized && mShowingPublic == oldShowingPublic) {
+        boolean isShowingLayoutNotChanged = mShowingPublic == oldShowingPublic;
+        if (mShowingPublicInitialized && isShowingLayoutNotChanged) {
             return;
         }
 
-        if (!animated) {
+        final boolean shouldSkipHideSensitiveAnimation =
+                mShowingPublic && isShowingLayoutNotChanged;
+
+        if (!animated || shouldSkipHideSensitiveAnimation) {
             mPublicLayout.animate().cancel();
             mPrivateLayout.animate().cancel();
             if (mChildrenContainer != null) {

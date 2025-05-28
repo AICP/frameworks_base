182b4af674329baa082e01447ae2a977689ee202
Julian Veit <claymore1298@gmail.com>
2025-05-28 21:49:31 +0200

    Revert "fixup! SystemUI: On-The-Go Mode (1/2)"
    This reverts commit 904699a31fa4cba6588906b312e88f4c9f12f5bd.
    
    Change-Id: I9cf7d6c17ecafa264896bd917d751dfd913d5e1b

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -476,7 +476,6 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 });
 
         mActions = mLineageGlobalActions.getUserActionsArray();
-        mActions.add(GLOBAL_ACTION_KEY_ONTHEGO);
     }
 
     /**

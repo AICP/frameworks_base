e52eef2bf39b4f364fda1115cd805cfd979700c9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-07-11 22:41:09 +0530

    fixup! base: Hide ADB and developer setting enable status [1/2]
    Change-Id: I0a2d02c27cdb90d79a5d1f5035350ac94df3e49a

--- core/java/com/android/internal/util/HideDeveloperStatusUtils.java
+++ core/java/com/android/internal/util/HideDeveloperStatusUtils.java
@@ -3,55 +3,56 @@ package com.android.internal.util;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.UserHandle;
+import android.provider.Settings;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import android.provider.Settings;
-
+/**
+ * @hide
+ */
 public class HideDeveloperStatusUtils {
-    private static Set<String> mApps = new HashSet<>();
-    private static final Set<String> settingsToHide = new HashSet<>(Arrays.asList(
+
+    private Context mContext;
+    private Set<String> mApps;
+    private static final Set<String> SETTINGS_TO_HIDE = Set.of(
         Settings.Global.ADB_ENABLED,
         Settings.Global.ADB_WIFI_ENABLED,
         Settings.Global.DEVELOPMENT_SETTINGS_ENABLED
-    ));
+    );
+
+    public HideDeveloperStatusUtils(Context context) {
+        mContext = context;
+        mApps = getApps(context.getContentResolver());
+    }
 
     public static boolean shouldHideDevStatus(ContentResolver cr, String packageName, String name) {
-        return getApps(cr).contains(packageName) && settingsToHide.contains(name);
+        return getApps(cr).contains(packageName) && SETTINGS_TO_HIDE.contains(name);
     }
 
     private static Set<String> getApps(ContentResolver cr) {
         String apps = Settings.Secure.getString(cr, Settings.Secure.HIDE_DEVELOPER_STATUS);
-        if (apps != null) {
-            mApps = new HashSet<>(Arrays.asList(apps.split(",")));
-        } else {
-            mApps = new HashSet<>();
-        }
-        return mApps;
+        return apps != null ? new HashSet<>(Arrays.asList(apps.split(","))) : new HashSet<>();
     }
 
-    public void addApp(Context mContext, String packageName) {
+    public void addApp(String packageName) {
         mApps.add(packageName);
         Settings.Secure.putString(mContext.getContentResolver(),
                 Settings.Secure.HIDE_DEVELOPER_STATUS, String.join(",", mApps));
     }
 
-    public void removeApp(Context mContext, String packageName) {
+    public void removeApp(String packageName) {
         mApps.remove(packageName);
         Settings.Secure.putString(mContext.getContentResolver(),
                 Settings.Secure.HIDE_DEVELOPER_STATUS, String.join(",", mApps));
     }
 
-    public void setApps(Context mContext) {
-        String apps = Settings.Secure.getStringForUser(mContext.getContentResolver(),
-                Settings.Secure.HIDE_DEVELOPER_STATUS,
-                UserHandle.USER_SYSTEM);
-        if (apps != null) {
-            mApps = new HashSet<>(Arrays.asList(apps.split(",")));
-        } else {
-            mApps = new HashSet<>();
-        }
+    public Set<String> getApps() {
+        return mApps;
+    }
+
+    public void setApps(Set<String> apps) {
+        mApps = apps;
     }
 }

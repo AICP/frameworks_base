91d37855621229bacc20f5eda9054353cd778e11
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-22 14:40:52 +0800

    SystemUI: Add ScrimUtils helper [SQUASHED]
    [neobuddy89: Squashed all fixes by same author.]
    
    Change-Id: I4ceb44d694c4bcd625cb949162fc87dde30f846e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -206,6 +206,7 @@ import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.SplitShadeStateController;
 import com.android.systemui.unfold.SysUIUnfoldComponent;
+import com.android.systemui.util.ScrimUtils;
 import com.android.systemui.util.Utils;
 import com.android.systemui.util.time.SystemClock;
 import com.android.systemui.window.domain.interactor.WindowRootViewBlurInteractor;
@@ -3171,6 +3172,7 @@ public final class NotificationPanelViewController implements
             }
             mExpandedFraction = Math.min(1f,
                     maxPanelHeight == 0 ? 0 : mExpandedHeight / maxPanelHeight);
+            ScrimUtils.get().setExpandedFraction(mExpandedFraction);
             if (mExpandedFraction > 0f && mExpectingSynthesizedDown) {
                 mExpectingSynthesizedDown = false;
             }
@@ -3611,6 +3613,7 @@ public final class NotificationPanelViewController implements
 
             // TODO: maybe add a listener for barstate
             mBarState = statusBarState;
+            ScrimUtils.get().setBarState(mBarState);
             mQsController.setBarState(statusBarState);
 
             boolean fromShadeToKeyguard = statusBarState == KEYGUARD

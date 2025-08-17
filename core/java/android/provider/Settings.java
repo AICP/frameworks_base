9726f92a22a5cd0cf3dcaaf4fc6be359765b5282
Pranav Vashi <neobuddy89@gmail.com>
2025-08-18 02:20:19 +0530

    SystemUI: Add customization to select QS tile shape
    * At present it checks only for the state. Inject setting to pre-select shape.
    
    Change-Id: Ie75b670c80982171a88d35de33bfaf677f72e5bc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6954,6 +6954,12 @@ public final class Settings {
         public static final String SMART_PIXELS_ON_POWER_SAVE = "smart_pixels_on_power_save";
 
         /**
+         * Customize QS tile shape.
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

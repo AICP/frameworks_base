631eddaa00654708caf9658ad8b21b0b676d0b1f
NurKeinNeid <nurkeinneid@derpfest.org>
2025-09-21 13:31:58 +0200

    SystemUI: Add option to tint status bar icons with system accent color [1/2]
    Change-Id: I302661ea129d8009928e31feb5d46e2d93a840d0
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/battery/shared/ui/BatteryColors.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/battery/shared/ui/BatteryColors.kt
@@ -120,6 +120,7 @@ sealed interface BatteryColors {
     /** Accent color theme for light mode */
     class AccentLightTheme(private val accentColor: Color) : LightTheme() {
         override val attribution = accentColor
+        override val glyph = accentColor.copy(alpha = 0.75f)
         override val fill = accentColor
         override val backgroundOnly = accentColor.copy(alpha = 0.20f)
         override val backgroundWithGlyph = accentColor.copy(alpha = 0.55f)
@@ -128,6 +129,7 @@ sealed interface BatteryColors {
     /** Accent color theme for dark mode */
     class AccentDarkTheme(private val accentColor: Color) : DarkTheme() {
         override val attribution = accentColor
+        override val glyph = accentColor.copy(alpha = 0.75f)
         override val fill = accentColor
         override val backgroundOnly = accentColor.copy(alpha = 0.45f)
         override val backgroundWithGlyph = accentColor.copy(alpha = 0.55f)

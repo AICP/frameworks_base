9ab89ec20927709f1509aad40c8455c476407c33
mosimchah <mosimchah@gmail.com>
2025-10-30 16:27:30 -0400

    Revert "Add Compass tile"
    This reverts commit e410648002e6d1268e7bff74ed0e33b66048d96a.
    
    Reason for revert: Going to break it
    
    Change-Id: Idfade8ae94c1f352234974505d01b1c7918b95f0

--- packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
+++ packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
@@ -25,7 +25,6 @@ import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
-import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.CPUInfoTile;
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.MusicTile
@@ -79,12 +78,6 @@ interface AicpModule {
     @IntoMap
     @StringKey(CellularTile.TILE_SPEC)
     fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
-
-    /** Inject CompassTile into tileMap in QSModule */
-    @Binds
-    @IntoMap
-    @StringKey(CompassTile.TILE_SPEC)
-    fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
 
     /** Inject CPUInfoTile into tileMap in QSModule */
     @Binds

d625a74c584a7ac434005ec02eb64fe487050c9a
Annie Lin <theannielin@google.com>
2025-12-03 17:21:15 -0800

    Prevent launchedFromPackage spoofing via FLAG_ACTIVITY_FORWARD_RESULT.
    Bug: 457742426
    Test: atest ActivityStarterTests
    Test: Verified via test app
    Flag: EXEMPT CVE_FIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:3bb240273822e41f3c6911c60d15983a600308f7
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:f057590327b4aef8c84be208db58a148b81fcdfc
    Merged-In: Ic9637c56803b00acc9fca59f8092ed02dd46a4fb
    Change-Id: Ic9637c56803b00acc9fca59f8092ed02dd46a4fb

--- services/tests/wmtests/src/com/android/server/wm/ActivityStarterTests.java
+++ services/tests/wmtests/src/com/android/server/wm/ActivityStarterTests.java
@@ -1558,6 +1558,212 @@ public class ActivityStarterTests extends WindowTestsBase {
         assertNull(starter2.mMovedToTopActivity);
     }
 
+    /**
+     * Tests a task with specific display category exist in system and then launching another
+     * activity with the same affinity but without define the display category. Make sure the
+     * lunching activity is placed on the different task.
+     */
+    @Test
+    public void testLaunchActivityWithoutDisplayCategory() {
+        final ActivityInfo info = new ActivityInfo();
+        info.applicationInfo = new ApplicationInfo();
+        info.taskAffinity = ActivityRecord.computeTaskAffinity("test", DEFAULT_FAKE_UID,
+                0 /* launchMode */, null /* componentName */);
+        info.requiredDisplayCategory = "automotive";
+        final Task task = new TaskBuilder(mSupervisor).setCreateActivity(true).setActivityInfo(info)
+                .build();
+
+        final ActivityRecord target = new ActivityBuilder(mAtm).setAffinity(info.taskAffinity)
+                .build();
+        final ActivityStarter starter = prepareStarter(FLAG_ACTIVITY_NEW_TASK, false);
+        spyOn(starter);
+        doReturn(START_SUCCESS).when(starter).isAllowedToStart(any(), anyBoolean(), any());
+        startActivityInner(starter, target, null /* source */, null /* options */,
+                null /* inTask */, null /* inTaskFragment */);
+
+        assertNotEquals(task, target.getTask());
+    }
+
+    /**
+     * Tests a task with a specific display category exist in the system and then launches another
+     * activity with the different display category. Make sure the launching activity is not placed
+     * on the sourceRecord's task.
+     */
+    @Test
+    public void testLaunchActivityWithDifferentDisplayCategory() {
+        final ActivityInfo info = new ActivityInfo();
+        info.applicationInfo = new ApplicationInfo();
+        info.taskAffinity = ActivityRecord.computeTaskAffinity("test", DEFAULT_FAKE_UID,
+                0 /* launchMode */, null /* componentName */);
+        info.requiredDisplayCategory = "automotive";
+        final Task task = new TaskBuilder(mSupervisor).setCreateActivity(true).setActivityInfo(info)
+                .build();
+
+        final ActivityRecord target = new ActivityBuilder(mAtm).setRequiredDisplayCategory("auto")
+                .setAffinity(info.taskAffinity).build();
+        final ActivityStarter starter = prepareStarter(0, false);
+        spyOn(starter);
+        doReturn(START_SUCCESS).when(starter).isAllowedToStart(any(), anyBoolean(), any());
+        startActivityInner(starter, target,  task.getBottomMostActivity(), null /* options */,
+                null /* inTask */, null /* inTaskFragment */);
+
+        assertNotEquals(task, target.getTask());
+    }
+
+    /**
+     * Tests a task with specific display category exist in system and then launching another
+     * activity with the same display category. Make sure the launching activity is placed on the
+     * same task.
+     */
+    @Test
+    public void testLaunchActivityWithSameDisplayCategory() {
+        final ActivityInfo info = new ActivityInfo();
+        info.applicationInfo = new ApplicationInfo();
+        info.taskAffinity = ActivityRecord.computeTaskAffinity("test", DEFAULT_FAKE_UID,
+                0 /* launchMode */, null /* componentName */);
+        info.requiredDisplayCategory = "automotive";
+        final Task task = new TaskBuilder(mSupervisor).setCreateActivity(true).setActivityInfo(info)
+                .build();
+
+        final ActivityRecord target = new ActivityBuilder(mAtm)
+                .setRequiredDisplayCategory(info.requiredDisplayCategory)
+                .setAffinity(info.taskAffinity).build();
+
+        final ActivityStarter starter = prepareStarter(0, false);
+        spyOn(starter);
+        doReturn(START_SUCCESS).when(starter).isAllowedToStart(any(), anyBoolean(), any());
+        startActivityInner(starter, target,  task.getBottomMostActivity(), null /* options */,
+                null /* inTask */, null /* inTaskFragment */);
+
+        assertEquals(task, target.getTask());
+    }
+
+    /**
+     * Tests a task with specific display category exist in system and launching activity into the
+     * specific task within inTask attribute. Make sure the activity is not placed on the task since
+     * the display category is different.
+     */
+    @Test
+    public void testLaunchActivityInTaskWithDisplayCategory() {
+        final ActivityInfo info = new ActivityInfo();
+        info.applicationInfo = new ApplicationInfo();
+        info.requiredDisplayCategory = "automotive";
+        final Task inTask = new TaskBuilder(mSupervisor).setActivityInfo(info).build();
+        inTask.inRecents = true;
+
+        final ActivityStarter starter = prepareStarter(0, false);
+        spyOn(starter);
+        doReturn(START_SUCCESS).when(starter).isAllowedToStart(any(), anyBoolean(), any());
+        final ActivityRecord target = new ActivityBuilder(mAtm).build();
+        startActivityInner(starter, target, null /* source */, null /* options */, inTask,
+                null /* inTaskFragment */);
+
+        assertNotEquals(inTask, target.getTask());
+    }
+
+    /**
+     * Tests a task without a specific display category exist in the system and launches activity
+     * with display category into the task within the inTask attribute. Make sure the activity is
+     * not placed on the task since the display category is different.
+     */
+    @Test
+    public void testLaunchDisplayCategoryActivityInTask() {
+        final Task inTask = new TaskBuilder(mSupervisor).build();
+        inTask.inRecents = true;
+
+        final ActivityStarter starter = prepareStarter(0, false);
+        spyOn(starter);
+        doReturn(START_SUCCESS).when(starter).isAllowedToStart(any(), anyBoolean(), any());
+        final ActivityRecord target = new ActivityBuilder(mAtm).setRequiredDisplayCategory("auto")
+                .build();
+        startActivityInner(starter, target, null /* source */, null /* options */, inTask,
+                null /* inTaskFragment */);
+
+        assertNotEquals(inTask, target.getTask());
+    }
+
+    /**
+     * This test simulates the following scenario:
+     * 1. Privileged app (P) starts malicious app's activity (M1).
+     * 2. M1 starts M2 (also in malicious app) using startNextMatchingActivity().
+     *     This causes M2's launchedFromPackage to be P.
+     * 3. M2 starts an activity in P (P2) using startActivity() with
+     *     FLAG_ACTIVITY_FORWARD_RESULT.
+     * The test verifies that P2's launchedFromPackage is M, not P.
+     * See b/457742426 for details.
+     */
+    @Test
+    public void testLaunchedFromPackage_nextMatchingActivity_forwardResult() {
+        final String privilegedPackage = "com.test.privileged";
+        final int privilegedUid = 10001;
+        final String maliciousPackage = "com.test.malicious";
+        final int maliciousUid = 10002;
+
+        // Setup P1 activity
+        final ActivityRecord p1 = new ActivityBuilder(mAtm)
+                .setComponent(new ComponentName(privilegedPackage, "P1Activity"))
+                .setUid(privilegedUid)
+                .setCreateTask(true)
+                .build();
+
+        // Setup M1 activity, launched by P1
+        final ActivityRecord m1 = new ActivityBuilder(mAtm)
+                .setComponent(new ComponentName(maliciousPackage, "M1Activity"))
+                .setUid(maliciousUid)
+                .setCreateTask(true)
+                .setLaunchedFromPackage(privilegedPackage)
+                .setLaunchedFromUid(privilegedUid)
+                .build();
+        m1.resultTo = p1;
+
+        // Setup M2 activity, as if launched from M1 via startNextMatchingActivity()
+        final ActivityRecord m2 = new ActivityBuilder(mAtm)
+                .setComponent(new ComponentName(maliciousPackage, "M2Activity"))
+                .setUid(maliciousUid)
+                .setCreateTask(true)
+                .setLaunchedFromPackage(privilegedPackage) // Spoofed package name
+                .setLaunchedFromUid(maliciousUid)
+                .build();
+        m2.resultTo = p1; // result is forwarded
+
+        // M2 starts P2
+        final ActivityStarter starter = prepareStarter(0);
+        doReturn(privilegedUid).when(mMockPackageManager).getPackageUid(
+                eq(privilegedPackage), anyLong(), anyInt());
+        doReturn(maliciousUid).when(mMockPackageManager).getPackageUid(
+                eq(maliciousPackage), anyLong(), anyInt());
+        starter.setCallingPackage(maliciousPackage);
+        starter.setCallingUid(maliciousUid);
+
+        final Intent p2Intent = new Intent();
+        p2Intent.setComponent(new ComponentName(privilegedPackage, "P2Activity"));
+        p2Intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
+
+        final ActivityInfo p2ActivityInfo = new ActivityInfo();
+        p2ActivityInfo.applicationInfo = new ApplicationInfo();
+        p2ActivityInfo.applicationInfo.packageName = privilegedPackage;
+        p2ActivityInfo.applicationInfo.uid = privilegedUid;
+        p2ActivityInfo.name = "P2Activity";
+
+        final ActivityRecord[] outActivity = new ActivityRecord[1];
+
+        // The request simulates M2 starting P2
+        starter.setIntent(p2Intent)
+                .setActivityInfo(p2ActivityInfo)
+                .setResultTo(m2.token) // sourceRecord is m2
+                .setRequestCode(-1) // for startActivity()
+                .setOutActivity(outActivity)
+                .execute();
+
+        final ActivityRecord p2 = outActivity[0];
+
+        assertNotNull(p2);
+        assertEquals("launchedFromPackage should be the immediate caller",
+                maliciousPackage, p2.launchedFromPackage);
+        assertEquals("launchedFromUid should be the immediate caller",
+                maliciousUid, p2.launchedFromUid);
+    }
+
     private static void startActivityInner(ActivityStarter starter, ActivityRecord target,
             ActivityRecord source, ActivityOptions options, Task inTask,
             TaskFragment inTaskFragment) {

a1d45cad8500288282470d02c7878b38c2ca16e5
NurKeinNeid <nurkeinneid@derpfest.org>
2026-01-04 18:10:41 +0100

    core: Add timestamp constants for keybox and PIF data
    Change-Id: I86f2ad421d5ec787b04fddf1a08c9f8e370472cb
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14082,11 +14082,25 @@ public final class Settings {
          *****************************/
 
         /**
+         * User selectable keybox data.
+         * @hide
+         */
+        @Readable
+        public static final String KEYBOX_DATA_TIMESTAMP = "keybox_data_timestamp";
+
+        /**
          * User selectable PIF data.
          * @hide
          */
         @Readable
         public static final String PIF_DATA = "pif_data";
+
+        /**
+         * User selectable PIF data.
+         * @hide
+         */
+        @Readable
+        public static final String PIF_DATA_TIMESTAMP = "pif_data_timestamp";
 
         /**
          * PIF data fetched from our repo.

cecb3b710838a04fdb2f0f0cf929c91281fcb8ba
NurKeinNeid <nurkeinneid@derpfest.org>
2026-01-04 18:14:14 +0100

    PropImitationHooks: Refactor pixel spoofing check to improve flexibility
    Change-Id: Ie1097ea40b3502a4327f2572f28d8ea5c4026d25
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -185,7 +185,7 @@ public class PropImitationHooks {
         }
 
         switch (packageName) {
-        if (getPixelSpoofingPackages(context).contains(packageName)) {
+        if (isPixelSpoofingTarget(packageName, processName, context)) {
             dlog("Spoofing Pixel 10 Pro XL for: " + packageName + " process: " + processName);
             setProps(sPixelTenXLProps);
             sIsPixelTenXLSpoof = true;
@@ -383,6 +383,26 @@ public class PropImitationHooks {
         }
         
         return packages;
+    }
+
+    private static boolean isPixelSpoofingTarget(String packageName, String processName, Context context) {
+        Set<String> spoofingTargets = getPixelSpoofingPackages(context);
+        
+        // Check exact package name match
+        if (spoofingTargets.contains(packageName)) {
+            dlog("Pixel spoofing target match (package): " + packageName);
+            return true;
+        }
+        
+        // Check process name contains any target (for flexibility with sub-processes)
+        for (String target : spoofingTargets) {
+            if (processName.contains(target)) {
+                dlog("Pixel spoofing target match (process): " + target + " in " + processName);
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     public static boolean hasSystemFeature(String name, boolean has) {

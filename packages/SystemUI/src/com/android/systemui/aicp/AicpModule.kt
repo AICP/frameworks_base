49dcd3ac22219faabaa9a7b1f95049c15e2dfc8a
Julian Veit <claymore1298@gmail.com>
2026-01-04 18:19:07 +0000

    fixup! QS: Add On-The-Go Tile
    Change-Id: Ic583c24e304d1edc903127237fc9eb5c6eeb7e3b

--- packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
+++ packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
@@ -167,6 +167,7 @@ interface AicpModule {
         const val COMPASS_TILE_SPEC = "compass"
         const val HEADS_UP_TILE_SPEC = "heads_up"
         const val MUSIC_TILE_SPEC = "music"
+        const val ONTHEGO_TILE_SPEC = "onthego"
         const val POWERSHARE_TILE_SPEC = "powershare"
         const val PROFILES_TILE_SPEC = "profiles"
         const val READING_MODE_TILE_SPEC = "reading_mode"
@@ -291,6 +292,21 @@ interface AicpModule {
                     QSTileUIConfig.Resource(
                         iconRes = R.drawable.ic_qs_media_play,
                         labelRes = R.string.quick_settings_music_label
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.UTILITIES,
+            )
+
+        @Provides
+        @IntoMap
+        @StringKey(ONTHEGO_TILE_SPEC)
+        fun provideOnTheGoTileConfig(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(ONTHEGO_TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = R.drawable.ic_qs_onthego,
+                        labelRes = R.string.quick_settings_onthego_label
                     ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.UTILITIES,

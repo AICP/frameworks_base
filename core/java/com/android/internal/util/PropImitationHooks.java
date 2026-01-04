a5613b8c9a9400fba6a789ccbc832a9035362eac
Abhay Singh Gill <abhaygill017@gmail.com>
2026-01-04 18:06:51 +0100

    core: User selectable PIF [1/2]
    Change-Id: I3f4b5f096b1e6c6d035751955f7138f6302bd055
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -31,6 +31,7 @@ import android.os.Binder;
 import android.os.Environment;
 import android.os.Process;
 import android.os.SystemProperties;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -228,18 +229,24 @@ public class PropImitationHooks {
             return;
         }
 
-        File dataFile = new File(Environment.getDataSystemDirectory(), DATA_FILE);
-        String savedProps = readFromFile(dataFile);
+        String savedProps = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.PIF_DATA);
+        if (savedProps == null || TextUtils.isEmpty(savedProps)) {
+            savedProps = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.FETCHED_PIF);
+        }
 
-        if (TextUtils.isEmpty(savedProps)) {
-            Log.d(TAG, "Parsing props locally - data file unavailable");
+        if (savedProps == null || TextUtils.isEmpty(savedProps)) {
+            File dataFile = new File(Environment.getDataSystemDirectory(), DATA_FILE);
+            savedProps = readFromFile(dataFile);
+        }
+
+        if (savedProps == null || TextUtils.isEmpty(savedProps)) {
+            dlog("Parsing props locally - fetched pif / user provided pif / data file unavailable");
             sCertifiedProps = Arrays.asList(context.getResources().getStringArray(R.array.config_certifiedBuildProperties));
         } else {
-            Log.d(TAG, "Parsing props fetched by attestation service");
+            dlog("Parsing props fetched / provided by user / from data file");
             try {
                 JSONObject parsedProps = new JSONObject(savedProps);
                 Iterator<String> keys = parsedProps.keys();
-
                 while (keys.hasNext()) {
                     String key = keys.next();
                     String value = parsedProps.getString(key);
@@ -247,7 +254,7 @@ public class PropImitationHooks {
                 }
             } catch (JSONException e) {
                 Log.e(TAG, "Error parsing JSON data", e);
-                Log.d(TAG, "Parsing props locally as fallback");
+                dlog("Parsing props locally as fallback");
                 sCertifiedProps = Arrays.asList(context.getResources().getStringArray(R.array.config_certifiedBuildProperties));
             }
         }

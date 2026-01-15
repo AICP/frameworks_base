c9acdf544b9b2251eaee61a46062fd6498687fe4
mosimchah <mosimchah@gmail.com>
2026-01-15 04:27:19 -0500

    hax: restore buildnumber.kt
    Change-Id: I94d0ccd480b136adcc3f43d3b021e822e9217ad6

--- packages/SystemUI/src/com/android/systemui/development/ui/compose/BuildNumber.kt
+++ packages/SystemUI/src/com/android/systemui/development/ui/compose/BuildNumber.kt
@@ -16,8 +16,6 @@
 
 package com.android.systemui.development.ui.compose
 
-import android.content.Context
-import android.provider.Settings
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Spacer
@@ -27,46 +25,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.onLongClick
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.android.systemui.communal.ui.compose.extensions.detectLongPressGesture
 import com.android.systemui.development.ui.viewmodel.BuildNumberViewModel
 import com.android.systemui.lifecycle.rememberViewModel
 import com.android.systemui.qs.ui.compose.borderOnFocus
 import com.android.systemui.res.R
-
-fun getQsFooterTextShow(context: Context): Int {
-    return Settings.System.getInt(
-        context.contentResolver,
-        Settings.System.QS_FOOTER_TEXT_SHOW,
-        0
-    )
-}
-
-fun getQsFooterTextString(context: Context): String {
-    return Settings.System.getString(
-        context.contentResolver,
-        Settings.System.QS_FOOTER_TEXT_STRING
-    ) ?: ""
-}
-
-fun getQsFooterTextFontWeight(context: Context): Int {
-    return Settings.System.getInt(
-        context.contentResolver,
-        Settings.System.QS_FOOTER_TEXT_FONT_WEIGHT,
-        0
-    )
-}
 
 @Composable
 fun BuildNumber(
@@ -75,30 +47,25 @@ fun BuildNumber(
     textColor: Color = MaterialTheme.colorScheme.onSurface,
 ) {
     val viewModel = rememberViewModel(traceName = "BuildNumber") { viewModelFactory.create() }
-    val context = LocalContext.current
 
-    val footerFontWeight = remember(context) {
-        if (getQsFooterTextFontWeight(context) == 1) {
-            FontWeight.Bold
-        } else {
-            FontWeight.Normal
-        }
-    }
+    BuildNumber(viewModel, modifier, textColor)
+}
 
-    val QsFooterTextenabled = getQsFooterTextShow(context)
-    if (QsFooterTextenabled == 1) {
+@Composable
+fun BuildNumber(
+    viewModel: BuildNumberViewModel,
+    modifier: Modifier,
+    textColor: Color = MaterialTheme.colorScheme.onSurface,
+) {
+    val buildNumber = viewModel.buildNumber
+
+    if (buildNumber != null) {
         val haptics = LocalHapticFeedback.current
         val copyToClipboardActionLabel = stringResource(id = R.string.copy_to_clipboard_a11y_action)
 
-
-        val qsFooterText = remember(context) {
-            getQsFooterTextString(context)
-        }
-
         Text(
-            text = qsFooterText,
-            style = MaterialTheme.typography.bodyMedium,
-            fontWeight = footerFontWeight,
+            text = buildNumber.value,
+            style = MaterialTheme.typography.bodySmall,
             modifier =
                 modifier
                     .borderOnFocus(

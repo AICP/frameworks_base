4355cc1a748212db6744d25d3342376fd5d1ec4c
mosimchah <mosimchah@gmail.com>
2026-01-15 03:42:07 -0500

    aicputils: hax
    Change-Id: I625e29d712a4fa1eabfe62ce7ea2cff4d6e9ca7e

--- core/java/com/android/internal/util/aicp/AicpUtils.java
+++ core/java/com/android/internal/util/aicp/AicpUtils.java
@@ -99,18 +99,18 @@ public class AicpUtils {
 
         final Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
-            @Override
+/*            @Override
             public void run() {
                 InputManager.getInstance().injectInputEvent(evDown,
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
-            }
+            }*/
         });
         handler.postDelayed(new Runnable() {
-            @Override
+/*            @Override
             public void run() {
                 InputManager.getInstance().injectInputEvent(evUp,
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
-            }
+            }*/
         }, 20);
     }
 

3bd8fb680f311c5a000f2a90df90406f3f05c024
Julian Veit <claymore1298@gmail.com>
2026-02-11 10:33:57 +0000

    SystemUI: add ENABLE_BLURS_ON_WINDOWS setting
    Change-Id: Ia4991066e71bfbe4c046964555c31c68de081afc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7271,6 +7271,12 @@ public final class Settings {
          */
         public static final String BACKGROUND_BLUR_RADIUS = "background_blur_radius";
 
+	/**
+        * Blur radius
+        * @hide
+         */
+        public static final String ENABLE_BLURS_ON_WINDOWS = "enable_blurs_on_windows";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -7480,6 +7486,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SCREEN_OFF_ANIMATION);
             PRIVATE_SETTINGS.add(LOCKSCREEN_SHOW_CUSTOM_CARRIER_TEXT);
             PRIVATE_SETTINGS.add(BACKGROUND_BLUR_RADIUS);
+            PRIVATE_SETTINGS.add(ENABLE_BLURS_ON_WINDOWS);
         }
 
         /**
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -192,7 +192,8 @@ public class SystemSettings {
                 Settings.System.QS_FOOTER_TEXT_FONT_WEIGHT,
                 Settings.System.SCREEN_OFF_ANIMATION,
                 Settings.System.LOCKSCREEN_SHOW_CUSTOM_CARRIER_TEXT,
-                Settings.System.BACKGROUND_BLUR_RADIUS
+                Settings.System.BACKGROUND_BLUR_RADIUS,
+                Settings.System.ENABLE_BLURS_ON_WINDOWS
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);

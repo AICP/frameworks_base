3670b547816a6bac096656ee4bb472958a6c85fa
Siluxsept <ronny.horn.rh@gmail.com>
2026-02-25 10:33:47 +0100

    Revert "SystemUI: Allow limiting edge lighting repeats [1/2]"
    This reverts commit 5ff67e73a0aeca1369023132f198fa3d4232cabd.
    
    Change-Id: I436ffb3f62e0691ce9f479b7dc5c8a15765f80eb

--- packages/SystemUI/src/com/android/systemui/aicp/NotificationLightsView.java
+++ packages/SystemUI/src/com/android/systemui/aicp/NotificationLightsView.java
@@ -94,9 +94,6 @@ public class NotificationLightsView extends RelativeLayout {
         int duration = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.PULSE_AMBIENT_LIGHT_DURATION, 2,
                 UserHandle.USER_CURRENT) * 1000;
-        int repeat = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                Settings.Secure.PULSE_AMBIENT_LIGHT_REPEAT_COUNT, 0,
-                UserHandle.USER_CURRENT);
         if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.PULSE_AMBIENT_LIGHT_AUTO_COLOR, 0,
                 UserHandle.USER_CURRENT) == 1) {
@@ -124,12 +121,6 @@ public class NotificationLightsView extends RelativeLayout {
         rightView.setColorFilter(color);
         mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
         mLightAnimator.setDuration(duration);
-        if (repeat == 0) {
-            mLightAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        } else {
-            mLightAnimator.setRepeatCount(repeat);
-        }
-        mLightAnimator.setRepeatMode(ValueAnimator.RESTART);
         mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
             public void onAnimationUpdate(ValueAnimator animation) {
                 Log.e("NotificationLightsView", "onAnimationUpdate");

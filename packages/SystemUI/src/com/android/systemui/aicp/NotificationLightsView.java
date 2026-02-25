249b971a34667eb15536e3d19e269edb26d0ff38
Siluxsept <ronny.horn.rh@gmail.com>
2026-02-25 10:30:40 +0100

    Revert "SystemUI: Add light width [1/3]"
    This reverts commit 38104d1faf28a5c2b2a5c17a9bd23e55d2b2987a.
    
    Change-Id: I762f8689799a6ea507dc2d1386f92718bbc9bbb1

--- packages/SystemUI/src/com/android/systemui/aicp/NotificationLightsView.java
+++ packages/SystemUI/src/com/android/systemui/aicp/NotificationLightsView.java
@@ -105,9 +105,6 @@ public class NotificationLightsView extends RelativeLayout {
         int colorMode = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.PULSE_AMBIENT_LIGHT_COLOR_MODE, 1,
                 UserHandle.USER_CURRENT);
-        int width = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                Settings.Secure.PULSE_AMBIENT_LIGHT_WIDTH, 125,
-                UserHandle.USER_CURRENT);
 
         if (colorMode == 0) {
             try {
@@ -136,8 +133,6 @@ public class NotificationLightsView extends RelativeLayout {
         ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
         leftView.setColorFilter(color);
         rightView.setColorFilter(color);
-        leftView.getLayoutParams().width = width;
-        rightView.getLayoutParams().width = width;
         mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
         mLightAnimator.setDuration(duration);
         mLightAnimator.setRepeatCount(repeat);

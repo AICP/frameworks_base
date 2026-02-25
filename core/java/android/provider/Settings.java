48d784138d0ba967d9b71a040bbb8a98d4c9af74
Siluxsept <ronny.horn.rh@gmail.com>
2026-02-25 10:42:12 +0100

    Revert "base: Add ambient pulse notification [1/2]"
    This reverts commit ae6e72970719f0de49ba0eecfab83ed21d4e7155.
    
    Change-Id: I54e57860180afcda8c222b519d6707e55bafa1c4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14142,18 +14142,6 @@ public final class Settings {
          */
         public static final String SECURE_LOCKSCREEN_QS_DISABLED = "secure_lockscreen_qs_disabled";
 
-        /**
-         * Whether to enable Ambient edge light
-         * @hide
-         */
-        public static final String PULSE_AMBIENT_LIGHT = "pulse_ambient_light";
-
-        /**
-         * What color to use for Ambient edge light
-         * @hide
-         */
-        public static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
-
         /*****************************
          * AICP Secure Settings end
          *****************************/
--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -66,11 +66,9 @@ import android.graphics.Shader;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.os.PowerManager;
-import android.provider.Settings;
 import android.os.Trace;
-import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
@@ -138,7 +136,6 @@ import com.android.systemui.model.SysUiState;
 import com.android.systemui.navigationbar.NavigationBarController;
 import com.android.systemui.navigationbar.NavigationModeController;
 import com.android.systemui.navigationbar.views.NavigationBarView;
-import com.android.systemui.aicp.NotificationLightsView;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.FalsingManager.FalsingTapListener;
@@ -418,8 +415,6 @@ public final class NotificationPanelViewController implements
     private final FalsingCollector mFalsingCollector;
     private final ShadeHeadsUpTrackerImpl mShadeHeadsUpTracker = new ShadeHeadsUpTrackerImpl();
     private final ShadeFoldAnimatorImpl mShadeFoldAnimator = new ShadeFoldAnimatorImpl();
-
-    private NotificationLightsView mPulseLightsView;
 
     @VisibleForTesting
     Set<Animator> mTestSetOfAnimatorsUsed;
@@ -936,8 +931,6 @@ public final class NotificationPanelViewController implements
         mQsController.init();
         mShadeHeadsUpTracker.addTrackingHeadsUpListener(
                 mNotificationStackScrollLayoutController::setTrackingHeadsUp);
-        mPulseLightsView = (NotificationLightsView) mView.findViewById(R.id.lights_container);
-
         mWakeUpCoordinator.setStackScroller(mNotificationStackScrollLayoutController);
         mWakeUpCoordinator.addListener(new NotificationWakeUpCoordinator.WakeUpListener() {
             @Override
@@ -2489,8 +2482,6 @@ public final class NotificationPanelViewController implements
         final boolean
                 animatePulse =
                 !mDozeParameters.getDisplayNeedsBlanking() && mDozeParameters.getAlwaysOn();
-        boolean pulseLights = Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
-                Settings.Secure.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
         if (animatePulse) {
             mAnimateNextPositionUpdate = true;
         }
@@ -2498,13 +2489,6 @@ public final class NotificationPanelViewController implements
         // The height callback will take care of pushing the clock to the right position.
         if (!mPulsing && !mDozing) {
             mAnimateNextPositionUpdate = false;
-        }
-                if ((mPulseLightsView != null) && pulseLights) {
-            mPulseLightsView.setVisibility(mPulsing ? View.VISIBLE : View.GONE);
-            if (mPulsing) {
-                mPulseLightsView.animateNotification();
-                mPulseLightsView.setPulsing(pulsing);
-            }
         }
         mNotificationStackScrollLayoutController.setPulsing(pulsing, animatePulse);
 

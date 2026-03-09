82c70aceb5bee515849ac3b5f42e6fbd6a5bd305
Julian Veit <claymore1298@gmail.com>
2026-03-09 16:28:10 +0000

    Revert "SystemUI: Add status bar battery disable toggle"
    This reverts commit 31247b40ee8a37551fa40a15d2bc514291e74ff2.
    
    Change-Id: Id3b25f00ed7e65a1157b6072a35c431385b930c2

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/ui/composable/StatusBarRoot.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/ui/composable/StatusBarRoot.kt
@@ -119,9 +119,6 @@ import javax.inject.Inject
 import javax.inject.Named
 import kotlinx.coroutines.DisposableHandle
 import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.collect
 
 /** Factory to simplify the dependency management for [StatusBarRoot] */
 @PerDisplaySingleton
@@ -551,8 +548,6 @@ fun chipsMaxWidth(
     return (widthInPx / density).dp
 }
 
-private const val SLOT_BATTERY = "battery"
-
 /** Create a new [UnifiedBattery] and add it to the end of the system_icons container */
 private fun addBatteryComposable(
     phoneStatusBarView: PhoneStatusBarView,
@@ -589,15 +584,6 @@ private fun addBatteryComposable(
         }
     phoneStatusBarView.findViewById<ViewGroup>(R.id.system_icons).apply {
         addView(batteryComposeView, -1)
-    }
-
-    batteryComposeView.repeatWhenAttached {
-        statusBarViewModel.iconBlockList
-            .map { blocked -> blocked.contains(SLOT_BATTERY) }
-            .distinctUntilChanged()
-            .collect { isBlocked ->
-                batteryComposeView.visibility = if (isBlocked) View.GONE else View.VISIBLE
-            }
     }
 }
 

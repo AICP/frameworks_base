ff7df5e48bc6d4f12ac6b5fcc23dcc11c626b6f8
mosimchah <mosimchah@gmail.com>
2026-03-22 16:44:14 -0400

    fwb: spoofing: fix link for props
    Change-Id: Iecec241ad830af2b2fe96e6f319aad986bbeea90

--- services/core/java/com/android/server/aicp/AttestationService.java
+++ services/core/java/com/android/server/aicp/AttestationService.java
@@ -36,7 +36,7 @@ public final class AttestationService extends SystemService {
 
     private static final String TAG = AttestationService.class.getSimpleName();
 
-    private static final String API = "https://raw.githubusercontent.com/AICP/vendor_aicp/w16.2/gms_certified_props.json";
+    private static final String API = "https://raw.githubusercontent.com/AICP/vendor_aicp/refs/heads/w16.2/certified_props.json";
     private static final String DATA_FILE = "gms_certified_props.json";
     private static final long INITIAL_DELAY = 0; // Start immediately on boot
     private static final long INTERVAL = 8; // Interval in hours
